6d412333e38d7275778415ed5a7aac2b0649cef9
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-19 14:28:09 +0530

    applicant migration table

---
+++ database/migrations/2018_03_19_084326_create_hr_applicants_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHrApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hr_applicants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->integer('hr_job_id')->unsigned();
+            $table->string('status');
+            $table->string('resume');
+            $table->timestamps();
+
+        });
+
+        Schema::table('hr_applicants', function(Blueprint $table) {
+            $table->foreign('hr_job_id')->references('id')->on('hr_jobs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hr_applicants', function (Blueprint $table) {
+            $table->dropForeign(['hr_job_id']);
+        });
+    }
+}
