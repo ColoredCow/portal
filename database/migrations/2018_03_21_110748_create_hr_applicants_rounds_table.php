c20411dbc5f6038e570685eeecfb96c4899c7425
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-21 17:00:40 +0530

    hr applicants rounds

---
+++ database/migrations/2018_03_21_110748_create_hr_applicants_rounds_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHrApplicantsRoundsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hr_applicants_rounds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hr_applicant_id')->unsigned();
+            $table->integer('hr_round_id')->unsigned();
+            $table->timestamp('scheduled_date')->nullable();
+            $table->integer('scheduled_person_id')->unsigned();
+            $table->timestamp('conducted_date')->nullable();
+            $table->integer('conducted_person_id')->unsigned();
+        });
+
+        Schema::table('hr_applicants_rounds', function(Blueprint $table) {
+            $table->foreign('hr_applicant_id')->references('id')->on('hr_applicants');
+            $table->foreign('hr_round_id')->references('id')->on('hr_rounds');
+            $table->foreign('scheduled_person_id')->references('id')->on('users');
+            $table->foreign('conducted_person_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hr_applicants_rounds', function (Blueprint $table) {
+            $table->dropForeign([
+                'hr_applicant_id',
+                'hr_round_id',
+                'scheduled_person_id',
+                'conducted_person_id',
+            ]);
+        });
+    }
+}
