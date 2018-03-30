0ec76e76f020ffa4bb9496800eef06d7c1ccad43
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-30 19:04:25 +0530

    project - model, migration, resource controller

---
+++ database/migrations/2018_03_30_132837_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('started_on')->nullable();
+            $table->string('invoice_email')->nullable();
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
