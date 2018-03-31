24cc2fde030930bacafb8c71d989eab5c00e7080
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-31 21:20:33 +0530

    Test coverage for Invoice Controller

---
+++ tests/Unit/Finance/InvoiceControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\Finance;
+
+use Tests\TestCase;
+
+class InvoiceControllerTest extends TestCase
+{
+    /**
+     * Testing success for Finance\InvoiceController@store
+     *
+     * @return void
+     */
+    public function testStoreSuccess()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Testing failure for Finance\InvoiceController@store
+     *
+     * @return void
+     */
+    public function testStoreFailure()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Testing success for Finance\InvoiceController@update
+     *
+     * @return void
+     */
+    public function testUpdateSuccess()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Testing failure for Finance\InvoiceController@update
+     *
+     * @return void
+     */
+    public function testUpdateFailure()
+    {
+        $this->assertTrue(true);
+    }
+}
