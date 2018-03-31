4075616b7d6651c2da1321ccd435e3f0def7f42e
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-31 10:27:01 +0530

    eloquent relationship

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
     $fillable = ['name', 'email', 'phone', 'address'];
+
+    /**
+     * The projects that belong to the client.
+     */
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     $fillable = ['name', 'started_on', 'invoice_email', 'status'];
+
+    /**
+     * The clients that belong to the project.
+     */
+    public function clients()
+    {
+        return $this->belongsToMany(Client::class);
+    }
 }
