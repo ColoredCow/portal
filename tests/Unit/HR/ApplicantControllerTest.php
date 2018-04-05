3bea48410953902b5b87d6e41a052f83aa692ecb
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-04-05 19:15:59 +0530

    HR test coverage (#67)
    * HR test coverage
    
    * removed dependencies

---
+++ tests/Unit/HR/ApplicantControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\HR;
+
+use Tests\TestCase;
+
+class ApplicantControllerTest extends TestCase
+{
+    /**
+     * Test case for ApplicantController@store
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test case for ApplicantController@update
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/HR/JobControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\HR;
+
+use Tests\TestCase;
+
+class JobControllerTest extends TestCase
+{
+    /**
+     * Test case for JobController@store
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test case for JobController@update
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+}
