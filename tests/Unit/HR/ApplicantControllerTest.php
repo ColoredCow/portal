f12678a19d4d639a767ef0e31176e335d7436e83
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-05 17:21:47 +0530

    HR test coverage

---
+++ tests/Unit/HR/ApplicantControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\HR;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ApplicantControllerTest extends TestCase
+{
+    /**
+     * Test case for ApplicantController@store
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test case for ApplicantController@update
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/HR/JobControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\HR;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class JobControllerTest extends TestCase
+{
+    /**
+     * Test case for JobController@store
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test case for JobController@update
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+}
