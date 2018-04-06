a80220c9c5f37520972acc5d5d7512a83d521493
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-06 10:31:20 +0530

    tabs to spaces

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -25,55 +25,55 @@ const app = new Vue({
 });
 
 $('#page-hr-applicant-edit .applicant-round-form').on('click', '.round-submit', function(){
-	var form = $(this).closest('.applicant-round-form');
-	form.find('[name="round_status"]').val($(this).data('status'));
-	form.submit();
+    var form = $(this).closest('.applicant-round-form');
+    form.find('[name="round_status"]').val($(this).data('status'));
+    form.submit();
 });
 
 $('.date-field').datepicker({
-	dateFormat: "dd/mm/yy"
+    dateFormat: "dd/mm/yy"
 });
 
 $(document).ready(() => {
-	if ($('#form_create_invoice').length) {
-		let form = $('#form_create_invoice');
-		let client_id = form.find('#client_id').val();
-		if (client_id) {
-			updateClientProjects(form, client_id);
-		}
-	}
+    if ($('#form_create_invoice').length) {
+        let form = $('#form_create_invoice');
+        let client_id = form.find('#client_id').val();
+        if (client_id) {
+            updateClientProjects(form, client_id);
+        }
+    }
 });
 
 $('#form_create_invoice, #form_edit_invoice').on('change', '#client_id', function(){
-	let form = $(this).closest('form');
-	let client_id = $(this).val();
-	if (! client_id) {
-		form.find('#project_ids').html('');
-		return false;
-	}
-	updateClientProjects(form, client_id);
+    let form = $(this).closest('form');
+    let client_id = $(this).val();
+    if (! client_id) {
+        form.find('#project_ids').html('');
+        return false;
+    }
+    updateClientProjects(form, client_id);
 });
 
 function updateClientProjects(form, client_id) {
-	$.ajax({
-		url : '/clients/' + client_id + '/get-projects',
-		method : 'GET',
-		success : function (res) {
-			form.find('#project_ids').html(getProjectList(res));
-		},
-		error : function (err) {
-			console.log(err);
-		}
-	});
+    $.ajax({
+        url : '/clients/' + client_id + '/get-projects',
+        method : 'GET',
+        success : function (res) {
+            form.find('#project_ids').html(getProjectList(res));
+        },
+        error : function (err) {
+            console.log(err);
+        }
+    });
 }
 
 function getProjectList(projects) {
-	let html = '';
-	for (var index = 0; index < projects.length; index++) {
-		let project = projects[index];
-		html += '<option value="' + project.id + '">';
-		html += project.name;
-		html += '</option>';
-	}
-	return html;
+    let html = '';
+    for (var index = 0; index < projects.length; index++) {
+        let project = projects[index];
+        html += '<option value="' + project.id + '">';
+        html += project.name;
+        html += '</option>';
+    }
+    return html;
 }
