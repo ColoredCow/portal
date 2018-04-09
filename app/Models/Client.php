217f9160ad454b36e07662cd1b4bbbf96da41918
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-09 18:23:48 +0530

    client nationality

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = ['name', 'email', 'phone', 'address'];
+    protected $fillable = ['name', 'email', 'phone', 'country', 'address'];
 
     /**
      * Get the projects for the client.
