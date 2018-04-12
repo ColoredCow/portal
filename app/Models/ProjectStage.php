24d9b24b1fb1d4901b84f93e4076548842e142b0
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-12 13:28:18 +0530

    formatting

--- app/Models/ProjectStage.php
+++ app/Models/ProjectStage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectStage extends Model
 {
-	protected $table = 'project_stages';
+    protected $table = 'project_stages';
 
     protected $fillable = ['project_id', 'name', 'cost', 'currency_cost', 'cost_include_gst'];
 
