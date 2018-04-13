56c32047a211a51516dcffe9bd8446a858bf7cb8
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-13 17:49:56 +0530

    used config display date format

--- resources/views/hr/applicant/timeline.blade.php
+++ resources/views/hr/applicant/timeline.blade.php
@@ -1,7 +1,7 @@
 <div class="timeline mb-5">
     <div class="timeline-container">
         <div class="content">
-            <b>{{ date('d/m/Y', strtotime($applicant->created_at)) }}</b><br>
+            <b>{{ date(config('constants.display_date_format'), strtotime($applicant->created_at)) }}</b><br>
             Applied for {{ $applicant->job->title }}
         </div>
     </div>
@@ -9,14 +9,14 @@
         @if ($applicantRound->scheduled_date)
             <div class="timeline-container">
                 <div class="content">
-                    {{ $applicantRound->round->name }} scheduled on {{ date('d/m/Y', strtotime($applicantRound->scheduled_date)) }}
+                    {{ $applicantRound->round->name }} scheduled on {{ date(config('constants.display_date_format'), strtotime($applicantRound->scheduled_date)) }}
                 </div>
             </div>
         @endif
         @if ($applicantRound->conducted_date)
             <div class="timeline-container">
                 <div class="content">
-                    <b>{{ date('d/m/Y', strtotime($applicantRound->conducted_date)) }}</b><br>
+                    <b>{{ date(config('constants.display_date_format'), strtotime($applicantRound->conducted_date)) }}</b><br>
                     {{ $applicantRound->round->name }} conducted by {{ $applicantRound->conductedPerson->name }}
                 </div>
             </div>
@@ -24,7 +24,7 @@
         @if ($applicantRound->mail_sent)
             <div class="timeline-container">
                 <div class="content">
-                    <b>{{ date('d/m/Y', strtotime($applicantRound->mail_sent_at)) }}</b><br>
+                    <b>{{ date(config('constants.display_date_format'), strtotime($applicantRound->mail_sent_at)) }}</b><br>
                     <span data-toggle="modal" data-target="#round_mail_{{ $applicantRound->id }}" class="modal-toggler-text text-primary">Communication mail for {{ $applicantRound->round->name }}</span>
                 </div>
             </div>
