5acdf7e16aee036d707474f872eb5503ff66867e
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-04-14 20:21:13 +0530

    (#29) applicant timeline (#114)
    * applicant timeline
    
    * used config display date format
    
    * minor ui changes

---
+++ resources/views/hr/applicant/timeline.blade.php
@@ -0,0 +1,38 @@
+<div class="timeline mb-5">
+    <div class="timeline-container">
+        <div class="content">
+            <b><u>{{ date(config('constants.display_date_format'), strtotime($applicant->created_at)) }}</u></b><br>
+            Applied for {{ $applicant->job->title }}
+        </div>
+    </div>
+    @foreach ($applicant->applicantRounds as $applicantRound)
+        @if ($applicantRound->scheduled_date)
+            <div class="timeline-container">
+                <div class="content">
+                    {{ $applicantRound->round->name }} scheduled on {{ date(config('constants.display_date_format'), strtotime($applicantRound->scheduled_date)) }}
+                </div>
+            </div>
+        @endif
+        @if ($applicantRound->conducted_date)
+            <div class="timeline-container">
+                <div class="content">
+                    <b><u>{{ date(config('constants.display_date_format'), strtotime($applicantRound->conducted_date)) }}</u></b><br>
+                    {{ $applicantRound->round->name }} conducted by {{ $applicantRound->conductedPerson->name }}<br>
+                    @if ($applicantRound->round_status == 'rejected')
+                        <span class="badge badge-danger">rejected</span>
+                    @else
+                        <span class="badge badge-success">moved to next round</span>
+                    @endif
+                </div>
+            </div>
+        @endif
+        @if ($applicantRound->mail_sent)
+            <div class="timeline-container">
+                <div class="content">
+                    <b><u>{{ date(config('constants.display_date_format'), strtotime($applicantRound->mail_sent_at)) }}</u></b><br>
+                    <span data-toggle="modal" data-target="#round_mail_{{ $applicantRound->id }}" class="modal-toggler-text text-primary">Communication mail for {{ $applicantRound->round->name }}</span>
+                </div>
+            </div>
+        @endif
+    @endforeach
+</div>
