d8e70fabfa9ea33c9e2f84eefbb3e1765e0e2582
Tushar Singh Bohra <tushar@coloredcow.in>
2018-04-15 14:35:44 +0530

    #46 test error check

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $this->assertTrue(true);
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
