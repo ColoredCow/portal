1e93b75af78bbe97874d7e9c110110e0342b475b
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-16 11:36:11 +0530

    icons added

--- resources/views/hr/menu.blade.php
+++ resources/views/hr/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="nav nav-pills">
     <li class="nav-item">
-        <a class="nav-item nav-link {{ $active === 'jobs' ? 'active' : '' }}" href="/hr/jobs">Jobs</a>
+        <a class="nav-item nav-link {{ $active === 'jobs' ? 'active' : '' }}" href="/hr/jobs"><i class="fa fa-list-ul"></i>&nbsp;Jobs</a>
     </li>
     <li class="nav-item">
-        <a class="nav-item nav-link {{ $active === 'applicants' ? 'active' : '' }}" href="/hr/applicants">Applicants</a>
+        <a class="nav-item nav-link {{ $active === 'applicants' ? 'active' : '' }}" href="/hr/applicants"><i class="fa fa-users"></i>&nbsp;Applicants</a>
     </li>
 </ul>
