2115c29d7a6fccf6b6a5bfc9b7cf4a9c71d314ed
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-26 17:59:41 +0530

    showing new instead of first round

--- resources/views/hr/applicant/index.blade.php
+++ resources/views/hr/applicant/index.blade.php
@@ -24,12 +24,12 @@
             <td>{{ date(config('constants.display_date_format'), strtotime($applicant->created_at)) }}</td>
             <td>
                 <span class="d-flex justify-content-start">
-                    @if ($applicant->status == 'in-progress')
+                    @if (in_array($applicant->status, ['in-progress', 'new']))
                         <span class="badge badge-warning badge-pill">{{ $applicant->applicantRounds->last()->round->name }}</span>
                         @if ($applicant->applicantRounds->count() > 1)
                             <span class="badge badge-info badge-pill ml-1 px-2">Completed: {{ $applicant->applicantRounds->count() - 1 }}</span>
                         @else
-                            <span class="badge badge-info badge-pill ml-1 px-2">First round</span>
+                            <span class="badge badge-info badge-pill ml-1 px-2">New</span>
                         @endif
                     @else
                         <span class="{{ config("constants.hr.status.$applicant->status.class") }} badge-pill">{{ config("constants.hr.status.$applicant->status.title") }}</span>
