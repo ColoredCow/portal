f017c750679b5a6ec6cb5f84c17d1254765a8d9c
panakj-ag <pankaj@coloredcow.in>
2018-04-26 16:37:01 +0530

    (#135) scaffolding

--- resources/views/knowledgecafe/index.blade.php
+++ resources/views/knowledgecafe/index.blade.php
@@ -5,7 +5,7 @@
     <br>
     <div class="d-flex justify-content-between">
         <div class="col-md-3  card">
-            <a class="card-body no-transition" href="/knowledgecafe/library/book">
+            <a class="card-body no-transition" href="/knowledgecafe/library/books">
                 <br><h2 class="text-center">Library</h2><br>
             </a>
         </div>
