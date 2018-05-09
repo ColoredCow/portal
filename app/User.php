3b7b701c9bb65e12b90d9bc59eb90e178282bca9
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-09 21:37:10 +0530

    used spatie/laravel-permission package

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
