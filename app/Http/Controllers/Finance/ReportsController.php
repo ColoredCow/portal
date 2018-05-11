bd3a88f739c6a4db564ab498ec640511aad8124f
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-05-11 18:55:11 +0530

    (#184) reports test feedback (#191)
    * invoices ordering
    
    * showing receivable and conversion rate
    
    * review feedback

--- app/Http/Controllers/Finance/ReportsController.php
+++ app/Http/Controllers/Finance/ReportsController.php
@@ -19,12 +19,12 @@ class ReportsController extends Controller
         $request = request();
 
         if ($request->get('show') && $request->get('show') == 'default') {
-            $startDate = new Carbon('first day of last month');
-            $endDate = new Carbon('last day of last month');
+            $startDate = new Carbon('first day of this month');
+            $endDate = new Carbon('today');
             $formattedStartDate = $startDate->format(config('constants.date_format'));
             $formattedEndDate = $endDate->format(config('constants.date_format'));
             $invoices = Invoice::filterByDates($formattedStartDate, $formattedEndDate);
-            $arrangedInvoices = self::arrangeInvoices($invoices, $startDate, $endDate);
+            $arrangedInvoices = self::arrangeInvoices($invoices, $formattedStartDate, $formattedEndDate);
             $attr = [
                 'sentInvoices' => $arrangedInvoices['sent'],
                 'paidInvoices' => $arrangedInvoices['paid'],
@@ -79,12 +79,17 @@ class ReportsController extends Controller
             $report['transactionCharge'][$currency] = 0;
             $report['transactionTax'][$currency] = 0;
             $report['dueAmount'][$currency] = 0;
+            $report['receivable'][$currency] = 0;
         }
 
         $report['gst'] = 0;
         foreach ($sentInvoices as $invoice) {
             $report['gst'] += $invoice->gst;
             $report['sentAmount'][$invoice->currency_sent_amount] += $invoice->sent_amount;
+
+            if ($invoice->status != 'paid') {
+                $report['receivable'][$invoice->currency_sent_amount] += $invoice->sent_amount;
+            }
         }
 
         $report['tds'] = 0;
