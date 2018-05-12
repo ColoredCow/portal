eaec5afbe97a3c16ab91da8103a67a9bfaa6c9d2
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-12 14:10:04 +0530

    seeder for new job round. trial program. fixes #200

---
+++ database/seeds/HRRoundsTrialProgramSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\HR\Job;
+use App\Models\HR\Round;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class HRRoundsTrialProgramSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $round = Round::create([ 'name' => 'Trial Program' ]);
+
+        $jobs = Job::all();
+        foreach ($jobs as $job) {
+            $job->rounds()->attach($round->id);
+        }
+    }
+}
