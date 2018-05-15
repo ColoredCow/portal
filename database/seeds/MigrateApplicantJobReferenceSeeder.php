410367ba035fe1a174d6626efa94b0473d6d4b3f
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-15 10:36:38 +0530

    application corrections

--- database/seeds/MigrateApplicantJobReferenceSeeder.php
+++ database/seeds/MigrateApplicantJobReferenceSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\HR\Applicant;
+use App\Models\HR\Application;
 use Illuminate\Database\Seeder;
 
 class MigrateApplicantJobReferenceSeeder extends Seeder
@@ -17,34 +18,38 @@ class MigrateApplicantJobReferenceSeeder extends Seeder
         $uniqueApplicants = [];
         foreach ($applicants as $applicant) {
 
-        	// checking if the current applicant entry is duplicate
-        	if (array_key_exists($applicant->email, $uniqueApplicants)) {
+            // checking if the current applicant entry is duplicate
+            if (array_key_exists($applicant->email, $uniqueApplicants)) {
 
-        		// find the original applicant with the same email. $applicant is duplicate of $originalApplicant
-        		$originalApplicant = $uniqueApplicants[$applicant->email];
+                // find the original applicant with the same email. $applicant is duplicate of $originalApplicant
+                $originalApplicant = $uniqueApplicants[$applicant->email];
 
-        		// add the job of this duplicate applicant as the job of original applicant
-        		$originalApplicant->jobs()->attach($applicant->hr_job_id, [
-        			'created_at' => $applicant->created_at,
-        			'updated_at' => $applicant->updated_at,
-        		]);
+                // add the job of this duplicate applicant as the job of original applicant
+                Application::create([
+                    'hr_job_id' => $applicant->hr_job_id,
+                    'hr_applicant_id' => $originalApplicant->id,
+                    'created_at' => $applicant->created_at,
+                    'updated_at' => $applicant->updated_at,
+                ]);
 
-        		foreach($applicant->applicantRounds as $applicantRound) {
-        			foreach ($applicantRound->applicantReviews as $applicantReview) {
-        				echo $applicantReview->id;
-        				$applicantReview->delete();
-        			}
-        			$applicantRound->delete();
-        		}
-        		$applicant->delete();
+                foreach($applicant->applicantRounds as $applicantRound) {
+                    foreach ($applicantRound->applicantReviews as $applicantReview) {
+                        echo $applicantReview->id;
+                        $applicantReview->delete();
+                    }
+                    $applicantRound->delete();
+                }
+                $applicant->delete();
 
-        		continue;
-        	}
-        	$uniqueApplicants[$applicant->email] = $applicant;
-        	$applicant->jobs()->attach($applicant->hr_job_id, [
-        		'created_at' => $applicant->created_at,
-    			'updated_at' => $applicant->updated_at,
-        	]);
+                continue;
+            }
+            $uniqueApplicants[$applicant->email] = $applicant;
+            Application::create([
+                'hr_job_id' => $applicant->hr_job_id,
+                'hr_applicant_id' => $applicant->id,
+                'created_at' => $applicant->created_at,
+                'updated_at' => $applicant->updated_at,
+            ]);
         }
     }
 }
