36572801a496cb5fa3e0ed2d62911b126b11a217
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-17 02:06:23 +0530

    self-review changes

--- resources/views/hr/application/index.blade.php
+++ resources/views/hr/application/index.blade.php
@@ -12,7 +12,7 @@
             <a class="nav-item nav-link {{ $status ? 'text-info' : 'active bg-info text-white' }}" href="/hr/applications"><i class="fa fa-clipboard"></i>&nbsp;Active</a>
         </li>
         <li class="nav-item">
-            <a class="nav-item nav-link {{ $status === config('constants.hr.status.rejected.label') ? 'active bg-info text-white' : 'text-info' }}" href="/hr/applications?status=rejected"><i class="fa fa-times-circle"></i>&nbsp;Rejected</a>
+            <a class="nav-item nav-link {{ $status === config('constants.hr.status.rejected.label') ? 'active bg-info text-white' : 'text-info' }}" href="/hr/applications?status={{ config('constants.hr.status.rejected.label') }}"><i class="fa fa-times-circle"></i>&nbsp;Rejected</a>
         </li>
     </ul>
     <table class="table table-striped table-bordered" id="applicants_table">
