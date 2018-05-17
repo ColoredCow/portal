2db4a5be2980ea071441cb641d44878b7690ac93
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-17 18:22:07 +0530

    more changes

--- resources/views/hr/application/timeline.blade.php
+++ resources/views/hr/application/timeline.blade.php
@@ -10,7 +10,7 @@
                         <b><u>{{ date(config('constants.display_date_format'), strtotime($application->created_at)) }}</u></b><br>
                         <div>Applied for {{ $application->job->title }}</div>
                         @if ($application->autoresponder_subject && $application->autoresponder_body)
-                            <span data-toggle="modal" data-target="#autoresponder_mail" class="modal-toggler-text text-primary">Auto-respond mail for system</span>
+                            <span data-toggle="modal" data-target="#autoresponder_mail" class="modal-toggler-text text-primary">Auto-respond mail from system</span>
                             @include('hr.application.auto-respond', ['applicant' => $application->applicant, 'application' => $application])
                         @endif
                         @break
@@ -19,7 +19,7 @@
                             $applicationRound = $item['applicationRound'];
                         @endphp
                         <b><u>{{ date(config('constants.display_date_format'), strtotime($applicationRound->conducted_date)) }}</u></b><br>
-                        {{ $applicationRound->round->name }} conducted by {{ $applicationRound->conductedPerson->name }}<br>
+                        {{ $applicationRound->round->name }} for {{ $applicationRound->application->job->title }} conducted by {{ $applicationRound->conductedPerson->name }}<br>
                         @if ($applicationRound->mail_sent)
                             <span data-toggle="modal" data-target="#round_mail_{{ $applicationRound->id }}" class="{{ config("constants.hr.status.$applicationRound->round_status.class") }} modal-toggler">Communication mail</span><br>
                             @include('hr.round-review-sent-mail-modal', [ 'applicationRound' => $applicationRound ])
