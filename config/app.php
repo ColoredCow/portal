ce291a7be7ce8e71f9587e0ae2293b56b1056d24
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-05-19 09:32:23 +0530

    Revert "sending notification to the scheduled user (#236)" (#237)
    This reverts commit 9ea89f4606900e3d61d818516580dfc3ba02fb7e.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Employee Portal'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
