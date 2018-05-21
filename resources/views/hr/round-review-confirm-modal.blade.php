cb2a65cb37dc9e98bb6e303332125f8ccf6978a0
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-22 00:31:25 +0530

    final changes

--- resources/views/hr/round-review-confirm-modal.blade.php
+++ resources/views/hr/round-review-confirm-modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade hr_round_guide" id="round_confirm_{{ $applicationRound->id }}" tabindex="-1" role="dialog" aria-labelledby="round_confirm_{{ $applicationRound->id }}" aria-hidden="true">
+<div class="modal fade" id="round_confirm_{{ $applicationRound->id }}" tabindex="-1" role="dialog" aria-labelledby="round_confirm_{{ $applicationRound->id }}" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
