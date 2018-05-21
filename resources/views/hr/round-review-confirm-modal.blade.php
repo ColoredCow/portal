63eb82f0e0dd6cea8df2df49b9c860d4d17627b4
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-22 00:30:12 +0530

    self review

--- resources/views/hr/round-review-confirm-modal.blade.php
+++ resources/views/hr/round-review-confirm-modal.blade.php
@@ -1,39 +1,33 @@
 <div class="modal fade hr_round_guide" id="round_confirm_{{ $applicationRound->id }}" tabindex="-1" role="dialog" aria-labelledby="round_confirm_{{ $applicationRound->id }}" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <form action="/hr/rounds/{{ $applicationRound->id }}" method="POST">
-
-                {{ csrf_field() }}
-                {{ method_field('PATCH') }}
-
-                <div class="modal-header">
+            <div class="modal-header">
                 <h5 class="modal-title" id="round_confirm_{{ $applicationRound->id }}">Schedule @{{ nextRoundName }}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-row">
-                        <div class="form-group col-md-5">
-                            <label for="next_scheduled_date">Scheduled date</label>
-                            <input type="datetime-local" name="next_scheduled_date" id="next_scheduled_date" class="form-control">
-                        </div>
-                        <div class="form-group offset-md-1 col-md-5">
-                            <label for="next_scheduled_person_id">Scheduled for</label>
-                            <select name="next_scheduled_person_id" id="next_scheduled_person_id" class="form-control">
-                                @foreach ($interviewers as $interviewer)
-                                    <option value="{{ $interviewer->id }}">{{ $interviewer->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="next_scheduled_date">Scheduled date</label>
+                        <input type="datetime-local" name="next_scheduled_date" id="next_scheduled_date" class="form-control">
                     </div>
-                    <div class="form-row mt-2">
-                        <div class="form-group col-md-11">
-                            <button type="button" class="btn btn-success px-4 round-submit" data-action="confirm">Confirm</button>
-                        </div>
+                    <div class="form-group offset-md-1 col-md-5">
+                        <label for="next_scheduled_person_id">Scheduled for</label>
+                        <select name="next_scheduled_person_id" id="next_scheduled_person_id" class="form-control">
+                            @foreach ($interviewers as $interviewer)
+                                <option value="{{ $interviewer->id }}">{{ $interviewer->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
                 </div>
-            </form>
+                <div class="form-row mt-2">
+                    <div class="form-group col-md-12">
+                        <button type="button" class="btn btn-success px-4 round-submit" data-action="confirm">Confirm</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
