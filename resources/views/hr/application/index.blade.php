bb6a20279474423cbbf4a04e098933ad9cdbf4a3
wevekamola <vivek.amola@coloredcow.in>
2018-05-22 17:09:10 +0530

    #235 message will only display on job dashboard

--- resources/views/hr/application/index.blade.php
+++ resources/views/hr/application/index.blade.php
@@ -16,9 +16,11 @@
                 <a class="nav-item nav-link {{ $status === config('constants.hr.status.rejected.label') ? 'active bg-info text-white' : 'text-info' }}" href="/hr/applications?status={{ config('constants.hr.status.rejected.label') }}"><i class="fa fa-times-circle"></i>&nbsp;Rejected</a>
             </li>
        </ul>
+        @if(Request::path() == 'hr/applications/job')
         <div class="alert alert-info mb-2 p-2">
             <span>There are <b>{{ $openJobsCount }}</b> open jobs and <b>{{ $applications->total() }}</b> open applications</span>
         </div>
+        @endif
     </div>
     
     <table class="table table-striped table-bordered" id="applicants_table">
