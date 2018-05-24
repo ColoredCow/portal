68e487c5c329ef3cf426f0cfd9c21b74b1fa168b
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-24 10:51:19 +0530

    fixes #279

--- app/Mail/HR/Applicant/RoundReviewed.php
+++ app/Mail/HR/Applicant/RoundReviewed.php
@@ -35,7 +35,12 @@ class RoundReviewed extends Mailable
      */
     public function build()
     {
-        return $this->from(env('HR_DEFAULT_FROM_EMAIL'), env('HR_DEFAULT_FROM_NAME'))
+        $application = $this->applicationRound->application;
+
+        return $this->to($application->applicant->email, $application->applicant->name)
+            ->bcc($application->job->posted_by)
+            ->bcc(env('HR_DEFAULT_FROM_EMAIL'))
+            ->from(env('HR_DEFAULT_FROM_EMAIL'), env('HR_DEFAULT_FROM_NAME'))
             ->subject($this->applicationRound->mail_subject)
             ->view('mail.plain')->with([
                 'body' => $this->applicationRound->mail_body
