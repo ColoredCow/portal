b228c64bcd4d150f098368626af3fcdb15215dd6
panakj-ag <pankaj@coloredcow.in>
2018-05-27 22:01:58 +0530

    bug fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -464,7 +464,7 @@ if (document.getElementById('books_category')) {
     const bookForm = new Vue({
         el: '#books_category',
         data: {
-            categories: document.getElementById('category_container').dataset.categories ? JSON.parse(document.getElementById('category_container').dataset.categories) : {},
+            categories: document.getElementById('category_container').dataset.categories ? JSON.parse(document.getElementById('category_container').dataset.categories) : [],
             categoryNameToChange: [],
             indexRoute:document.getElementById('category_container').dataset.indexRoute  || '',
             newCategoryName:'',
