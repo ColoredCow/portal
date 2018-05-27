40d8ab6f26aea7802cefca488457881780182e72
Pankaj Agrawal <pankaj@coloredcow.in>
2018-05-27 21:40:40 +0530

    (140) new book category (#299)
    * new book categories
    
    * name chnages
    
    * bug fix
    
    * updates
    
    * (#140) fix
    
    * updated
    
    * update biikks
    
    * self review changes
    
    * self review changes
    
    * (feedback)
    
    * review feedback
    
    * review feedback
    
    * update routes
    
    * (#140) add pagenations
    
    * Feedbacks
    
    * UI changes

---
+++ database/migrations/2018_05_25_172849_update_book_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateBookCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('book_categories', function(Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropColumn(['deleted_at']);
+    }
+}
