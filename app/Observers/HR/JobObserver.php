7e04b1cfbb48ba61962e0c9914e97700e62908c9
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-05-28 18:33:51 +0530

    indentation fix

--- app/Observers/HR/JobObserver.php
+++ app/Observers/HR/JobObserver.php
@@ -7,14 +7,14 @@ use App\Models\HR\Round;
 
 class JobObserver
 {
-	/**
-	 * Listen to the Job create event.
-	 *
-	 * @param  \App\Models\HR\Job  $job
-	 * @return void
-	 */
-	public function created(Job $job)
-	{
-		$job->rounds()->attach(Round::all()->pluck('id')->toArray());
-	}
+    /**
+     * Listen to the Job create event.
+     *
+     * @param  \App\Models\HR\Job  $job
+     * @return void
+     */
+    public function created(Job $job)
+    {
+        $job->rounds()->attach(Round::all()->pluck('id')->toArray());
+    }
 }
