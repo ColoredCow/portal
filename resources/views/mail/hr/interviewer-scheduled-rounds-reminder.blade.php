44966713eb38d7a6905ba69b1a2c28a7d31b9fa9
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-06-03 15:46:27 +0530

    formatting

--- resources/views/mail/hr/interviewer-scheduled-rounds-reminder.blade.php
+++ resources/views/mail/hr/interviewer-scheduled-rounds-reminder.blade.php
@@ -1,16 +1,16 @@
 <div>
-	<p>Hi,</p>
-	<p>The following applications on {{ config('app.name') }} are scheduled for you today:</p>
+    <p>Hi,</p>
+    <p>The following applications on {{ config('app.name') }} are scheduled for you today:</p>
 
-	<ul>
-	@foreach ($applicationRounds as $applicationRound)
-		<li>
-			<b>{{ date(config('constants.hr.interview-time-format'), strtotime($applicationRound->scheduled_date)) }} –</b>&nbsp;{{ $applicationRound->round->name }} for {{ $applicationRound->application->applicant->name }}
-			@php
-				$route = 'applications.' . $applicationRound->application->job->type . '.edit';
-			@endphp
-			&nbsp;&nbsp;<a href="{{ URL::route($route, $applicationRound->application->id) }}">View</a>
-		</li>
-	@endforeach
-	</ul>
+    <ul>
+    @foreach ($applicationRounds as $applicationRound)
+        <li>
+            <b>{{ date(config('constants.hr.interview-time-format'), strtotime($applicationRound->scheduled_date)) }} –</b>&nbsp;{{ $applicationRound->round->name }} for {{ $applicationRound->application->applicant->name }}
+            @php
+                $route = 'applications.' . $applicationRound->application->job->type . '.edit';
+            @endphp
+            &nbsp;&nbsp;<a href="{{ URL::route($route, $applicationRound->application->id) }}">View</a>
+        </li>
+    @endforeach
+    </ul>
 </div>
