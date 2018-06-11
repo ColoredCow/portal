bdf89089204761cfac0a7043d46df99cbcff7c02
satendra95 <officialsatendrarawat@gmail.com>
2018-06-11 18:51:27 +0530

    swaped library_books.view with weeklydoses as they were misplaced

--- resources/views/knowledgecafe/index.blade.php
+++ resources/views/knowledgecafe/index.blade.php
@@ -4,14 +4,14 @@
 <div class="container">
     <br>
     <div class="d-flex justify-content-start row flex-wrap">
-            @can('weeklydoses.view')
+            @can('library_books.view')
         <div class="col-md-3 card mx-5 my-3">
             <a class="card-body no-transition" href="/knowledgecafe/library/books">
                 <br><h2 class="text-center">Library</h2><br>
             </a>
         </div>
             @endcan
-            @can('library_books.view')
+            @can('weeklydoses.view')
         <div class="col-md-3 card mx-5 my-3">
             <a class="card-body no-transition" href="/knowledgecafe/weeklydoses/">
             	<br><h2 class="text-center">WeeklyDose</h2><br>
