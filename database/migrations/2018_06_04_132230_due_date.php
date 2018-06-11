33bf1386d3c8e128489fac77f7590fa6dfe67df5
Mohit Gusain <mohitdreamer05@gmail.com>
2018-06-11 11:47:09 +0530

    Review Feedback

--- database/migrations/2018_06_04_132230_due_date.php
+++ database/migrations/2018_06_04_132230_due_date.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class DueDate extends Migration
 {
@@ -13,9 +13,9 @@ class DueDate extends Migration
      */
     public function up()
     {
-       Schema::table('finance_invoices', function (Blueprint $table) {
-        $table->date('due_date')->nullable();
-       }); //
+        Schema::table('finance_invoices', function (Blueprint $table) {
+            $table->date('due_date')->nullable();
+        });
     }
 
     /**
@@ -28,6 +28,5 @@ class DueDate extends Migration
         Schema::table('finance_invoices', function (Blueprint $table) {
             $table->dropColumn(['due_date']);
         });
-//
     }
 }
