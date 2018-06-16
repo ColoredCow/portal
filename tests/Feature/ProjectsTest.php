79518ecf7483dfc35c074e24a93b980a56cf5646
panakj-ag <pankaj@coloredcow.in>
2018-06-16 19:46:10 +0530

    bug fix

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -4,8 +4,16 @@ namespace Tests\Feature;
 
 class ProjectsTest extends FeatureTest
 {
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+   
+     /** @test */
+    public function an_authorised_user_can_see_projects() {
+        $this->anAuthorizedUser();
+        $this->get(route('projects'))
+            ->assertStatus(200);
     }
+
+    public function anAuthorizedUser() {
+        $this->signIn(create(User::class)->assignRole('super-admin'));
+    }
+
 }
