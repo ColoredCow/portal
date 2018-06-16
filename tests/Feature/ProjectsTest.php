645d3493a0048600be9163c1f35346d73433c73e
panakj-ag <pankaj@coloredcow.in>
2018-06-16 19:47:18 +0530

    style ci

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -4,16 +4,16 @@ namespace Tests\Feature;
 
 class ProjectsTest extends FeatureTest
 {
-   
-     /** @test */
-    public function an_authorised_user_can_see_projects() {
+    /** @test */
+    public function an_authorised_user_can_see_projects()
+    {
         $this->anAuthorizedUser();
         $this->get(route('projects'))
             ->assertStatus(200);
     }
 
-    public function anAuthorizedUser() {
+    public function anAuthorizedUser()
+    {
         $this->signIn(create(User::class)->assignRole('super-admin'));
     }
-
 }
