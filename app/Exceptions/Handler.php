cc083ec6db5e0d9cd0a6913a7a38a4b7db835730
Abhishek <abhip099@gmail.com>
2018-06-16 19:15:11 +0530

    modified render method to return custom view for any HttpException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +49,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof HttpException) {
+            $statusCode = $exception->getStatusCode();
+            return response()->view('errors.custom', ['statusCode' => $statusCode], 500);
+        }
         return parent::render($request, $exception);
     }
 }
