99104a556a8be3fa9c446099babdc5f3ab909daf
vikas rinvi <vikasmrnv@gmail.com>
2018-06-16 13:44:32 -0700

    345 resolve broken UI in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,27 +5,28 @@
 <div class="container" id="home_page">
     @include('status', ['errors' => $errors->all()])
     <br>
-    <div class="d-flex justify-content-start row flex-wrap">
+    <div class="d-flex justify-content-center row flex-wrap text-center ">
         @can('hr_applicants.view')
-        <div class="col-md-3 card mx-5 my-3">
+        <div class="col-md-3 col-lg-3 col-sm-4 card m-3">
             <a class="card-body no-transition" href="/hr/applications/job">
-                <br><h2 class="text-center">HR</h2><br>
+                <br><h2 class="text-center" style="font-size:2.9vw;">HR</h2><br>
             </a>
         </div>
         @endcan
 
         @can('finance_reports.view')
-        <div class="col-md-3 card mx-5 my-3">
+        <div class="col-md-3 col-lg-3 col-sm-4 card m-3">
             <a class="card-body no-transition" href="/finance/reports?type=monthly">
-                <br><h2 class="text-center">Finance</h2><br>
+                <br><h2 class="text-center" style="font-size:2.9vw;">Finance</h2><br>
             </a>
         </div>
         @endcan
 
         @if(auth()->user()->can('weeklydoses.view') || auth()->user()->can('library_books.view'))
-        <div class="col-md-3 card mx-5 my-3">
+        <div class="col-md-3 col-lg-3 col-sm-4 card m-3">
             <a class="card-body no-transition" href="/knowledgecafe">
-                <br><h2 class="text-center">Knowledge Cafe</h2><br>
+                <br><h2 class="text-center" style="font-size:2.9vw;">Knowledge Cafe</h2><br>
+
             </a>
         </div>
         @endif
