c03d8b2f95a94a264616b7b305694c365d6243a8
panakj-ag <pankaj@coloredcow.in>
2018-06-17 13:46:35 +0530

    test 12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" href="{{ asset('favicon.png') }}" sizes="32x32" />
 
     <!-- Styles -->
-    <link href="{{ 'css/app.css' }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
@@ -109,6 +109,6 @@
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
     <script src="{{ asset('src/tinymce/js/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ '/js/app.js' }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
