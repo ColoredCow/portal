c1e8e3983f80d1400da2ddae9406e08f0f708505
panakj-ag <pankaj@coloredcow.in>
2018-06-17 13:22:05 +0530

    test 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        $this->app->bind('path.public', function() {
+            return base_path().'/../public_html';
+        });
     }
 
     /**
