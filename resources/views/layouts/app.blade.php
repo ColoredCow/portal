e2ae1e84c89e3abb0927079132cf520729cdf555
panakj-ag <pankaj@coloredcow.in>
2018-06-17 13:07:01 +0530

    bug fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" href="{{ asset('favicon.png') }}" sizes="32x32" />
 
     <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ 'css/app.css' }}" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
