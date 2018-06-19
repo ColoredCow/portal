cfcf3b8179d6ce1d41204738d2270c917ec6f3de
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-06-20 02:26:41 +0530

    ui-cleanup

--- resources/views/hr/application/onboard-applicant-modal.blade.php
+++ resources/views/hr/application/onboard-applicant-modal.blade.php
@@ -2,25 +2,33 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Approve</h5>
+                <div class="d-block">
+                    <h5 class="modal-title">Approve</h5>
+                    <h6 class="text-secondary">{{ $applicant->name }} &mdash; {{ $applicant->email }}</h6>
+                </div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="form-row">
-                    <div class="form-group col-md-5">
+                    <div class="form-group col-md-6">
                         <label for="onboard_email">Email</label>
-                        <input type="text" class="form-control" name="onboard_email" id="onboard_email" required="required">coloredcow.com
+                        <div class="input-group">
+                            <input type="text" name="onboard_email" id="onboard_email" class="form-control" placeholder="username" autocomplete="off">
+                            <div class="input-group-append">
+                                <span class="input-group-text">{{ '@' . env('GOOGLE_CLIENT_HD') }}</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group offset-md-1 col-md-5">
-                        <label for="onboard_email">Password</label>
-                        <input type="password" class="form-control" name="onboard_password" id="onboard_password" required="required">
+                    <div class="form-group col-md-6">
+                        <label for="onboard_password">Password</label>
+                        <input type="password" class="form-control" name="onboard_password" id="onboard_password" required="required" autocomplete="off">
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="form-group col-md-5">
-                        <label for="onboard_email">Designation</label>
+                    <div class="form-group col-md-6">
+                        <label for="designation">Designation</label>
                         <input type="text" class="form-control" name="designation" id="designation" required="required">
                     </div>
                 </div>
