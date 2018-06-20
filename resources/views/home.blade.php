ee0ff7a188547d176132767db9df1933b0dd062f
vikasrnv <32789328+vikasrnv@users.noreply.github.com>
2018-06-19 22:40:21 -0700

    345 UI broken in home page (#465)
    * 345 resolve broken UI in home page
    
    * style put in app.scss file
    
    * some extra line removed
    
    * extra line is removed
    
    * fommating correction and classes name covention solved
    
    * extra line removed
    
    * space removed
    
    * formating resolve

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
 <div class="container" id="home_page">
     @include('status', ['errors' => $errors->all()])
     <br>
+
     <div class="d-flex justify-content-start row flex-wrap">
         <div class="col-md-3 card mx-5 my-3">
             <a class="card-body no-transition" href="/hr">
@@ -13,7 +14,7 @@
         </div>
 
         @can('finance_reports.view')
-        <div class="col-md-3 card mx-5 my-3">
+        <div class="col-md-3 col-sm-4 card m-3">
             <a class="card-body no-transition" href="/finance/reports?type=monthly">
                 <br><h2 class="text-center">Finance</h2><br>
             </a>
@@ -21,7 +22,7 @@
         @endcan
 
         @if(auth()->user()->can('weeklydoses.view') || auth()->user()->can('library_books.view'))
-        <div class="col-md-3 card mx-5 my-3">
+        <div class="col-md-3 col-sm-4 card m-3">
             <a class="card-body no-transition" href="/knowledgecafe">
                 <br><h2 class="text-center">Knowledge Cafe</h2><br>
             </a>
