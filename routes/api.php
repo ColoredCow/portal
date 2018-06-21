0c63d2d4704648b37cac38c030a5d9c33fe9e34e
vikas rinvi <vikasmrnv@gmail.com>
2018-06-20 23:50:54 -0700

    test route is added

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
  */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('/user', function (Request $request) {
 })->middleware('client');
