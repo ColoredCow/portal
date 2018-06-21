c28dd8df1ffa347053b1b900c0e8b1a70b1bf292
vikas rinvi <vikasmrnv@gmail.com>
2018-06-21 00:24:22 -0700

    group added to the route

--- routes/api.php
+++ routes/api.php
@@ -11,18 +11,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
  */
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/user', function (Request $request) {
-})->middleware('client');
+})->middleware('client')->group(function () {
 
-Route::resource('hr/jobs', 'HR\JobController')->only(['store']);
-Route::resource('hr/applicants', 'HR\ApplicantController')->only(['store']);
-Route::prefix('knowledgecafe')->namespace('KnowledgeCafe')->group(function () {
-    Route::prefix('library')->namespace('Library')->group(function () {
-        Route::get('book/getList', 'BookController@getBookList');
+    Route::resource('hr/jobs', 'HR\JobController')->only(['store']);
+    Route::resource('hr/applicants', 'HR\ApplicantController')->only(['store']);
+    Route::prefix('knowledgecafe')->namespace('KnowledgeCafe')->group(function () {
+        Route::prefix('library')->namespace('Library')->group(function () {
+            Route::get('book/getList', 'BookController@getBookList');
+        });
+        Route::resource('weeklydoses', 'WeeklyDoseController')->only(['store']);
     });
-    Route::resource('weeklydoses', 'WeeklyDoseController')->only(['store']);
 });
