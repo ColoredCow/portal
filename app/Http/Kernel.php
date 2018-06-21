747a5b24e3272804ef277fe802a5841a42af0e4a
vikas rinvi <vikasmrnv@gmail.com>
2018-06-20 18:32:20 -0700

    testing route is remove

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,8 +36,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \Laravel\Passport\Middleware\CreatFreshApiToken::class,
-
         ],
 
         'api' => [
--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,19 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
  */
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/user', function (Request $request) {
-    return 'you are authorized';
-})->middleware('client');
+})->middleware('client')
 
-Route::resource('hr/jobs', 'HR\JobController')->only(['store']);
-Route::resource('hr/applicants', 'HR\ApplicantController')->only(['store']);
-Route::prefix('knowledgecafe')->namespace('KnowledgeCafe')->group(function () {
-    Route::prefix('library')->namespace('Library')->group(function () {
-        Route::get('book/getList', 'BookController@getBookList');
+    Route::resource('hr/jobs', 'HR\JobController')->only(['store']);
+    Route::resource('hr/applicants', 'HR\ApplicantController')->only(['store']);
+    Route::prefix('knowledgecafe')->namespace('KnowledgeCafe')->group(function () {
+        Route::prefix('library')->namespace('Library')->group(function () {
+            Route::get('book/getList', 'BookController@getBookList');
+        });
+        Route::resource('weeklydoses', 'WeeklyDoseController')->only(['store']);
     });
-    Route::resource('weeklydoses', 'WeeklyDoseController')->only(['store']);
-});
+
