04810aba3602be5ede22e2ab7dfbfb9b41af7ede
Abhishek <abhip099@gmail.com>
2018-06-22 13:38:14 +0530

    removed hardcoded styling, used bootstrap flex

--- resources/views/errors/custom.blade.php
+++ resources/views/errors/custom.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container" >
-	<div class="card" style="margin-top: 100px; margin-left: 100px; margin-right: 100px;">
-		<div class="card-body">
-			<h2 class="text-center text-danger">
-				<i class="fa fa-warning"></i>Error - {{ $statusCode }}
-			</h2>
-			<br>
-			<h4 class="text-danger text-center">The server was not able to handle this request.</h4>
+	<div class="d-flex justify-content-center">
+		<div class="card">
+			<div class="card-body">
+				<h2 class="text-center text-danger">
+					<i class="fa fa-warning"></i>Error - {{ $statusCode }}
+				</h2>
+				<br>
+				<h4 class="text-danger text-center">The server was not able to handle this request.</h4>
+			</div>
 		</div>
 	</div>
 </div>
