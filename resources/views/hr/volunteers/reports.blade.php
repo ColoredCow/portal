791d8ff3f72981efc0382d9a6c526371646c3bb7
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-06-22 19:39:25 +0530

    volunteer reports and campaigns (#508)

--- resources/views/hr/volunteers/reports.blade.php
+++ resources/views/hr/volunteers/reports.blade.php
@@ -16,7 +16,7 @@
             <h1>Volunteering Reports</h1>
         </div>
         <div class="col-md-12">
-            <img src="/images/recruitment-report-min.png" alt="employee reports" class="w-100">
+            <img src="/images/volunteer-reports-min.png" alt="volunteer reports" class="w-100">
         </div>
     </div>
     <div class="row">
