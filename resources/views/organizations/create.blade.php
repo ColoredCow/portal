ea5593b4802e81337bfb59e100233a6633d285b6
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-06-26 16:16:33 +0530

    layout changes

--- resources/views/organizations/create.blade.php
+++ resources/views/organizations/create.blade.php
@@ -20,40 +20,40 @@
         <form action="{{ route('organizations.store') }}" method="POST" enctype="multipart/form-data" id="onboard_organization">
             {{ csrf_field() }}
             <section class="step-onboarding" v-show="step == 1">
-                <h3 class="mb-3"><strong>Step 1:&nbsp;</strong>Setup your organization</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 1:&nbsp;</strong>Setup your organizational details</h3>
                 <div class="form-group">
-                    <label for="name">Organization name</label>
+                    <label class="font-weight-bold" for="name">Organization name</label>
                     <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required="required">
                 </div>
                 <div class="form-group">
-                    <label for="admin_email">G Suite admin email</label>
+                    <label class="font-weight-bold" for="admin_email">G Suite admin email</label>
                     <input type="text" name="admin_email" id="admin_email" class="form-control" value="{{ old('admin_email') }}" required="required">
                 </div>
             </section>
             <section class="step-onboarding" v-show="step == 2">
-                <h3 class="mb-3"><strong>Step 2:&nbsp;</strong>Setup G Suite API credentials</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 2:&nbsp;</strong>Setup G Suite API credentials</h3>
                 <ul>
                     <li class="mb-2"><strong>Create a new project</strong> from your Google Developer Console<br><a href="https://console.developers.google.com/projectcreate" target="_blank">Create a new project</a></li>
                     <li class="mb-2">Next, <strong>create a service account</strong> for the project that you just created. While creating the service account, <strong>make sure you check the private key and domain-wide delegation options</strong>. Download the private key. We'll need it in the next step.<br><a href="https://console.developers.google.com/iam-admin/serviceaccounts" target="_blank">Create a service account</a></li>
                 </ul>
             </section>
             <section class="step-onboarding" v-show="step == 3">
-                <h3 class="mb-3"><strong>Step 3:&nbsp;</strong>Complete your service account details.</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 3:&nbsp;</strong>Complete your service account details.</h3>
                 <div class="form-group">
-                    <label for="gsuite_sa_client_id">Service Account Client ID</label>
+                    <label class="font-weight-bold" for="gsuite_sa_client_id">Service Account Client ID</label>
                     <input type="text" name="gsuite_sa_client_id" id="gsuite_sa_client_id" class="form-control" value="{{ old('gsuite_sa_client_id') }}" required="required">
                 </div>
                 <div class="form-group">
-                    <label for="gsuite_dwd_private_key">Service Account Private Key (downloaded in the previous step)</label>
+                    <label class="font-weight-bold" for="gsuite_dwd_private_key">Service Account Private Key (downloaded in the previous step)</label>
 
                     <input type="file" name="gsuite_dwd_private_key" id="gsuite_dwd_private_key" class="form-control-file" accept=".json" required="required">
                     <small class="form-text text-muted">We'll never share these credentials with anyone else.</small>
                 </div>
             </section>
             <section class="step-onboarding" v-show="step == 4">
-                <h3 class="mb-3"><strong>Step 4:&nbsp;</strong>Enable APIs</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 4:&nbsp;</strong>Enable APIs</h3>
                 <div class="form-group">
-                    <label>Enable the following Google APIs for your project so that Employee Portal can make requests and access your organizational data.</label>
+                    <p>Enable the following Google APIs for your project so that Employee Portal can make requests and access your organizational data.</p>
                     <ul>
                         <li><a href="https://console.developers.google.com/apis/api/admin.googleapis.com/overview" target="_blank">Enable Admin SDK</a></li>
                         <li><a href="https://console.developers.google.com/apis/library/calendar-json.googleapis.com" target="_blank">Enable Calendar API</a></li>
@@ -61,7 +61,7 @@
                 </div>
             </section>
             <section class="step-onboarding" v-show="step == 5">
-                <h3 class="mb-3"><strong>Step 5:&nbsp;</strong>Enable domain-wide delegation from your G Suite admin</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 5:&nbsp;</strong>Enable domain-wide delegation from your G Suite admin</h3>
                 <div class="form-group">
                     <p class="mb-1">This will help us to access your following organizational G Suite details without troubling your team members for their consent.</p>
                     <ul>
@@ -82,9 +82,9 @@
                 </div>
             </section>
             <section class="step-onboarding" v-show="step == 6">
-                <h3 class="mb-3"><strong>Step 6:&nbsp;</strong>Set up your workspace</h3>
+                <h3 class="mt-3 mb-4"><strong>Step 6:&nbsp;</strong>Set up your workspace</h3>
                 <div class="form-group">
-                    <label for="slug">Your workspace name</label>
+                    <label class="font-weight-bold" for="slug">Your workspace name</label>
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="myorganization" name="slug" id="slug" value="{{ old('slug') }}" required="required">
                         <div class="input-group-append">
