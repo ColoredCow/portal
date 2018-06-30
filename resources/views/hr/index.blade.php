e5a855cfdd4e0c080cac28304320a6b33e48f1d1
Vaibhav Rathore <vaibhav@coloredcow.com>
2018-06-30 18:39:28 +0530

    ACL cleanup for existing modules and users (#532)
    * date bug fix in finance invoice screens
    
    * Scrutinizer Badge
    
    * initial fix
    
    * review feedback

--- resources/views/hr/index.blade.php
+++ resources/views/hr/index.blade.php
@@ -4,21 +4,27 @@
 <div class="container">
     <br>
     <div class="d-flex justify-content-start row flex-wrap">
-        <div class="col-md-3 card mx-5 my-3">
+        @can('hr_recruitment_applications.view')
+        <div class="col-md-3 card mx-5 mt-3 mb-5">
             <a class="card-body no-transition" href="{{ route('applications.job.index') }}">
                 <br><h2 class="text-center">Recruitment</h2><br>
             </a>
         </div>
-        <div class="col-md-3 card mx-5 my-3">
+        @endcan
+        @can('hr_employees.view')
+        <div class="col-md-3 card mx-5 mt-3 mb-5">
             <a class="card-body no-transition" href="{{ route('employees') }}">
             	<br><h2 class="text-center">Employees</h2><br>
             </a>
         </div>
-        <div class="col-md-3 card mx-5 my-3">
+        @endcan
+        @can('hr_volunteers_applications.view')
+        <div class="col-md-3 card mx-5 mt-3 mb-5">
             <a class="card-body no-transition" href="{{ route('applications.volunteer.index') }}">
                 <br><h2 class="text-center">Volunteer</h2><br>
             </a>
         </div>
+        @endcan
     </div>
 </div>
 @endsection
