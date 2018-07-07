123180f485450c3e5750a34f7412aa142745ae3c
vikas rinvi <vikasmrnv@gmail.com>
2018-07-07 00:00:52 -0700

    UI aling to center

--- resources/views/hr/index.blade.php
+++ resources/views/hr/index.blade.php
@@ -3,27 +3,31 @@
 @section('content')
 <div class="container">
     <br>
-    <div class="d-flex justify-content-center row flex-wrap">
-        <div class="col-md-3 card m-3">
+    <div class="row d-flex justify-content-start flex-wrap">
         @can('hr_recruitment_applications.view')
+                <div class="col-md-4">
+    <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="{{ route('applications.job.index') }}">
                 <br><h2 class="text-center">Recruitment</h2><br>
             </a>
-        </div>
-        <div class="col-md-3 card m-3">
+        </div></div>
         @endcan
         @can('hr_employees.view')
+                <div class="col-md-4">
+    <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="{{ route('employees') }}">
             	<br><h2 class="text-center">Employees</h2><br>
             </a>
-        </div>
-        <div class="col-md-3 card m-3">
+        </div></div>
+
         @endcan
         @can('hr_volunteers_applications.view')
+                <div class="col-md-4">
+    <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="{{ route('applications.volunteer.index') }}">
                 <br><h2 class="text-center">Volunteer</h2><br>
             </a>
-        </div>
+        </div></div>
         @endcan
     </div>
 </div>
--- resources/views/knowledgecafe/index.blade.php
+++ resources/views/knowledgecafe/index.blade.php
@@ -3,20 +3,22 @@
 @section('content')
 <div class="container">
     <br>
-    <div class="d-flex justify-content-center row flex-wrap">
+   <div class="row d-flex justify-content-center flex-wrap">
         @can('library_books.view')
-        <div class="col-md-3 card m-3">
+            <div class="col-md-4">
+    <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="/knowledgecafe/library/books">
                 <br><h2 class="text-center">Library</h2><br>
             </a>
-        </div>
+        </div></div>
         @endcan
         @can('weeklydoses.view')
-        <div class="col-md-3 card m-3">
+            <div class="col-md-4">
+    <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="/knowledgecafe/weeklydoses/">
             	<br><h2 class="text-center">Weekly Dose</h2><br>
             </a>
-        </div>
+        </div></div>
         @endcan
     </div>
 </div>
