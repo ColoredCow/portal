304d8c3d60a84a8b2df758b815cdf5dbdc891d7c
vikas rinvi <vikasmrnv@gmail.com>
2018-07-08 22:58:42 -0700

    some minute change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
     @include('status', ['errors' => $errors->all()])
     <br>
 
-    <div class="row d-flex justify-content-start flex-wrap">
-        @if(auth()->user()->hasAnyPermission(['hr_recruitment_applications.view', 'hr_employees.view', 'hr_volunteers_applications.view']))
+    <div class="d-flex justify-content-start row flex-wrap">
+              @if(auth()->user()->hasAnyPermission(['hr_recruitment_applications.view', 'hr_employees.view', 'hr_volunteers_applications.view']))
             <div class="col-md-4">
     <div class="card h-75 mx-4 mt-3 mb-5 ">
             <a class="card-body no-transition" href="/hr">
--- resources/views/knowledgecafe/index.blade.php
+++ resources/views/knowledgecafe/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <br>
-   <div class="row d-flex justify-content-start flex-wrap">
+   <div class="d-flex justify-content-start row flex-wrap">
         @can('library_books.view')
             <div class="col-md-4">
     <div class="card h-75 mx-4 mt-3 mb-5 ">
