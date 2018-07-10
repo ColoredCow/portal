d05c56ed79f58ba430f876cca911e35725a7f88f
vikasrnv <32789328+vikasrnv@users.noreply.github.com>
2018-07-10 00:17:34 -0700

    345 ui fix home page (#505)
    * futher adjustment in broken UI
    
    * minor change in margin
    
    * UI ifx for volunteer module
    
    * extra classes os css removed
    
    * UI aling to center
    
    * change in knowledgecafe
    
    * resolving some styleci error
    
    * some minute change
    
    * travis issue solve
    
    * minute change
    
    * minute change
    
    * fommating update

--- resources/views/hr/index.blade.php
+++ resources/views/hr/index.blade.php
@@ -5,24 +5,31 @@
     <br>
     <div class="d-flex justify-content-start row flex-wrap">
         @can('hr_recruitment_applications.view')
-        <div class="col-md-3 card mx-5 mt-3 mb-5">
-            <a class="card-body no-transition" href="{{ route('applications.job.index') }}">
-                <br><h2 class="text-center">Recruitment</h2><br>
-            </a>
+        <div class="col-md-4">
+            <div class="card h-75 mx-4 mt-3 mb-5 ">
+                <a class="card-body no-transition" href="{{ route('applications.job.index') }}">
+                    <br><h2 class="text-center">Recruitment</h2><br>
+                </a>
+            </div>
         </div>
         @endcan
         @can('hr_employees.view')
-        <div class="col-md-3 card mx-5 mt-3 mb-5">
-            <a class="card-body no-transition" href="{{ route('employees') }}">
-            	<br><h2 class="text-center">Employees</h2><br>
-            </a>
+        <div class="col-md-4">
+            <div class="card h-75 mx-4 mt-3 mb-5 ">
+                <a class="card-body no-transition" href="{{ route('employees') }}">
+            	   <br><h2 class="text-center">Employees</h2><br>
+                </a>
+            </div>
         </div>
+
         @endcan
         @can('hr_volunteers_applications.view')
-        <div class="col-md-3 card mx-5 mt-3 mb-5">
-            <a class="card-body no-transition" href="{{ route('applications.volunteer.index') }}">
-                <br><h2 class="text-center">Volunteer</h2><br>
-            </a>
+        <div class="col-md-4">
+            <div class="card h-75 mx-4 mt-3 mb-5 ">
+                <a class="card-body no-transition" href="{{ route('applications.volunteer.index') }}">
+                    <br><h2 class="text-center">Volunteer</h2><br>
+                </a>
+            </div>
         </div>
         @endcan
     </div>
