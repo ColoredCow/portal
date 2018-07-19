13edeaf29ac2378db4ebd263dc1c1f505a0eb001
Mohit Gusain <mohitdreamer05@gmail.com>
2018-07-19 23:40:56 +0530

    changes

---
+++ app/Http/Controllers/HR/Applications/ApplicationController.php
@@ -0,0 +1,115 @@
+<?php
+namespace App\Http\Controllers\HR\Applications;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\HR\ApplicationRequest;
+use App\Mail\HR\Application\JobChanged;
+use App\Mail\HR\Application\RoundNotConducted;
+use App\Models\HR\Application;
+use App\Models\HR\ApplicationMeta;
+use App\Models\HR\Job;
+use App\Models\Setting;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Mail;
+abstract class ApplicationController extends Controller
+{
+    abstract public function getApplicationType();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $filters = [
+            'status' => request()->get('status') ?: 'non-rejected',
+            'job-type' => $this->getApplicationType(),
+            'job' => request()->get('hr_job_id'),
+            'name' => request()->get('search'),
+        ];
+        $applications = Application::with(['applicant', 'job'])
+            ->applyFilter($filters)
+            ->latest()
+            ->paginate(config('constants.pagination_size'))
+            ->appends(Input::except('page'));
+        $countFilters = array_except($filters, ['status']);
+        $attr = [
+            'applications' => $applications,
+            'status' => request()->get('status'),
+        ];
+        $strings = array_pluck(config('constants.hr.status'), 'label');
+        foreach ($strings as $string) {
+            $attr[camel_case($string) . 'ApplicationsCount'] = Application::applyFilter($countFilters)
+                ->where('status', $string)
+                ->get()
+                ->count();
+        }
+        return view('hr.application.index')->with($attr);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  String  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $application = Application::findOrFail($id);
+        $application->load(['evaluations', 'evaluations.evaluationParameter', 'evaluations.evaluationOption', 'job', 'job.rounds', 'job.rounds.evaluationParameters', 'job.rounds.evaluationParameters.options', 'applicant', 'applicant.applications', 'applicationRounds', 'applicationRounds.evaluations', 'applicationRounds.round', 'applicationMeta']);
+        $job = $application->job;
+        $attr = [
+            'applicant' => $application->applicant,
+            'application' => $application,
+            'timeline' => $application->applicant->timeline(),
+            'interviewers' => User::interviewers()->get(),
+            'applicantOpenApplications' => $application->applicant->openApplications(),
+            'applicationFormDetails' => $application->applicationMeta()->formData()->first(),
+            'settings' => [
+                'noShow' => Setting::getNoShowEmail(),
+            ],
+            'type' => config("constants.hr.opportunities.$job->type.type"),
+        ];
+        if ($job->type == 'job') {
+            $attr['hasGraduated'] = $application->applicant->hasGraduated();
+            $attr['internships'] = Job::isInternship()->latest()->get();
+        }
+        return view('hr.application.edit')->with($attr);
+    }
+    /**
+     * Update the specified resource
+     *
+     * @param ApplicationRequest $request
+     * @param integer $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(ApplicationRequest $request, int $id)
+    {
+        $validated = $request->validated();
+        $application = Application::findOrFail($id);
+        $application->load('applicant');
+        switch ($validated['action']) {
+            case config('constants.hr.application-meta.keys.change-job')
+                $changeJobMeta = $application->changeJob($validated);
+                Mail::send(new JobChanged($application, $changeJobMeta));
+                return redirect()->route('applications.internship.edit', $id)->with('status', 'Application updated successfully!');
+                break;
+            case config('constants.hr.application-meta.keys.no-show'):
+                $roundNotConductedMeta = ApplicationMeta::create([
+                    'hr_application_id' => $application->id,
+                    'key' => $validated['action'],
+                    'value' => json_encode([
+                        'round' => $validated['application_round_id'],
+                        'reason' => $validated['no_show_reason'],
+                        'user' => Auth::id(),
+                        'mail_subject' => $validated['no_show_mail_subject'],
+                        'mail_body' => $validated['no_show_mail_body'],
+                    ]),
+                ]);
+                Mail::send(new RoundNotConducted($application, $roundNotConductedMeta));
+                return redirect()->back()->with('status', 'Application updated successfully!');
+                break;
+        }
+        return redirect()->back()->with('No changes were done to the application. Please make sure your are submitting valid data.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,138 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('home');
+    }
+    return redirect('login');
+});
+
+Auth::routes();
+
+Route::get('home', 'HomeController@index')->name('home');
+
+Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::middleware('auth')->group(function () {
+    Route::prefix('profile')->group(function () {
+        Route::get('gsuite-sync', 'UserController@syncWithGSuite')->name('profile.gsuite-sync');
+    });
+
+    Route::prefix('hr')->namespace('HR')->group(function () {
+        Route::get('/', 'HRController@index');
+
+        Route::prefix('recruitment')->namespace('Recruitment')->group(function () {
+            Route::get('reports', 'ReportsController@index')->name('recruitment.reports');
+            Route::get('campaigns', 'CampaignsController@index')->name('recruitment.campaigns');
+            Route::resource('opportunities', 'RecruitmentOpportunityController')
+                ->only(['index', 'store', 'update', 'edit'])
+                ->names([
+                    'index' => 'recruitment.opportunities',
+                    'store' => 'recruitment.opportunities.store',
+                    'update' => 'recruitment.opportunities.update',
+                    'edit' => 'recruitment.opportunities.edit',
+                ]);
+        });
+
+        Route::prefix('volunteers')->namespace('Volunteers')->group(function () {
+            Route::get('reports', 'ReportsController@index')->name('volunteers.reports');
+            Route::get('campaigns', 'CampaignsController@index')->name('volunteers.campaigns');
+            Route::resource('opportunities', 'VolunteerOpportunityController')
+                ->only(['index', 'store', 'update', 'edit'])
+                ->names([
+                    'index' => 'volunteer.opportunities',
+                    'store' => 'volunteer.opportunities.store',
+                    'update' => 'volunteer.opportunities.update',
+                    'edit' => 'volunteer.opportunities.edit',
+                ]);
+        });
+
+        Route::prefix('applications')->namespace('Applications')->group(function () {
+            Route::resource('/evaluation', 'EvaluationController')->only(['show', 'update']);
+            Route::resource('job', 'JobApplicationController')
+                ->only(['index', 'edit', 'update'])
+                ->names(['index' => 'applications.job.index', 'edit' => 'applications.job.edit', 'update' => 'applications.job.update']);
+            Route::resource('internship', 'InternshipApplicationController')
+                ->only(['index', 'edit'])
+                ->names(['index' => 'applications.internship.index', 'edit' => 'applications.internship.edit']);
+            Route::resource('volunteer', 'VolunteerApplicationController')
+                ->only(['index', 'edit'])
+                ->names([
+                    'index' => 'applications.volunteer.index',
+                    'edit' => 'applications.volunteer.edit',
+                ]);
+
+        });
+
+        Route::resource('employees', 'Employees\EmployeeController')
+            ->only(['index', 'show'])
+            ->names([
+                'index' => 'employees',
+                'show' => 'employees.show',
+            ]);
+        Route::get('employee-reports', 'Employees\ReportsController@index')->name('employees.reports');
+
+        Route::resource('applicants', 'ApplicantController')->only(['index', 'edit']);
+        Route::resource('applications/rounds', 'ApplicationRoundController')->only(['store', 'update']);
+
+        Route::resource('rounds', 'RoundController')->only(['update'])->names(['update' => 'hr.round.update']);
+        Route::post('application-round/{applicationRound}/sendmail', 'ApplicationRoundController@sendMail');
+    });
+
+    Route::prefix('finance')->namespace('Finance')->group(function () {
+        Route::resource('invoices', 'InvoiceController')->except(['show', 'destroy']);
+        Route::get('invoices/download/{year}/{month}/{file}', 'InvoiceController@download');
+        Route::get('/reports', 'ReportsController@index');
+    });
+
+    Route::resource('clients', 'ClientController')->except(['show', 'destroy']);
+    Route::resource('projects', 'ProjectController')
+        ->except(['show', 'destroy'])
+        ->names(['index' => 'projects', 'create' => 'projects.create', 'edit' => 'projects.edit', 'store' => 'projects.store', 'update' => 'projects.update']);
+    Route::get('clients/{client}/get-projects', 'ClientController@getProjects');
+    Route::resource('project/stages', 'ProjectStageController')->only(['store', 'update']);
+    Route::get('settings/{module}', 'SettingController@index');
+    Route::post('settings/{module}/update', 'SettingController@update');
+
+    Route::prefix('knowledgecafe')->namespace('KnowledgeCafe')->group(function () {
+        Route::get('/', 'KnowledgeCafeController@index');
+        Route::prefix('library')->namespace('Library')->group(function () {
+            Route::resource('books', 'BookController')
+                ->names([
+                    'index' => 'books.index',
+                    'create' => 'books.create',
+                    'show' => 'books.show',
+                    'store' => 'books.store',
+                    'destroy' => 'books.delete',
+                    'update' => 'books.update',
+                ]);
+
+            Route::prefix('book')->group(function () {
+                Route::post('fetchinfo', 'BookController@fetchBookInfo')->name('books.fetchInfo');
+                Route::post('markbook', 'BookController@markBook')->name('books.toggleReadStatus');
+                Route::post('addtowishlist', 'BookController@addToUserWishList')->name('books.addToWishList');
+                Route::get('disablesuggestion', 'BookController@disableSuggestion')->name('books.disableSuggestion');
+                Route::get('enablesuggestion', 'BookController@enableSuggestion')->name('books.enableSuggestion');
+            });
+
+            Route::resource('book-categories', 'BookCategoryController')
+                ->only(['index', 'store', 'update', 'destroy'])
+                ->names(['index' => 'books.category.index']);
+        });
+        Route::resource('weeklydoses', 'WeeklyDoseController')->only(['index'])->names(['index' => 'weeklydoses']);
+    });
+
+    Route::get('crm', 'CRM\CRMController@index')->name('crm');
+});
