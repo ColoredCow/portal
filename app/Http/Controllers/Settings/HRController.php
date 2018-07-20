e67cf36fea6c7527a2014ed75a3d66302b411879
Abhishek <abhip099@gmail.com>
2018-07-20 13:17:41 +0530

    A new folder app/Http/Controllers/Settings created to accomodate all other module's setting controller

---
+++ app/Http/Controllers/Settings/HRController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HRController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Settings/SettingController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use App\Http\Controllers\Controller;
+
+class SettingController extends Controller
+{
+    //
+}
