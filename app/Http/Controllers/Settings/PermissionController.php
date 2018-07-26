01071e584fb6572f6e5e0d0b071620ae80c82e38
Abhishek <abhip099@gmail.com>
2018-07-26 11:27:09 +0530

    style ci correction psr-2

--- app/Http/Controllers/Settings/PermissionController.php
+++ app/Http/Controllers/Settings/PermissionController.php
@@ -10,49 +10,53 @@ use App\User;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
-class PermissionController extends Controller {
-	public function index(String $module) {
-		$this->authorize('settings.view', Permission::class);
-		$attr = [];
-		switch ($module) {
-		case 'users':
-			$attr['users'] = User::with('roles')->get();
-			$attr['roles'] = Role::all();
-			break;
-		default:
-			$attr['roles'] = Role::with('permissions')->get();
-			$attr['permissions'] = Permission::all();
-			break;
-		}
-		$attr['settings'] = Setting::where('module', $module)->get()->keyBy('setting_key');
-		return view("settings.permissions.$module")->with($attr);
-	}
+class PermissionController extends Controller
+{
+    public function index(String $module)
+    {
+        $this->authorize('settings.view', Permission::class);
+        $attr = [];
+        switch ($module) {
+            case 'users':
+                $attr['users'] = User::with('roles')->get();
+                $attr['roles'] = Role::all();
+                break;
+            default:
+                $attr['roles'] = Role::with('permissions')->get();
+                $attr['permissions'] = Permission::all();
+                break;
+        }
+        $attr['settings'] = Setting::where('module', $module)->get()->keyBy('setting_key');
+        return view("settings.permissions.$module")->with($attr);
+    }
 
-	public function updateUserRoles(UpdateUserRolesRequest $request) {
-		$this->authorize('settings.update', Permission::class);
-		$validatedData = $request->validated();
-		if (!isset($validatedData['roles'])) {
-			return response()->json([
-				'isUpdated' => false,
-			]);
-		}
-		$user = User::find($validatedData['userID']);
-		$roles = array_pluck($validatedData['roles'], 'id');
-		$isUpdated = $user->syncRoles($roles);
-		return response()->json(['isUpdated' => $isUpdated]);
-	}
+    public function updateUserRoles(UpdateUserRolesRequest $request)
+    {
+        $this->authorize('settings.update', Permission::class);
+        $validatedData = $request->validated();
+        if (!isset($validatedData['roles'])) {
+            return response()->json([
+                'isUpdated' => false,
+            ]);
+        }
+        $user = User::find($validatedData['userID']);
+        $roles = array_pluck($validatedData['roles'], 'id');
+        $isUpdated = $user->syncRoles($roles);
+        return response()->json(['isUpdated' => $isUpdated]);
+    }
 
-	public function updateRolePermissions(UpdateRolePermissionsRequest $request) {
-		$this->authorize('settings.update', Permission::class);
-		$validatedData = $request->validated();
-		if (!isset($validatedData['permissions'])) {
-			return response()->json([
-				'isUpdated' => false,
-			]);
-		}
-		$role = Role::find($validatedData['roleID']);
-		$permissions = array_pluck($validatedData['permissions'], 'id');
-		$isUpdated = $role->syncPermissions($permissions);
-		return response()->json(['isUpdated' => $isUpdated]);
-	}
+    public function updateRolePermissions(UpdateRolePermissionsRequest $request)
+    {
+        $this->authorize('settings.update', Permission::class);
+        $validatedData = $request->validated();
+        if (!isset($validatedData['permissions'])) {
+            return response()->json([
+                'isUpdated' => false,
+            ]);
+        }
+        $role = Role::find($validatedData['roleID']);
+        $permissions = array_pluck($validatedData['permissions'], 'id');
+        $isUpdated = $role->syncPermissions($permissions);
+        return response()->json(['isUpdated' => $isUpdated]);
+    }
 }
--- app/Http/Controllers/Settings/SettingController.php
+++ app/Http/Controllers/Settings/SettingController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Settings;
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
 
-class SettingController extends Controller {
-	public function index() {
-		$this->authorize('view', Setting::class);
-		return view('settings.index');
-	}
+class SettingController extends Controller
+{
+    public function index()
+    {
+        $this->authorize('view', Setting::class);
+        return view('settings.index');
+    }
 }
