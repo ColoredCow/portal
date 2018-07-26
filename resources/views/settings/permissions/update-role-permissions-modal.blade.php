59f7d826435cba803abd6d08f0e9b4f177133e4e
Abhishek <abhip099@gmail.com>
2018-07-26 13:12:04 +0530

    formatting

--- resources/views/settings/permissions/update-role-permissions-modal.blade.php
+++ resources/views/settings/permissions/update-role-permissions-modal.blade.php
@@ -9,18 +9,18 @@
 			</div>
 			<div class="modal-body">
 				<ul class="list-group" >
-	                <li v-for="(permission, index) in permissions" class="list-group-item">
-	                    <div class="form-check">
-	                        <label class="form-check-label">
-	                        <input type="checkbox" :data-permission="permission.name"  class="form-check-input book_category_input" :value="permission.id"> @{{ permission.name }}
-	                        </label>
-	                    </div>
-	                </li>
-                </ul>
-            </div>
+					<li v-for="(permission, index) in permissions" class="list-group-item">
+						<div class="form-check">
+							<label class="form-check-label">
+								<input type="checkbox" :data-permission="permission.name"  class="form-check-input book_category_input" :value="permission.id"> @{{ permission.name }}
+							</label>
+						</div>
+					</li>
+				</ul>
+			</div>
 			<div class="modal-footer">
-                <button id="close_update_permission_modal" type="type" class="btn btn-light" data-dismiss="modal" aria-label="Close">Cancel</button>
-                <button @click="updatePermissions" type="button" type="button" class="btn btn-primary">Save</button>
+				<button id="close_update_permission_modal" type="type" class="btn btn-light" data-dismiss="modal" aria-label="Close">Cancel</button>
+				<button @click="updatePermissions" type="button" type="button" class="btn btn-primary">Save</button>
 			</div>
 		</div>
 	</div>
