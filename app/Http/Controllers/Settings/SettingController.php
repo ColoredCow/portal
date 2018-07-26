a47eedb9eb1cee14db0ad037533eb967d2b9ac63
Abhishek <abhip099@gmail.com>
2018-07-26 10:47:05 +0530

    styleci fixes

--- app/Http/Controllers/Settings/SettingController.php
+++ app/Http/Controllers/Settings/SettingController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers\Settings;
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
 
-class SettingController extends Controller
-{
-    public function index()
-    {
-        $this->authorize('view', Setting::class);
-        return view('settings.index');
-    }
+class SettingController extends Controller {
+	public function index() {
+		$this->authorize('view', Setting::class);
+		return view('settings.index');
+	}
 }
