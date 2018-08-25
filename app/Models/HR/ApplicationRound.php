fd6b7d098a76d55ec34402af9ea2fafc9d5e8b4a
vikas rinvi <vikasmrnv@gmail.com>
2018-08-25 16:31:30 +0530

    mail for approve

--- app/Models/HR/ApplicationRound.php
+++ app/Models/HR/ApplicationRound.php
@@ -76,6 +76,12 @@ class ApplicationRound extends Model
             case 'approve':
                 $fillable['round_status'] = 'approved';
                 $application->approve();
+                $mail_Data =[
+                    'subject' => 'subject';
+                    'body' => 'body';
+                ];
+
+                Mail::send(new SendOfferLetter($application, $mail_Data));
                 break;
 
             case 'onboard':
