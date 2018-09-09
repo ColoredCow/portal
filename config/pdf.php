d914a90f6ba11f0475f42a5139a7a345b2477d1c
Mohit Gusain <mohitdreamer05@gmail.com>
2018-09-09 15:07:12 +0530

    style ci fix

--- config/pdf.php
+++ config/pdf.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-	'mode'                  => 'utf-8',
-	'format'                => 'A4',
-	'author'                => '',
-	'subject'               => '',
-	'keywords'              => '',
-	'creator'               => 'Laravel Pdf',
-	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('storage/app/temp/')
+   'mode'                  => 'utf-8',
+   'format'                => 'A4',
+   'author'                => '',
+   'subject'               => '',
+   'keywords'              => '',
+   'creator'               => 'Laravel Pdf',
+   'display_mode'          => 'fullpage',
+   'tempDir'               => base_path('storage/app/temp/')
 ];
