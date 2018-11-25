3df153f6b84dfc2b5bd96bbbee774d8728f2fcd0
Shubham Joshi <shubhamjoshi2301@gmail.com>
2018-11-26 01:03:31 +0530

    indentation formatting

--- resources/assets/js/components/ProjectDetailsComponent.vue
+++ resources/assets/js/components/ProjectDetailsComponent.vue
@@ -31,79 +31,79 @@
                                 <ul class="unstyled-list p-0">
                                     <li v-for="employee in project.employees">0</li>
                                 </ul>
-                          </div>
+                            </div>
                         </div>
-                  </div>
+                    </div>
                 </div>
             </div>
             <hr>
-          <div class="project-actions row">
-            <div class="col-12 project-action text-center">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th>
-                      Employee
-                      <span @click="showAddEmployeeForm">
-                        <button type="button" class="p-0 btn btn-default">
-                          <i class="fa fa-plus"></i>
-                        </button>
-                      </span>
-                    </th>
-                    <th>Contribution</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody v-if="project">
-                  <tr v-for="employee in project.employees">
-                    <td>
-                      {{employee.name}}
-                    </td>
-                    <td>{{employee.pivot.contribution_type}}</td>
-                    <td><i class="btn btn-default fa fa-close" @click="removeEmployee(employee.id)"></i></td>
-                  </tr>
-                </tbody>
-              </table>
+            <div class="project-actions row">
+                <div class="col-12 project-action text-center">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>
+                                  Employee
+                                    <span @click="showAddEmployeeForm">
+                                        <button type="button" class="p-0 btn btn-default">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </span>
+                                </th>
+                                <th>Contribution</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody v-if="project">
+                            <tr v-for="employee in project.employees">
+                                <td>
+                                  {{employee.name}}
+                                </td>
+                                <td>{{employee.pivot.contribution_type}}</td>
+                                <td><i class="btn btn-default fa fa-close" @click="removeEmployee(employee.id)"></i></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
         </div>
 
         <div class="add-employee-form" v-if="showAddEmployeeForm">
-          <div class="modal fade slide-up" data-backdrop="static" data-keyboard="false" id="addEmployeeForm" tabindex="-1" role="dialog" aria-hidden="false">
-            <div class="modal-dialog">
-              <div class="modal-content-wrapper">
-                <div class="modal-content form-modal">
-                  <div class="modal-header card-header">
-                    <h3 class="modal-title">Add Employee to this Project</h3>
-                    <button type="button" class="close" @click="destroyFormModal" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body card-body">
-                    <form>
-                      <label for="employee">Select Employee</label>
-                      <select required class="form-control" name="employee" v-model="selectedEmployee">
-                        <option disabled value="">Please select one</option>
-                        <option v-for="employee in employees" :value="employee.id">{{employee.name}}</option>
-                      </select>
+            <div class="modal fade slide-up" data-backdrop="static" data-keyboard="false" id="addEmployeeForm" tabindex="-1" role="dialog" aria-hidden="false">
+                <div class="modal-dialog">
+                    <div class="modal-content-wrapper">
+                        <div class="modal-content form-modal">
+                            <div class="modal-header card-header">
+                                <h3 class="modal-title">Add Employee to this Project</h3>
+                                <button type="button" class="close" @click="destroyFormModal" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body card-body">
+                                <form>
+                                    <label for="employee">Select Employee</label>
+                                    <select required class="form-control" name="employee" v-model="selectedEmployee">
+                                        <option disabled value="">Please select one</option>
+                                        <option v-for="employee in employees" :value="employee.id">{{employee.name}}</option>
+                                    </select>
 
-                      <label for="contribution">Contribution</label>
-                      <select required class="form-control" name="contribution" v-model="contributionType">
-                        <option disabled value="">Please select one</option>
-                        <option value="weekly">Weekly</option>
-                        <option value="hourly">Hourly</option>
-                      </select>
-                    </form>
-                  </div>
+                                    <label for="contribution">Contribution</label>
+                                    <select required class="form-control" name="contribution" v-model="contributionType">
+                                        <option disabled value="">Please select one</option>
+                                        <option value="weekly">Weekly</option>
+                                        <option value="hourly">Hourly</option>
+                                      </select>
+                                </form>
+                            </div>
 
-                  <div class="card-footer">
-                    <button type="button" class="btn btn-primary" @click="addEmployeeToProject">Add</button>
-                  </div>
+                            <div class="card-footer">
+                                <button type="button" class="btn btn-primary" @click="addEmployeeToProject">Add</button>
+                            </div>
 
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
     </div>
 </template>
@@ -114,7 +114,7 @@
     props: ['project', 'clients', 'employees'],
 
     data() {
-      return {
+        return {
         loadAddEmployeeForm: false,
         selectedEmployee: null,
         contributionType: null,
