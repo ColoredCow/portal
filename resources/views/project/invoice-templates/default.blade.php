8de50520014f68ddaba0427273fd9063570e0517
pankaj-ag <pankaj@coloredcow.in>
2019-11-17 18:00:36 +0530

    updates

--- resources/views/project/invoice-templates/default.blade.php
+++ resources/views/project/invoice-templates/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,14 +9,15 @@
     <!-- CSRF Token -->
 
     <!-- Styles -->
-    @if($isPdf) 
-        <link href="{{ public_path('css/app.css') }}" rel="stylesheet">
+    @if($isPdf)
+    <link href="{{ public_path('css/app.css') }}" rel="stylesheet">
     @else
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @endif
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <div class="py-4" style="background-color: #fff">
@@ -26,18 +28,19 @@
             <div class="row mb-4">
                 <div class="col-6">
                     <div class="w-75">
-                            @if($isPdf) 
-                            <img class="image-responsive w-100" src="{{ public_path('images/coloredcow-logo.png') }}" alt="">
-                            @else
-                            <img class="image-responsive w-100" src="{{ 'images/coloredcow-logo.png'}}" alt="">
-                            @endif
+                        @if($isPdf)
+                        <img class="image-responsive w-100" src="{{ public_path('images/coloredcow-logo.png') }}"
+                            alt="">
+                        @else
+                        <img class="image-responsive w-100" src="{{ 'images/coloredcow-logo.png'}}" alt="">
+                        @endif
                     </div>
-                   
+
                 </div>
                 <div class="col-6 text-right">
                     <p class="text-muted">
-                        <span> F-61, Suncity, Sector - 54 </span><br>  
-                        <span> Gurgaon, Haryana, 122003, India </span> <br>  
+                        <span> F-61, Suncity, Sector - 54 </span><br>
+                        <span> Gurgaon, Haryana, 122003, India </span> <br>
                         <span> finance@coloredcow.com </span> <br>
                         <span> 91 9818571035 </span> <br>
                     </p>
@@ -83,49 +86,122 @@
             <div class="row mb-4">
                 <div class="col-6">
                     <span class="font-weight-bold" style="font-size:15px;">illuma(001) Total Amount Due</span><br>
-                    <span class="font-weight-bold" style="font-size:15px;">Account ID : illuma Care Connections(012)</span><br>
-                    <span class="font-weight-bold" style="font-size:15px;">Category : Web app development</span>        
+                    <span class="font-weight-bold" style="font-size:15px;">Account ID : illuma Care
+                        Connections(012)</span><br>
+                    <span class="font-weight-bold" style="font-size:15px;">Category : Web app development</span>
                 </div>
                 <div class="col-6 text-right">
                     <div class="row">
                         <div class="col-6">Total Amount Due</div>
                         <div class="col-6">
-                            <span class="font-weight-bold" style="font-size:15px;">$440.00</span>
+                            <span class="font-weight-bold" style="font-size:20px;">$440.00</span>
                         </div>
                     </div>
                 </div>
             </div>
 
+            <div class="mb-4">
+                <div class="row ">
+                    <div class="col-4 pr-0">
+                        <div style="background-color: #000; margin-right: 1px;">
+                            <span class="text-white font-weight-bold pl-1">Description</span>
+                        </div>
+                    </div>
+                    <div class="col-4 px-0 text-center">
+                        <div style="background-color: #000">
+                            <span class="text-white font-weight-bold pl-1">Hours</span>
+                        </div>
+                    </div>
+                    <div class="col-2 px-0">
+                        <div style="background-color: #000; margin-right: 1px;">
+                            <span class="text-white font-weight-bold pl-1">Rate <span style="font-size:12px;">(US $)</span></span>
+                        </div>
+                    </div>
+                    <div class="col-2 px-0">
+                        <div style="background-color: #000">
+                            <span class="text-white font-weight-bold pl-1">Cost <span style="font-size:12px;">(US $)</span></span>
+                        </div>
+                    </div>
+                </div>
 
-            <div class=row>
-                <div class="col-4">Description</div>
-                <div class="col-4">Hours</div>
-                <div class="col-2">Rate (US $)</div>
-                <div class="col-2">Cost (US $)</div>
+                <div class=row>
+                    <div class="col-4">All Member Export Updates</div>
+                    <div class="col-4 text-center">3.25</div>
+                    <div class="col-2">35</div>
+                    <div class="col-2">113.75</div>
+                </div>
+                <hr class="my-1">
+
+                <div class=row>
+                    <div class="col-4">Static Code Analysis</div>
+                    <div class="col-4 text-center">8</div>
+                    <div class="col-2">35</div>
+                    <div class="col-2">280</div>
+                </div>
+                <hr class="my-1">
+
+                <div class=row>
+                    <div class="col-4">Moving Proivders</div>
+                    <div class="col-4 text-center">1</div>
+                    <div class="col-2">35</div>
+                    <div class="col-2">35</div>
+                </div>
+                <hr class="my-1">
+
+                <div class=row>
+                    <div class="col-4">DB Cleanup</div>
+                    <div class="col-4 text-center">0.5</div>
+                    <div class="col-2">35</div>
+                    <div class="col-2">17.5</div>
+                </div>
+                <hr class="my-1">
+
+                <div class=row>
+                    <div class="col-4">Network Activiation</div>
+                    <div class="col-4 text-center">0.25</div>
+                    <div class="col-2">35</div>
+                    <div class="col-2">8.75</div>
+                </div>
+                <hr class="my-1">
+
+                <div class=row>
+                        <div class="col-4">Bank Transaction Charges</div>
+                        <div class="col-4 text-center"></div>
+                        <div class="col-2"></div>
+                        <div class="col-2">50</div>
+                </div>
+                <hr class="my-1">
             </div>
 
-            <div class=row>
-                <div class="col-4">All Member Export Updates</div>
-                <div class="col-4">3.25</div>
-                <div class="col-2">35</div>
-                <div class="col-2">113.75</div>
-            </div>
 
-            <div class=row>
-                <div class="col-4">Static Code Analysis</div>
-                <div class="col-4">8</div>
-                <div class="col-2">35</div>
-                <div class="col-2">280</div>
-            </div>
+            <div class="row mb-4">
+                <div class="offset-4 col-8">
+                    <div class="row">
+                        <div class="col-10"> Total</div>
+                        <div class="col-1">$505.00</div>
+                    </div>
+                    <hr class="my-1">
 
+                    <div class="row">
+                        <div class="col-10"> Discount ( $5 Per hours )</div>
+                        <div class="col-1">$65.00</div>
+                    </div>
+                    <hr class="my-1 border-dark">
 
-            <div class=row>
-                <div class="offset-4 col-7">
-                    Total <br>
-                    Discount ( $5 Per hours )<br>
-                    Total After Discount<br>
-                    Amount Paid<br>
-                    Amount Due in US $
+                    <div class="row">
+                        <div class="col-10">Total After Discount</div>
+                        <div class="col-1">$440.00</div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-10">Amount Paid</div>
+                        <div class="col-1">$0.00</div>
+                    </div>
+                    <hr class="my-1 border-dark">
+                    <div class="row">
+                        <div class="col-10"> <span class="font-weight-bold text-dark">Amount Due in US $</span> </div>
+                        <div class="col-1">$440.00</div>
+                    </div>
                 </div>
             </div>
 
@@ -146,10 +222,10 @@
             </div>
 
             <div>
-                    Sincerely,<br>
-                    Prateek Narang <br>
-                    prateek.narang@coloredcow.com <br>
-                    ColoredCow <br>
+                Sincerely,<br>
+                Prateek Narang <br>
+                prateek.narang@coloredcow.com <br>
+                ColoredCow <br>
             </div>
 
         </div>
@@ -157,12 +233,13 @@
 
     <!-- Scripts -->
 
-    @if($isPdf) 
-        <script src="{{ public_path('js/app.js') }}"></script>
+    @if($isPdf)
+    <script src="{{ public_path('js/app.js') }}"></script>
     @else
-        <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     @endif
 
-   
+
 </body>
+
 </html>
