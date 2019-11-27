6b3d4d5c566c4c5b9c28fbce137d56435a1d3f4a
abhishek-pokhriyal <pokhi@coloredcow.com>
2019-11-27 15:01:35 +0530

    Generate Invoice button on project.show

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -8,6 +8,9 @@
     <br><br>
     <div class="row">
         <div class="col-md-6"><h1>View Project</h1></div>
+        <div class="col-md-6">
+            <a href="{{ route('project.generate-invoice', $project) }}" class="btn btn-info float-right">Generate Invoice</a>
+        </div>
     </div>
     @include('status', ['errors' => $errors->all()])
     <div class="card">
