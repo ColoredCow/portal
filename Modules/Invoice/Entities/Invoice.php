756f25f694e34cd17157f9440760de1c7195ff56
pankaj-ag <pankaj@coloredcow.in>
2020-05-24 17:32:08 +0530

    updates

--- Modules/Invoice/Entities/Invoice.php
+++ Modules/Invoice/Entities/Invoice.php
@@ -2,14 +2,21 @@
 
 namespace Modules\Invoice\Entities;
 
+use App\Traits\Encryptable;
 use Modules\Client\Entities\Client;
 use Modules\Project\Entities\Project;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
+    use Encryptable;
+
     protected $fillable = ['client_id', 'project_id', 'status', 'currency', 'amount', 'sent_on', 'due_on', 'gst', 'file_path', 'comments'];
     protected $dates = ['sent_on', 'due_on'];
+
+    protected $encryptable = [
+        'amount', 'gst',
+    ];
 
     public function scopeStatus($query, $status)
     {
---
+++ app/Traits/Encryptable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use Crypt;
+
+trait Encryptable
+{
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+
+        if (in_array($key, $this->encryptable)) {
+            try {
+                $value = $this->decryptValue($value);
+            } catch (\Throwable $th) {
+            }
+
+            return $value;
+        }
+        return $value;
+    }
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = $this->encryptValue($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+
+    private function encryptValue($value)
+    {
+        return Crypt::encrypt($value);
+    }
+
+    private function decryptValue($value)
+    {
+        return Crypt::decrypt($value);
+    }
+}
