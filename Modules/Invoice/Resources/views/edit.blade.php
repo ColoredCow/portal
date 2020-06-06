a365c61adae59a1f35108511fcd8420505d9522d
pankaj-ag <pankaj@coloredcow.in>
2020-06-06 19:42:48 +0530

    Update invoice

--- Modules/Invoice/Resources/views/edit.blade.php
+++ Modules/Invoice/Resources/views/edit.blade.php
@@ -6,7 +6,7 @@
     
     <div class="d-flex justify-content-between mb-2"> 
         <h4>Update Invoice Info</h4>
-        <a class="text-underline" target="_blank" href="{{ route('invoice.get-file', $invoice->id) }}">Download invoice PDF here</a>
+        <a class="text-underline" target="_blank" href="{{ route('invoice.get-file', $invoice->id) }}">View invoice PDF here</a>
     </div>
     <form method="POST" action="{{ route('invoice.update', $invoice) }}" enctype="multipart/form-data">
         @csrf
