e014b8dddfc144b81b2b7b3c4476746c7c1f60d0
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-07-20 11:58:30 +0530

    Communication submodule

--- Modules/Communication/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('communication::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('communication.name') !!}
-    </p>
-@endsection
