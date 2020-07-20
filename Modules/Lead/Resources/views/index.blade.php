7cc5d670d1d115f691a1acf85031bd008311231b
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-07-20 12:29:04 +0530

    Lead submodule

--- Modules/Lead/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('lead::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('lead.name') !!}
-    </p>
-@endsection
