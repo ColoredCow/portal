57d1a953a4df719bfed2ccac46dcd62e0d335261
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-07-20 12:31:47 +0530

    Task submodule

--- Modules/Task/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('task::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('task.name') !!}
-    </p>
-@endsection
