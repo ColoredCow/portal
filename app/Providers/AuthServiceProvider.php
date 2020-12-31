018f7fc82fd1cf23be264cc740c4191174ca94eb
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-12-31 19:49:59 +0530

    policy moved to HR

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Modules\HR\Entities\Applicant;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -18,7 +17,6 @@ class AuthServiceProvider extends ServiceProvider
         'App\Models\Finance\Invoice' => 'App\Policies\Finance\InvoicePolicy',
         'App\Models\Project' => 'App\Policies\ProjectPolicy',
         'App\Models\KnowledgeCafe\WeeklyDose' => 'App\Policies\KnowledgeCafe\WeeklyDosePolicy',
-        Applicant::class => 'App\Policies\HR\ApplicantPolicy',
         'App\Models\KnowledgeCafe\Library\Book' => 'App\Policies\KnowledgeCafe\Library\BookPolicy',
         'App\Models\KnowledgeCafe\Library\BookCategory' => 'App\Policies\KnowledgeCafe\Library\BookCategoryPolicy',
         'App\Models\Setting' => 'App\Policies\SettingPolicy',
