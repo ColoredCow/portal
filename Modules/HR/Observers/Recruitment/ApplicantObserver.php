01ba836fd02db6228277634167d5bf59cfa3e8c7
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-07-28 00:28:41 +0530

    Created ApplicantObserver for university mapping

---
+++ Modules/HR/Observers/Recruitment/ApplicantObserver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\HR\Observers\Recruitment;
+
+use Modules\HR\Entities\Applicant;
+use Modules\HR\Entities\University;
+use Modules\HR\Entities\UniversityAlias;
+
+class ApplicantObserver
+{
+    /**
+     * Listen to the Applicant create event.
+     *
+     * @param  \Modules\HR\Entities\Applicant  $Applicant
+     * @return void
+     */
+    public function created(Applicant $applicant)
+    {   $data = request()->all();
+        $university = University::select('id')->where('name', $data['college'])->first();
+        if($university==null)
+        {
+            $universityAlias = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
+            if($universityAlias==null)
+            {
+                University::create(['name'=>$data['college']]);
+                $university = University::select('id')->where('name', $data['college'])->first();
+                $applicant->update(['hr_university_id' => $university->id]);
+            }
+            else
+            {
+                $university = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
+                $applicant->update(['hr_university_id' => $university->hr_university_id]);
+            }
+        }
+        else
+        {
+            $applicant->update(['hr_university_id' => $university->id]);
+        }
+    }
+}
