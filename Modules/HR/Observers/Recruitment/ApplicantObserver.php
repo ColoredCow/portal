35e10471beb16b92393edfb1ff62011946a17f81
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-07-28 10:20:07 +0530

    Added code college field is filled or not

--- Modules/HR/Observers/Recruitment/ApplicantObserver.php
+++ Modules/HR/Observers/Recruitment/ApplicantObserver.php
@@ -16,25 +16,28 @@ class ApplicantObserver
      */
     public function created(Applicant $applicant)
     {   $data = request()->all();
-        $university = University::select('id')->where('name', $data['college'])->first();
-        if($university==null)
+        if(isset($data['college']))
         {
-            $universityAlias = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
-            if($universityAlias==null)
+            $university = University::select('id')->where('name', $data['college'])->first();
+            if($university==null)
             {
-                University::create(['name'=>$data['college']]);
-                $university = University::select('id')->where('name', $data['college'])->first();
-                $applicant->update(['hr_university_id' => $university->id]);
+                $universityAlias = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
+                if($universityAlias==null)
+                {
+                    University::create(['name'=>$data['college']]);
+                    $university = University::select('id')->where('name', $data['college'])->first();
+                    $applicant->update(['hr_university_id' => $university->id]);
+                }
+                else
+                {
+                    $university = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
+                    $applicant->update(['hr_university_id' => $university->hr_university_id]);
+                }
             }
             else
             {
-                $university = UniversityAlias::select('hr_university_id')->where('name', $data['college'])->first();
-                $applicant->update(['hr_university_id' => $university->hr_university_id]);
+                $applicant->update(['hr_university_id' => $university->id]);
             }
-        }
-        else
-        {
-            $applicant->update(['hr_university_id' => $university->id]);
         }
     }
 }
