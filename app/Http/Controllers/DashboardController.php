6800250d4482c45953dc29f6facc7711862cdf74
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-09 17:59:55 +0530

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,48 +8,49 @@ use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-  public function index()
-  {
-    $today_count = Applicant::whereDate('created_at' , '=' , now())
+    public function index()
+    {
+        $today_count = Applicant::whereDate('created_at' , '=' , now())
     ->count();  
 
-    $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"),\DB::raw("DATE(created_at) as date"))
+        $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"),\DB::raw("DATE(created_at) as date"))
     ->where('created_at', '>', Carbon::today()->subDay(7))
     ->groupBY('date')
     ->orderBy('date', 'ASC')
     ->get();
        
-    $data = [];
+        $data = [];
     
-    foreach($record as $row) {
-       $data['data'][] = (int) $row->count;
-       $data['label'][] = $row->date;
-    }
+        foreach($record as $row) {
+           $data['data'][] = (int) $row->count;
+           $data['label'][] = $row->date;
+        }
     
-    $data['chart_data'] = json_encode($data);
-    return view('graph', $data,compact('today_count'));      
+        $data['chart_data'] = json_encode($data);
+        return view('graph', $data,compact('today_count'));      
   }
   
-  function searchBydate(Request $req){
-    $today_count = Applicant::whereDate('created_at', '=',now())
+    public function searchBydate(Request $req)
+    {
+        $today_count = Applicant::whereDate('created_at', '=',now())
     ->count();
 
-    $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"),\DB::raw("DATE(created_at) as date"))
+        $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"),\DB::raw("DATE(created_at) as date"))
     ->where('created_at', '>=',$req->from )
     ->where('created_at', '<=',$req->to )
     ->groupBy('date')
     ->orderBy('date','ASC')
     ->get();
     
-    $data = [];
+        $data = [];
     
-    foreach($record as $row) {
-        $data['label'][] = $row->date;
-        $data['data'][] = (int) $row->count;
-    }
+        foreach($record as $row) {
+            $data['label'][] = $row->date;
+            $data['data'][] = (int) $row->count;
+        }
     
-    $data['chart_data'] = json_encode($data);
-    return view('graph', $data,compact('today_count'));
+        $data['chart_data'] = json_encode($data);
+        return view('graph', $data,compact('today_count'));
   }
            
 }
