6cda6ae89a8d633fd5f86ead7442effe2fcada23
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-09 18:49:23 +0530

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,13 +10,13 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $todayCount = Applicant::whereDate('created_at', '=' , now())
+        $todayCount = Applicant::whereDate('created_at', '=', now())
     ->count();  
 
         $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"), \DB::raw("DATE(created_at) as date"))
     ->where('created_at', '>', Carbon::today()->subDay(7))
     ->groupBY('date')
-    ->orderBy('date',  'ASC')
+    ->orderBy('date', 'ASC')
     ->get();
        
         $data = [];
@@ -27,30 +27,30 @@ class DashboardController extends Controller
         }
     
         $data['chart_data'] = json_encode($data);
-        return view('graph', $data,compact('todayCount'));      
+        return view('graph', $data, compact('todayCount'));      
     }
   
     public function searchBydate(Request $req)
     {
-        $todayCount = Applicant::whereDate('created_at', '=',now())
+        $todayCount = Applicant::whereDate('created_at', '=', now())
     ->count();
 
         $record = Applicant::select(\DB::raw("COUNT(*) as count"), \DB::raw("MONTHNAME(created_at) as month"), \DB::raw("DATE(created_at) as date"))
-    ->where('created_at', '>=',$req->from )
-    ->where('created_at', '<=',$req->to )
+    ->where('created_at', '>=', $req->from )
+    ->where('created_at', '<=', $req->to )
     ->groupBy('date')
     ->orderBy('date', 'ASC')
     ->get();
     
         $data = [];
     
-        foreach($record as $row) {
+        foreach ($record as $row) {
             $data['label'][] = $row->date;
             $data['data'][] = (int) $row->count;
         }
     
         $data['chart_data'] = json_encode($data);
-        return view('graph', $data,compact('todayCount'));
-   }
+        return view('graph', $data, compact('todayCount'));
+    }
            
 }
