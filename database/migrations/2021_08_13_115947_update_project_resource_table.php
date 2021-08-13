6ea4b7ef20f53d2e55542287b543f83db6481bc8
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-08-13 22:14:19 +0530

    migration for project module schema updation

---
+++ database/migrations/2021_08_13_115947_update_project_resource_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateProjectResourceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename( 'project_resource', 'project_resources' );
+        Schema::table('project_resources', function (Blueprint $table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('resource_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_resources', function (Blueprint $table) {
+            $table->dropForeign('project_resources_resource_id_foreign');
+            $table->dropIndex('project_resources_resource_id_foreign');
+            $table->dropForeign('project_resources_project_id_foreign');
+            $table->dropIndex('project_resources_project_id_foreign');
+        });
+        Schema::rename( 'project_resources', 'project_resource' );
+    }
+}
