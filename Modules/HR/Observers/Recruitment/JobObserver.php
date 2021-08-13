0bfe49323f3fb7a66e10bdd2af98f4950582746e
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-14 00:14:36 +0530

    Added created,updated,delete function so that job reflects on website

--- Modules/HR/Observers/Recruitment/JobObserver.php
+++ Modules/HR/Observers/Recruitment/JobObserver.php
@@ -4,6 +4,8 @@ namespace Modules\HR\Observers\Recruitment;
 
 use Modules\HR\Entities\Job;
 use Modules\HR\Entities\Round;
+use Modules\HR\Http\Controllers\Recruitment\JobController;
+use Corcel\Model\Post as Corcel;
 
 class JobObserver
 {
@@ -16,5 +18,49 @@ class JobObserver
     public function created(Job $job)
     {
         $job->rounds()->attach(Round::all()->pluck('id')->toArray());
+        $data = request()->all();
+
+        if ($data['status'] == "published") {
+            $Corcel=new Corcel();
+            $Corcel->post_title=$data['title'];
+            $Corcel->post_content=$data['description'];
+            $Corcel->post_type="career";
+            $Corcel->save();
+            $Corcel->saveMeta('hr_id', $job['id']);
+        }
+    }
+
+    /**
+     * Listen to the Job update event.
+     *
+     * @param  \Modules\HR\Entities\Job  $job
+     * @return void
+     */
+    public function updated(Job $job)
+    {
+        $data = request()->all();
+
+        if ($data['status'] == "published") {
+            $Corcel = new Corcel();
+            $Corcel->post_title=$data['title'];
+            $Corcel->post_content=$data['description'];
+            $Corcel->post_type="career";
+            $Corcel->save();
+            $Corcel->saveMeta('hr_id', $job['id']);
+            $post = Corcel::hasMeta('hr_id', $job['id'])->first();
+            $corcel = Corcel::find($post->ID);
+            $corcel->delete();
+        }
+    }
+
+    /**
+     * Listen to the Job delete event.
+     *
+     * @param  \Modules\HR\Entities\Job  $job
+     * @return void
+     */
+    public function deleted(Job $job)
+    {
+        Corcel::where(['post_type' => 'career', 'post_title' => $job['title']])->delete();
     }
 }
