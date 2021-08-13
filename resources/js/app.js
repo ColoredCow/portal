b5039feba02fc5e020818a7596142ca1a0d29c0d
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-13 15:21:23 +0530

    Addition of JavaScript to JS file

--- resources/js/app.js
+++ resources/js/app.js
@@ -988,6 +988,17 @@ function saveFollowUp() {
 	}
 }
 
+$(function(){
+	$("#EndDate").change(function () {
+	  var startDate = document.getElementById("StartDate").value;
+	  var endDate = document.getElementById("EndDate").value;
+	  if ((Date.parse(endDate) <= Date.parse(startDate))) {
+	  alert("End date should be greater than Start date");
+	  document.getElementById("EndDate").value = "";
+	  }
+	});
+});
+
 /*
  * HR Module JS code end
  */
