5259834ece042542a8be266a87ca6efa80ff1fa3
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-08-16 12:51:06 +0530

    made required changes

--- database/migrations/2021_08_16_063114_update_project_resource_table.php
+++ database/migrations/2021_08_16_063114_update_project_resource_table.php
@@ -13,15 +13,13 @@ class UpdateProjectResourceTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('id')->change();
-        });
         Schema::rename( 'project_resource', 'project_resources' );
         Schema::table('project_resources', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id')->change();
+            $table->unsignedInteger('resource_id')->change();
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
             $table->foreign('resource_id')->references('id')->on('users')->onDelete('cascade');
         });
-
     }
 
     /**
@@ -32,14 +30,13 @@ class UpdateProjectResourceTable extends Migration
     public function down()
     {
         Schema::table('project_resources', function (Blueprint $table) {
+            $table->bigInteger('project_id')->change();
+            $table->bigInteger('resource_id')->change();
             $table->dropForeign('project_resources_resource_id_foreign');
             $table->dropIndex('project_resources_resource_id_foreign');
             $table->dropForeign('project_resources_project_id_foreign');
             $table->dropIndex('project_resources_project_id_foreign');
         });
         Schema::rename( 'project_resources', 'project_resource' );
-        Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('id')->change();
-        });
     }
 }
