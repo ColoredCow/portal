26ea592c5a44706d058ea111a6348757dbc4d190
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-18 14:49:40 +0530

    solve larastan error

--- Modules/Client/Entities/Scopes/ClientGlobalScope.php
+++ Modules/Client/Entities/Scopes/ClientGlobalScope.php
@@ -13,6 +13,7 @@ class ClientGlobalScope implements Scope
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $builder
      * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return \Illuminate\Database\Eloquent\Builder
      */
     public function apply(Builder $builder, Model $model)
     {
