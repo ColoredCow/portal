151f841cc735dc83aad8eff9d2f941f4b1ee55fb
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-18 07:17:40 +0530

    Indentation changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -990,12 +990,12 @@ function saveFollowUp() {
 
 $(function(){
 	$("#EndDate").change(function () {
-	  var startDate = document.getElementById("StartDate").value;
-	  var endDate = document.getElementById("EndDate").value;
-	  if ((Date.parse(endDate) <= Date.parse(startDate))) {
-	  alert("End date should be greater than Start date");
-	  document.getElementById("EndDate").value = "";
-	  }
+		var startDate = document.getElementById("StartDate").value;
+		var endDate = document.getElementById("EndDate").value;
+		if ((Date.parse(endDate) <= Date.parse(startDate))) {
+		alert("End date should be greater than Start date");
+		document.getElementById("EndDate").value = "";
+		}
 	});
 });
 
