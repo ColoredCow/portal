5a54fc62b014ed70dc1a21eddf6a5f1374995eae
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-18 07:18:40 +0530

    Removed head tags in graph file

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<head>
-  <script type="text/javascript" src="{{ URL::asset('js/.js') }}"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
-</head>
-<br>
 
 <body>
   <div class="chart-container" align="center">
@@ -31,54 +24,54 @@
       </div>
       <script>
         $(function(){
-        var cData = JSON.parse(`<?php echo $chart_data; ?>`);
-        var ctx = $("#line-chart");
+          var cData = JSON.parse(`<?php echo $chart_data; ?>`);
+          var ctx = $("#line-chart");
 
-            var data = {
-              labels: cData.label,
-              datasets: [
-                {
-                  label: "Count",
-                  data: cData.data,
-                  backgroundColor: "#67A7E2",
-                  borderColor:"#67A7E2",
-                  borderWidth: 1,
-                  pointHoverRadius: 7
-                }
-              ]
-            };
-            var options = {
-              responsive: true,
-              tooltips:{
-                displayColors:false,
-                bodyFontSize: 20,
-                bodyFontStyle: 'bold',
-                backgroundColor:"#E5E5E5",
-                bodyFontColor:"#E0DEDE",
-                cornerRadius:0,
-                borderWidth:2
-              },
-              title: {
-               display: false,
-              },
-              legend: {
-               display: false,
-              }, 
-              scales: {
-                yAxes: [{
-                ticks: {stepSize: 1, suggestedMin: 0.5, suggestedMax: 5.5},
-                }]
-              },  
-              elements: {
-               line: {
-                 fill: false,
-                 tension: 0
-                },
-               point:{
-                 radius:0
-                }
-              }    
-            };
+          var data = {
+            labels: cData.label,
+            datasets: [
+            {
+              label: "Count",
+              data: cData.data,
+              backgroundColor: "#67A7E2",
+              borderColor:"#67A7E2",
+              borderWidth: 1,
+              pointHoverRadius: 7
+            }
+            ]
+          };
+          var options = {
+            responsive: true,
+            tooltips:{
+            displayColors:false,
+            bodyFontSize: 20,
+            bodyFontStyle: 'bold',
+            backgroundColor:"#E5E5E5",
+            bodyFontColor:"#E0DEDE",
+            cornerRadius:0,
+            borderWidth:2
+            },
+            title: {
+            display: false,
+            },
+            legend: {
+            display: false,
+            }, 
+            scales: {
+            yAxes: [{
+            ticks: {stepSize: 1, suggestedMin: 0.5, suggestedMax: 5.5},
+            }]
+            },  
+            elements: {
+            line: {
+            fill: false,
+            tension: 0
+            },
+            point:{
+            radius:0
+            }
+            }    
+          };
 
           var charts = new Chart(ctx, {
             type: "line",
