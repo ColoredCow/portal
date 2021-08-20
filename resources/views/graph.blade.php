5b7ece1bd1ec1084a17c4c930e6b63fe4d967347
Gautam <73119570+kickb71@users.noreply.github.com>
2021-08-20 08:32:38 +0530

    Style changes have been removed

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -4,27 +4,29 @@
 
 <body>
   <div class="chart-container" align="center">
-   <div class="line-chart-container" style="width : 1000px">
+   <div class="line-chart-container">
       <h2 align="left">Analytics</h2>
       <form action="/dashboard" method="POST" align="right">
         {{csrf_field()}}
-        <input type="date" name="from" id='StartDate'> to
+        <input type="date" name="from" id='StartDate'> to 
         <input type="date" name="to" id="EndDate">
-        <input type="submit" value="View">
-      </form>
+        <input type="submit" value="View"> 
+      </form>  
       <br>
-
+      
       <div class="card">
         <div class="card-header" align='left'>
-          <span style="font-size: 35px;">Application Recived</span> &emsp;&emsp; <span style="font-size: 20px;">Today's Count:<?php echo $todayCount; ?></span>
+          <span class="chartHeading">Application Received</span> &emsp;&emsp; <span class="totalChartCount">Today's Count:<?php echo $todayCount; ?></span> 
         </div>
         <div class="card-body">
           <canvas id="line-chart"></canvas>
         </div>
       </div>
-       <a id="value" value="{{ $chart_data }}"></a>
-   </div>
+      <script type="text/javascript">
+          var value = {!! $chart_data !!};
+      </script>
+   </div> 
  </div>
 </body>
-
+       
 @endsection
