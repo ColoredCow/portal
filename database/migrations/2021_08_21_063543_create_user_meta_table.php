ba0039040dbbadc11b6120908c6ef26508e383d8
Jyoti Srivastava <srivastavajyoti510@gmail.com>
2021-08-21 18:22:26 +0530

    Created user_meta table

---
+++ database/migrations/2021_08_21_063543_create_user_meta_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_meta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('max_appointments_per_day')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_meta');
+    }
+}
