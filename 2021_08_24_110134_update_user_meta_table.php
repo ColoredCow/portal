c7c2056c04108ba81730098dbb650063f162e934
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-08-24 17:05:33 +0530

    Updated user_meta table

---
+++ 2021_08_24_110134_update_user_meta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_meta', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned(); 
+            $table->foreign('user_id')->references('id')->on('users'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_meta', function(Blueprint $table){
+            $table->dropForeign('user_meta_user_id_foreign');
+            $table->dropColumn('user_id');
+            });
+    }
+}
