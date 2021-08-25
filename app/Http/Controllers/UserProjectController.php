a942acc9d8a9aa106f63341c680444f74fe62a08
mdshamoon <mohdshamoon841@gmail.com>
2021-08-25 16:43:46 +0530

    removed unwanted code and fixed height of card

--- app/Http/Controllers/UserProjectController.php
+++ app/Http/Controllers/UserProjectController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Modules\User\Entities\User;
 use Illuminate\Support\Facades\Auth;
 
 class UserProjectController extends Controller
 {
     public function index()
     {
-        $userId = request('user_id', null);
-        $user = $userId ? User::find($userId) : Auth::user()->projects;
+        $user_projects = Auth::user()->projects;
 
-        return $user;
+        return $user_projects;
     }
 }
