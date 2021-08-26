459f295e8a78667126f7980203adcc5abcd7f4cb
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-08-26 16:44:52 +0530

    Corrected Route

--- Modules/AppointmentSlots/Routes/web.php
+++ Modules/AppointmentSlots/Routes/web.php
@@ -12,11 +12,11 @@
  */
 
 Route::prefix('appointmentslots')->group(function () {
-    Route::get('/', 'AppointmentSlotsController@index');
-    Route::get('/', 'MaximumSlotsController@index');
+    Route::get('/', 'AppointmentSlotsController@index');  
 });
 
 Route::get('appointment-slots/select/{user_id}', 'AppointmentSlotsController@showAppointments')->name('select-appointments');
+Route::get('appointment-slots/select/{user_id}','MaximumSlotsController@index');
 Route::post('appointment-slots/selected', 'AppointmentSlotsController@appointmentSelected');
 
 Route::middleware('auth')->group(function () {
