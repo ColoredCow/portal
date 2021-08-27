a8a268a5567484f70556fe2cadb33f2002b2f875
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-27 13:18:55 +0530

    spacing indent in tag controller

--- Modules/HR/Http/Controllers/TagsController.php
+++ Modules/HR/Http/Controllers/TagsController.php
@@ -3,9 +3,7 @@
 namespace Modules\HR\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Modules\HR\Http\Requests\TagRequest;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\Models\Tag;
 
 class TagsController extends Controller
@@ -16,6 +14,7 @@ class TagsController extends Controller
     public function index()
     {
         $attr['tags'] = Tag::orderBy('tname')->get();
+
         return view('hr::tags.index')->with($attr);
     }
 
@@ -26,12 +25,14 @@ class TagsController extends Controller
             'description'=>$request['description'] ?? null,
             'background_color'=>$request['color']
         ]);
+
         return redirect(route('hr.tags.index'))->with('status', 'Tag created successfully!');
     }
 
     public function edit(int $id)
     {
         $attr['tags'] = Tag::find($id);
+
         return view('hr::tags\edit')->with($attr);
     }
 
@@ -43,12 +44,15 @@ class TagsController extends Controller
             'description'=>$request['description'] ?? null,
             'background_color'=>$request['color']
         ]);
+
         return redirect(route('hr.tags.index'))->with('status', 'Tag updated successfully!');
     }
 
     public function destroy($id)
     {
         Tag::destroy($id);
+        
         return redirect(route('hr.tags.index'))->with('status', 'Tag deleted successfully!');
     }
+
 }
