68bd04e9f823066e64c082b8689c4e6f3bb660db
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-27 12:57:03 +0530

    added index blade file for tags

---
+++ Modules/HR/Resources/views/tags/index.blade.php
@@ -0,0 +1,43 @@
+@extends('hr::layouts.master')
+@section('content')
+<div class="container">
+    <br>
+    <div class="row">
+        <div class="col-md-10">
+            <h1>Tags</h1>
+        </div>
+        <div class="col-md-2">
+            <button type="button" class="btn btn-success btn-block btn-lg" data-bs-toggle="modal" data-bs-target="#createModal">
+                New Tag
+            </button>
+        </div>
+    </div>
+    <br>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered" id="tags_table">
+            <tr>
+                <th>Tag Name</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+            @foreach ($tags as $tag)
+            <tr>
+            
+                <th><div class="rounded w-13 h-13 d-inline-block mr-1" style="background-color: {{$tag->background_color}};color: {{$tag->text_color}};"></div>{{ $tag->tname }}</th>
+                <th>{{ $tag->description }}</th>
+                <th>
+                    <form action="{{route('hr.tags.delete',$tag->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <a title="Edit" class="pr-1 btn btn-link" href="{{route('hr.tags.edit',$tag->id) }}"><i class="text-success fa fa-edit fa-lg"></i></a>
+                        <button type="submit" class="pl-1 btn btn-link" onclick="return confirm('Are you sure you want to delete?')" ><i class="text-danger fa fa-trash fa-lg"></i></button>
+                    </form>
+                </th>
+          
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+@include('hr::tags\create-tag-modal')
+@endsection
