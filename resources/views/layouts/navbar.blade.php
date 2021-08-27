a6917582eb2e8751d2afb7fd87239caa34108a46
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-27 18:58:06 +0530

    Removed view i.e not reqired in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,9 +24,7 @@
                     <a class="dropdown-item" href="{{ route('universities.index') }}">Universities</a>
                 @endcan
                 <a class="dropdown-item" href="{{route('userappointmentslots.show',auth()->id())}}">Appointment Slots</a>
-                @can('hr_universities.view')
-                    <a class="dropdown-item" href="{{ route('hr.tags.index') }}">Manage Tags</a>
-                @endcan
+                <a class="dropdown-item" href="{{ route('hr.tags.index') }}">Manage Tags</a>
             </div>
         </li>
     @endif
