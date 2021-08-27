8c7d461be95a3100ad4fdcc0c6185784814ee2e7
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-27 12:54:24 +0530

    created tag controller with create,edit,update,delete fuunctions

---
+++ Modules/HR/Http/Controllers/TagsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\HR\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Modules\HR\Http\Requests\TagRequest;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Models\Tag;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {   $attr['tags'] = Tag::orderBy('tname')->get();
+        return view('hr::tags.index')->with($attr);
+    }
+
+    public function store(Request $request)
+    {
+        $tag = Tag::create([
+            'tname'=>$request['name'],
+            'description'=>$request['description'] ?? null,
+            'background_color'=>$request['color']
+        ]);
+        return redirect(route('hr.tags.index'))->with('status', 'Tag created successfully!');
+    }
+
+    public function edit(int $id)
+    {
+        $attr['tags'] = Tag::find($id);
+        return view('hr::tags\edit')->with($attr);
+    }
+
+    public function update(Request $request,int $id)
+    {
+        $tag = Tag::find($id);
+        $tag->update([
+            'tname'=>$request['name'],
+            'description'=>$request['description'] ?? null,
+            'background_color'=>$request['color']
+        ]);
+        return redirect(route('hr.tags.index'))->with('status', 'Tag updated successfully!');
+    }
+
+    public function destroy($id)
+    {
+        Tag::destroy($id);
+        return redirect(route('hr.tags.index'))->with('status', 'Tag deleted successfully!');
+    }
+}
+
