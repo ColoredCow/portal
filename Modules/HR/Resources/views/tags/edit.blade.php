8ded70c32d56239f303413239b052c2118a3a219
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-08-28 11:07:39 +0530

    Centred edit form and decrease box size

--- Modules/HR/Resources/views/tags/edit.blade.php
+++ Modules/HR/Resources/views/tags/edit.blade.php
@@ -1,24 +1,30 @@
 @extends('hr::layouts.master')
 @section('content')
-<div class="container">
+<div class="row d-flex justify-content-center text-center">
     <br>
-    <div class="col-md-10">
+    <div class="col-md-12">
         <h1> {{ __('Update Tags') }} </h1>
     </div>
     <br>
     <form action="{{route('hr.tags.update', $tags->id)}}" method="POST" id="update-form">
         @csrf
-        <div class="mb-3">
-            <label for="label" class="form-label"> {{ __('Label name') }} <strong class="text-danger">*</strong></label>
-            <input type="text" class="form-control" id="label" name="name" value="{{ $tags->tag_name }}" required>
+        <div class="col-md-12">
+            <div class="mb-3">
+                <label for="label" class="form-label"> {{ __('Label name') }} <strong class="text-danger">*</strong></label>
+                <input type="text" class="form-control" id="label" name="name" value="{{ $tags->tag_name }}" required>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="description" class="form-label"> {{ __('Description') }} </label>
-            <input type="text" class="form-control" id="description" name="description" value="{{ $tags->description }}">
+        <div class="col-md-12">
+            <div class="mb-3">
+                <label for="description" class="form-label"> {{ __('Description') }} </label>
+                <input type="text" class="form-control" id="description" name="description" value="{{ $tags->description }}">
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="color" class="form-label"> {{ __('Color') }} <strong class="text-danger">*</strong></label>
-            <input type="color" class="form-control" id="color" name="color" value="{{ $tags->background_color}}" required>
+        <div class="col-md-12">
+            <div class="mb-3">
+                <label for="color" class="form-label"> {{ __('Color') }} <strong class="text-danger">*</strong></label>
+                <input type="color" class="form-control" id="color" name="color" value="{{ $tags->background_color}}" required>
+            </div>
         </div>
         <button type="button" class="btn btn-secondary" onclick="location.href='{{ route('hr.tags.index') }}';" > {{ __('Back') }} </button>
         <button type="submit" class="btn btn-primary ml-2" form="update-form"> {{ __('Update') }} </button>
