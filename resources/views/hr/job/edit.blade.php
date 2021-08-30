46fad813566987d1918ab72a82c258df7f6da1fa
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-08-30 12:39:47 +0530

    Removed JavaScript Section from edit.blade.php

--- resources/views/hr/job/edit.blade.php
+++ resources/views/hr/job/edit.blade.php
@@ -138,22 +138,3 @@
     </div>
 </div>
 @endsection
-
-@section('js_scripts')
-<script>
-    var date = new Date();
-    var tdate = date.getDate();
-    if(tdate < 10){
-        tdate = '0' + tdate;
-    }
-    var month = date.getMonth() + 1;
-    if(month < 10){
-        month = '0' + month;
-    }
-    var year = date.getUTCFullYear();
-    var minDate = year + "-" + month + "-" + tdate;
-    document.getElementById("start_date").setAttribute('min',minDate);
-    document.getElementById("end_date").setAttribute('min',minDate);
-</script>
-@endsection
-
