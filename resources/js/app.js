04d59f8bacf4d063a61f808fd874057f0d8bf2aa
Jyoti Srivastava <srivastavajyoti510@gmail.com>
2021-08-31 10:44:46 +0530

    updated blade file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1001,21 +1001,7 @@ function datePickerChart(){
 	   }
 	 });
 }
-
-var date = new Date();
-var tdate = date.getDate();
-if (tdate < 10) {
-	tdate = "0" + tdate;
-}
-var month = date.getMonth() + 1;
-if (month < 10) {
-	month = "0" + month;
-}
-var year = date.getUTCFullYear();
-var minDate = year + "-" + month + "-" + tdate;
-document.getElementById("start_date").setAttribute("min", minDate);
-document.getElementById("end_date").setAttribute("min", minDate);
-
+ 
 function lineChart(){
 	 var cData = value;
 	 var ctx = $("#line-chart");
