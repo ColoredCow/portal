5ab5d625ab8b5ba971bbabf07efd5233c8259819
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-03 18:21:03 +0530

    Corrected Formatting

--- MaximumSlots.php
+++ MaximumSlots.php
@@ -5,14 +5,12 @@ namespace Modules\HR\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-
-class MaximumSlots extends Model{
-
-    public static function getUserData(){
-
+class MaximumSlots extends Model
+{
+    public static function getUserData()
+    {
         $max_slot = DB::table('user_meta')->select('max_appointments_per_day')->orderBy('id', 'desc')->first();
 
         return $max_slot;
-      }
-
+    }
 }
