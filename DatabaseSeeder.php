d7da50728fcdd152e6d7d986ac03c9a94a32ee7a
Jyoti Srivastava <srivastavajyoti510@gmail.com>
2021-09-03 10:24:20 +0530

    Revert "Made Changes"
    This reverts commit 10748740771ffe7aa01b78a5e4fcaa42d682c8e3.

---
+++ DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            RolesTableSeeder::class,
+            PermissionsTableSeeder::class,
+            RoleHasPermissionsTableSeeder::class,
+            CountriesTableSeeder::class,
+            BookCategoriesTableSeeder::class,
+            UsersTableSeeder::class,
+            UserMetaTableSeeder::class,
+        ]);
+    }
+}
