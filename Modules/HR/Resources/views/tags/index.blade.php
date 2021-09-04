d45045fc85fc0541fbf908bd7168bc8723b98ecb
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-09-04 12:12:48 +0530

    spacing indent

--- Modules/HR/Resources/views/tags/index.blade.php
+++ Modules/HR/Resources/views/tags/index.blade.php
@@ -22,13 +22,13 @@
             </tr>
             @foreach ($tags as $tag)
             <tr>
-                <td><div class="rounded w-13 h-13 d-inline-block mr-1" style="background-color: {{$tag->background_color}};color: {{$tag->text_color}};"></div>{{ $tag->tag_name }}</td>
+                <td><div class="rounded w-13 h-13 d-inline-block mr-1" style="background-color: {{ $tag->background_color }};color: {{ $tag->text_color }};"></div>{{ $tag->tag_name }}</td>
                 <td>{{ $tag->description }}</td>
                 <td>
                     <form action="{{ route('hr.tags.delete', $tag->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <a title="Edit" class="pr-1 btn btn-link" href="{{ route('hr.tags.edit',$tag) }}"><i class="text-success fa fa-edit fa-lg"></i></a>
+                        <a title="Edit" class="pr-1 btn btn-link" href="{{ route('hr.tags.edit', $tag) }}"><i class="text-success fa fa-edit fa-lg"></i></a>
                         <button type="submit" class="pl-1 btn btn-link" onclick="return confirm('Are you sure you want to delete?')"><i class="text-danger fa fa-trash fa-lg"></i></button>
                     </form>
                 </td>
