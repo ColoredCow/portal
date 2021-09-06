5005e8154a3fdc574d50435f7ae1594d1154e00d
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-06 11:12:42 +0530

    Created MaximumSlot.php in Hr Module

---
+++ Modules/HR/Entities/MaximumSlot.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\HR\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Maxslot extends Model
+{
+  public static function getUserData()
+  {
+    $max_slot = DB::table('user_meta')->select('max_appointments_per_day')->orderBy('id','desc')->first()->max_appointments_per_day;
+    return $max_slot;
+  }
+}
