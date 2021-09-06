96c2dc90fcaf3b58281e32bb0c84d3a1e52fb826
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-06 14:49:02 +0530

    Corrected Formatting

--- Modules/User/Http/Controllers/UserSettingsController.php
+++ Modules/User/Http/Controllers/UserSettingsController.php
@@ -13,11 +13,10 @@ class UserSettingsController extends ModuleBaseController
 {
     public function index()
     {
-
         return view('user::user-settings.index');
     }
   
-    public function storeData(Request $request)  
+    public function storeData(Request $request)
     {
         DB::table('user_meta')->insert([
             'user_id' => Auth::user()->id,
@@ -27,4 +26,3 @@ class UserSettingsController extends ModuleBaseController
         return redirect('/user/user-settings/hr')->with('status', 'Saved Successfully!');
     }
 }
-  
