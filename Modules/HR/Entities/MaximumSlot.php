fc74b8a6cb7e91a505f825b9fbc3c533d3f9ad11
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-08 13:13:09 +0530

    Corrected Formatting

--- Modules/HR/Entities/MaximumSlot.php
+++ Modules/HR/Entities/MaximumSlot.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\DB;
 class UserMeta extends Model
 {
     public function user()
-    {   
+    {
         return  $this->belongsTo (User::class);
     }
 
-     public static function getUserData()
+    public static function getUserData()
     {
         $maxslots=DB::table('user_meta')->select('max_appointments_per_day')->where('user_id', 'users.id')->orderByDesc('created_at')->first();
         
