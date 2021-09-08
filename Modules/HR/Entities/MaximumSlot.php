0a931bd822f5ba3b5e6e7e01557571cf73bf67df
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-08 13:14:55 +0530

    Corrected formatting

--- Modules/HR/Entities/MaximumSlot.php
+++ Modules/HR/Entities/MaximumSlot.php
@@ -10,7 +10,7 @@ class UserMeta extends Model
 {
     public function user()
     {
-        return  $this->belongsTo (User::class);
+        return  $this->belongsTo(User::class);
     }
 
     public static function getUserData()
