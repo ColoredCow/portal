0c32e6f65ffef74eb18bc9156035ae3f4539c812
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-08 13:58:03 +0530

    Added routes for saving values

--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -24,4 +24,5 @@ Route::prefix('user')->middleware('auth')->group(function () {
     Route::get('/profile', 'ProfileController@index')->name('user.profile');
     Route::get('/user-settings', 'UserSettingsController@index')->name('user.settings');
     Route::post('/user-settings/hr', 'UserSettingsController@storeData')->name('user.settings-hr');
+    Route::get('/user-settings/hr', 'UserSettingsController@save')->name('user.settings-hr');
 });
