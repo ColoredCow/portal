c4cbbfdc317adfdd320be6aa856088f47e07257c
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-08 13:36:15 +0530

    corrected php cs error

--- Modules/HR/Entities/MaximumSlot.php
+++ Modules/HR/Entities/MaximumSlot.php
@@ -18,7 +18,7 @@ class UserMeta extends Model
     {
         $userId = Auth::user('id');
         $maxslots = DB::table('user_meta')->select('max_appointments_per_day')->where('user_id', $userId)->orderByDesc('created_at')->first();
-        
+
         return $maxslots;
     }
 }
