e49b6e0a7ebff5833154e6a1eb495fd35b519253
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-11 00:38:18 +0530

    Corrected PHP CS Errors

--- Modules/User/Entities/UserMeta.php
+++ Modules/User/Entities/UserMeta.php
@@ -2,15 +2,12 @@
 
 namespace Modules\User\Entities;
 
-use Modules\User\Entities\User;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
 
 class UserMeta extends Model
 {
     protected $table = 'user_meta';
-    protected $fillable = ['max_interviews_per_day',];
+    protected $fillable = ['max_interviews_per_day'];
 
     public function user()
     {
