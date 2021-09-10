bdb35f5f52f9161c3df4dedd15849884f6b2e03e
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-11 00:34:21 +0530

    Corrected PHP CS error

--- Modules/User/Http/Controllers/UserSettingsController.php
+++ Modules/User/Http/Controllers/UserSettingsController.php
@@ -4,13 +4,7 @@ namespace Modules\User\Http\Controllers;
 
 use DB;
 use Auth;
-use Illuminate\Database\Eloquent\Model;
-use App\Http\Controllers\Controller;
-use Modules\User\Entities\UserMeta;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Modules\User\Entities\User;
-use Illuminate\Foundation\Http\FormRequest;
 
 class UserSettingsController extends ModuleBaseController
 {
@@ -22,8 +16,8 @@ class UserSettingsController extends ModuleBaseController
     public function update(Request $request)
     {
         DB::table('user_meta')->updateOrInsert(
-            ['user_id' => Auth::user()->id,],
-            ['max_interviews_per_day'=>$request->max_interviews_per_day,]
+            ['user_id' => Auth::user()->id],
+            ['max_interviews_per_day'=>$request->max_interviews_per_day]
         );
 
         return redirect('/user/user-settings')->with('status', 'Saved Successfully!');
