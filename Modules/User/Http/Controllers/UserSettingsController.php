dc92e3a4dcd07d18d6a3400c561386df4d1906f8
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-11 23:07:37 +0530

    Corrected formatting

--- Modules/User/Http/Controllers/UserSettingsController.php
+++ Modules/User/Http/Controllers/UserSettingsController.php
@@ -10,16 +10,16 @@ use Modules\User\Entities\UserMeta;
 class UserSettingsController extends ModuleBaseController
 {
   public function index()
-     {
+  {
         return view('user::user-settings.index');
-     }
+  }
   public function update(Request $request)
-    {
+  {
         UserMeta::updateOrCreate(
             ['user_id' => Auth::user()->id],
             ['max_interviews_per_day' => $request->max_interviews_per_day]
         );
 
         return redirect()->back()->with('status', 'Saved Successfully!');
-    }
+   }
 }
