ac1339b468999b65362cd15f311e079936b4305e
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-11 21:56:46 +0530

    By using where clause no slots are visible to the applicant

--- Modules/AppointmentSlots/Services/AppointmentSlotsService.php
+++ Modules/AppointmentSlots/Services/AppointmentSlotsService.php
@@ -214,9 +214,13 @@ class AppointmentSlotsService implements AppointmentSlotsServiceContract
         });
 
         $datesToRemove = $reservedSlotsCount->filter(function ($value, $key) use ($userId) {
-            $userMeta = UserMeta::where('user_id', $userId)->first();
+            $userMeta = User::find($userId)->meta;
 
-            $maxInterviewsPerDay = $userMeta ? $userMeta->max_interviews_per_day : config('hr.daily-appointment-slots.max-reserved-allowed', 3);
+            if ($userMeta) {
+                $maxInterviewsPerDay = $userMeta->max_interviews_per_day;
+            } else {
+                $maxInterviewsPerDay = config('hr.daily-appointment-slots.max-reserved-allowed', 3);
+            }
 
             return $value >= $maxInterviewsPerDay;
         })->keys()->all();
