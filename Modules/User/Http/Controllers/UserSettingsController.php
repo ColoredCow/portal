140dbdb69e7ff55be88e5e1be502234c4239a048
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-11 21:34:28 +0530

    Updated User Settings Controller
    Tried Updating values in UserController. I was getting this error: There is no permission named `hr_recruitment_jobs.view` for guard `web`.

---
+++ Modules/User/Http/Controllers/UserSettingsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\User\Http\Controllers;
+
+use DB;
+use Auth;
+use Illuminate\Http\Request;
+
+class UserSettingsController extends ModuleBaseController
+{
+    public function index()
+    {
+        return view('user::user-settings.index');
+    }
+
+    public function update(Request $request)
+    {
+        DB::table('user_meta')->updateOrInsert(
+            ['user_id' => Auth::user()->id],
+            ['max_interviews_per_day' => $request->max_interviews_per_day]
+        );
+
+        return redirect('/user/user-settings')->with('status', 'Saved Successfully!');
+    }
+}
