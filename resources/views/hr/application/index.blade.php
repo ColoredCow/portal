6bfdd7faaa31c6ca58adfea040cf8929ed7850dd
Jyoti Srivastava <srivastavajyoti510@gmail.com>
2021-09-14 17:01:36 +0530

    Removed Commented lines

--- resources/views/hr/application/index.blade.php
+++ resources/views/hr/application/index.blade.php
@@ -192,22 +192,3 @@
 
 @endsection
 
-{{-- @section('js_scripts') --}}
-
-{{-- <script>            
-    var btnConatiner = document.getElementById("navbar");
-    var btns = btnConatiner.getElementsByClassName("btn");
-
-    for(var i=0; i<=btns.length; i++) {
-        btns[i].addEventListener('click',function() {
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active");
-            this.className += " active";
-        })
-     
-    }   
-</script> --}}
-
-
-    
-{{-- @endsection  --}}
