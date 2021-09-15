e7b5de55ff7ec971aecc63149e84871e7da1ec27
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-15 20:19:14 +0530

    Removed unused class names and corrected formatting

--- Modules/user/Resources/views/user-settings/index.blade.php
+++ Modules/user/Resources/views/user-settings/index.blade.php
@@ -1,5 +1,7 @@
 @extends('user::layouts.master')
+
 @section('content')
+
 <form action ="{{ route('user.settings') }}" method = "POST">
 @csrf
 <div class="container">
@@ -8,23 +10,23 @@
             <label class="user-settings">User Settings</label>
         </div>
         <div class=".col-6 .offset-md-3">
-            <input type="text" class="form-control search-user-settings" placeholder="Search Settings">    
+            <input type="text" class="form-control search-user-settings" placeholder="Search Settings">
         </div>
     </div>
     <div class="col user-settings-contents">
-        <div class="card1 h-200 user-settings-menu">
+        <div class= "user-settings-menu">
             <div class="card-header">
-                <ul class="nav nav-tabs card1-header-tabs">
+                <ul class="nav nav-tabs card-header-tabs">
                     <li class="nav-item">
                         <a class="nav-link" href = "{{ route ('user.settings') }}">HR</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled"  href="#">Finance(Coming soon)</a>
+                        <a class="nav-link disabled" href="#">Finance(Coming soon)</a>
                     </li>
                 </ul>
             </div>
             <div class="card-body">
-                <h5 class="card1-title max">Maximum interviews in a day:
+                <h5 class="max">Maximum interviews in a day:
                 <input type="number" class="quantity" id="quantity" name="max_interviews_per_day" min="0" max="4" value="{{ old ('max_interviews_per_day', optional(Auth::user()->meta)->max_interviews_per_day) }}">
                 </h5>        
                 <input type="submit" class="btn btn-secondary save" value="Save">
