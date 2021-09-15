03f6fb19cbbeb23ca9d57714f52146100a3144a2
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-15 11:14:09 +0530

    Corrected Formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1012,10 +1012,10 @@ if ( document.getElementById("job_start_date") && document.getElementById("job_e
 //Job application js codes for change of icons color on clicking
 if ( document.getElementById("list-styling") ){
 	const currentLocation = location.href;
-	const menuItem  = document.querySelectorAll("#job-application-listings");
+	const menuItem = document.querySelectorAll("#job-application-listings");
 	const menuLength = menuItem.length;
-	for(let i=0; i<menuLength; i++){
-		if(menuItem[i].href === currentLocation){
+	for (let i = 0; i < menuLength; i++) {
+		if (menuItem[i].href === currentLocation) {
 			menuItem[i].className = "job-application-status";
 		}
 	}
