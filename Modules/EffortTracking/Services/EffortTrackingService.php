33c7124c25674716ba2eb9c42dcda81e3c62303d
mdshamoon <mohdshamoon841@gmail.com>
2021-09-16 17:46:22 +0530

    added the sheet id

--- Modules/EffortTracking/Services/EffortTrackingService.php
+++ Modules/EffortTracking/Services/EffortTrackingService.php
@@ -4,51 +4,59 @@ namespace Modules\EffortTracking\Services;
 
 // namespace Revolution\Google\Sheets;
 
+use Modules\Project\Entities\Project;
 use Revolution\Google\Sheets\Sheets;
 
 class EffortTrackingService
 {
     public static function sync()
     {
-        // dd("inside service sync");
 
-        // $user = auth()->user();
+        // Fetch all projects
 
-        // dd($user);
+        $projects = Project::where('status', 'active')->get();
 
-        // $token = [
-        //     'access_token'  => $user->access_token,
-        //     'refresh_token' => $user->refresh_token,
-        //     'expires_in'    => $user->expires_in,
-        //     'created'       => now()->getTimestamp(),
-        // ];
+        // loop over each project
 
-        // dd($token);
+        foreach ($projects as $project) {
+            $effortSheetURL = $project->effort_sheet_url;
 
-        $spreadSheetId = '1g4kGalUsI-78MNJo1EyPbok5-P28fbbc2vjB-fK1XOo';
-        $sheetName = 'September, 2021';
+            // $spreadSheetId = '1g4kGalUsI-78MNJo1EyPbok5-P28fbbc2vjB-fK1XOo'; // need to get from project efforstsheet url
 
-        $sheet = new Sheets();
+            $matchesId = [];
+            preg_match('/.*[^-\w]([-\w]{25,})[^-\w]?.*/', $effortSheetURL, $matchesId);
 
-        $range = 'C2:G6'; // this will depend on the number of people on the project
+            $spreadSheetId = $matchesId[1];
 
-        $sheets = $sheet->spreadsheet($spreadSheetId)
-                        ->sheet($sheetName)
-                        ->range($range)
-                        ->get();
+            $sheetName = '756414304'; // get from efforstsheet url
+            // $sheetName =
 
-        // this will return a structure like this
-        //     0 => array:5 [▼
-        //     0 => "Shamoon"    // name of the team member
-        //     1 => "September 1, 2021" // Start of the month
-        //     2 => "September 30, 2021" // End of the month
-        //     3 => "6" // Total working days
-        //     4 => "48" // Total hours put in the month
-        //   ]
+            $sheet = new Sheets();
 
-        // loop over each person in the project and add his hours in the database
-        foreach ($sheets as $row) {
-            dd($row);
+            $range = 'C2:G6'; // this will depend on the number of people on the project
+
+            $sheets = $sheet->spreadsheet($spreadSheetId)
+                            ->sheetById($sheetName)
+                            ->range($range)
+                            ->get();
+
+            // this will return a structure like this
+            //     0 => array:5 [
+            //     0 => "Shamoon"    // name of the team member
+            //     1 => "September 1, 2021" // Start of the month
+            //     2 => "September 30, 2021" // End of the month
+            //     3 => "6" // Total working days
+            //     4 => "48" // Total hours put in the month
+            //   ]
+            dd($sheets);
+
+            // loop over each person in the project and add his hours in the database
+            foreach ($sheets as $user) {
+
+                // get user in portal from the name of the user in effortsheet
+
+                // insert into project_team_member_effort
+            }
         }
 
         // $values = $sheet->setAccessToken($token)->spreadsheet($spreadSheetId)->sheet($sheetName)->all();
