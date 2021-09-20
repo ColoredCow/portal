22cc5f52ffc901da9e827f3439866671e5f5da63
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-20 11:19:09 +0530

    Removed Style tag

--- Modules/AppointmentSlots/Resources/views/select_appointments/interview_schedule_error_message.blade.php
+++ Modules/AppointmentSlots/Resources/views/select_appointments/interview_schedule_error_message.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <div>
-    <div style = "text-align: center;">
-        <svg style = "width:44; height:44; color:#d44950">{!! file_get_contents(public_path('icons/exclamation-circle.svg')) !!}</svg>
+    <div class="placing-of-content text-center">
+        <svg class="exclamation-icon h-44 w-44">{!! file_get_contents(public_path('icons/exclamation-circle.svg')) !!}</svg>
     </div>
-    <h1 class="error-heading" style ="text-align: center;">Page Not found</h1><br>
+    <h1 class="placing-of-content text-center">Page Not found</h1><br>
 
-    <p class="error-message" style ="text-align: center; font-size: 20px;">
+    <p class="error-message text-center fz-20">
         Looks like your interview is already scheduled. Please check your inbox for confirmation.<br>
 
         If you are not able to find the email, please check in your spam folder. If it's there, mark it as "Not Spam".<br>
