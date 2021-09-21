5c13364f47f20830699ff126ffe58164555614d1
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-21 11:18:20 +0530

    Removed svg tag & corrected text formatting

--- Modules/AppointmentSlots/Resources/views/select_appointments/interview_schedule_error_message.blade.php
+++ Modules/AppointmentSlots/Resources/views/select_appointments/interview_schedule_error_message.blade.php
@@ -4,16 +4,16 @@
 
 <div>
     <div class="placing-of-content text-center">
-        <svg class="exclamation-icon h-44 w-44">{!! file_get_contents(public_path('icons/exclamation-circle.svg')) !!}</svg>
+        <div class="mx-auto exclamation-icon h-44 w-44">{!! file_get_contents(public_path('icons/exclamation-circle.svg')) !!}</div>
     </div>
     <h1 class="placing-of-content text-center">Page Not found</h1><br>
 
-    <p class="error-message text-center fz-20">
-        Looks like your interview is already scheduled. Please check your inbox for confirmation.<br>
+    <p class="error-message text-center fz-20 leading-25">
+        {{__('Looks like your interview is already scheduled. Please check your inbox for confirmation.') }}<br>
 
-        If you are not able to find the email, please check in your spam folder. If it's there, mark it as "Not Spam".<br>
+        {{__('If you are not able to find the email, please check in your spam folder. If it\'s there, mark it as "Not Spam".') }}<br>
         
-        If you are still facing issues, please write to us at careers@coloredcow.com
+        {{__('If you are still facing issues, please write to us at careers@coloredcow.com') }}
     </p>
 </div>
 @endsection
