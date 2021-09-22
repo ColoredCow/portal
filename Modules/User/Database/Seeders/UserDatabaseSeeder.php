9838b6ec9aabbe08f9b929d91e441e4c8cd470a8
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2021-09-23 00:54:52 +0530

    assign  superadmin role to user created by seeder

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Modules\User\Entities\User;
+use Spatie\Permission\Models\Role;
 
 class UserDatabaseSeeder extends Seeder
 {
@@ -14,8 +15,13 @@ class UserDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()
-        ->count(5)
+        $role = Role::where('name', 'super-admin')->first();
+        $users = User::factory()
+        ->count(3)
         ->create();
+
+        foreach ($users as $user) {
+            $user->assignRole($role);
+        }
     }
 }
