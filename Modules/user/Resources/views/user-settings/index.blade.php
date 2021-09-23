83b582ff4fcab926468a0e59ee9a71773d3f4dab
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-09-23 13:56:14 +0530

    Corrected indentation

--- Modules/user/Resources/views/user-settings/index.blade.php
+++ Modules/user/Resources/views/user-settings/index.blade.php
@@ -4,17 +4,16 @@
 
 <form action ="{{ route('user.settings') }}" method = "POST">
 @csrf
-<div class="container">
-    <div class="row justify-content-around">
-        <div class="col-6">
-            <label class="user-settings fz-24">User Settings</label>
+    <div class="container">
+        <div class="row justify-content-around">
+            <div class="col-6">
+                <label class="user-settings fz-24">User Settings</label>
+            </div>
+            <div>
+                <input type="text" class="form-control search-user-settings" placeholder="Search Settings">
+            </div>
         </div>
-        <div>
-            <input type="text" class="form-control search-user-settings" placeholder="Search Settings">
-        </div>
-    </div>
-    <div class="col" id="user-settings-content">
-        <div class= "user-settings-menu">
+        <div class="col" id="user-settings-content">
             <div class="card-header">
                 <ul class="nav nav-tabs card-header-tabs">
                     <li class="nav-item">
@@ -27,12 +26,11 @@
             </div>
             <div class="card-body">
                 <h5 class="max-interview-heading fz-20">Maximum interviews in a day:
-                    <input type="number" class="col-xs text-center outline-none h-40 w-72 rounded-12 quantity" id="quantity" name="max_interviews_per_day" min="0" max="4" value="{{ old ('max_interviews_per_day', optional(Auth::user()->meta)->max_interviews_per_day) }}">
+                    <input type="number" class="col-xs text-center outline-none h-40 w-68 rounded-12 quantity" id="quantity" name="max_interviews_per_day" min="0" max="4" value="{{ old ('max_interviews_per_day', optional(Auth::user()->meta)->max_interviews_per_day) }}">
                 </h5>        
                 <input type="submit" class="btn btn-primary" value="Save">
-            </div>    
+            </div>
         </div>
     </div>
-</div>
 </form>
 @endsection
