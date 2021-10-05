3e405596518d85dcc632147ff7db13d21265a3c7
mdshamoon <mohdshamoon841@gmail.com>
2021-10-05 11:22:04 +0530

    updated table name

--- Modules/User/Database/Migrations/2021_10_05_054926_update_user_table_with_nickname.php
+++ Modules/User/Database/Migrations/2021_10_05_054926_update_user_table_with_nickname.php
@@ -13,7 +13,7 @@ class UpdateUserTableWithNickname extends Migration
      */
     public function up()
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('nickname')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class UpdateUserTableWithNickname extends Migration
      */
     public function down()
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('nickname');
         });
     }
