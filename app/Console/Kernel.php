6d889cf82ff15a01a67fb5c3d6b1f56310595a06
wevekamola <vivek.amola@coloredcow.in>
2021-10-14 12:28:01 +0530

    updated the send mail time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('application:no-show')->everyThirtyMinutes();
         $schedule->command('application:send-interview-reminders')->dailyAt('08:00');
         $schedule->command('sync:effortsheet')->weekdays()->timezone(config('constants.timezone.indian'))->at('20:00');
-        $schedule->command('effort-summary:send')->weekdays()->timezone(config('constants.timezone.indian'))->at('20:00');
+        $schedule->command('effort-summary:send')->weekdays()->timezone(config('constants.timezone.indian'))->at('21:00');
         $schedule->command('hr:check-follow-ups')->daily();
         $schedule->command('mapping-of-jobs-and-hr-rounds');
         $schedule->command('invoice:send-unpaid-invoice-list')->weekly()->mondays()->at('09:00');
