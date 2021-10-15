408101a5f1ed8d14f3d8a307cc86049223abcc93
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-10-15 15:44:57 +0530

    cube js first draft

--- Modules/EffortTracking/Http/Controllers/EffortTrackingController.php
+++ Modules/EffortTracking/Http/Controllers/EffortTrackingController.php
@@ -3,6 +3,7 @@
 namespace Modules\EffortTracking\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Http;
 use Modules\EffortTracking\Services\EffortTrackingService;
 use Modules\Project\Entities\Project;
 
@@ -22,6 +23,8 @@ class EffortTrackingController extends Controller
     public function show(Project $project)
     {
         $data = $this->service->show($project);
+        $cube_js_response = Http::get('http://localhost:4000/cubejs-api/v1/load?query={"measures":["HrJobs.count"]}')->json();
+        $data['totalWorkingDays'] = $cube_js_response['data']['0']['HrJobs.count'];
 
         return view('efforttracking::show')->with($data);
     }
