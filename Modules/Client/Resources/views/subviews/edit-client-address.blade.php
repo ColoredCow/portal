4c4c2b4950a2eccf1bfbdca6e226a69704c4e0dc
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-16 15:17:45 +0530

    Added save and Close button

--- Modules/Client/Resources/views/subviews/edit-client-address.blade.php
+++ Modules/Client/Resources/views/subviews/edit-client-address.blade.php
@@ -76,25 +76,23 @@
         </div>
     </div>
 </div>
-<div class="modal job-application-modal" id="myModal">
+<div class="modal" id="myModal">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-            <h4 class="modal-title">Filter By</h4>
+            <h4 class="modal-title">Country</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
         <div class="modal-body">
             <form>
                 <div class="form-group row">
-                    {{-- <div class="col-sm-10">
-                        <job-application-component :options="options"></job-application-component>    
-                    </div> --}}
+
                 </div>
             </form>     
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-outline-primary" disabled data-dismiss="modal">Apply(Coming Soon)</button>
-            <button type="button" class="btn btn-outline-danger" disabled data-dismiss="modal">Close(Coming Soon)</button>
+            <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Save</button>
+            <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
         </div>
     </div>
 </div>
