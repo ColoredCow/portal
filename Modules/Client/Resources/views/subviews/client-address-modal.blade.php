9133c6e27b427830e71344a7753974dc618802f7
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-18 16:57:27 +0530

    Created modal for adding new country

---
+++ Modules/Client/Resources/views/subviews/client-address-modal.blade.php
@@ -0,0 +1,41 @@
+<div class="modal" id="myModalClientCountry">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h4 class="modal-title">Country Details</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+        <div class="modal-body">
+            <form>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Country:</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" placeholder="Country">
+                    </div>
+                    <label class="col-sm-2 col-form-label">Initials:</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" placeholder="Initials">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Currency:</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" placeholder="Currency">
+                    </div>
+                    <label class="col-sm-2 col-form-label">Symbols:</label>
+                    <div class="col-sm-4">
+                        <select id="inputState" class="form-control">
+                            <option>₹</option>
+                            <option>$</option>
+                            <option>€</option>
+                        </select>
+                    </div>
+                </div>    
+            </form>     
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Save</button>
+            <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
