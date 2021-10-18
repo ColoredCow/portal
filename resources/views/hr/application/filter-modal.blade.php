d6d9ee22f0a7088e18e70dedce8ca65cc2f51f83
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-18 13:20:01 +0530

    Added new file for modal

---
+++ resources/views/hr/application/filter-modal.blade.php
@@ -0,0 +1,24 @@
+<div class="container" id="vueContainer">   
+    <div class="modal job-application-modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="modal-title">Filter By</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <job-application-component :options="options"></job-application-component>    
+                        </div>
+                    </div>
+                </form>     
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-primary" disabled data-dismiss="modal">Apply(Coming Soon)</button>
+                <button type="button" class="btn btn-outline-danger" disabled data-dismiss="modal">Close(Coming Soon)</button>
+            </div>
+        </div>
+    </div>
+</div>
