d8d337552f5b954c8545536a04388cea684aba20
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-18 17:02:09 +0530

    Removed modal from blade file

--- Modules/Client/Resources/views/subviews/edit-client-address.blade.php
+++ Modules/Client/Resources/views/subviews/edit-client-address.blade.php
@@ -30,7 +30,7 @@
                                 </select>
                             </div>
                             <div class="mt-2">
-                                <button type="button" style="text-decoration: underline;" data-toggle="modal" data-target="#myModal" class="btn btn-sm">Add new country</button>
+                                <button type="button" style="text-decoration: underline;" data-toggle="modal" data-target="#myModalClientCountry" class="btn btn-sm">Add new country</button>
                             </div>
                         </div>
                         <div class="form-group">
@@ -76,50 +76,7 @@
         </div>
     </div>
 </div>
-<div class="modal" id="myModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h4 class="modal-title">Country</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-        <div class="modal-body">
-            <form>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Country:</label>
-                    <div class="col-sm-8">
-                      <input type="text" class="form-control" placeholder="Country">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Intials:</label>
-                    <div class="col-sm-8">
-                      <input type="text" class="form-control" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Currency:</label>
-                    <div class="col-sm-8">
-                      <input type="text" class="form-control" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Symbols:</label>
-                    <div class="col-sm-8">
-                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                    </div>
-                </div>    
-            </form>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Save</button>
-            <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
-        </div>
-    </div>
-</div>
-
-
-
+@include('client::subviews.client-address-modal')
 @section('js_scripts')
 <script>
     new Vue({
