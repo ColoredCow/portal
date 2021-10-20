e536ca1db9accaef8f4f01f4de4119017da55b53
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-20 17:22:18 +0530

    Corrected formatting

--- resources/js/components/HR/JobApplicationComponent.vue
+++ resources/js/components/HR/JobApplicationComponent.vue
@@ -2,7 +2,7 @@
 	<div class="filter form-group-row-sm-10" style="display: flex;">
 		<label>Status: </label>
 		<div class="row justify-content-center mt-100" style="width: 120%;">
-			<div class="col-sm-10"> 
+			<div class="col-sm-10">
 				<select class="job-application-filters" id="choices-multiple-remove-button" placeholder="Select one or more status"  multiple>
 					<option v-for="option in options" :value="option" :key="option.id">{{ option.name }}</option>
 				</select>
@@ -12,22 +12,22 @@
 </template>
 
 <script>
-	 export default {
-	 data() {
-      return {
-        selected: null,
-        options: [
-			{ id: '1', name: 'New Application' },
-			{ id: '2', name: 'In Progress' },
-			{ id: '3', name: 'Awaiting Confirmation' },
-			{ id: '4', name: 'Need Follow Up'},
-			{ id: '5', name: 'Send For Approval'},
-			{ id: '6', name: 'No Show'},
-			{ id: '7', name: 'No Show Reminded'},
-			{ id: '8', name: 'OnBoard'},
-			{ id: '9', name: 'Closed'}
-        ]
-      }
-    }
-}
+	export default {
+	data() {
+		return {
+			selected: null,
+			options: [
+				{ id: "1", name: "New Application" },
+				{ id: "2", name: "In Progress"},
+				{ id: "3", name: "Awaiting Confirmation"},
+				{ id: "4", name: "Need Follow Up"},
+				{ id: "5", name: "Send For Approval"},
+				{ id: "6", name: "No Show"},
+				{ id: "7", name: "No Show Reminded"},
+				{ id: "8", name: "OnBoard"},
+				{ id: "9", name: "Closed"
+				]
+			};
+		}
+	};
 </script>
