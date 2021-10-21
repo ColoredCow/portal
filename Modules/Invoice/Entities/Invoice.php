413ea14ad6b5c4ce5b67a8a0a4a75f13833d65b5
Riya Suntwal <carrisun91rio@gmail.com>
2021-10-21 21:00:18 +0530

    finance-currency-before-amount

--- Modules/Invoice/Entities/Invoice.php
+++ Modules/Invoice/Entities/Invoice.php
@@ -82,7 +82,7 @@ class Invoice extends Model
             $amount = $this->amount + $this->gst;
         }
 
-        return $amount . ' ' . optional($country)->currency_symbol;
+        return optional($country)->currency_symbol . ' ' . $amount;
     }
 
     public function shouldHighlighted()
