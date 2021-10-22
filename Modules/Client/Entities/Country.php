dcc30cfc4befc53102743428702b2facc577ea35
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-22 13:12:58 +0530

    Removed spacings

--- Modules/Client/Entities/Country.php
+++ Modules/Client/Entities/Country.php
@@ -5,7 +5,7 @@ namespace Modules\Client\Entities;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
-{   
+{
     protected $table = 'countries'; 
     
     protected $fillable =['name','initials','currency','currency_symbol'];
