0e7af6f1f0ef1bd66b6f914e660a112f800210e5
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-10-22 12:39:47 +0530

    Corrected modal file

--- Modules/Client/Resources/views/subviews/client-address-modal.blade.php
+++ Modules/Client/Resources/views/subviews/client-address-modal.blade.php
@@ -4,38 +4,40 @@
             <div class="modal-header">
             <h4 class="modal-title">Country Details</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
+        </div>
+        <form action="{{route("country.store")}}" method ="POST">    
+        @csrf
         <div class="modal-body">
-            <form>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Country:</label>
-                    <div class="col-sm-4">
-                      <input type="text" class="form-control" placeholder="Country">
-                    </div>
-                    <label class="col-sm-2 col-form-label">Initials:</label>
-                    <div class="col-sm-4">
-                      <input type="text" class="form-control" placeholder="Initials">
-                    </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Country<strong class="text-danger">*</strong></label>
+                <div class="col-sm-4">
+                    <input type="text" id="name" name="name" class="form-control" placeholder="Country" required>
                 </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Currency:</label>
+                <label class="col-sm-2 col-form-label">Initials<strong class="text-danger">*</strong></label>
+                <div class="col-sm-4">
+                    <input type="text" id ="initials" name="initials" class="form-control" placeholder="Initials" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Currency<strong class="text-danger">*</strong></label>
                     <div class="col-sm-4">
-                      <input type="text" class="form-control" placeholder="Currency">
+                        <input type="text" id ="currency" name="currency" class="form-control" placeholder="Currency" required>
                     </div>
-                    <label class="col-sm-2 col-form-label">Symbols:</label>
+                    <label class="col-sm-2 col-form-label">Symbols</label>
                     <div class="col-sm-4">
-                        <select id="inputState" class="form-control">
-                            <option>₹</option>
-                            <option>$</option>
-                            <option>€</option>
+                        <select id="currency_symbol" name="currency_symbol" class="form-control">
+                            <option id="option_for_currency_symbol">₹</option>
+                            <option id="option_for_currency_symbol">$</option>
+                            <option id="option_for_currency_symbol">€</option>
+                            <option id="option_for_currency_symbol">none</option>
                         </select>
                     </div>
-                </div>    
-            </form>     
+            </div>     
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Save</button>
-            <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
-        </div>
+            <div class="modal-footer">
+                <input type="submit" class='btn btn-outline-primary' value="Save">
+                <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
+            </div>
+        </form>
     </div>
 </div>
