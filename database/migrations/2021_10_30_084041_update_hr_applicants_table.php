eba072404fedbc96513374ef0ddaa1e4b0e87db0
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-01 15:43:43 +0530

    Corrected formatting

--- database/migrations/2021_10_30_084041_update_hr_applicants_table.php
+++ database/migrations/2021_10_30_084041_update_hr_applicants_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class UpdateHrApplicantsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('hr_applicants', function (Blueprint $table) {
@@ -18,11 +13,6 @@ class UpdateHrApplicantsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('hr_applicants', function (Blueprint $table) {
