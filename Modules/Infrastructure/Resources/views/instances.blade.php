cfc77c79ae1ae92e45ebd1669fad5db5add8e549
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-08 15:14:45 +0530

    Created permission for instances

--- Modules/Infrastructure/Resources/views/instances.blade.php
+++ Modules/Infrastructure/Resources/views/instances.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 <div class="container">
-    @include('infrastructure::menu_header', ['tab' => 'EC2 Instances'])
+    @if(auth()->user()->can('infrastructure.ec2-instances.view'))
+        @include('infrastructure::menu_header', ['tab' => 'EC2 Instances'])
 
-    <ul class="mt-5 w-50p list list-group">
-        @foreach($instances as $instance)
-        <li class="list-item list-group-item">{{ \Str::title($instance['Instances'][0]['Tags'][0]['Value']) }}</li>
-        @endforeach
-    </ul>
+        <ul class="mt-5 w-50p list list-group">
+            @foreach($instances as $instance)
+            <li class="list-item list-group-item">{{ \Str::title($instance['Instances'][0]['Tags'][0]['Value']) }}</li>
+            @endforeach
+        </ul>
+    @else
+        @include('errors.403')  
+    @endif  
 </div>
 @endsection
