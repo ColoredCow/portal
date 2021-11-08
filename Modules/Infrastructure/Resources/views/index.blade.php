9322a4f7b0606c38022e30ebc96dcedebead837b
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-08 15:09:15 +0530

    Added permissions for backups

--- Modules/Infrastructure/Resources/views/index.blade.php
+++ Modules/Infrastructure/Resources/views/index.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
 <div class="container">
-   
-    @include('infrastructure::menu_header', ['tab' => 'DataBase Backups'])
+    @if(auth()->user()->can('infrastructure.backups.view'))
+        @include('infrastructure::menu_header', ['tab' => 'DataBase Backups'])
 
-    <ul class="mt-5 w-50p list list-group">
-        @foreach($storageBuckets as $storageBucket) 
-            <li class="list-item list-group-item">{{ \Str::title($storageBucket['Name']) }}</li>
-        @endforeach
-    </ul>
+        <ul class="mt-5 w-50p list list-group">
+            @foreach($storageBuckets as $storageBucket)
+                <li class="list-item list-group-item">{{ \Str::title($storageBucket['Name']) }}</li>
+            @endforeach
+        </ul>
+    @else
+        @include('errors.403')
+    @endif
 </div>
-
-
-
-
-
-
-
-
 @endsection
