a81d218b8ba850e0942535ec0aba75d109f414f7
Abhay Agarwal <72443500+AbhayTheEngineer@users.noreply.github.com>
2021-11-12 15:46:56 +0530

    Update index-filters.blade.php
    Added "Email Invoices" button with a modal view input textfield to send the recipient email address.

--- Modules/Invoice/Resources/views/index-filters.blade.php
+++ Modules/Invoice/Resources/views/index-filters.blade.php
@@ -38,5 +38,41 @@
                 <option {{ request()->input('status') == 'paid' ? "selected=selected" : '' }} value="paid">Paid</option>
             </select>
         </div>
+        <span>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#emailModal">Email Invoice</button>
+
+            <div class="modal fade" id="emailModal" tabindex="-1" role="dialog" aria-labelledby="emailModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Enter Recipient Email</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                      <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Email:</label>
+                        <input type="email" class="form-control" id="recipient-email">
+                      </div>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+
+                    {{-- The below line of code will be used when sending the email integration takes place. --}}
+
+                    {{-- <button type="submit" class="btn btn-primary">Send Email</button>  --}}
+
+                    <button type="#">Coming Soon</button>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>        
+
+        </span>
     </div>
 </form>
