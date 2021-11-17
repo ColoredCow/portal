978dfe229c543e21a379fdd7959f5f88ae94b7b6
Riya Suntwal <carrisun91rio@gmail.com>
2021-11-17 22:54:10 +0530

    converted total amounts to words

--- Modules/Invoice/Resources/views/subviews/create/invoice-details.blade.php
+++ Modules/Invoice/Resources/views/subviews/create/invoice-details.blade.php
@@ -63,7 +63,8 @@
                             @endforeach 
                         </select>
                     </div>
-                    <input v-model="amount" type="number" class="form-control" name="amount" id="amount"
+                    <input onkeyup="number2text();"
+                     v-model="amount" type="number" class="form-control" name="amount" id="amount"
                         placeholder="Invoice Amount" required="required" step=".01" min="0">
                 </div>
             </div>
@@ -78,6 +79,9 @@
                         step=".01" min="0">
                 </div>
             </div>
+            <p class="text-danger" v-if="currency == 'INR' " >Total Amount : @{{tot}}</p>
+            <p class="text-danger" v-if="currency == 'USD' " >Total Amount : @{{amt}}</p>
+            <div class="text-danger" id="container"></div><br/>
 
             <div class="form-group">
                 <label for="sent_on" class="field-required">Sent on</label>
@@ -102,6 +106,7 @@
 
 @section('js_scripts')
 <script>
+    
     new Vue({
     el:'#create_invoice_details_form',
 
@@ -112,7 +117,7 @@
             clientId:"",
             client:null,
             currency:'',
-            amount:''
+            amount:'',
         }
     },
 
@@ -138,10 +143,121 @@
     computed: {
         gst: function () {
             return (this.amount * 0.18).toFixed(2);
-        }
+        },
+        tot: function () {
+            let total = this.amount * 0.18 + 1 * this.amount;
+            return (total);
+        },
+        amt: function (){
+            return (this.amount);
+        },
     }
 });
 
+function getcurrency(){
+    var currency = document.getElementById('currency').value;
+
+    return currency;
+}
+function number2text(value) {
+    var curr = getcurrency();
+    var value = document.getElementById('amount').value;
+    switch (curr) {
+  case 'INR':
+    value = value * 0.18 + 1 * value;
+    var fraction = Math.round(frac(value)*100);
+    var f_text  = "";
+    if(fraction > 0) {
+        f_text = "AND "+convert_number(fraction)+" PAISE";
+    }
+    var output = convert_number(value)+" RUPEE "+f_text+" ONLY";
+    break;
+  case 'USD':
+    var fraction = Math.round(frac(value)*100);
+    var f_text  = "";
+    if(fraction > 0) {
+        f_text = "AND "+convert_number(fraction)+" CENTS";
+    }
+    var output = convert_number(value)+" DOLLAR "+f_text+" ONLY";
+    break;
+  }
+	document.getElementById('container').innerHTML = output;
+}
+
+function frac(f) {
+    return f % 1;
+}
+
+function convert_number(number)
+{
+    if ((number < 0) || (number > 999999999)) 
+    { 
+        return "NUMBER OUT OF RANGE!";
+    }
+    var Gn = Math.floor(number / 10000000);  /* Crore */ 
+    number -= Gn * 10000000; 
+    var kn = Math.floor(number / 100000);     /* lakhs */ 
+    number -= kn * 100000; 
+    var Hn = Math.floor(number / 1000);      /* thousand */ 
+    number -= Hn * 1000; 
+    var Dn = Math.floor(number / 100);       /* Tens (deca) */ 
+    number = number % 100;               /* Ones */ 
+    var tn= Math.floor(number / 10); 
+    var one=Math.floor(number % 10); 
+    var res = ""; 
+
+    if (Gn>0) 
+    { 
+        res += (convert_number(Gn) + " CRORE"); 
+    } 
+    if (kn>0) 
+    { 
+            res += (((res=="") ? "" : " ") + 
+            convert_number(kn) + " LAKH"); 
+    } 
+    if (Hn>0) 
+    { 
+        res += (((res=="") ? "" : " ") +
+            convert_number(Hn) + " THOUSAND"); 
+    } 
+
+    if (Dn) 
+    { 
+        res += (((res=="") ? "" : " ") + 
+            convert_number(Dn) + " HUNDRED"); 
+    } 
+
+
+    var ones = Array("", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX","SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN","FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN","NINETEEN"); 
+var tens = Array("", "", "TWENTY", "THIRTY", "FOURTY", "FIFTY", "SIXTY","SEVENTY", "EIGHTY", "NINETY"); 
+
+    if (tn>0 || one>0) 
+    { 
+        if (!(res=="")) 
+        { 
+            res += " AND "; 
+        } 
+        if (tn < 2) 
+        { 
+            res += ones[tn * 10 + one]; 
+        } 
+        else 
+        { 
+
+            res += tens[tn];
+            if (one>0) 
+            { 
+                res += ("-" + ones[one]); 
+            } 
+        } 
+    }
+
+    if (res=="")
+    { 
+        res = "ZERO"; 
+    } 
+    return res;
+}
 </script>
 
 @endsection
