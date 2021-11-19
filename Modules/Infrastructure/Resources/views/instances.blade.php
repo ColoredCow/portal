cc881ef526a36e14184569c6887cdd22ce04fa90
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-19 10:56:20 +0530

    Removed permissions

--- Modules/Infrastructure/Resources/views/instances.blade.php
+++ Modules/Infrastructure/Resources/views/instances.blade.php
@@ -2,16 +2,12 @@
 
 @section('content')
 <div class="container">
-    @if(auth()->user()->can('infrastructure.ec2-instances.view'))
-        @include('infrastructure::menu_header', ['tab' => 'EC2 Instances'])
+    @include('infrastructure::menu_header', ['tab' => 'EC2 Instances'])
 
-        <ul class="mt-5 w-50p list list-group">
-            @foreach($instances as $instance)
-            <li class="list-item list-group-item">{{ \Str::title($instance['Instances'][0]['Tags'][0]['Value']) }}</li>
-            @endforeach
-        </ul>
-    @else
-        @include('errors.403')  
-    @endif  
+    <ul class="mt-5 w-50p list list-group">
+        @foreach($instances as $instance)
+        <li class="list-item list-group-item">{{ \Str::title($instance['Instances'][0]['Tags'][0]['Value']) }}</li>
+        @endforeach
+    </ul> 
 </div>
 @endsection
