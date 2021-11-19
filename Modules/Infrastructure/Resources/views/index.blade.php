b807924da174e913f4e4022175a019fe2567e173
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-19 10:54:02 +0530

    Correct formatting

--- Modules/Infrastructure/Resources/views/index.blade.php
+++ Modules/Infrastructure/Resources/views/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-        @include('infrastructure::menu_header', ['tab' => 'DataBase Backups'])
+    @include('infrastructure::menu_header', ['tab' => 'DataBase Backups'])
 
-        <ul class="mt-5 w-50p list list-group">
-            @foreach($storageBuckets as $storageBucket)
-                <li class="list-item list-group-item">{{ \Str::title($storageBucket['Name']) }}</li>
-            @endforeach
-        </ul>
+    <ul class="mt-5 w-50p list list-group">
+        @foreach($storageBuckets as $storageBucket)
+            <li class="list-item list-group-item">{{ \Str::title($storageBucket['Name']) }}</li>
+        @endforeach
+    </ul>
 </div>
 @endsection
