d24515e99f03abe715ea012a04a69ba3764601cd
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-19 11:04:56 +0530

    Backups Policy

---
+++ app/Policies/Infrastructure/BackupsPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use Modules\User\Entities\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BackupsPolicy
+{
+    use HandlesAuthorization;
+        /**
+     * Determine whether the user can view the backups.
+     *
+     * @param  \Modules\User\Entities\User  $user
+     * @return mixed
+     */
+
+    public function view(User $user)
+    {
+        return $user->hasPermissionTo('infrastructure.backups.view');
+    }
+}
