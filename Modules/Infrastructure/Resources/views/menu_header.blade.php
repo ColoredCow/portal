bed31ddd2888f0b5608e1edba68e3d3ff71a3c25
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-19 11:00:52 +0530

    Removed permissions

--- Modules/Infrastructure/Resources/views/menu_header.blade.php
+++ Modules/Infrastructure/Resources/views/menu_header.blade.php
@@ -1,16 +1,12 @@
 <div class="d-flex justify-content-between">
-    <ul class="nav nav-pills">
-    @can('infrastructure.backups.view')
-        <li class="nav-item">
-            <a class="nav-item nav-link font-weight-bold ml-0 pl-2 {{ $tab == 'DataBase Backups' ? 'active' : ''}}" href="{{ route('infrastructure.index') }}"><i class="fa fa-users"></i>&nbsp;DataBase Backups</a>
-        </li>
-    @endcan
-    @can('infrastructure.ec2-instances.view')
-        <li class="nav-item">
-            <a class="nav-item nav-link font-weight-bold {{ $tab == 'EC2 Instances' ? 'active' : ''}}" href="{{ route('infrastructure.get-instances') }}"><i class="fa fa-users"></i>&nbsp;EC2 Instances</a>
-        </li>
-    @endcan
-    </ul>
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-item nav-link font-weight-bold ml-0 pl-2 {{ $tab == 'DataBase Backups' ? 'active' : ''}}" href="{{ route('infrastructure.index') }}"><i class="fa fa-users"></i>&nbsp;DataBase Backups</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-item nav-link font-weight-bold {{ $tab == 'EC2 Instances' ? 'active' : ''}}" href="{{ route('infrastructure.get-instances') }}"><i class="fa fa-users"></i>&nbsp;EC2 Instances</a>
+    </li>
+ </ul>
 
     <div>
         <a target="_blank" href="https://ap-south-1.console.aws.amazon.com/console" class="btn btn-info text-white">Go to AWS console</a>
