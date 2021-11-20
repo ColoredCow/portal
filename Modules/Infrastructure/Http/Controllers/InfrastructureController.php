4f1d0cd01b90e282bd23359a0ccf85713c4107ef
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-11-20 17:23:29 +0530

    corrected formatting

--- Modules/Infrastructure/Http/Controllers/InfrastructureController.php
+++ Modules/Infrastructure/Http/Controllers/InfrastructureController.php
@@ -36,7 +36,7 @@ class InfrastructureController extends Controller
     public function getBillingDetails()
     {
         $this->authorize('Billingview', $this);
-        return $this->service->getBillingDetails(); 
+        return $this->service->getBillingDetails();
     }
 
     /**
