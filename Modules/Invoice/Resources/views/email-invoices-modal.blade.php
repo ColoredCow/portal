14687f1492548533970d2fe1e4352521200ef009
Jyoti Srivastava <srivastavajyoti510@gmail.com>
2021-11-30 16:00:19 +0530

    Added email-invoices button

---
+++ Modules/Invoice/Resources/views/email-invoices-modal.blade.php
@@ -0,0 +1,26 @@
+<div class="container" id="vueContainer">   
+    <div class="modal" id="emailinvoicesmodal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Email Invoices</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group row">
+                            <label for="recipient-name" class="col-sm-2 col-form-label">Recipient:   </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="recipient-name">
+                            </div>
+                        </div>
+                    </form>    
+                </div>
+                
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-primary" disabled data-dismiss="modal">Email</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
