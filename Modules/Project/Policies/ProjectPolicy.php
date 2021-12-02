58f98c2d94cefc84aba68bb8bd159d70370ffc09
Balendu Rawat <balendurawat1@gmail.com>
2021-12-02 15:30:17 +0530

    minor changes

--- Modules/Project/Policies/ProjectPolicy.php
+++ Modules/Project/Policies/ProjectPolicy.php
@@ -3,15 +3,15 @@
 namespace Modules\Project\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\User\Entities\User;
- 
+use Modules\User\Entities\User; 
 
 class ProjectPolicy
 {
     use HandlesAuthorization;
-    
+
     public function view(User $user)
     {
         return $user->hasPermissionTo('projects.view');
     }
+
 }
