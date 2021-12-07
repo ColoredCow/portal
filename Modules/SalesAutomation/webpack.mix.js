3dd8b6a85158f48c436dde04d08c02fc394b9d8f
Balendu Rawat <balendurawat1@gmail.com>
2021-12-07 15:24:07 +0530

    navigation links error fix

--- Modules/SalesAutomation/webpack.mix.js
+++ Modules/SalesAutomation/webpack.mix.js
@@ -6,8 +6,8 @@ require('laravel-mix-merge-manifest');
 
 mix.setPublicPath('../../public').mergeManifest();
 
-mix.js(__dirname + '/Resources/assets/js/app.js', 'js/app.js')
-    .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/app.css');
+mix.js(__dirname + '/Resources/assets/js/app.js', 'js/salesautomation.js')
+    .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/salesautomation.css');
 
 if (mix.inProduction()) {
     mix.version();
--- Modules/User/Resources/views/layouts/master.blade.php
+++ Modules/User/Resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('js_scripts')
-<script src="{{ mix('/js/app.js') }}" defer></script>
+<script src="{{ mix('/js/user.js') }}" defer></script>
 @endsection
 
 @section('css_scripts')
-<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+<link href="{{ mix('/css/user.css') }}" rel="stylesheet">
 @endsection
