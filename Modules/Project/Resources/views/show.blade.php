26cd371c84596e8534ec1fe98f9016721cdf7820
Ayush <80516839+Ayush8923@users.noreply.github.com>
2021-12-09 19:01:52 +0530

    Correction done in the file.

--- Modules/Project/Resources/views/show.blade.php
+++ Modules/Project/Resources/views/show.blade.php
@@ -4,14 +4,14 @@
 <div class="container" id="vueContainer">
     <br>
     <div class="d-flex">
-        <h4 clss="c-pointer" class="d-inline-block" v-on:click="counter += 1">{{ $project->name }}</h4>
-        <a  id="view_effort_sheet_badge" target="_self" href= "{{route('project.edit', $project )}}" class="btn btn-primary text-white ml-auto">{{ _('Edit') }}</a>
+        <h4 class="c-pointer d-inline-block" v-on:click="counter += 1">{{ $project->name }}</h4>
+        <a id="view_effort_sheet_badge" target="_self" href= "{{route('project.edit', $project )}}" class="btn btn-primary text-white ml-auto">{{ _('Edit') }}</a>
     </div> 
-    <div class="card mt-3" >
-    <div class="card-header" data-toggle="collapse" data-target="#project_detail_form">
-        <h4>Project details <span>(Coming soon...)</span></h4>
-    </div>
-
+    <div class="card mt-3">
+        <div class="card-header" data-toggle="collapse" data-target="#project_detail_form">
+            <h4>Project details <span>(Coming soon...)</span></h4>
+        </div>
+    </div>    
 </div>
 
 @endsection
