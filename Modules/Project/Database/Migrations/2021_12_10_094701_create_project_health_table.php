043302e201842753047045fc9131a8bb4e98a112
Balendu Rawat <balendurawat1@gmail.com>
2021-12-10 15:41:30 +0530

    phase 3 solved

---
+++ Modules/Project/Database/Migrations/2021_12_10_094701_create_project_health_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectHealthTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_health', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->foreignId('project_id');
+            $table->string('staging_url')->nullable();
+            $table->string('onboarding_documents_url')->nullable();
+            $table->boolean('has_issue_templates')->default(false);
+            $table->boolean('has_unit_testing')->default(false);
+            $table->boolean('has_ci_check')->default(false);
+            $table->boolean('has_site_monitoring')->default(false);
+            $table->boolean('has_error_logging')->default(false);
+            $table->boolean('has_error_reporting')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_health');
+    }
+}
