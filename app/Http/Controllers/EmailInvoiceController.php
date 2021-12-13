6f95a1573cf7f1a4550193fecda5b13945d96b19
Jyoti Srivastava <79009032+srivastava-jyoti@users.noreply.github.com>
2021-12-13 12:56:50 +0530

    Corrected Formatting

--- app/Http/Controllers/EmailInvoiceController.php
+++ app/Http/Controllers/EmailInvoiceController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Modules\User\Entities\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
@@ -15,17 +16,18 @@ class EmailInvoiceController extends Controller
 
     public function handle()
     {
-        $invoices=Invoice::with(['client', 'project'])->get(); 
+        $invoices=Invoice::with(['client', 'project'])->get();
         $user = new User;
         $user->email = 'finance@coloredcow.com';
-        Mail::send('invoice::mail.invoices-list',['user' => $user, 'invoices' =>$invoices] ,function ($m) use ($user){
+        Mail::send(
+            'invoice::mail.invoices-list',
+            ['user' => $user, 'invoices' =>$invoices] ,
+            function ($m) use ($user){
             $m->from('finance@coloredcow.com');
 
             $m->to($user->email)->subject('List Of Invoices Sent');
-            
         }
-    );
+        );
         return redirect()->back();
     }
-
 }
