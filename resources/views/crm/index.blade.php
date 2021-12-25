1558048c68bc35bbdcbeb64914f62374784f649b
kickb71 <gautam.rajesh@coloredcow.com>
2021-12-25 14:15:19 +0530

    WIP

--- resources/views/crm/index.blade.php
+++ resources/views/crm/index.blade.php
@@ -2,17 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="d-flex justify-content-start row flex-wrap">
+    <h1>CRM Dashboard</h1>
     <br>
-    <?php foreach ($data['prospectes'] as $prospectes) : ?>
-        <div class="col-md-4">
-            <div class="card h-75 mx-4 mt-3 mb-5 ">
-                <a class="card-body no-transition">
-            	   <br><h2 class="text-center">{{ $prospectes->name; }}</h2><br>
-                </a>
-            </div>
-        </div>
-    <?php endforeach; ?>
+    <div class="widgetbar mb-10">
+        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#createCrmConnectionModal">Add New Connection</button>
     </div>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Last connected</th>
+            <th>Status</th>
+            <th>Contact</th>
+            <th>Source</th>
+            <th>Last interaction</th>
+            <th>Next steps</th>
+            <th>Notes</th>
+        </thead>
+        <?php foreach ($data['crm_details'] as $details) : ?>
+            <tbody>
+                <td>{{ $details->id ?? '-' }}</td>
+                <td>{{ $details->prospect_id ?? '-' }}</td>
+                <td>{{ $details->last_connected ?? '-' }}</td>
+                <td>{{ $details->status ?? '-' }}</td>
+                <td>{{ $details->client_contact_person_id ?? '-' }}</td>
+                <td>{{ $details->source ?? '-' }}</td>
+                <td>{{ $details->last_interaction ?? '-' }}</td>
+                <td>{{ $details->next_step ?? '-' }}</td>
+                <td>{{ $details->notes ?? '-' }}</td>
+            </tbody>  
+        <?php endforeach; ?>
+    </table>
 </div>
+
+@include( 'crm.create' )
 @endsection
