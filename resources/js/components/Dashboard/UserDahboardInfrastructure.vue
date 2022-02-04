ed8d1553f6a5eafe60ac8306a4785218aeb87ee7
Ayush <80516839+Ayush8923@users.noreply.github.com>
2022-02-04 01:27:10 -0800

    Some changes in Infrastructure Dashboard.

--- resources/js/components/Dashboard/UserDahboardInfrastructure.vue
+++ resources/js/components/Dashboard/UserDahboardInfrastructure.vue
@@ -6,33 +6,33 @@
          <div class="card-body pt-3" style="height: 18em;overflow: auto;">
             <ul v-if="this.billingDetails.current_amount" class="list list-group unstyled-list">
                 <li style="font-size:16px;">
-                    <div> Unbilled amount till today </div> 
-                    <div :class="'cost badge p-2 ' + this.currentAmountClass "> {{ this.billingDetails.current_amount }} </div> 
+                    <div class="d-inline-block"> Unbilled amount till today </div> <br>
+                    <div :class="'cost badge p-2 ml-4 ' + this.currentAmountClass "><span>$</span>{{ this.billingDetails.current_amount }}</div> 
                 </li>
                 <hr class="mt-1 w-full">
 
                 <li style="font-size:16px;">
-                    <div> Forcast for this month </div> 
-                    <div class="cost badge badge-info p-2">{{ this.billingDetails.forcast_amount }}</div> 
+                    <div> Forcast for this month </div> <br>
+                    <div class="cost badge badge-info p-2 ml-4"><span>$</span>{{ this.billingDetails.forcast_amount }}</div> 
                 </li>
                 <hr class="mt-1 w-full">
 
                 <li style="font-size:16px;">
-                    <div> Last Month amount </div> 
-                    <div class="cost badge badge-info p-2">{{ this.billingDetails.last_month_amount }}</div> 
+                    <div> Last Month amount </div> <br> 
+                    <div class="cost badge badge-info p-2 ml-4"><span>$</span>{{ this.billingDetails.last_month_amount }}</div> 
                 </li>
                 <hr class="mt-1 w-full">
 
-                <li style="font-size:16px;">
-                    <div><span> Average <span class="fz-12">(Based on last year data)</span> </span></div>
-                    <div class="cost badge badge-warning p-2">{{ this.billingDetails.avg_by_last_year }}</div> 
+                <li style="font-size:16px;"> <br>
+                    <div class="d-inline-block"> Average <span class="fz-12 ">(Based on last year data)</span></div><br>
+                    <div class="cost badge badge-warning p-2 ml-4"><span>$</span>{{ this.billingDetails.avg_by_last_year }}</div> 
                 </li>
                 <hr class="mt-1 w-full">
 
             </ul>
 
             <div v-else>
-                <p>Fetching latest data...</p>
+                 <p>Fetching latest data...</p>
             </div>
         </div>
     </div>
