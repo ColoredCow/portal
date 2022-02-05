fa96c7a9c0598110a0399c4b75d69f0c2945b4c3
Shivam-Samant <shivamsamant000@gmail.com>
2022-02-05 17:00:24 +0530

    minor changes are done

--- Modules/Report/Http/Controllers/ReportController.php
+++ Modules/Report/Http/Controllers/ReportController.php
@@ -10,7 +10,6 @@ class ReportController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Renderable
      */
     public function index()
     {
@@ -19,7 +18,6 @@ class ReportController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * @return Renderable
      */
     public function create()
     {
@@ -29,7 +27,6 @@ class ReportController extends Controller
     /**
      * Store a newly created resource in storage.
      * @param Request $request
-     * @return Renderable
      */
     public function store(Request $request)
     {
@@ -39,7 +36,6 @@ class ReportController extends Controller
     /**
      * Show the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function show($id)
     {
@@ -49,7 +45,6 @@ class ReportController extends Controller
     /**
      * Show the form for editing the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function edit($id)
     {
@@ -60,7 +55,6 @@ class ReportController extends Controller
      * Update the specified resource in storage.
      * @param Request $request
      * @param int $id
-     * @return Renderable
      */
     public function update(Request $request, $id)
     {
@@ -70,7 +64,6 @@ class ReportController extends Controller
     /**
      * Remove the specified resource from storage.
      * @param int $id
-     * @return Renderable
      */
     public function destroy($id)
     {
