044ef697ec5eb0d397a50604d996404c4457d055
Ayush <80516839+Ayush8923@users.noreply.github.com>
2022-02-17 06:06:44 -1200

    Minor changes in project health page.

--- Modules/Project/Resources/views/subviews/create-project-health-details.php
+++ Modules/Project/Resources/views/subviews/create-project-health-details.php
@@ -9,56 +9,56 @@
             <div class="card-body">
                 <div class="form-row">
                     <div class="form-group col-md-12 d-flex mt-2">
-                        <label for="projectHealth"><h4>Staging URL</h4></label>
+                        <label for="staging_url"><h4>Staging URL</h4></label>
                         <input type="url" class="form-control col-md-6 ml-5" name="staging_url" id="staging_url">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-12 d-flex mt-3">
-                        <label for="projectHealth" class="lb-md"><h4>Training & Onboarding Documents</h4></label>
-                        <input type="url" class="form-control col-md-6 ml-5" name="onboarding_documents_url" id="staging_url">
+                        <label for="documents_url" class="lb-md"><h4>Training & Onboarding Documents</h4></label>
+                        <input type="url" class="form-control col-md-6 ml-5" name="onboarding_documents_url" id="documents_url">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20" name="has_issue_templates" value="0" />
                         <input type="checkbox" class="h-20 w-20" name="has_issue_templates" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">Issue templates</h4></label>
+                        <label for="issue_templates"><h4 class="ml-3">Issue templates</h4></label>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20" name="has_unit_testing" value="0" />
                         <input type="checkbox" class="h-20 w-20" name="has_unit_testing" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">Unit testing</h4></label>
+                        <label for="unit_testing"><h4 class="ml-3">Unit testing</h4></label>
                     </div> 
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20" name="has_ci_check" value="0" />
                         <input type="checkbox" class="h-20 w-20" name="has_ci_check" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">CI</h4></label>
+                        <label for="ci_check"><h4 class="ml-3">CI</h4></label>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20" name="has_site_monitoring" value="0" />
                         <input type="checkbox" class="h-20 w-20" name="has_site_monitoring" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">Site monitoring</h4></label>
+                        <label for="site_monitoring"><h4 class="ml-3">Site monitoring</h4></label>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20" name="has_error_logging" value="0" />
                         <input type="checkbox" class="h-20 w-20" name="has_error_logging" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">Error logging</h4></label>
+                        <label for="error_logging"><h4 class="ml-3">Error logging</h4></label>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-check mt-2">
                         <input type="hidden" class="h-20 w-20 " name="has_error_reporting" value="0" />
                         <input type="checkbox" class="h-20 w-20 " name="has_error_reporting" value="1" />
-                        <label for="flexCheckDefault"><h4 class="ml-3">Error reporting</h4></label>
+                        <label for="error_reporting"><h4 class="ml-3">Error reporting</h4></label>
                     </div>
                 </div>
             </div>
