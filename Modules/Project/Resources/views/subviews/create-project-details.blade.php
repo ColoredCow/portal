ad2a69f243c04c3f33ffd58edf0283b37c9f1e9e
Ayush <80516839+Ayush8923@users.noreply.github.com>
2022-03-14 04:25:51 -1200

    some changes are done.

--- Modules/Project/Resources/views/subviews/create-project-details.blade.php
+++ Modules/Project/Resources/views/subviews/create-project-details.blade.php
@@ -3,8 +3,7 @@
     <div class="form-row">
         <div class="form-group col-md-5">
             <label for="name" class="field-required">Name</label>
-            <input type="text" class="form-control" name="name" id="name" placeholder="Enter project name"
-                required="required" value="{{ old('name') }}">
+            <input type="text" class="form-control" name="name" id="name" placeholder="Enter project name" required="required" value="{{ old('name') }}">
         </div>
         <div class="form-group offset-md-1 col-md-5">
             <label for="client_id" class="field-required">Client</label>
@@ -20,8 +19,7 @@
         </div>
         <div class="form-group col-md-5">
             <label for="effort_sheet_url">{{ __('Effort Sheet URL') }}</label>
-            <input type="url" class="form-control" name="effort_sheet_url" id="effort_sheet_url"
-                placeholder="Enter effort sheet url" value="{{ old('effort_sheet_url') }}">
+            <input type="url" class="form-control" name="effort_sheet_url" id="effort_sheet_url" placeholder="Enter effort sheet url" value="{{ old('effort_sheet_url') }}">
         </div>
         <div class="form-group offset-md-1 col-md-5">
             <label for="project_type" class="field-required">{{ __('Project Type') }}</label>
@@ -37,16 +35,22 @@
         </div>
         <div class="form-group col-md-5">
             <label for="total_estimated_hours">{{ __('Total Estimated Hours') }}</label>
-            <input type="number" class="form-control" name="total_estimated_hours" id="total_estimated_hours"
-                placeholder="Enter total estimated hours" value="{{ old('total_estimated_hours') }}">
+            <input type="number" class="form-control" name="total_estimated_hours" id="total_estimated_hours" placeholder="Enter total estimated hours" value="{{ old('total_estimated_hours') }}">
         </div>
         <div class="form-group offset-md-1 col-md-5">
             <label for="monthly_estimated_hours">{{ __('Monthly Estimated Hours') }}</label>
-            <input type="number" class="form-control" name="monthly_estimated_hours" id="monthly_estimated_hours"
-                placeholder="Enter monthly estimated hours" value="{{ old('monthly_estimated_hours') }}">
+            <input type="number" class="form-control" name="monthly_estimated_hours" id="monthly_estimated_hours" placeholder="Enter monthly estimated hours" value="{{ old('monthly_estimated_hours') }}">
+        </div>
+        <div class="form-group col-md-5">
+            <label for="contract_file">Upload File</label>
+            <div class="custom-file mb-3">
+                <input type="file" id="contract_file" name="contract_file" class="custom-file-input" multiple>
+                <label for="contract" class="custom-file-label">Choose file</label>
+            </div>
         </div>
     </div>
-    <br>
+</div>
+<br>
 </div>
 <div class="card-footer">
     <button type="submit" class="btn btn-primary">Create</button>
