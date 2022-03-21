5832b75831b1185e84d2c0e00ca3c36411d0e36f
Ayush <80516839+Ayush8923@users.noreply.github.com>
2022-03-20 23:49:45 -1200

    CI checks are fixes.

--- Modules/Project/Resources/assets/js/app.js
+++ Modules/Project/Resources/assets/js/app.js
@@ -1,18 +1,18 @@
 $("#remove_contract_file").on("click", function() {
-  $("#upload_contract_file").remove();
+	$("#upload_contract_file").remove();
 });
 
 $("#add_contract_file").on("click", function() {
-  var addContractFile = `<div class="form-row col-12" id="upload_contract_file">
-  	<div class="form-group col-md-5">
-		<div class="custom-file mb-3">
-			<input type="text" id="contract_file" name="contract_file" class="custom-file-input">
-			<label for="contract" class="custom-file-label">Choose file</label>
+	var addContractFile = `<div class="form-row col-12" id="upload_contract_file">
+		<div class="form-group col-md-5">
+			<div class="custom-file mb-3">
+				<input type="text" id="contract_file" name="contract_file" class="custom-file-input">
+				<label for="contract" class="custom-file-label">Choose file</label>
+			</div>
 		</div>
-	</div>
-	<div class="col-4">
-		<button type="button" class="btn btn-danger btn-sm mt-1 ml-2 text-white fz-14" id="remove_contract_file">Remove</button>
-	</div>
-</div>`;
-  $("#upload_contract_file").append(addContractFile);
+		<div class="col-4">
+			<button type="button" class="btn btn-danger btn-sm mt-1 ml-2 text-white fz-14" id="remove_contract_file">Remove</button>
+		</div>
+	</div>`;
+	$("#upload_contract_file").append(addContractFile);
 });
