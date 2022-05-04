701feab397e556dec7110eb1f003b4e8a2d057ca
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-05-04 22:44:04 +0530

    Fix show contract option

--- Modules/Project/Resources/views/show.blade.php
+++ Modules/Project/Resources/views/show.blade.php
@@ -13,7 +13,7 @@
         </div>
         <div id="project_detail_form" class="collapse show">
             <div class="card-body">
-                @if($contractFileName)
+                @if($contractFilePath)
                     <div class="form-row ">
                         <div class="form-group col-md-6 pl-4 mt-2">
                             <h4 class="d-inline-block ">
@@ -21,8 +21,8 @@
                             </h4>
                             <div class="row gy-5">
                                 <div class="col-xs-6 py-2">
-                                    <div class="text-capitalize d-inline ml-2 fz-lg-20"> {{$contractFileName}} </div>
-                                    <a href="{{route('pdf.show', ['contractFileName' => $contractFileName])}}" target="_blank" class="btn btn-sm btn-primary text-white ml-4">View</a>
+                                    <div class="text-capitalize d-inline ml-2 fz-lg-20"> {{pathinfo($contractFilePath)['filename']}} </div>
+                                    <a href="{{route('pdf.show', ['contract' => $contract])}}" target="_blank" class="btn btn-sm btn-primary text-white ml-4">View</a>
                                 </div>
                             </div>
                         </div>
