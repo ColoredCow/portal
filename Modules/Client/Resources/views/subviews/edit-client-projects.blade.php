dbcb04e29727bf57c91056f2576aaf89c32347f0
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-05-06 00:44:00 +0530

    Add Project section in Clients

---
+++ Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div id="client_projects_container" class="collapse show">
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('client.index') }}" class="btn btn-primary btn-theme-gray-lighter mr-3">Exit</a>
+        </div>
+    </div>
+
+</div>
