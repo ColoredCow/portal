24a3758cc488e5b9b4fd47325f75428cfa56c238
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-05-06 01:10:31 +0530

    Show projects

--- Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
+++ Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
@@ -1,7 +1,35 @@
 <div class="card">
     <div id="client_projects_container" class="collapse show">
         <div class="card-body">
-
+            <div class="d-flex px-5">
+                <div class="w-33p ml-2">
+                    <h5 class="font-muli-bold">Name</h5>
+                </div>
+                <div class="w-40p ml-2">
+                    <h5 class="font-muli-bold">Team Members</h5>
+                </div>
+                <div class="w-20p ml-2">
+                    <h5 class="font-muli-bold">Details</h5>
+                </div>
+            </div>
+            <hr class="mt-0 mx-4">
+            @foreach ($projects as $project)
+                <div class="d-flex px-5 mt-5">
+                    <div class="w-33p ml-2">
+                        <h5 class="card-title">{{ $project->name }}</h5>
+                    </div>
+                    <div class="w-40p ml-2">
+                        @foreach($project->teamMembers ?:[] as $teamMember)
+                            <span class="tooltip-wrapper" data-html="true" data-toggle="tooltip" title="{{ $teamMember->name }} - {{ config('project.designation')[$teamMember->pivot->designation] }}">
+                                <img src="{{ $teamMember->avatar }}" class="w-35 h-30 rounded-circle mr-1 mb-1">
+                            </span>
+                        @endforeach 
+                    </div>
+                    <div class="w-20p ml-2">
+                        <a href="{{route('project.show', $project )}}">View Details</a>
+                    </div>
+                </div>
+            @endforeach
         </div>
         <div class="card-footer">
             <a href="{{ route('client.index') }}" class="btn btn-primary btn-theme-gray-lighter mr-3">Exit</a>
