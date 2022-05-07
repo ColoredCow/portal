95c36c7fd1406b3d59bc26294c2176705e33f603
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-05-07 19:43:52 +0530

    Make projects name clickable and also show status of project

--- Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
+++ Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
@@ -9,16 +9,20 @@
                     <h5 class="font-muli-bold">Team Members</h5>
                 </div>
                 <div class="w-20p ml-2">
-                    <h5 class="font-muli-bold">Details</h5>
+                    <h5 class="font-muli-bold">Project Status</h5>
                 </div>
             </div>
             <hr class="mt-0 mx-4">
             @foreach ($projects as $project)
                 <div class="d-flex px-5 mt-5">
                     <div class="w-33p ml-2">
-                        <h5 class="card-title">{{ $project->name }}</h5>
+                        <h5 class="card-title">
+                            <a href="{{route('project.show', $project )}}">
+                                {{ $project->name }}
+                            </a>
+                        </h5>
                     </div>
-                    <div class="w-40p ml-2">
+                    <div class="w-33p w-md-40p ml-2">
                         @foreach($project->teamMembers ?:[] as $teamMember)
                             <span class="tooltip-wrapper" data-html="true" data-toggle="tooltip" title="{{ $teamMember->name }} - {{ config('project.designation')[$teamMember->pivot->designation] }}">
                                 <img src="{{ $teamMember->avatar }}" class="w-35 h-30 rounded-circle mr-1 mb-1">
@@ -26,7 +30,7 @@
                         @endforeach 
                     </div>
                     <div class="w-20p ml-2">
-                        <a href="{{route('project.show', $project )}}">View Details</a>
+                        <h5 class="">{{ config('project.status')[$project->status] }}</h5>
                     </div>
                 </div>
             @endforeach
