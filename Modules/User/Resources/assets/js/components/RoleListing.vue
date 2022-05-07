e9feb7b1276856f83e4590bfe17eca7f7bdde5d0
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-05-07 19:38:07 +0530

    formatting fix

--- Modules/User/Resources/assets/js/components/RoleListing.vue
+++ Modules/User/Resources/assets/js/components/RoleListing.vue
@@ -1,65 +1,65 @@
 <template>
-    <div>
-        <table class="table table-bordered table-striped">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Role</th>
-                    <th>Permissions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(role, index) in allRoles" :key="index">
-                    <td width="50%">
-                        <p>{{ role.label }}</p>
-                        <p class="text-muted" style="font-size:12px;">{{ role.description }}</p>
-                    </td>
-                    <td>
-                        <button class="btn btn-sm btn-outline-info" @click="updatePermissionModal(index)" data-toggle="modal" data-target="#update_role_permissions_modal">Manage permissions</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+	<div>
+		<table class="table table-bordered table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th>Role</th>
+					<th>Permissions</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(role, index) in allRoles" :key="index">
+					<td width="50%">
+						<p>{{ role.label }}</p>
+						<p class="text-muted" style="font-size:12px;">{{ role.description }}</p>
+					</td>
+					<td>
+						<button class="btn btn-sm btn-outline-info" @click="updatePermissionModal(index)" data-toggle="modal" data-target="#update_role_permissions_modal">Manage permissions</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 
-        <role-permission-update-modal :updateRoute="this.updateRoute"  :role = "this.selectedRole" :permissions = "this.permissions" :rolePermissionsUpdated="onRolePressionsUpdated"/>
+		<role-permission-update-modal :updateRoute="this.updateRoute"  :role = "this.selectedRole" :permissions = "this.permissions" :rolePermissionsUpdated="onRolePressionsUpdated"/>
 
-    </div>
+	</div>
 </template>
 
 <script>
 export default {
-    props:[ "roles", "updateRoute", "permissions"],
+	props:[ "roles", "updateRoute", "permissions"],
 
-    data(){
-        return {
-            currentUserIndex: 0,
-            roleInputs: [],
-            allRoles: this.roles,
-            selectedRole:{}
-        };
-    },
+	data(){
+		return {
+			currentUserIndex: 0,
+			roleInputs: [],
+			allRoles: this.roles,
+			selectedRole:{}
+		};
+	},
 
-    methods: {
-        formatRoles(user) {
-            let roleNames = [];
-            let userRoles = user.roles;
-            for(var i in userRoles) {
-                let roleName = userRoles[i].label;
-                roleNames.push(roleName);
-            }
+	methods: {
+		formatRoles(user) {
+			let roleNames = [];
+			let userRoles = user.roles;
+			for(var i in userRoles) {
+				let roleName = userRoles[i].label;
+				roleNames.push(roleName);
+			}
 
-            return roleNames.join(", ");
-        },
+			return roleNames.join(", ");
+		},
 
-        updatePermissionModal: function(index) {
-            this.currentUserIndex = index;
-            this.selectedRole = this.roles[index];
-        },
+		updatePermissionModal: function(index) {
+			this.currentUserIndex = index;
+			this.selectedRole = this.roles[index];
+		},
 
-        onRolePressionsUpdated: function(selectedPermissions) {
-            Vue.set(this.selectedRole, "permissions",  selectedPermissions);
-        }
+		onRolePressionsUpdated: function(selectedPermissions) {
+			Vue.set(this.selectedRole, "permissions",  selectedPermissions);
+		}
 
 
-    }
+	}
 };
 </script>
