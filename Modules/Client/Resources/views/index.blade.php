6a5e1ca00cd75e178fa8103b20d40639fc2d83e1
gautam071 <gautam.rajes@coloredcow.in>
2022-05-16 12:08:37 +0530

    Added the fix for add client button" / /

--- Modules/Client/Resources/views/index.blade.php
+++ Modules/Client/Resources/views/index.blade.php
@@ -1,17 +1,22 @@
 @extends('project::layouts.master')
 @section('content')
+
+@if (session('success'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
 <div class="container" id="vueContainer">
-    @include('client::menu_header')
-    <br>
-    @if (session('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endif
-    <div class="d-flex justify-content-between mb-2">
+    @can('clients.create')
+    <div class="d-none d-md-flex justify-content-between my-2">
+        @include('client::menu_header')
+        <a href= "{{ route('client.create') }}" class="btn btn-info text-white">Add client</a>
+    </div>
+    @endcan
+    <div class="d-md-flex justify-content-between mt-5 mb-2">
         <h4 class="mb-1 pb-1">{{ config('client.status')[request()->input('status', 'active')] }} Clients ({{ $count }})</h4>
         <div>
             <form action="{{ route('client.index') }}" method="GET">
