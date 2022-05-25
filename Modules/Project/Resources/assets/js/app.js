d5e7046a1112398679550c8f7d26abc6da5b7cea
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-05-25 19:39:45 +0530

    Checks resolved

--- Modules/Project/Resources/assets/js/app.js
+++ Modules/Project/Resources/assets/js/app.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
 	});
 
 	$(".form-body").on("keyup", ".daily-effort",function(){
-	var daily_effort=$(this).val();
-	$(this).parent('.daily-effort-div').siblings('.weekly-effort-div').children('.weekly-effort').val(daily_effort*5);
+		var dailyEffort=$(this).val();
+		$(this).parent(".daily-effort-div").siblings(".weekly-effort-div").children(".weekly-effort").val(dailyEffort*5);
 	});
+
 	$(".form-body").on("keyup", ".weekly-effort",function(){
-	var weekly_effort=$(this).val();
-	$(this).parent('.weekly-effort-div').siblings('.daily-effort-div').children('.daily-effort').val(weekly_effort/5);
-	});
-    
+		var weeklyEffort=$(this).val();
+		$(this).parent(".weekly-effort-div").siblings(".daily-effort-div").children(".daily-effort").val(weeklyEffort/5);
+	});    
 });
