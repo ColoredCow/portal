b35f459c4b25b7e7fef500cf5452996b5010e38e
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-05-28 02:00:07 +0530

    Minor UI Fix

--- Modules/Project/Resources/views/show.blade.php
+++ Modules/Project/Resources/views/show.blade.php
@@ -15,16 +15,12 @@
             <div class="card-body">
                 @if($contractFilePath)
                     <div class="form-row ">
-                        <div class="form-group col-md-6 pl-4 mt-2">
+                        <div class="form-group pl-4 mt-2">
                             <h4 class="d-inline-block ">
                                 <label for="name" class="font-weight-bold">Project Contract:</label>
+                                <span class="text-capitalize d-inline ml-2 fz-lg-20"> {{pathinfo($contractFilePath)['filename']}} </span>
+                                <a href="{{route('pdf.show', ['contract' => $contract])}}" target="_blank" class="btn-sm btn-primary text-white ml-2">View</a>
                             </h4>
-                            <div class="row">
-                                <div class="col-xs-6 pt-2">
-                                    <div class="text-capitalize d-inline ml-2 fz-lg-20"> {{pathinfo($contractFilePath)['filename']}} </div>
-                                    <a href="{{route('pdf.show', ['contract' => $contract])}}" target="_blank" class="btn btn-sm btn-primary text-white ml-4">View</a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 @endif
@@ -113,7 +109,7 @@
                             </div>
                         </div>
                         <hr class="border mx-2">
-                        @foreach($project->getTeamMembers ?:[] as $teamMember)
+                        @forelse($project->getTeamMembers ?:[] as $teamMember)
                         <div class="mb-2 d-flex align-items-center">
                             <div class="w-30p">
                                 <span class="ml-1 tooltip-wrapper" data-html="true" data-toggle="tooltip" title="{{ $teamMember->user->name }} - {{ config('project.designation')[$teamMember->designation] }}">
@@ -133,8 +129,12 @@
                                 {{ $teamMember->current_fte }}
                             </div>
                         </div>
-                        <hr class="border mx-2">
-                        @endforeach
+                        <hr class="border mx-2">    
+                        @empty
+                        <div class="text-center my-5">
+                            <span class="font-weight-bold">{{ __('No Team Members') }}</span>
+                        </div>
+                        @endforelse
                     </div>
                 </div>
             </div>
