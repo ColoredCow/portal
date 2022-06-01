95eb297f903fe4364df795cbd1890d1f0e591a6f
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-06-01 20:00:58 +0530

    Created table to store alerts details and other meta for projects

---
+++ app/Models/ProjectMeta.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectMeta extends Model
+{
+    protected $table = "project_meta";
+    protected $guarded = [];
+    /**
+     * Get the project that has the alert detail or meta.
+     */
+
+}
