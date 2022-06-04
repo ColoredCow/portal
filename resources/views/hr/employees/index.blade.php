7ac0a79c89464a9076149f9c7ba2d3eb2972cb38
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-06-04 12:06:31 +0530

    minor fix

--- resources/views/hr/employees/index.blade.php
+++ resources/views/hr/employees/index.blade.php
@@ -39,7 +39,7 @@
             <td>
                 <a href="">{{ "View Projects" }}</a>
             </td>
-            <td class="{{ optional($employee->user)->fte ? ($employee->user->fte > 1 ? 'text-success' : 'text-danger') :'text-secondary' }} font-weight-bold">{{ optional($employee->user)->fte ?:'NA' }}</td>
+            <td class="{{ $employee->user ? ($employee->user->fte > 1 ? 'text-success' : 'text-danger') : 'text-secondary'}} font-weight-bold">{{ $employee->user ? $employee->user->fte :'NA' }}</td>
         </tr>
         @endforeach
     </table>
