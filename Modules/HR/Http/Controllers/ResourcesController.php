68848f85ca3945edf50253a3053d5b5c1b97733c
jagdish <jagdisharya73528@gmail.com>
2022-06-22 18:07:37 +0530

    changed

--- Modules/HR/Http/Controllers/ResourcesController.php
+++ Modules/HR/Http/Controllers/ResourcesController.php
@@ -8,12 +8,11 @@ use Modules\HR\Entities\Job;
 
 class ResourcesController extends Controller
 {
-    public function index()
-    {
-        $jobs = Job::select('title')->get();
-
-        return view('hr::guidelines-resources.index', compact('jobs'));   
-    }
+     public function index()
+     {
+         $jobs = Job::select('title')->get();   
+        return view('hr::guidelines-resources.index', compact('jobs'));
+     }
 
     public function show()
     {
