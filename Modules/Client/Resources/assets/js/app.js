972e015ae6803234e412d2fac7cbf01bdf3489a4
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-06-24 13:13:37 +0530

    Added billing date coloumn in billing details page in client module

--- Modules/Client/Resources/assets/js/app.js
+++ Modules/Client/Resources/assets/js/app.js
@@ -2,4 +2,23 @@ $(document).ready(function () {
 	$("input").on("change", function () {
 		this.value = (this.value).replace(/\s+/g, " ");
 	});
+
+	$("#billing_frequency").on("change", function() {
+
+  		if($("#billing_frequency option:selected").data('value') == 'Monthly') {
+			$(".dates").show();
+		}
+ 		else if($("#billing_frequency option:selected").data('value') == 'Quarterly') {
+			$(".dates").show();
+		}
+		else if($("#billing_frequency option:selected").data('value') == 'Yearly') {
+			$(".dates").show();
+		}
+ 		else if($("#billing_frequency option:selected").data('value') == 'Net 15 days') {
+			$(".dates").hide();
+		}
+		else if($("#billing_frequency option:selected").data('value') == 'Based on project terms') {
+			$(".dates").hide();
+		}
+	});
 });
