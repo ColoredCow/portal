ebb71793e816864d41df71e0058b32e5084e8fe6
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-06-24 14:52:34 +0530

    checks resolved

--- Modules/Client/Resources/assets/js/app.js
+++ Modules/Client/Resources/assets/js/app.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
 		this.value = (this.value).replace(/\s+/g, " ");
 	});
 	$("#billing_frequency").on("change", function() {
-		$dateAvailableForValue = ['Monthly', 'Quaterly', 'Yearly'];
-  			if($dateAvailableForValue.includes($("#billing_frequency option:selected").data("value"))) {
-				$(".dates").show();
-				return;
-			}
-			$(".dates").hide();
+		$dateAvailableForValue = ["Monthly", "Quaterly", "Yearly"];
+  		if($dateAvailableForValue.includes($("#billing_frequency option:selected").data("value"))) {
+			$(".dates").show();
+			return;
+		}
+		$(".dates").hide();
 	});
 });
