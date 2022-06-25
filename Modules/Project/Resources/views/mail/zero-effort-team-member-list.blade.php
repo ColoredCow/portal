5e1e046d14c275cb0129f85d9e2e5b80f5d647c5
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-06-25 15:25:16 +0530

    fix indentation commit

--- Modules/Project/Resources/views/mail/zero-effort-team-member-list.blade.php
+++ Modules/Project/Resources/views/mail/zero-effort-team-member-list.blade.php
@@ -1,29 +1,29 @@
 <div>
-  <style>
-    .line {
-      line-height: 1px;
-    }
-  </style>
-    <p>Hello {{ $projectManager['name'] }},</p>
-    <p>We found some projects where the expected hours are zero for you or team members where you are assigned as project manager. Please update these projects:</p>
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Project Name</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($projectManager['projects'] as $project)
-            <tr>
-                <td>
-                  <li>{{ $project->name }}</li>
-                </td>
-            </tr>
-             @endforeach
-        </tbody>
-      </table>
-    <br>
-    <p class="line">Thanks,</p>
-    <p class="line">Portal Team</p>
-    <p class="line">ColoredCow</p>
+	<style>
+		.line {
+		line-height: 1px;
+		}
+	</style>
+	<p>Hello {{ $projectManager['name'] }},</p>
+	<p>We found some projects where the expected hours are zero for you or team members where you are assigned as project manager. Please update these projects:</p>
+	<table class="table">
+		<thead>
+		  <tr>
+			<th scope="col">Project Name</th>
+		  </tr>
+		</thead>
+		<tbody>
+			@foreach ($projectManager['projects'] as $project)
+				<tr>
+					<td>
+					<li>{{ $project->name }}</li>
+					</td>
+				</tr>
+			 @endforeach
+		</tbody>
+	  </table>
+	<br>
+	<p class="line">Thanks,</p>
+	<p class="line">Portal Team</p>
+	<p class="line">ColoredCow</p>
 </div>
