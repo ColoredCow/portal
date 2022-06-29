ffb847154da45fad0239c7245b0ca087c30ee170
Aman <amanbahuguna009@gmail.com>
2022-06-29 12:20:38 +0530

    Indendation fixes

--- Modules/HR/Http/Controllers/Recruitment/ApplicantController.php
+++ Modules/HR/Http/Controllers/Recruitment/ApplicantController.php
@@ -109,7 +109,7 @@ class ApplicantController extends Controller
 
         return redirect()->back()->with('status', 'Your application is put on hold successfully');
     }
-    
+
     public function applicantEmailVerification($applicantEmail, $applicationID)
     {
         $application = Application::find($applicationID);
