3af4591651ba2d9dba27c71f197e80a4b28bf7a3
jagdish <jagdisharya73528@gmail.com>
2022-06-30 17:29:06 +0530

    ci fixed

--- Modules/HR/Http/Controllers/ResourcesController.php
+++ Modules/HR/Http/Controllers/ResourcesController.php
@@ -12,7 +12,7 @@ class ResourcesController extends Controller
     public function index()
     {
         $jobs = Job::all();
-        
+                
         return view('hr::guidelines-resources.index', compact('jobs'));
     }
 
