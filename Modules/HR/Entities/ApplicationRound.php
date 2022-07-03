674dfd1e925de0cfb8960b322f1053096e45a31a
Aman <amanbahuguna009@gmail.com>
2022-07-03 17:48:13 +0530

    Minor changes

--- Modules/HR/Entities/ApplicationRound.php
+++ Modules/HR/Entities/ApplicationRound.php
@@ -156,25 +156,25 @@ class ApplicationRound extends Model
                 $application->onHold();
 
                 ApplicationMeta::create([
-                    'hr_application_id' => $application->id,
-                    'key' => 'on-hold',
-                    'value' => json_encode([
-                    'on-hold_by' => $fillable['conducted_person_id']])
-                    ]);
+                'hr_application_id' => $application->id,
+                'key' => 'on-hold',
+                'value' => json_encode([
+                'on-hold_by' => $fillable['conducted_person_id']])
+                ]);
 
-                    $applicant = $application->applicant;
+                $applicant = $application->applicant;
 
-                    $subject = Setting::where('module', 'hr')->where('setting_key', 'application_on_hold_subject')->first();
-                    $body = Setting::where('module', 'hr')->where('setting_key', 'application_on_hold_body')->first();
-                    $job_title = Job::find($application->hr_job_id)->title;
-                    $body->setting_value = str_replace(config('constants.hr.template-variables.applicant-name'), $applicant->name, $body->setting_value);
-                    $body->setting_value = str_replace(config('constants.hr.template-variables.job-title'), $job_title, $body->setting_value);
+                $subject = Setting::where('module', 'hr')->where('setting_key', 'application_on_hold_subject')->first();
+                $body = Setting::where('module', 'hr')->where('setting_key', 'application_on_hold_body')->first();
+                $job_title = Job::find($application->hr_job_id)->title;
+                $body->setting_value = str_replace(config('constants.hr.template-variables.applicant-name'), $applicant->name, $body->setting_value);
+                $body->setting_value = str_replace(config('constants.hr.template-variables.job-title'), $job_title, $body->setting_value);
 
-                    Mail::to($applicant->email, $applicant->name)
-                        ->send(new OnHold($subject->setting_value, $body->setting_value));
+                Mail::to($applicant->email, $applicant->name)
+                    ->send(new OnHold($subject->setting_value, $body->setting_value));
 
-                    return redirect()->route('applications.job.index');
-                    break;
+                return redirect()->route('applications.job.index');
+                break;
 
             case 'send-for-approval':
                 $application->untag('new-application');
