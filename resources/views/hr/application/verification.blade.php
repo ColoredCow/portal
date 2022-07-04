5c98da0220b3e5d8e9f168699dc917f221d32bd6
Pankaj Kandpal <pankaj.kandpal@coloredcow.in>
2022-07-04 23:32:53 +0530

    code cleanups

--- resources/views/hr/application/verification.blade.php
+++ resources/views/hr/application/verification.blade.php
@@ -7,7 +7,7 @@
 		<div class="mt-9">
 			<h4 class="fz-md-36 fz-28">Hello {{ $application->applicant->name }} 👋</h4>
 			<p class="mt-10 fz-md-28 fz-24 leading-30"><b><i>Thanks for the verification 🙌</i></b></p>
-			<p class="mt-5">We'll be in touch via emails 📩 here onwards. In fact we've sent one with  useful resources to help clear the interviews</p>
+			<p class="mt-5">We'll be in touch via emails 📩 here onwards. In fact we've sent one with useful resources to help clear the interviews</p>
 			{{-- We need to make this email dynamic, it will be fetched from config in future --}}
 			<p class="mt-5">Queries? 🤔  Write at<a href="mailto:pankaj.kandpal@coloredcow.in"> pankaj.kandpal@coloredcow.in</a></p>
 		</div>
