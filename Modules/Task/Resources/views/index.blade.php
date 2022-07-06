bcdb1a62cc8d3790caf80cf35d21d754b3165bff
Vivek Negi <user@coloredcow.com>
2022-07-06 15:27:26 +0530

    Centralised Text

--- Modules/Task/Resources/views/index.blade.php
+++ Modules/Task/Resources/views/index.blade.php
@@ -1,8 +1,16 @@
 @extends('task::layouts.master')
 
 @section('content')
-    <div class="content">
-        <h1>Coming Soon!!</h1>
-    </div>
-
+<style>
+    div.content{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+</style>
+        <div class="content">
+            <h1>Coming Soon!!</h1>
+        </div>
 @endsection
