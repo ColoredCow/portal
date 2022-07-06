d8a652c21b61741db724ddb85084d33ba612a81d
abhishekkhanduri <68751333+abhishekkhanduri@users.noreply.github.com>
2022-07-06 16:18:22 +0530

    checks resolved

--- Modules/Invoice/Services/InvoiceService.php
+++ Modules/Invoice/Services/InvoiceService.php
@@ -249,8 +249,7 @@ class InvoiceService implements InvoiceServiceContract
     public function invoiceDetails($filters)
     {
         $invoices = Invoice::query()->applyFilters($filters)
-            ->orderBy('sent_on', 'desc');
-            
+            ->orderBy('sent_on', 'desc');            
 
         $igst = [];
         $cgst = [];
@@ -259,10 +258,10 @@ class InvoiceService implements InvoiceServiceContract
         $clientAddress = [];
         foreach ($invoices->get() as $invoice) :
             $clients[] = Client::select('*')->where('id', $invoice->client_id)->first();
-            $clientAddress[] = ClientAddress::select('*')->where('client_id', $invoice->client_id)->first();
-            $igst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.igst')) / 100;
-            $cgst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.cgst')) / 100;
-            $sgst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.sgst')) / 100;
+        $clientAddress[] = ClientAddress::select('*')->where('client_id', $invoice->client_id)->first();
+        $igst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.igst')) / 100;
+        $cgst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.cgst')) / 100;            
+        $sgst[] = ((int) $invoice->display_amount * (int) config('invoice.invoice-details.sgst')) / 100;
         endforeach;
 
         return [
