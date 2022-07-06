512c45f784848e19533c7a6409487267b281b2a2
SHARDUL <shardulsgusain1@gmail.com>
2022-07-06 11:30:31 +0530

    Removed the function and Implemented in the tag itself

--- Modules/HR/Resources/views/guidelines-resources/create-category-modal.blade.php
+++ Modules/HR/Resources/views/guidelines-resources/create-category-modal.blade.php
@@ -8,8 +8,8 @@
                 <form method="POST" action="{{ route('resources.store') }}" id="create-form">
                     @csrf
                     <div class="mb-3">
+                        <input type="text" onkeyup="if(this.value.length > 0) document.getElementById('save-btn-action').disabled = false; else document.getElementById('save-btn-action').disabled = true;"/>
                         <label for="name" class="form-label">Category Name<strong class="text-danger">*</strong></label>
-                        <input type="text" class="form-control" id="name" onkeyup="ButtonEnable()" name="name" required>
                     </div>
                 </form>
             </div>
