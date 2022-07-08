1550d9b2d32685d702142726b1d7e6ee47227713
amisha bisht <amishabisht33@gmail.comgit status>
2022-07-08 15:17:11 +0530

    Fixed the miss-alignment of UI dashboard

--- resources/js/components/Dashboard/UserDashboardInvoice.vue
+++ resources/js/components/Dashboard/UserDashboardInvoice.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="card">
-    <div class="card-header p-1">
-      <h3 class="text-center"><a href="/invoice">Invoices</a></h3>
-    </div>
-    <div class="card-body pt-3" style="height: 18em;overflow: auto;">
-      <ul class="list list-group unstyled-list">
-        <li style="font-size:16px;">
-          <div class="text-secondary"> Pending invoices </div>
-          <h4 v-text="sentInvoiceCount"></h4>
-        </li>
-        <hr class="mt-1 w-full" />
-      </ul>
-    </div>
-  </div>
+    <div class = "media">
+		<div class="card">
+			<div class="card-header p-1">
+			<h3 class="text-center"><a href="/invoice">Invoices</a></h3>
+			</div>
+			<div class="md-0">
+				<div class="card-body pt-3 h-325 w-md-389 w-389 overflow-y-scroll">
+					<ul class="list list-group unstyled-list">
+						<li style="font-size:16px;">
+						<div class="text-secondary"> Pending invoices </div>
+						<h4 v-text="sentInvoiceCount"></h4>
+						</li>
+						<hr class="mt-1" />
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
-
 <script>
 export default {
 	props: [],
