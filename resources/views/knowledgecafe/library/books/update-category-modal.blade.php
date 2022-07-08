25cdf912a571c47803dd53dae354d6bb0f50ac8c
abhisheknegi <negiabhishek253@gmail.com>
2022-07-08 15:12:18 +0530

    features/1723/UI-correction-on-update-category-of-books

--- resources/views/knowledgecafe/library/books/update-category-modal.blade.php
+++ resources/views/knowledgecafe/library/books/update-category-modal.blade.php
@@ -26,8 +26,8 @@
                             <li class="list-group-item">
                                 <span>
                                     <div class="d-flex justify-content-between">
-                                        <input class="form-control mr-3 w-200" type="text" v-model="newCategory" placeholder="Enter New Category" autofocus>
-                                        <button type="button" class="btn btn-info btn-sm "  @click="addNewCategory()">  Add New </button>
+                                        <input class="form-control mr-3" type="text" v-model="newCategory" placeholder="Enter New Category" autofocus>
+                                        <button type="button" class="btn btn-info btn-sm w-200"  @click="addNewCategory()">  Add New </button>
                                         
                                     </div>
                                 </span>
