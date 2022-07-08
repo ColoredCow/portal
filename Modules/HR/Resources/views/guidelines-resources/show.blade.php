b952951213686acc6b4179ff9d521f7e2779de12
jagdish <jagdisharya73528@gmail.com>
2022-07-08 11:46:11 +0530

    show categries in table

--- Modules/HR/Resources/views/guidelines-resources/show.blade.php
+++ Modules/HR/Resources/views/guidelines-resources/show.blade.php
@@ -5,7 +5,7 @@
         <h3>Resources for Laravel Developer</h3>
     </div>
     <div class="col-md-2">
-        <button type="button"class="btn btn-primary btn-block btn-lg" data-bs-toggle="modal" data-bs-target="#create-Modal"><i class="fa fa-plus"></i>
+        <button type="button" class="btn btn-primary btn-block btn-lg" data-bs-toggle="modal" data-bs-target="#create-Modal"><i class="fa fa-plus"></i>
         {{ __('Add Resources') }}
         </button>
     </div>
@@ -16,9 +16,29 @@
     </div>
 </div>
 <div class="container mx-15 mt-5">
-    <p>No resource exist for this job. </p>
+    <table class="table table-bordered table-striped">
+        <thead class="thead-primary">
+            <tr>
+                <th>Sno</th>
+                <th>Categories</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($categories as $category)
+                <tr>
+                    <td>{{ $category->id }}</td>
+                    <td>
+                        <a href="#">{{ $category->name }}</a>  
+                    </td>
+                    <td>
+                        <a class="pr-1 btn btn-link" href="#"><i class="text-success fa fa-edit fa-lg"></i></a>
+                        <button type="submit" class="pl-1 btn btn-link"><i class="text-danger fa fa-trash fa-lg"></i></button>
+                    </td>
+            @endforeach
+        </tbody>
+    </table>
 </div>
-<!-- Functionality yet to be created -->
 @include('hr::guidelines-resources.create-category-modal')
 @include('hr::guidelines-resources.create-resources-modal')
 @endsection
