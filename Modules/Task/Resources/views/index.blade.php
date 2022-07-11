dbac530b1916b79c06c907e4266704de39acb748
Vivek Negi <100990660+viveknegi84@users.noreply.github.com>
2022-07-11 13:02:53 +0530

    Update index.blade.php

--- Modules/Task/Resources/views/index.blade.php
+++ Modules/Task/Resources/views/index.blade.php
@@ -1,16 +1,7 @@
 @extends('task::layouts.master')
 
 @section('content')
-<style>
-    div.content{
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-</style>
-        <div class="content">
+        <div class="task-content">
             <h1>Coming Soon!!</h1>
         </div>
 @endsection
