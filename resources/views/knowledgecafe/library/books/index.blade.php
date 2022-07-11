ef656ddaa4284b569599ada8ba90220f84e10b91
akriti12negi <negiakriti12@gmail.com>
2022-07-11 17:14:47 +0530

    change in color

--- resources/views/knowledgecafe/library/books/index.blade.php
+++ resources/views/knowledgecafe/library/books/index.blade.php
@@ -68,8 +68,28 @@
                                 <ul class="dropdown-menu ">
                                     <li @click="updateCategoryMode(index)" data-toggle="modal" data-target="#update_category_modal" class="dropdown-item">Update Category</li>
                                     <li @click="updateCopiesCount(index)" class="dropdown-item">Copies Available</li>
-                                    <li @click="deleteBook(index)" class="dropdown-item text-danger">Delete</li>
+                                    <li data-toggle="modal" data-target="#exampleModal" class="dropdown-item text-danger">Delete</li>
                                 </ul>
+                            </div>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">portal.says</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                <p>Are you sure?</p>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="button" class="btn btn-danger">Yes</button>
+                                </div>
+                            </div>
                             </div>
                         </div>
                     @endcan
