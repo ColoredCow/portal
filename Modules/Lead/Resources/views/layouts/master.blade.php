fb489a4de82e22e0064732a7a89b82863dd5f978
Sachin Bisht <sachinbisht27@gmail.com>
2022-07-12 18:06:38 +0530

    Add coming soon template to Leads page

--- Modules/Lead/Resources/views/layouts/master.blade.php
+++ Modules/Lead/Resources/views/layouts/master.blade.php
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Lead</title>
-
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/lead.css') }}"> --}}
-
-    </head>
-    <body>
-        @yield('content')
-
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/lead.js') }}"></script> --}}
-    </body>
-</html>
-=======
 @extends('layouts.app')
 @section('js_scripts')
 {{-- <script src="{{ mix('/js/user.js') }}" defer></script> --}}
@@ -27,5 +6,3 @@
 @section('css_scripts')
 {{-- <link href="{{ mix('/css/user.css') }}" rel="stylesheet"> --}}
 @endsection
-
->>>>>>> Stashed changes
