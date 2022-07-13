599cd8c6a3e3c73419916a8b79f5eef0dd890365
Sachin Bisht <sachinbisht27@gmail.com>
2022-07-13 17:19:23 +0530

    Add coming soon template to Leads page

--- Modules/Lead/Resources/views/index.blade.php
+++ Modules/Lead/Resources/views/index.blade.php
@@ -1,9 +1,6 @@
 @extends('lead::layouts.master')
 
 @section('content')
-    <h1>Coming Soon!!</h1>
+<p class="text-center">Coming Soon!!</p>
+<h1></h1>
 @endsection
-<style>
-    h1 {text-align: center;}
-    p {text-align: center;}
-</style>    
