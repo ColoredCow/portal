3955aca849de32849e6866c1b91f9c360aaa9216
Chetan <chetanbhatt1618@gmail.com>
2022-07-13 18:36:42 +0530

    HR report analytics heading alignment fix

--- resources/views/hr/recruitment/reports.blade.php
+++ resources/views/hr/recruitment/reports.blade.php
@@ -4,18 +4,20 @@
 
 <div class="chart-container" align="center">
     <div class="bar-chart-container">
-        <div align="left">
+        <div align="left" class="report-heading">
          <h2>Analytics </h2>  
         </div>
-        <form action="{{route('recruitment.report')}}" method="POST" align="right">
-            {{csrf_field()}}
-            <input type="date" name="report_start_date" id='StartDate'> to
-            <input type="date" name="report_end_date" id="EndDate">
-            <input type="submit" class="btn btn-sm btn-primary text-white" value="View">
-        </form>
+        <div class="report-date-filter-form">
+            <form action="{{route('recruitment.report')}}" method="POST" align="right">
+                {{csrf_field()}}
+                <input type="date" name="report_start_date" id='StartDate'> to
+                <input type="date" name="report_end_date" id="EndDate">
+                <input type="submit" class="btn btn-sm btn-primary text-white" value="View">
+            </form>
+        </div>
         <br>
 
-        <div class="card">
+        <div class="card report-detail-section">
             <div class="card-header" align='left'>
                 <span class="chart-heading mr-5">Application Received</span><span class="total-chart-count">Today's Count:<?php echo $todayCount ?></span>
             </div>
