b39068e6ab44b5a6b72befecdce94337566318d7
Lakshya Varshney <lakshya.varshney_iot18@gla.ac.in>
2022-07-14 20:35:34 +0530

    made required changes

--- Modules/HR/Database/Migrations/2022_07_14_124838_update_hr_applications_with_hr_channel.php
+++ Modules/HR/Database/Migrations/2022_07_14_124838_update_hr_applications_with_hr_channel.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateHrApplicationsWithHrChannels extends Migration
+class UpdateHrApplicationsWithHrChannel extends Migration
 {
     /**
      * Run the migrations.
--- Modules/HR/Database/Migrations/2022_07_14_144257_add_soft_delete_column_to_hr_channels.php
+++ Modules/HR/Database/Migrations/2022_07_14_144257_add_soft_delete_column_to_hr_channels.php
@@ -26,6 +26,7 @@ class AddSoftDeleteColumnToHrChannels extends Migration
     public function down()
     {
         Schema::table('hr_channels', function (Blueprint $table) {
+            
             $table->dropSoftDeletes();    
         });
     }
