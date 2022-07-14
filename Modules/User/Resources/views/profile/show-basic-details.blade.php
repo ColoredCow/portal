17f5687a21b69fc4eeccfc9c9539f092b6600291
rishabhnegi <negirishabh28gmail.com>
2022-07-14 19:38:29 +0530

    correct profile ui alignment

--- Modules/User/Resources/views/profile/show-basic-details.blade.php
+++ Modules/User/Resources/views/profile/show-basic-details.blade.php
@@ -1,27 +1,25 @@
 <div class="card">
-    <div class="card-body">
-        <div class="text-right">
-<p class="btn btn-info">Edit</p>
-</div>
-
-<div class="d-flex mt-4">
-<div class="mr-10">
-    <div class="rounded w-200">
-        <img class="rounded w-100p" src="http://portal.test/images/default_profile.png" alt="">
+    <div class="d-flex mt-4">
+        <div class="mr-10">
+            <div class="rounded w-200">
+                <img class="rounded w-100p" src="http://portal.test/images/default_profile.png"alt>
+            </div>
+        </div>
+    <div>
+        <div class="form-group">
+            <label class="font-weight-bold" for>Name:</label>
+            <span>coloredcow user</span>
+        </div>
+            <div class="form-group">
+                <label class="font-weight-bold" for>Email:</label>
+                <span>user@coloredcow.com</span>
+            </div>
+        </div>
+        <div class="card-body" style="padding-top: 0;">
+            <div class="text-right">
+                <p class="btn btn-info">EDIT</p>
+            </div>
+        </div>
     </div>
 </div>
-
-<div>
-    <div class="form-group">
-        <label class="font-weight-bold" for="">Name:</label>
-        <span>ColoredCow User</span>
-    </div>
-    <div class="form-group">
-        <label class="font-weight-bold" for="">Email:</label>
-        <span>user@coloredcow.com</span>
-    </div>
-
-  
-</div>
-</div>                                </div>
-</div>
+        
