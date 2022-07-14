683eb94b9d2026784b0fc9b8bd229780f8126115
Lakshya Varshney <lakshya.varshney_iot18@gla.ac.in>
2022-07-14 19:32:41 +0530

    changes2

--- Modules/HR/Database/Seeders/HrChannelSeederTableSeeder.php
+++ Modules/HR/Database/Seeders/HrChannelSeederTableSeeder.php
@@ -21,11 +21,13 @@ class HrChannelSeederTableSeeder extends Seeder
                 'name'=>'LinkedIn',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],[
+            
+            ], [
                 'name'=>'Indeed',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],[
+            
+            ], [
                 'name'=>'Website',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
