888a2e5569477d5340e81267d87978584a38254c
Sachin Bisht <sachinbisht27@gmail.com>
2022-07-15 13:48:33 +0530

    Add coming soon template to Leads page

--- Modules/Lead/Resources/views/index.blade.php
+++ Modules/Lead/Resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('lead::layouts.master')
 
 @section('content')
 <div class="task-content">
--- Modules/Lead/Resources/views/layouts/master.blade.php
+++ Modules/Lead/Resources/views/layouts/master.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 @section('js_scripts')
-{{-- <script src="{{ mix('/js/user.js') }}" defer></script> --}}
 @endsection
 
 @section('css_scripts')
-{{-- <link href="{{ mix('/css/user.css') }}" rel="stylesheet"> --}}
 @endsection
