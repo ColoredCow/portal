fd7d8b08d72c17d9f71d25ac7b2915964c2c5ddf
LavishRawat <user@colouredcow.com>
2022-07-15 15:29:28 +0530

    add_a_count_of_total_verified_applications

--- resources/views/hr/recruitment/reports.blade.php
+++ resources/views/hr/recruitment/reports.blade.php
@@ -6,7 +6,7 @@
     <div class="bar-chart-container">
         <div align="left">
          <span class="chart-heading mr-5">Analytics</span>
-            <span class="total-chart">Total Verified Applications:<?php echo $i ?></span>
+            <span class="total-chart">Total Verified Applications: <?php echo $i ?></span>
         </div>
         <form action="{{route('recruitment.report')}}" method="POST" align="right">
             {{csrf_field()}}
