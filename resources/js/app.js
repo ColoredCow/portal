ab74564f1509d84364295e7f39c97bc9c4c34725
Shardul <shardulsgusain1@gmail.com>
2022-07-15 15:36:15 +0530

    Solved ES lint errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -962,12 +962,12 @@ $(function(){
 	});
 });
 function check_save(){
-	if ($(this).val().length == 0){
-	  $("#save-btn-action").attr("disabled", true);
-	} else{
-	  $("#save-btn-action").removeAttr("disabled");
+	 if ($(this).val().length == 0){
+	     $("#save-btn-action").attr("disabled", true);
+	}  else{
+	     $("#save-btn-action").removeAttr("disabled");
 	}
-  }
+}
 function showCommentBlock() {
 	  var blockId = $(this).data("block-id");
 	  $(blockId).removeClass("d-none").find("input").focus();
