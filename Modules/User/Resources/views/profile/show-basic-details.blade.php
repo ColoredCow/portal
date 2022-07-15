e0c49c15ded042ea4f12f9741add0c56512beb46
rishabhnegi <negirishabh28gmail.com>
2022-07-15 18:06:45 +0530

    fix the user profile UI

--- Modules/User/Resources/views/profile/show-basic-details.blade.php
+++ Modules/User/Resources/views/profile/show-basic-details.blade.php
@@ -1,29 +1,21 @@
-<div class="card">
-    <div class="d-flex mt-4">
-        <div class="mr-10">
-            <div class="rounded w-200">
-                <img class="rounded w-100p" src="http://portal.test/images/default_profile.png"alt>
-            </div>
+<div class="d-flex justify-content-between">
+    <div class="d-flex">
+        <div class="rounded w-200 mr-10">
+            <img class="rounded w-100p" src="{{ $user->avatar }}" alt="">
         </div>
-    <div>
-        <div class="form-group">
-            <label class="font-weight-bold" for>Name:</label>
-            <span>
-                <input type="type your name" placeholder="name">
-            </span>
-        </div>
+        <div>
             <div class="form-group">
-                <label class="font-weight-bold" for>Email:</label>
-                <span>
-                    <input type="type your username" placeholder="username">
-                </span>
+                <label class="font-weight-bold" for="">Name:</label>
+                <span>{{ $user->name }}</span>
             </div>
-        </div>
-        <div class="card-body" style="padding-top: 0;">
-            <div class="text-right">
-                <p class="btn btn-info">EDIT</p>
+            <div class="form-group">
+                <label class="font-weight-bold" for="">Email:</label>
+                <span>{{ $user->email }}</span>
             </div>
+            @includeWhen($user->profile, 'user::profile.subviews.show-user-profile-info')
         </div>
     </div>
+    <div>
+        <button class="btn btn-info">Edit</button>
+    </div>
 </div>
-        
