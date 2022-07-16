701d80118ef432cadbc1362d7aa22d3899f8f01d
Junaid <junaidqureshi1010@gmail.com>
2022-07-16 17:59:03 +0530

    Close button is added

--- Modules/HR/Resources/views/guidelines-resources/create-category-modal.blade.php
+++ Modules/HR/Resources/views/guidelines-resources/create-category-modal.blade.php
@@ -3,6 +3,9 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Add Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
             </div>
             <div class="modal-body">
                 <form method="POST" action="{{ route('resources.store') }}" id="create-form">
