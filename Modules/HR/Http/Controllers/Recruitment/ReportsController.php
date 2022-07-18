36ca9e4be5f3af228f58e4695deda9b8fa0cdf34
abhisheknegi <negiabhishek253@gmail.com>
2022-07-18 08:10:28 +0530

    bugfix_when_clicking_on_view_button_avilable_in_analytics

--- Modules/HR/Http/Controllers/Recruitment/ReportsController.php
+++ Modules/HR/Http/Controllers/Recruitment/ReportsController.php
@@ -50,8 +50,8 @@ class ReportsController extends Controller
             \DB::raw('MONTHNAME(created_at) as month_created_at'),
             \DB::raw('DATE(created_at) as date_created_at')
         )
-            ->where('created_at',$req->report_start_date,Carbon::getWeekStartsAt())
-            ->where('created_at',$req->report_end_date,Carbon::getWeekEndsAt())
+            ->where('created_at',$req->Carbon::getWeekStartsAt())
+            ->where('created_at',$req->Carbon::getWeekEndsAt())
             ->groupBy('date_created_at', 'month_created_at')
             ->orderBy('date_created_at', 'ASC')
             ->get();
