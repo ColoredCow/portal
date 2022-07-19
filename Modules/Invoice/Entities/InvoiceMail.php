45ce8d46fe9987438df2aed21e1e531f5d683383
amisha bisht <amishabisht33@gmail.comgit status>
2022-07-19 16:50:29 +0530

    passed the CI checks

--- Modules/Invoice/Entities/InvoiceMail.php
+++ Modules/Invoice/Entities/InvoiceMail.php
@@ -4,7 +4,6 @@ namespace Modules\Invoice\Entities;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Modules\Invoice\Entities\Invoice;
 
 class InvoiceMail extends Model
 {
