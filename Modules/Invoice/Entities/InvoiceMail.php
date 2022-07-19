c85b637f9b81f23b1b2bf3dde2370cbd38b073da
amisha bisht <amishabisht33@gmail.comgit status>
2022-07-19 16:58:19 +0530

    passed the CI checks

--- Modules/Invoice/Entities/InvoiceMail.php
+++ Modules/Invoice/Entities/InvoiceMail.php
@@ -10,7 +10,7 @@ class InvoiceMail extends Model
     use HasFactory;
     protected $table = 'invoice_mails';
     protected $guarded = [];
-  
+
     public function Invoice()
     {
         return $this->belongsTo(Invoice::class);
