df6fc1bab018ba906b2fc6fe66268cfd6d86a604
shubham belwal <belwalshubham08@gmail.com>
2022-07-21 11:16:02 +0530

    minor changes

--- app/Mail/sendEmail.php
+++ app/Mail/sendEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class sendEmail extends Mailable
+class sendEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
