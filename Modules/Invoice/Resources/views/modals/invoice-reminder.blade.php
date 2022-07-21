5e2736a22e58ff598dc98a4dce4347279d8c5a2e
vivek uniyal <vivekuniyal62@gmail.com>
2022-07-21 15:41:49 +0530

    Validation issue fixed in CC and BCC field

--- Modules/Invoice/Resources/views/modals/invoice-reminder.blade.php
+++ Modules/Invoice/Resources/views/modals/invoice-reminder.blade.php
@@ -31,7 +31,7 @@
                                     <i class="fa fa-question-circle"></i>
                                 </span>
                             </label>
-                            <input type="text" name="cc" id="cc" class="form-control" value="{{ config('invoice.mail.send-invoice.email') }}">
+                            <input type="email" name="cc" id="cc" class="form-control" value="{{ config('invoice.mail.send-invoice.email') }}">
                         </div>
                         <div class="form-group col-md-12">
                             <label class="leading-none" for="bcc">
@@ -40,7 +40,7 @@
                                     <i class="fa fa-question-circle"></i>
                                 </span>
                             </label>
-                            <input type="text" name="bcc" id="bcc" class="form-control" value="">
+                            <input type="email" name="bcc" id="bcc" class="form-control" value="">
                         </div>
                         <div class="form-group col-md-12">
                             <label class="leading-none" for="emailSubject">{{ __('Subject') }}</label>
