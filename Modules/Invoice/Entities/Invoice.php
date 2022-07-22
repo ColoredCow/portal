c90c75375bb8fd6730ab2ed50e7e78ff1436210d
amisha bisht <amishabisht33@gmail.comgit status>
2022-07-22 13:19:24 +0530

    passed the CI checks

--- Modules/Invoice/Entities/Invoice.php
+++ Modules/Invoice/Entities/Invoice.php
@@ -200,7 +200,7 @@ class Invoice extends Model
         return $this->invoiceMail()->where('type', config('invoice.mail-type.payment-confirmation.slug'))
         ->first();
     }
-    
+
     public function invoiceOfInvoiceTypeMail()
     {
         return $this->invoiceMail()->where('type', config('invoice.mail-type.invoice.slug'))
