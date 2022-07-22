839a08205a0fc5a7e50a7479dacb22e5a8970bc7
jagdish <jagdisharya73528@gmail.com>
2022-07-22 15:45:23 +0530

    enable category save button

--- resources/js/app.js
+++ resources/js/app.js
@@ -964,6 +964,19 @@ function resourceGuidelineEditClicked(event) {
 	$("#edit-Modal").modal("show");
 }
 
+$(function(){
+	$("#categoryName").keyup(check_save).each(function(){
+	  check_save();
+	});
+});
+function check_save(){
+	if ($(this).val().length == 0){
+	    $("#save-btn-action").attr("disabled", true);
+	}  else{
+	    $("#save-btn-action").removeAttr("disabled");
+	}
+}
+
 function showCommentBlock() {
 	var blockId = $(this).data("block-id");
 	$(blockId).removeClass("d-none").find("input").focus();
