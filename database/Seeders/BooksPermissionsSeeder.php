1d92fcfbac16d42801aba207efc9ee799d1e2851
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-23 17:50:19 +0530

    db:seed command cleanup

---
+++ database/Seeders/BooksPermissionsSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class BooksPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $weeklyDosesPermissions = [
+            ['name' => 'weeklydoses.view']
+        ];
+
+        $libraryBooksPermissions = [
+            ['name' => 'library_books.create'],
+            ['name' => 'library_books.view'],
+            ['name' => 'library_books.update'],
+            ['name' => 'library_books.delete'],
+        ];
+
+        $libraryBookCategoriesPermissions = [
+            ['name' => 'library_book_category.create'],
+            ['name' => 'library_book_category.view'],
+            ['name' => 'library_book_category.update'],
+            ['name' => 'library_book_category.delete'],
+        ];
+        foreach (
+            array_merge($weeklyDosesPermissions, $libraryBooksPermissions, $libraryBookCategoriesPermissions)
+            as $permission
+        ) {
+            Permission::updateOrCreate($permission);
+        }
+
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach (
+            array_merge($weeklyDosesPermissions, $libraryBooksPermissions, $libraryBookCategoriesPermissions)
+            as $permission
+        ) {
+            $adminRole->givePermissionTo($permission);
+        }
+
+        // set permissions for book-manager role
+        $bookManagerRole = Role::where(['name' => 'book-manager'])->first();
+        foreach (
+            array_merge($libraryBooksPermissions, $libraryBookCategoriesPermissions)
+            as $permission
+        ) {
+            $bookManagerRole->givePermissionTo($permission);
+        }
+    }
+}
--- database/Seeders/RolesTableSeeder.php
+++ database/Seeders/RolesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class RolesTableSeeder extends Seeder
 {
@@ -14,93 +15,85 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('roles')->delete();
-
-        DB::table('roles')->insert([
+        $defaultRoles = [
             [
-                'id' => 1,
                 'name' => 'super-admin',
                 'label' => 'Super Admin',
                 'guard_name' => 'web',
                 'description' => 'User having this role can access everything into the system.',
             ],
             [
-                'id' => 2,
                 'name' => 'admin',
                 'label' => 'Admin',
                 'guard_name' => 'web',
                 'description' => 'This will also have all the permissions except adding new Admin into the system.',
             ],
             [
-                'id' => 3,
                 'name' => 'employee',
                 'label' => 'Employee',
                 'guard_name' => 'web',
                 'description' => 'Limited actions',
             ],
             [
-                'id' => 4,
                 'name' => 'hr-manager',
                 'label' => 'HR',
                 'guard_name' => 'web',
                 'description' => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
             ],
             [
-                'id' => 5,
                 'name' => 'Finance',
                 'label' => 'Finance',
                 'guard_name' => 'web',
                 'description' => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
             ],
             [
-                'id' => 6,
                 'name' => 'volunteer-manager',
                 'label' => 'Volunteer Manager',
                 'guard_name' => 'web',
                 'description' => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
             ],
             [
-                'id' => 7,
                 'name' => 'book-manager',
                 'label' => 'Library',
                 'guard_name' => 'web',
                 'description' => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
             ],
             [
-                'id' => 8,
                 'name' => 'user-management',
                 'label' => 'User Management',
                 'guard_name' => 'web',
                 'description' => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
             ],
             [
-                'id' => 9,
                 'name' => 'client-manager',
                 'label' => 'Client Manager',
                 'guard_name' => 'web',
                 'description' => 'User having this role will be able to Create, Update and Delete the clients',
             ],
             [
-                'id' => 10,
                 'name' => 'key-account-manager',
                 'label' => 'Key Account Manager',
                 'guard_name' => 'web',
                 'description' => 'User having this role will be able to Create, Update and Delete the clients having him as a key account manager.',
             ],
             [
-                'id' => 11,
                 'name' => 'project-manager',
                 'label' => 'Project Manager',
                 'guard_name' => 'web',
                 'description' => 'User having this role will be able to perform all operations in the projects module.',
             ],
             [
-                'id' => 12,
                 'name' => 'accountant',
                 'label' => 'Accountant',
                 'guard_name' => 'web',
                 'description' => 'User having this role will be able to view the finance report',
             ]
-        ]);
+        ];
+
+        foreach ($defaultRoles as $role) {
+            Role::updateOrCreate([
+                'name' => $role['name'],
+            ], $role);
+        }
     }
 }
