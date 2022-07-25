72c8f95de818d61473b698f7e72228a1b8f63ee8
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-25 22:27:31 +0530

    seeder fix for lead

--- Modules/Lead/Database/Seeders/LeadDatabaseSeeder.php
+++ Modules/Lead/Database/Seeders/LeadDatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\Lead\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
@@ -16,10 +17,20 @@ class LeadDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        $leadPermissions = [
+            ['name' => 'lead.create'],
+            ['name' => 'lead.view'],
+            ['name' => 'lead.update'],
+            ['name' => 'lead.delete'],
+        ];
+        foreach ($leadPermissions as $permission) {
+            Permission::updateOrCreate($permission);
+        }
 
-        Permission::updateOrCreate(['name' => 'lead.create']);
-        Permission::updateOrCreate(['name' => 'lead.view']);
-        Permission::updateOrCreate(['name' => 'lead.update']);
-        Permission::updateOrCreate(['name' => 'lead.delete']);
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach ($leadPermissions as $permission) {
+            $adminRole->givePermissionTo($permission);
+        }
     }
 }
