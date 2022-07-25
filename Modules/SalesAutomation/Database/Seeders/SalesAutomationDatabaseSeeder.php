f7368a9996f578b5194e18a786e8b41b0975b9f1
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-25 22:43:52 +0530

    seeder fix for salesautomation

--- Modules/SalesAutomation/Database/Seeders/SalesAutomationDatabaseSeeder.php
+++ Modules/SalesAutomation/Database/Seeders/SalesAutomationDatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\SalesAutomation\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
@@ -16,15 +17,29 @@ class SalesAutomationDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        $salesAutomationPermissions = [
+            ['name' => 'sales_automation.create'],
+            ['name' => 'sales_automation.view'],
+            ['name' => 'sales_automation.update'],
+            ['name' => 'sales_automation.delete'],
+        ];
 
-        Permission::updateOrCreate(['name' => 'sales_automation.create']);
-        Permission::updateOrCreate(['name' => 'sales_automation.view']);
-        Permission::updateOrCreate(['name' => 'sales_automation.update']);
-        Permission::updateOrCreate(['name' => 'sales_automation.delete']);
+        $salesReportsPermissions = [
+            ['name' => 'sales_reports.create'],
+            ['name' => 'sales_reports.view'],
+            ['name' => 'sales_reports.update'],
+            ['name' => 'sales_reports.delete'],
+        ];
 
-        Permission::updateOrCreate(['name' => 'sales_reports.create']);
-        Permission::updateOrCreate(['name' => 'sales_reports.view']);
-        Permission::updateOrCreate(['name' => 'sales_reports.update']);
-        Permission::updateOrCreate(['name' => 'sales_reports.delete']);
+        $allSalesPermissions = array_merge($salesAutomationPermissions, $salesReportsPermissions);
+        foreach ($allSalesPermissions as $permission) {
+            Permission::updateOrCreate($permission);
+        }
+
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach ($allSalesPermissions as $permission) {
+            $adminRole->givePermissionTo($permission);
+        }
     }
 }
