ebd89273e5e062d6de7c7a173bd8942b2c5b99fc
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-25 22:29:33 +0530

    seeder fix for legal document

--- Modules/LegalDocument/Database/Seeders/LegalDocumentPermissionsTableSeeder.php
+++ Modules/LegalDocument/Database/Seeders/LegalDocumentPermissionsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\LegalDocument\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
@@ -16,9 +17,26 @@ class LegalDocumentPermissionsTableSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-        Permission::updateOrCreate(['name' => 'nda_settings.create']);
-        Permission::updateOrCreate(['name' => 'nda_settings.view']);
-        Permission::updateOrCreate(['name' => 'nda_settings.update']);
-        Permission::updateOrCreate(['name' => 'nda_settings.delete']);
+        $ndaSettingsPermissions = [
+            ['name' => 'nda_settings.create'],
+            ['name' => 'nda_settings.view'],
+            ['name' => 'nda_settings.update'],
+            ['name' => 'nda_settings.delete'],
+        ];
+        foreach ($ndaSettingsPermissions as $permission) {
+            Permission::updateOrCreate($permission);
+        }
+
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach ($ndaSettingsPermissions as $permission) {
+            $adminRole->givePermissionTo($permission);
+        }
+
+        // set permissions for finance-manager role
+        $financeManagerRole = Role::where(['name' => 'finance-manager'])->first();
+        foreach ($ndaSettingsPermissions as $permission) {
+            $financeManagerRole->givePermissionTo($permission);
+        }
     }
 }
