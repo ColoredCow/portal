9a89b14e44cf31c6016be9e2c9966242a0b297f2
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-07-25 22:33:41 +0530

    seeder fix for prospect

--- Modules/Prospect/Database/Seeders/ProspectDatabaseSeeder.php
+++ Modules/Prospect/Database/Seeders/ProspectDatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\Prospect\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
@@ -21,5 +22,21 @@ class ProspectDatabaseSeeder extends Seeder
         Permission::updateOrCreate(['name' => 'prospect.view']);
         Permission::updateOrCreate(['name' => 'prospect.update']);
         Permission::updateOrCreate(['name' => 'prospect.delete']);
+
+        $prospectPermissions = [
+            ['name' => 'prospect.create'],
+            ['name' => 'prospect.view'],
+            ['name' => 'prospect.update'],
+            ['name' => 'prospect.delete'],
+        ];
+        foreach ($prospectPermissions as $permission) {
+            Permission::updateOrCreate($permission);
+        }
+
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach ($prospectPermissions as $permission) {
+            $adminRole->givePermissionTo($permission);
+        }
     }
 }
