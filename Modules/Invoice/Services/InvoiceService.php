bbe63e7cc1dc7754c5c8581d7bfa693e147b611d
amisha bisht <amishabisht33@gmail.comgit status>
2022-07-25 14:29:15 +0530

    resolved merge conflict

--- Modules/Invoice/Services/InvoiceService.php
+++ Modules/Invoice/Services/InvoiceService.php
@@ -541,11 +541,9 @@ class InvoiceService implements InvoiceServiceContract
             'body' => $data['email_body'] ?? null,
             'subject' => $data['email_subject'] ?? null
         ];
-        $year = (int) substr($term, 0, 4);
-        $monthNumber = (int) substr($term, 5, 2);
-        $invoiceNumber = str_replace('-', '', $client->next_invoice_number);
-        $invoice = $this->generateInvoiceForClient($client, $monthNumber, $year, $term);
-        Mail::queue(new SendInvoiceMail($client, $invoice, $monthNumber, $year, $invoiceNumber, $email));
+        $invoiceNumber = str_replace('-', '', optional($client)->next_invoice_number ?: $project->next_invoice_number);
+        $invoice = $this->createInvoice($client, $project, $term);
+        Mail::queue(new SendInvoiceMail($invoice, $invoiceNumber, $email));
         InvoiceMail::create([
             'invoice_id'=> $invoice->id,
             'subject' => $email['subject'],
