42261dbe9f65ecae6a193691154d197d7b4be43b
shubham belwal <belwalshubham08@gmail.com>
2022-07-25 14:54:26 +0530

    remove extra file

--- Modules/Invoice/Resources/views/index.blade.php
+++ Modules/Invoice/Resources/views/index.blade.php
@@ -7,7 +7,7 @@
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success') }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
         @endif
@@ -19,13 +19,15 @@
                 @php
                     $request['invoice_status'] = 'ready';
                 @endphp
-                <a class="nav-link {{ (request()->input('invoice_status', 'sent') == 'ready') ? 'active' : '' }}" href="{{ route('invoice.index', $request) }}">Ready to Send</a>
+                <a class="nav-link {{ request()->input('invoice_status', 'sent') == 'ready' ? 'active' : '' }}"
+                    href="{{ route('invoice.index', $request) }}">Ready to Send</a>
             </li>
             <li class="nav-item mr-2">
                 @php
                     $request['invoice_status'] = 'sent';
                 @endphp
-                <a class="nav-link {{ (request()->input('invoice_status', 'sent') == 'sent') ? 'active' : '' }}" href="{{ route('invoice.index', $request) }}">Sent</a>
+                <a class="nav-link {{ request()->input('invoice_status', 'sent') == 'sent' ? 'active' : '' }}"
+                    href="{{ route('invoice.index', $request) }}">Sent</a>
             </li>
         </ul>
         <div class="d-flex justify-content-between mb-2">
@@ -48,52 +50,56 @@
         <div class="font-muli-bold my-4">
             Current Exchange rates ($1) : &nbsp; ₹{{ $currencyService->getCurrentRatesInINR() }}
         </div>
-        @if(request()->invoice_status == 'sent' || $invoiceStatus == 'sent')
+        @if (request()->invoice_status == 'sent' || $invoiceStatus == 'sent')
             <div class="font-muli-bold my-4">
                 Receivable amount (for current filters): &nbsp; ₹{{ $totalReceivableAmount }}
             </div>
         @endif
         <div>
             @php
-                $month = now(config('constants.timezone.indian'))->subMonth()->format('m');
-                $year = now(config('constants.timezone.indian'))->subMonth()->format('Y');
+                $month = now(config('constants.timezone.indian'))
+                    ->subMonth()
+                    ->format('m');
+                $year = now(config('constants.timezone.indian'))
+                    ->subMonth()
+                    ->format('Y');
                 $monthToSubtract = 1;
             @endphp
             <table class="table table-bordered table-striped">
                 <thead class="thead-dark">
                     <tr class="text-center sticky-top">
                         <th></th>
-                        @if (request()->invoice_status == "sent" || $invoiceStatus == 'sent')
+                        @if (request()->invoice_status == 'sent' || $invoiceStatus == 'sent')
                             <th>Client</th>
                         @endif
                         <th class="w-150">Project</th>
-                        @if (request()->invoice_status == "sent" || $invoiceStatus == 'sent')
+                        @if (request()->invoice_status == 'sent' || $invoiceStatus == 'sent')
                             <th>Invoice Number</th>
                         @else
-                        <th>Rates</th>
-                        <th>Billable Hours</th>
+                            <th>Rates</th>
+                            <th>Billable Hours</th>
                         @endif
                         <th>Amount ( + taxes)</th>
-                        @if (request()->invoice_status == "sent" || $invoiceStatus == 'sent')
+                        @if (request()->invoice_status == 'sent' || $invoiceStatus == 'sent')
                             <th>Sent on</th>
                             <th>Due on</th>
                             <th>Paid on</th>
                             <th>Status</th>
                             <th>Email</th>
-                        @elseif (request()->invoice_status == "ready")
+                        @elseif (request()->invoice_status == 'ready')
                             <th>EffortSheet</th>
                             <th>Preview Invoice</th>
-                            <th>Action</th>   
+                            <th>Action</th>
                         @endif
                     </tr>
                 </thead>
                 <tbody>
-                    @if((request()->invoice_status == 'sent' || $invoiceStatus == 'sent') && $invoices->isNotEmpty())
+                    @if ((request()->invoice_status == 'sent' || $invoiceStatus == 'sent') && $invoices->isNotEmpty())
                         @foreach ($invoices as $invoice)
                             @php
                                 $invoiceYear = $invoice->sent_on->subMonth()->year;
                                 $invoiceData = [
-                                    'projectName' => optional($invoice->project)->name ?: ($invoice->client->name . 'Projects'),
+                                    'projectName' => optional($invoice->project)->name ?: $invoice->client->name . 'Projects',
                                     'billingPersonName' => optional($invoice->client->billing_contact)->name,
                                     'billingPersonFirstName' => optional($invoice->client->billing_contact)->first_name,
                                     'billingPersonEmail' => optional($invoice->client->billing_contact)->email,
@@ -106,37 +112,45 @@
                                     'invoiceNumber' => $invoice->invoice_number,
                                     'invoiceAmount' => $invoice->invoiceAmount(),
                                     'bccEmails' => $invoice->client->bcc_emails,
-                                    'ccEmails' => $invoice->client->cc_emails
+                                    'ccEmails' => $invoice->client->cc_emails,
                                 ];
                             @endphp
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $invoice->client->name }}</td>
-                                <td>{{ optional($invoice->project)->name ?: ($invoice->client->name . ' Projects') }}</td>
-                                <td><a href="{{ route('invoice.edit', $invoice) }}">{{ $invoice->invoice_number }}</a></td>
+                                <td>{{ optional($invoice->project)->name ?: $invoice->client->name . ' Projects' }}</td>
+                                <td><a href="{{ route('invoice.edit', $invoice) }}">{{ $invoice->invoice_number }}</a>
+                                </td>
                                 <td>{{ $invoice->invoiceAmount() }}</td>
-                                <td class="text-center">{{ $invoice->sent_on->format(config('invoice.default-date-format')) }}</td>
-                                <td class='{{ $invoice->shouldHighlighted() ? 'font-weight-bold text-danger ' : '' }} text-center'>
+                                <td class="text-center">
+                                    {{ $invoice->sent_on->format(config('invoice.default-date-format')) }}</td>
+                                <td
+                                    class='{{ $invoice->shouldHighlighted() ? 'font-weight-bold text-danger ' : '' }} text-center'>
                                     {{ $invoice->receivable_date->format(config('invoice.default-date-format')) }}
                                 </td>
-                                <td class="{{ $invoice->status == 'paid' ? 'font-weight-bold text-success' : '' }} text-center">
+                                <td
+                                    class="{{ $invoice->status == 'paid' ? 'font-weight-bold text-success' : '' }} text-center">
                                     {{ $invoice->payment_at ? $invoice->payment_at->format(config('invoice.default-date-format')) : '' }}
                                 </td>
-                                <td class="{{ $invoice->shouldHighlighted() ? 'font-weight-bold text-danger' : '' }}{{ $invoice->status == 'paid' ? 'font-weight-bold text-success' : '' }} text-center">
+                                <td
+                                    class="{{ $invoice->shouldHighlighted() ? 'font-weight-bold text-danger' : '' }}{{ $invoice->status == 'paid' ? 'font-weight-bold text-success' : '' }} text-center">
                                     {{ $invoice->shouldHighlighted() ? __('Overdue') : $invoice->status }}
                                 </td>
                                 <td class="text-center">
-                                    @if($invoice->reminder_mail_count)
+                                    @if ($invoice->reminder_mail_count)
                                         <div class="text-success">{{ __('Reminder Sent') }}</div>
                                     @elseif($invoice->shouldHighlighted())
-                                        <div class="btn btn-sm btn-primary send-reminder" data-invoice-data="{{ json_encode($invoiceData) }}" data-toggle="modal" data-target="#invoiceReminder" >{{ __('Reminder') }}</div>
+                                        <div class="btn btn-sm btn-primary send-reminder"
+                                            data-invoice-data="{{ json_encode($invoiceData) }}" data-toggle="modal"
+                                            data-target="#invoiceReminder">{{ __('Reminder') }}</div>
                                     @else
-                                        <div> - </div> 
+                                        <div> - </div>
                                     @endif
                                 </td>
                             </tr>
                         @endforeach
-                    @elseif((request()->invoice_status == 'ready'  || $invoiceStatus == 'ready') && $clientsReadyToSendInvoicesData->isNotEmpty())
+                    @elseif((request()->invoice_status == 'ready' || $invoiceStatus == 'ready') &&
+                        $clientsReadyToSendInvoicesData->isNotEmpty())
                         <tr class="font-weight-bold bg-theme-warning-lighter">
                             <td colspan="8">{{ __('Client Level Billing Projects') }}</td>
                         </tr>
@@ -169,7 +183,7 @@
                                     'clientId' => $client->id,
                                     'projectId' => null,
                                     'bccEmails' => $client->bcc_emails,
-                                    'ccEmails' => $client->cc_emails
+                                    'ccEmails' => $client->cc_emails,
                                 ];
                             @endphp
                             <tr>
@@ -177,13 +191,16 @@
                                 <td>
                                     {{ $client->name . ' Projects' }}
                                 </td>
-                                <td>{{ config('constants.currency.' . $client->currency . '.symbol') . '' . $client->billingDetails->service_rates . __('/hour')}}</td>
+                                <td>{{ config('constants.currency.' . $client->currency . '.symbol') . '' . $client->billingDetails->service_rates . __('/hour') }}
+                                </td>
                                 <td>
                                     {{ $client->getClientLevelProjectsBillableHoursForInvoice() }}
                                     {{-- <i class="pt-1 ml-1 fa fa-external-link-square" data-toggle="modal" data-target="#InvoiceDetailsForClient{{ $client->id }}"></i> --}}
                                 </td>
                                 <td>{{ $amount }}</td>
-                                <td align="center"> <a class="btn btn-sm btn-info text-light" href="{{ $client->effort_sheet_url }}" target="_blank">{{ __('Link') }}</a></td>
+                                <td align="center"> <a class="btn btn-sm btn-info text-light"
+                                        href="{{ $client->effort_sheet_url }}" target="_blank">{{ __('Link') }}</a>
+                                </td>
                                 <td class="text-center">
                                     <form action="{{ route('invoice.generate-invoice') }}" target="_blank" method="POST">
                                         @csrf
@@ -192,12 +209,15 @@
                                     </form>
                                 </td>
                                 <td class="text-center">
-                                    <div class="btn btn-sm btn-success text-light show-preview" data-invoice-data="{{ json_encode($invoiceData) }}">{{ __('View Mail') }}</div>
+                                    <div class="btn btn-sm btn-success text-light show-preview"
+                                        data-invoice-data="{{ json_encode($invoiceData) }}">{{ __('View Mail') }}</div>
                                 </td>
                             </tr>
                         @endforeach
                         @if ($index == 0)
-                            <tr><td colspan=9 class="text-center fz-24 text-theme-gray">No invoices available</td></tr>
+                            <tr>
+                                <td colspan=9 class="text-center fz-24 text-theme-gray">No invoices available</td>
+                            </tr>
                         @endif
                         <tr class="font-weight-bold bg-theme-warning-lighter">
                             <td colspan="8">{{ __('Project Level Billing Projects') }}</td>
@@ -231,7 +251,7 @@
                                     'projectId' => $project->id,
                                     'clientId' => null,
                                     'bccEmails' => $project->client->bcc_emails,
-                                    'ccEmails' => $project->client->cc_emails
+                                    'ccEmails' => $project->client->cc_emails,
                                 ];
                             @endphp
                             <tr>
@@ -239,10 +259,13 @@
                                 <td>
                                     {{ $project->name }}
                                 </td>
-                                <td>{{ config('constants.currency.' . $project->client->currency . '.symbol') . '' . $project->client->billingDetails->service_rates . __('/hour')}}</td>
+                                <td>{{ config('constants.currency.' . $project->client->currency . '.symbol') . '' . $project->client->billingDetails->service_rates . __('/hour') }}
+                                </td>
                                 <td>{{ $project->getBillableHoursForMonth($monthToSubtract) }}</td>
                                 <td>{{ $amount }}</td>
-                                <td align="center"> <a class="btn btn-sm btn-info text-light" href="{{ $project->effort_sheet_url }}" target="_blank">{{ __('Link') }}</a></td>
+                                <td align="center"> <a class="btn btn-sm btn-info text-light"
+                                        href="{{ $project->effort_sheet_url }}" target="_blank">{{ __('Link') }}</a>
+                                </td>
                                 <td class="text-center">
                                     <form action="{{ route('invoice.generate-invoice') }}" target="_blank" method="POST">
                                         @csrf
@@ -251,31 +274,33 @@
                                     </form>
                                 </td>
                                 <td class="text-center">
-                                    <div class="btn btn-sm btn-success text-light show-preview" data-invoice-data="{{ json_encode($invoiceData) }}">{{ __('View Mail') }}</div>
+                                    <div class="btn btn-sm btn-success text-light show-preview"
+                                        data-invoice-data="{{ json_encode($invoiceData) }}">{{ __('View Mail') }}</div>
                                 </td>
                             </tr>
                         @endforeach
                         @if ($index == 0)
-                            <tr><td colspan=9 class="text-center fz-24 text-theme-gray">No invoices available</td></tr>
+                            <tr>
+                                <td colspan=9 class="text-center fz-24 text-theme-gray">No invoices available</td>
+                            </tr>
                         @endif
                     @else
-                        <tr><td colspan=9 class="text-center fz-24 text-theme-gray py-6">No invoices available</td></tr>
+                        <tr>
+                            <td colspan=9 class="text-center fz-24 text-theme-gray py-6">No invoices available</td>
+                        </tr>
                     @endif
                 </tbody>
             </table>
         </div>
         @foreach ($clientsReadyToSendInvoicesData as $client)
-            @include(
-                'invoice::subviews.invoice-report.invoice-details-modal', 
-                [
-                    'modalId' => "InvoiceDetailsForClient" . $client->id,
-                    'teamMembers' => $client->TeamMembersEffortData($monthToSubtract)
-                ]
-            )
+            @include('invoice::subviews.invoice-report.invoice-details-modal', [
+                'modalId' => 'InvoiceDetailsForClient' . $client->id,
+                'teamMembers' => $client->TeamMembersEffortData($monthToSubtract),
+            ])
         @endforeach
     </div>
-    @if(request()->invoice_status == 'ready' || $invoiceStatus == 'ready')
-       @include('invoice::modals.invoice-email-preview')
+    @if (request()->invoice_status == 'ready' || $invoiceStatus == 'ready')
+        @include('invoice::modals.invoice-email-preview')
     @endif
     @include('invoice::modals.invoice-reminder')
 @endsection
