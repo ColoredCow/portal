0fd60b2e6d1b24dc875f1feab45d6cea077a28c3
Paras <parasnegi2002@gmail.com>
2022-07-28 15:22:22 +0530

    fixed issue

--- Modules/Project/Services/ProjectService.php
+++ Modules/Project/Services/ProjectService.php
@@ -70,20 +70,23 @@ class ProjectService implements ProjectServiceContract
                 $query->where('team_member_id', $userId);
             })->count();
         }
-        foreach ($clients as $client):
+
+        if ($clients->isNotEmpty()) {
+            foreach ($clients as $client):
             foreach ($client->projects as $project):
                 if (empty($project->projectContracts->first()->contract_file_path)) {
                     $project->tag('no-contract');
                 } elseif (! empty($project->projectContracts->first()->contract_file_path)) {
                     $project->untag('no-contract');
                 }
-        if (empty($project->effort_sheet_url)) {
-            $project->tag('project-unavailable');
-        } elseif (! empty($project->effort_sheet_url)) {
-            $project->untag('project-unavailable');
+            if (empty($project->effort_sheet_url)) {
+                $project->tag('project-unavailable');
+            } elseif (! empty($project->effort_sheet_url)) {
+                $project->untag('project-unavailable');
+            }
+            endforeach;
+            endforeach;
         }
-        endforeach;
-        endforeach;
 
         return [
             'clients' => $clients->appends($data),
