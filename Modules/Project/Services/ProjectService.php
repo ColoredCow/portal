7396c2b2d7d3dbb19930c5c88948bdd7b8a2601c
Paras <parasnegi2002@gmail.com>
2022-07-28 18:19:05 +0530

    CI fixes

--- Modules/Project/Services/ProjectService.php
+++ Modules/Project/Services/ProjectService.php
@@ -70,23 +70,21 @@ class ProjectService implements ProjectServiceContract
                 $query->where('team_member_id', $userId);
             })->count();
         }
-
-        if ($clients->isNotEmpty()) {
-            foreach ($clients as $client):
-                foreach ($client->projects as $project):
-                    if (empty($project->projectContracts->first()->contract_file_path)) {
-                        $project->tag('no-contract');
-                    } elseif (!empty($project->projectContracts->first()->contract_file_path)) {
-                        $project->untag('no-contract');
-                    }
-            if (empty($project->effort_sheet_url)) {
-                $project->tag('project-unavailable');
-            } elseif (!empty($project->effort_sheet_url)) {
-                $project->untag('project-unavailable');
-            }
-            endforeach;
-            endforeach;
+        
+        foreach ($clients as $client):
+            foreach ($client->projects as $project):
+                if (empty($project->projectContracts->first()->contract_file_path)) {
+                    $project->tag('no-contract');
+                } elseif (! empty($project->projectContracts->first()->contract_file_path)) {
+                    $project->untag('no-contract');
+                }
+        if (empty($project->effort_sheet_url)) {
+            $project->tag('project-unavailable');
+        } elseif (! empty($project->effort_sheet_url)) {
+            $project->untag('project-unavailable');
         }
+        endforeach;
+        endforeach;
 
         return [
             'clients' => $clients->appends($data),
@@ -160,7 +158,7 @@ class ProjectService implements ProjectServiceContract
     public function updateProjectData($data, $project)
     {
         $updateSection = $data['update_section'] ?? '';
-        if (!$updateSection) {
+        if (! $updateSection) {
             return false;
         }
 
@@ -206,7 +204,7 @@ class ProjectService implements ProjectServiceContract
         if ($data['status'] == 'active') {
             $project->client->update(['status' => 'active']);
         } else {
-            if (!$project->client->projects()->where('status', 'active')->exists()) {
+            if (! $project->client->projects()->where('status', 'active')->exists()) {
                 $project->client->update(['status' => 'inactive']);
             }
             $project->getTeamMembers()->update(['ended_on' => now()]);
@@ -229,7 +227,7 @@ class ProjectService implements ProjectServiceContract
                     $member->update($tempArray);
                 }
             }
-            if (!$flag) {
+            if (! $flag) {
                 $member->update(['ended_on' => Carbon::now()]);
             }
         }
@@ -248,7 +246,7 @@ class ProjectService implements ProjectServiceContract
 
     private function updateProjectRepositories($data, $project)
     {
-        if (!isset($data['url'])) {
+        if (! isset($data['url'])) {
             $project->repositories()->delete();
 
             return;
@@ -284,7 +282,7 @@ class ProjectService implements ProjectServiceContract
         $numberOfWorkingDays = 0;
         $weekend = ['Saturday', 'Sunday'];
         foreach ($period as $date) {
-            if (!in_array($date->format('l'), $weekend)) {
+            if (! in_array($date->format('l'), $weekend)) {
                 $numberOfWorkingDays++;
             }
         }
@@ -324,7 +322,7 @@ class ProjectService implements ProjectServiceContract
                     }
                 }
             }
-            if (!empty($managerProjects)) {
+            if (! empty($managerProjects)) {
                 $dataForMail[] = [
                     'projects' => $managerProjects,
                     'name' => $user->name,
