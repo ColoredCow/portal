8fa05e48d3adec10419cfe01ae061da2fd7474c8
Paras <parasnegi2002@gmail.com>
2022-07-28 17:56:39 +0530

    CI fixes

--- Modules/Project/Services/ProjectService.php
+++ Modules/Project/Services/ProjectService.php
@@ -79,12 +79,12 @@ class ProjectService implements ProjectServiceContract
                     } elseif (!empty($project->projectContracts->first()->contract_file_path)) {
                         $project->untag('no-contract');
                     }
-            if (empty($project->effort_sheet_url)) {
-                $project->tag('project-unavailable');
-            } elseif (!empty($project->effort_sheet_url)) {
-                $project->untag('project-unavailable');
-            }
-            endforeach;
+                    if (empty($project->effort_sheet_url)) {
+                        $project->tag('project-unavailable');
+                    } elseif (!empty($project->effort_sheet_url)) {
+                        $project->untag('project-unavailable');
+                    }
+                endforeach;
             endforeach;
         }
 
