d3e45cbadcc6a13ea627517c71a8c2ae648c6678
Anshul Saklani <anshulsaklani316@gmail.com>
2022-07-28 19:35:22 +0530

    changes done

--- resources/views/hr/recruitment/reports.blade.php
+++ resources/views/hr/recruitment/reports.blade.php
@@ -7,7 +7,7 @@
         <div class="d-flex justify-content-between">
             <div class="my-2">
 							<div align="left">
-               <h2><span class="chart-heading-mr-5">Analytics</span><span class="total-chart"> Total Verified Applications: <?php echo $verifiedApplicationsCount ?></span><h2>
+               <h2><span class="chart-heading-mr-5">Analytics</span><span class="total-chart"> Total Verified Applications: <?php echo $verifiedApplicationsCount ?></span></h2>
 							</div>
             </div>
             <div class="my-2">
