d1256f83f12b737982ba241012043ce7744fbeb1
tarun12joshi@gmail.com <user@coloredcow.com>
2022-07-29 16:30:04 +0530

    Fix the error

--- Modules/Client/Resources/views/subviews/listing-client-row.blade.php
+++ Modules/Client/Resources/views/subviews/listing-client-row.blade.php
@@ -14,11 +14,12 @@
         @include('client::subviews.listing-client-type', ['client' => $client])
     </td>
     <td>
-    @if ($client->keyAccountManager)
-        <span data-html="true" data-toggle="tooltip" title="{{ $client->keyAccountManager->name}}" class="content tooltip-wrapper">
-        <img src="{{ $client->keyAccountManager->avatar }}" class="w-35 h-30 rounded-circle mb-1"> </a>
-    @else
-        -
-    @endif
+        @if ($client->keyAccountManager)
+            <span data-html="true" data-toggle="tooltip" title="{{ $client->keyAccountManager->name}}" class="content tooltip-wrapper">
+            <img src="{{ $client->keyAccountManager->avatar }}" class="w-35 h-30 rounded-circle mb-1"></a>
+        @else
+            -
+        @endif
     </td>
+
 </tr>
