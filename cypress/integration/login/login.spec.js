5dd48e76a76d45c035dbbc6013b6f619387783eb
dsajwan2020 <96309135+dsajwan2020@users.noreply.github.com>
2022-07-30 20:03:55 +0530

    login test cases bases on user role

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -3,6 +3,7 @@ describe("Login", () => {
     const user = Cypress.env("users").default;
     cy.login(user);
     cy.contains('Hello, there!');
+    cy.contains('Dashboard')
     cy.get('ul.navbar-nav').should('not.contain', 'CRM'); // default user should not CRM
     cy.get('ul.navbar-nav').should('not.contain', 'KnowledgeCafe'); // default user should not CRM
   });
@@ -10,7 +11,24 @@ describe("Login", () => {
   it("logs in the employee user", () => {
     const user = Cypress.env("users").employee;
     cy.login(user);
+    cy.get("#wishlist").contains('Your wishlist');
     cy.get('ul.navbar-nav').contains('CRM'); // employee should see CRM
     cy.get('ul.navbar-nav').contains('KnowledgeCafe'); // employee should see KnowledgeCafe
   });
+
+
+it("logs in the admin user", () => {
+  const user = Cypress.env("users").admin;
+  cy.login(user);
+  cy.get('ul.navbar-nav').contains('CRM'); // employee should see CRM
+  cy.get('ul.navbar-nav').contains('KnowledgeCafe'); // employee should see KnowledgeCafe
+});
+
+it("logs in the superadmin user", () => {
+  const user = Cypress.env("users").superadmin;
+  cy.login(user);
+  cy.get('ul.navbar-nav').contains('CRM'); // employee should see CRM
+  cy.get('ul.navbar-nav').contains('KnowledgeCafe'); // employee should see KnowledgeCafe
+});
+
 });
