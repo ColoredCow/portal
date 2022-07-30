e569e1f25ead973cff35422467236dc417851ed9
Lakshya Varshney <lakshya.varshney_iot18@gla.ac.in>
2022-07-30 15:29:38 +0530

    channel validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -165,26 +165,26 @@ $(document).ready(() => {
 			.addClass("selected")
 			.html(fileName);
 	});
+	$("#addChannel").on("submit",function(e){
+		e.preventDefault();
+		let form = $("#addChannel");
+		let button = $("#channel_button");
+		
+		$.ajax({
+			url: form.attr("action"),
+			type: form.attr("method"),
+			data: form.serialize(),
+			success: function(response) {
+				$("#channelName").modal("hide");
+				$("#success").toggleClass("d-none");
+				$("#success").fadeToggle(5000);
+			},
+			error: function(response) {
+				$("#errorMessage").toggleClass("d-none");
+			},
+		});		
+	});
 
-$("#addChannel").on("submit",function(e){
-	e.preventDefault();
-	let form = $("#addChannel");
-	let button = $("#channel_button")
-    
-	$.ajax({
-		url: form.attr("action"),
-		type: form.attr("method"),
-		data: form.serialize(),
-		success: function(response) {
-			$("#channelName").modal("hide");
-			$("#success").toggleClass("d-none");
-			$("#success").fadeToggle(5000);
-		},
-		error: function(response) {
-			$("#errorMessage").toggleClass("d-none");
-		},
-	});		
-});
 	if ($(".chart-data").length) {
 		datePickerChart();
 		barChart();
