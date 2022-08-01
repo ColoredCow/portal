7abc4900ebca0bbce2d9c3d812d42f359c5c4685
Sachin Bisht <sachinbisht27@gmail.com>
2022-08-01 17:27:35 +0530

    Fixed Larastan & PHP CS error

--- Modules/Project/Entities/ProjectTeamMember.php
+++ Modules/Project/Entities/ProjectTeamMember.php
@@ -47,7 +47,7 @@ class ProjectTeamMember extends Model
 
     public function getCurrentActualEffortAttribute($startDate, $endDate)
     {
-        $startDate=$startDate??$this->project->client->month_start_date;
+        $startDate = $startDate ?? $this->project->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
         return $this->projectTeamMemberEffort()->where('added_on', '>=', $startDate)->sum('actual_effort');
     }
@@ -56,14 +56,14 @@ class ProjectTeamMember extends Model
     {
         $project = new Project;
         $currentDate = today(config('constants.timezone.indian'));
-        $startDate=$startDate??$this->project->client->month_start_date;
+        $startDate = $startDate ?? $this->project->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
 
         if (now(config('constants.timezone.indian'))->format('H:i:s') < config('efforttracking.update_date_count_after_time')) {
             $currentDate = $currentDate->subDay();
         }
 
-        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate, $currentDate));
+        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate));
 
         return $this->daily_expected_effort * $daysTillToday;
     }
@@ -71,17 +71,17 @@ class ProjectTeamMember extends Model
     public function getExpectedEffortTillTodayAttribute($startDate, $endDate)
     {
         $project = new Project;
-        $startDate=$startDate??$this->project->client->month_start_date;
+        $startDate = $startDate ?? $this->project->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
 
-        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate, today(config('constants.timezone.indian'))));
+        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate));
 
         return $this->daily_expected_effort * $daysTillToday;
     }
 
     public function getVelocityAttribute($startDate, $endDate)
     {
-        $startDate= $startDate??$this->project->client->month_start_date;
+        $startDate = $startDate ?? $this->project->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
 
         return $this->getCurrentExpectedEffortAttribute($startDate, $endDate) ? round($this->getCurrentActualEffortAttribute($startDate, $endDate) / $this->getCurrentExpectedEffortAttribute($startDate, $endDate), 2) : 0;
@@ -91,14 +91,14 @@ class ProjectTeamMember extends Model
     {
         $project = new Project;
         $currentDate = today(config('constants.timezone.indian'));
-        $startDate=$startDate??$this->project->client->month_start_date;
+        $startDate = $startDate ?? $this->project->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
 
         if (now(config('constants.timezone.indian'))->format('H:i:s') < config('efforttracking.update_date_count_after_time')) {
             $currentDate = $currentDate->subDay();
         }
 
-        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate, $currentDate));
+        $daysTillToday = count($project->getWorkingDaysList($startDate, $endDate));
         if ($daysTillToday == 0) {
             return 0;
         }
