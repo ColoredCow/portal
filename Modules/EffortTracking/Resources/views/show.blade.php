86b8eb5c0df49253127340f7100ab942203a9ef4
akriti12negi <negiakriti12@gmail.com>
2022-08-02 17:57:20 +0530

    Changed date format

--- Modules/EffortTracking/Resources/views/show.blade.php
+++ Modules/EffortTracking/Resources/views/show.blade.php
@@ -23,11 +23,11 @@
             <div class="d-flex flex-row p-5 d-flex justify-content-center">
                 <div class="d-flex flex-column mr-3 form-group">
                     <label>Start date</label>
-                    <input type="date" name="start_date" disabled="disabled" value="{{ $startDate->toDateString() }}">
+                    <input type="text" name="start_date" disabled="disabled" value="{{ $startDate->format('d-F-Y') }}">
                 </div>
                 <div class="d-flex flex-column ml-3">
                     <label>End date</label>
-                    <input type="date" name="end_date" disabled="disabled" value="{{ $endDate->toDateString() }}">
+                    <input type="text" name="end_date" disabled="disabled" value="{{ $endDate->format('d-F-Y') }}">
                 </div>
             </div>
             @if($project->current_hours_for_month === 0)
