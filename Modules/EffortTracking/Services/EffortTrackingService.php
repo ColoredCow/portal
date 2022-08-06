9832ef21c0cf509a9994e562a07039681e7cde59
saurabhpatel123456 <sp4839876@gmail.com>
2022-08-06 12:55:08 +0530

    fix the error

--- Modules/EffortTracking/Services/EffortTrackingService.php
+++ Modules/EffortTracking/Services/EffortTrackingService.php
@@ -28,14 +28,9 @@ class EffortTrackingService
         $endDate = $project->client->month_end_date;
         $totalWorkingDays = count($this->getworkingDays($startDate, $endDate));
         $daysTillToday = count($project->getWorkingDaysList($project->client->month_start_date, $currentDate));
-      
-        
-        
+     
         return [
             'project' => $project,
-          
-            
-            
             'teamMembersEffort' => empty($teamMembersDetails['teamMembersEffort']) ? 0 : json_encode($teamMembersDetails['teamMembersEffort']),
             'users' => json_encode($teamMembersDetails['users']),
             'workingDays' => json_encode($workingDays),
