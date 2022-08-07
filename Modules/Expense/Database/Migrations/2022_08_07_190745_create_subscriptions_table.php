073a4d2a248acaf862cfcee2f6f0214c87b2dd01
Pankaj Agrawal <pankaj@coloredcow.com>
2022-08-07 20:00:37 +0530

    subscription

---
+++ Modules/Expense/Database/Migrations/2022_08_07_190745_create_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->string('currency');
+            $table->string('frequency')->comment('monthly, yearly');
+            $table->string('due_date')->comment('Date of every month or year');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
