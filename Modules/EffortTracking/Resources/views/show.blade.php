1b9a7fb4d8bc9570af4996052d49b805bd13a320
Kiran <negiaaradhya02@gmail.com>
2022-08-08 12:21:16 +0530

    Feedback resolved

--- Modules/EffortTracking/Resources/views/show.blade.php
+++ Modules/EffortTracking/Resources/views/show.blade.php
@@ -25,31 +25,29 @@
                         </h2>
                     </div>
             </div>
-
-        </div>
-        <div class="effort-tracking-data">
-            <div class="d-flex flex-row p-5 d-flex justify-content-center">
-                <div class="d-flex flex-column mr-3 form-group">
-                    <label>Start date</label>
-                    <input type="text" name="start_date" disabled="disabled" value="{{ $startDate->format('d-F-Y') }}">
-                </div>
-                <div class="d-flex flex-column ml-3">
-                    <label>End date</label>
-                    <input type="text" name="end_date" disabled="disabled" value="{{ $endDate->format('d-F-Y') }}">
-
-                </div>
-            </div>
-            <div>
-                @if ($project->current_hours_for_month === 0)
-                    <h2 class="text-center pb-6 font-weight-bold text-uppercase text-danger">No data available</h2>
-                @else
-                    <div class="mt-4">
-                        <input type="hidden" name="team_members_effort" value="{{ $teamMembersEffort }}">
-                        <input type="hidden" name="workingDays" value="{{ $workingDays }}">
-                        <input type="hidden" name="users" value="{{ $users }}">
-                        <canvas class="w-full" id="effortTrackingGraph"></canvas>
+            <div class="effort-tracking-data">
+                <div class="d-flex flex-row p-5 d-flex justify-content-center">
+                    <div class="d-flex flex-column mr-3 form-group">
+                        <label>Start date</label>
+                        <input type="text" name="start_date" disabled="disabled" value="{{ $startDate->format('d-F-Y') }}">
                     </div>
-                @endif
+                    <div class="d-flex flex-column ml-3">
+                        <label>End date</label>
+                        <input type="text" name="end_date" disabled="disabled" value="{{ $endDate->format('d-F-Y') }}">
+                    </div>
+                </div>
+                <div>
+                    @if ($project->current_hours_for_month === 0)
+                        <h2 class="text-center pb-6 font-weight-bold text-uppercase text-danger">No data available</h2>
+                    @else
+                        <div class="mt-4">
+                            <input type="hidden" name="team_members_effort" value="{{ $teamMembersEffort }}">
+                            <input type="hidden" name="workingDays" value="{{ $workingDays }}">
+                            <input type="hidden" name="users" value="{{ $users }}">
+                            <canvas class="w-full" id="effortTrackingGraph"></canvas>
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
