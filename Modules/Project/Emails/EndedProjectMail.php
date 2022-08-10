2ccd1055fde8cd0fa7c88abd5c2128a43bbc733c
Sachin Bisht <sachinbisht27@gmail.com>
2022-08-10 15:30:55 +0530

    Fixed Larastan error

--- Modules/Project/Emails/EndedProjectMail.php
+++ Modules/Project/Emails/EndedProjectMail.php
@@ -26,7 +26,7 @@ class EndedProjectMail extends Mailable implements ShouldQueue
     /**
      * Build the message.
      *
-     * @return $projectData
+     * @return $project
      */
     public function build()
     {
