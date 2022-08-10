06910a4a7c688d0f5b8d255da01e5d0bd2298422
Sachin Bisht <sachinbisht27@gmail.com>
2022-08-10 19:27:06 +0530

    Fixed Larastan error

--- Modules/Project/Services/ProjectService.php
+++ Modules/Project/Services/ProjectService.php
@@ -20,7 +20,6 @@ class ProjectService implements ProjectServiceContract
 {
     public function index(array $data = [])
     {
-
         $filters = [
             'status' => $data['status'] ?? 'active',
             'name' => $data['name'] ?? null,
