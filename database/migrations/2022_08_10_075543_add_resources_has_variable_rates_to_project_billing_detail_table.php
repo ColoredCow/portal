b3b8657ddcb2ba8d1516200d620436089c8a28d6
Himanshunegi0332 <himanshunegi0332@gmail.com>
2022-08-10 13:35:02 +0530

    created a column

---
+++ database/migrations/2022_08_10_075543_add_resources_has_variable_rates_to_project_billing_detail_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddResourcesHasVariableRatesToProjectBillingDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('project_billing_details', function (Blueprint $table) {
+            $table->float('resources_has_variable_rates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_billing_details', function (Blueprint $table) {
+            $table->dropColumn(['resources_has_variable_rates']);
+        });
+    }
+}
