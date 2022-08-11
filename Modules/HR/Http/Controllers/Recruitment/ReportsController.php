57be8e10d4b76c56e2c58e4bf9dae150a588475f
Shubham Belwal <belwalshubham08@gmail.com>
2022-08-11 14:47:03 +0530

    rename route name

--- Modules/HR/Http/Controllers/Recruitment/ReportsController.php
+++ Modules/HR/Http/Controllers/Recruitment/ReportsController.php
@@ -116,7 +116,7 @@ class ReportsController extends Controller
             'application' => $applicationCount,
         ];
 
-        return view('hr.recruitment.horizontalBarGraph')->with([
+        return view('hr.recruitment.job-Wise-Applications-Graph')->with([
             'totalCount' => $totalApplicationCount,
             'chartData' => json_encode($chartData)
         ]);
