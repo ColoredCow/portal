a0ae36cc229e1e9a235f524241215df1ed907a9d
Pankaj Kandpal <pankaj.kandpal@coloredcow.in>
2022-08-11 12:28:40 +0530

    ci checks

--- Modules/HR/Observers/Recruitment/JobObserver.php
+++ Modules/HR/Observers/Recruitment/JobObserver.php
@@ -57,7 +57,7 @@ class JobObserver
         $corcel = new Corcel();
         $job_status = $job->status;
         $post = $corcel->hasMeta('hr_id', $job->id)->first();
-        if($post) {
+        if ($post) {
             $corcel = $corcel->find($post->ID);
             $corcel->post_title = $job->title;
             $corcel->post_content = $job->description;
