e2b91a2ecb3969ffd32b471f620fc2c9b0f17ba6
LavishRawat <user@colouredcow.com>
2022-08-13 14:40:47 +0530

    Multiple Options for on hold email

--- Modules/HR/Database/Seeders/SettingsTableSeeder.php
+++ Modules/HR/Database/Seeders/SettingsTableSeeder.php
@@ -83,7 +83,17 @@ class SettingsTableSeeder extends Seeder
         Setting::updateOrCreate([
             'module' => 'hr',
             'setting_key' => 'application_on_hold_body',
-            'setting_value' => '<div>Dear |*applicant_name*|,</div><div> </div><div>Thanks for applying to ColoredCow for the position |*job_title*|.We don\'t have a relevant opening for the position right now. Your application is kept under waiting and we will reach out to you if there are any possibilities we can explore together. </div><div>Thanks,</div><div> </div><div>HR Team,</div><div>ColoredCow</div> ',
+            'setting_value' => '<div>Dear |*applicant_name*|,</div><div> </div><div>Thanks for applying to ColoredCow for the position |*job_title*|.We have put your application into consideration. Some skills match our requirements and some are missing. Your application is kept on hold and we will reach out to you if there are any possibilities we can explore together.</div><div>Thanks,</div><div> </div><div>HR Team,</div><div>ColoredCow</div> ',
+        ]);
+        Setting::updateOrCreate([
+            'module' => 'hr',
+            'setting_key' => 'application_on_hold_subject_2',
+            'setting_value' => 'Your application is put on hold',
+        ]);
+        Setting::updateOrCreate([
+            'module' => 'hr',
+            'setting_key' => 'application_on_hold_body_2',
+            'setting_value' => 'Your application is put on hold.Hello Lavish',
         ]);
     }
 }
