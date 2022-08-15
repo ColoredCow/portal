f599f783d6e238ff22b6f4eee2ec36488e2fc621
aquibmoin786 <aquibmoin786@gmail.com>
2022-08-15 18:22:16 +0530

    some changes are done

--- Modules/HR/Resources/views/application/application-handover-request.blade.php
+++ Modules/HR/Resources/views/application/application-handover-request.blade.php
@@ -1,6 +1,6 @@
 @php
 	$assignee = $application->latestApplicationRound->scheduledPerson;
 @endphp
-<p>Hey {{$assignee->name}}, <br> The {{$application->job->type}} application of {{$application->applicant->name}} applied for {{$application->job->title}} is requested for evaluation by {{auth()->user()->name}}. Please click the link to handover the application : {{$application->name}}</p>
+<p>Hey {{$assignee->name}}, <br> <br>The {{$application->job->type}} application of {{$application->applicant->name}} applied for {{$application->job->title}} is requested for evaluation by {{auth()->user()->name}}. Please click the link to handover the application : {{$application->name}}</p>
 <a href="{{ route('application.handover.confirmation', ['application' => $application->id, 'user' => auth()->user()->id])}}">Handover application</a>
 <p>Thanks,<br>HR,<br>Coloredcow</p>
