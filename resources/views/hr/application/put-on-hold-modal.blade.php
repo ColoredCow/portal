fc087eae23755f512ae8b96656851ae52a7fcfb2
Pankaj Kandpal <pankaj.kandpal@coloredcow.in>
2022-08-16 15:49:28 +0530

    modal removed from application edit form

---
+++ resources/views/hr/application/put-on-hold-modal.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle"aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLongTitle">NOTE
+                </h5>
+                <button type="button" class="close"
+                    data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Do you want to put this application on Hold?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary"
+                    data-dismiss="modal">No</button>
+                <button type="submit"
+                    class="btn btn-success px-4 round-submit"
+                    data-action="on-hold">Yes</button>
+            </div>
+        </div>
+    </div>
+</div>
