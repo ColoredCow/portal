b9b1d67d5b823493eba13c857f584dae2d16a776
gulshan81 <gulshansoni613@gmail.com>
2022-08-16 04:31:49 -1200

    fixed the error

--- Modules/Prospect/Routes/web.php
+++ Modules/Prospect/Routes/web.php
@@ -14,7 +14,7 @@
 Route::prefix('prospect')->middleware('auth')->group(function () {
     Route::get('/', 'ProspectController@index')->name('prospect.index');
     Route::get('/create', 'ProspectController@create')->name('prospect.create');
-    Route::get('/delete/{id}','ProspectController@delete')->name('prospect.delete');
+    Route::get('/delete/{id}', 'ProspectController@delete')->name('prospect.delete');
     Route::post('/', 'ProspectController@store')->name('prospect.store');
     Route::get('/{prospect}/edit/{section?}', 'ProspectController@edit')->name('prospect.edit');
     Route::post('/{prospect}/update', 'ProspectController@update')->name('prospect.update');
