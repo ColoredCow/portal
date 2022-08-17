86fb9c35db0350d279202a14ff503505f6ce8444
gulshan81 <gulshansoni613@gmail.com>
2022-08-17 07:32:57 -1200

    created a soft delete column in the prospects table

--- Modules/Prospect/Database/Migrations/2020_04_25_143840_create_prospects_table.php
+++ Modules/Prospect/Database/Migrations/2020_04_25_143840_create_prospects_table.php
@@ -20,7 +20,6 @@ class CreateProspectsTable extends Migration
             $table->string('name');
             $table->string('coming_from')->nullable();
             $table->text('brief_info')->nullable();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
