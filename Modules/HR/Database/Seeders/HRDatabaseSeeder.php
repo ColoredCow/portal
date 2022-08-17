3293b634ddabac10333ece22edf371b5d7cb4649
Shubham Belwal <belwalshubham08@gmail.com>
2022-08-18 00:11:51 +0530

    indentation fixses

--- Modules/HR/Database/Seeders/HRDatabaseSeeder.php
+++ Modules/HR/Database/Seeders/HRDatabaseSeeder.php
@@ -38,6 +38,6 @@ class HRDatabaseSeeder extends Seeder
         $this->call(HrResourcesCategoriesTableSeeder::class);
         $this->call(HrResourcesTableSeeder::class);
         $this->call(HrFollowUpTableSeeder::class);
-		$this->call(TagTableTableSeeder::class);
+        $this->call(TagTableTableSeeder::class);
     }
 }
--- Modules/HR/Database/Seeders/TagTableSeeder.php
+++ Modules/HR/Database/Seeders/TagTableSeeder.php
@@ -3,10 +3,7 @@
 namespace Modules\HR\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use App\Models\Tag;
-use Google\Service\Compute\Tags;
 
 class TagTableTableSeeder extends Seeder
 {
@@ -17,13 +14,12 @@ class TagTableTableSeeder extends Seeder
      */
     public function run()
     {
-
-		$statuses = config('hr.status');		
+        $statuses = config('hr.status');
         foreach ($statuses as $status) {
-			Tag::updateOrCreate([
-				'slug' => $status['label'],
-				'name' => $status['title'],
-			]);
-		}
-	}
+            Tag::updateOrCreate([
+                'slug' => $status['label'],
+                'name' => $status['title'],
+            ]);
+        }
+    }
 }
