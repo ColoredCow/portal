40faa01278024422f8f67d3dd786271567c425d8
Abhishek25081999 <negiabhi03@gmail.com>
2022-08-18 21:49:57 +0530

    made suggested changes

--- Modules/HR/Resources/views/evaluation/segment/delete.blade.php
+++ Modules/HR/Resources/views/evaluation/segment/delete.blade.php
@@ -11,7 +11,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Are you sure you want to delete this segment - @{{ segmentName }}?</p>
+                    <p>Are you sure you want to delete this segment?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
