37be77b1126f0a770bf59e142355aa73a039f26f
rohit123-rawat <rohitrawat0881@gmail.com>
2022-08-19 12:46:53 +0530

    fix graph size

--- resources/views/hr/recruitment/rejected-applications.blade.php
+++ resources/views/hr/recruitment/rejected-applications.blade.php
@@ -4,18 +4,18 @@
 
   <ul class="nav nav-tabs" id="myTab" role="tablist">
     <li class="nav-item">
-      <a class="nav-link" id="round-wise-rejections-tab" data-toggle="tab" href="#round-wise-rejections" role="tab" aria-controls="round-wise-rejections" aria-selected="true">Round wise rejections</a>
+      <a class="nav-link active" id="round-wise-rejections-tab" data-toggle="tab" href="#round-wise-rejections" role="tab" aria-controls="round-wise-rejections" aria-selected="true">Round wise rejections</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link active" id="rejection-reasons-tab" data-toggle="tab" href="#rejection-reasons" role="tab" aria-controls="rejection-reasons" aria-selected="false">Rejection reasons</a>
+      <a class="nav-link" id="rejection-reasons-tab" data-toggle="tab" href="#rejection-reasons" role="tab" aria-controls="rejection-reasons" aria-selected="false">Rejection reasons</a>
     </li>
   </ul>
 
   <div class="tab-content">
-    <div class="tab-pane" id="round-wise-rejections" role="tabpanel" aria-labelledby="round-wise-rejections-tab">
+    <div class="tab-pane active" id="round-wise-rejections" role="tabpanel" aria-labelledby="round-wise-rejections-tab">
       <div class="card">
-        <div class="card-header" align='left'>
-          <span class="chart-heading mr-5">Rounds</span><span class="total-chart-count">Total rejected application count</span>
+        <div class="card-header" hight="10px" align='left'>
+          <span class="chart-heading mr-2">Rounds</span><span class="total-chart-count">Total rejected application count</span>
           <span>
             <div class="d-inline-flex p-2 ml-25" >
               <form id="filterform1" action="{{ route('recruitment.rejected-applications') }}" method="get" align="Right">
@@ -27,18 +27,16 @@
           </span>
         </div>
         <div class="card-body chart-data" data-target="{{ $chartData }}">
-          <canvas id="myGraph" data-target="{{ $chartData }}"></canvas>
+          <canvas id="myGraph" data-target="{{ $chartData }}"width="800%" height="280px"></canvas>
         </div>
       </div>
     </div>
-
-    <div class="tab-pane active" id="rejection-reasons" role="tabpanel" aria-labelledby="rejection-reasons-tab">
-      
+    <div class="tab-pane" id="rejection-reasons" role="tabpanel" aria-labelledby="rejection-reasons-tab">
       <div class="card">
-        <div class="card-header" align='left'>
-          <span class="chart-heading mr-5">Rejected Reason</span><span class="total-chart-count">rejected application count</span>
+        <div class="card-header" hight="10px" align='left'>
+          <span class="chart-heading mr-2">Reason</span> <span class="total-chart-count">Rejected Application Count</span>
           <span>
-            <div class="d-inline-flex p-2 ml-20" >
+            <div class="d-inline-flex p-2 ml-25" >
               <form id="filterform" action="{{ route('recruitment.rejected-applications') }}" method="get" align="Right">
                 <input type="date" name="start_date" id="StartDate" value="{{ old('start_date', request()->get('start_date')) }}" required> to
                 <input type="date" name="end_date" id="EndDate" value="{{ old('end_date', request()->get('end_date')) }}" required>
@@ -48,7 +46,7 @@
           </span>
         </div>
         <div id="BarGraph" class="card-body chart-data" data-target="{{ $chartBarData }}">
-          <canvas id="myBarGraph" data-target="{{ $chartBarData }}"></canvas>
+          <canvas id="myBarGraph" data-target="{{ $chartBarData }}" width="800%" height="250px"></canvas>
         </div>
       </div>
     </div>
