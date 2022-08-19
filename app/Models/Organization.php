71c64ee9ab6e7234ccb5f5f7f690d27d1fc33abe
vivek uniyal <vivekuniyal62@gmail.com>
2022-08-19 11:26:10 +0530

    changes have been done

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Organization extends Model
 {
     use HasFactory;
-    
     protected $table = 'organization';
     protected $fillable = ['id', 'name', 'address', 'annual_sales', 'members', 'industry', 'email', 'billing_details', 'website'];
 }
