bfaedd8aff422be908d4f3098b095b6acc2caf90
aquibmoin786 <aquibmoin786@gmail.com>
2022-08-23 18:42:51 +0530

    fronted Part

--- Modules/Media/Resources/views/layouts/master.blade.php
+++ Modules/Media/Resources/views/layouts/master.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+@extends('layouts.app')
+
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,10 +11,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> --}}
 
   {{-- <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css' /> --}}
-</head>
+{{-- </head>
 
 <body>
   <div class="container">
@@ -31,4 +36,4 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></script>
 </body>
-</html>
+</html> --}}
