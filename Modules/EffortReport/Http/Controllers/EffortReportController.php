4e87ab87a929f04650b52616997ab22bffcd9c66
jagdish <jagdisharya73528@gmail.com>
2022-08-27 13:38:35 +0530

    ci fix

--- Modules/EffortReport/Http/Controllers/EffortReportController.php
+++ Modules/EffortReport/Http/Controllers/EffortReportController.php
@@ -22,15 +22,19 @@ class EffortReportController extends Controller
     public function barGraph(Request $request, $employeeId)
     {
         $employee = User::find($employeeId);
+
         $projectTeamMembers = $employee->projectTeamMembers;
+
         $result = [];
 
         $start = Carbon::now()->addDays(-7);
         $end = Carbon::now();
         $dates = [];
+
         for($i = 0; $i < $end->diffInDays($start); $i++){
             $dates[] = (clone $start)->addDays($i)->format('Y-m-d');
         }
+
         $color = ['#ff0080', '#00bfff', '#ffff00'];
        
         foreach($projectTeamMembers as $projectTeamMember)
@@ -61,6 +65,7 @@ class EffortReportController extends Controller
         'projects' => $projectNames,
         'efforts' => $efforsts
         );
+        
         return view('effortreport::bar-graph', ['employee' => $employee, 'chartData' => $chartData]);
     }
 }
