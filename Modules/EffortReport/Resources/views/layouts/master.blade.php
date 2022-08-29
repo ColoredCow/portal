61b0178769e727e9f056ffe49f3997c6390b7018
jagdish <jagdisharya73528@gmail.com>
2022-08-29 20:13:32 +0530

    ci fixed

--- Modules/EffortReport/Resources/views/layouts/master.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('js_scripts')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
-   <script src="{{ mix('js/effortreport.js') }}"></script>
-@endsection
-
-
-@section('css_scripts')
-@endsection
