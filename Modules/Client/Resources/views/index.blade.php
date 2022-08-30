40270d494d8899afe5f4e3bff06cf897b0842e82
Arjun <sariyallarjun@gmail.com>
2022-08-30 13:55:00 +0530

    added plus icon and changed the background color

--- Modules/Client/Resources/views/index.blade.php
+++ Modules/Client/Resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="d-none d-md-flex justify-content-between my-2">
         @include('client::menu_header')
         @can('clients.create')
-            <a href= "{{ route('client.create') }}" class="btn btn-primary text-white">Add New Client</a>
+            <a href= "{{ route('client.create') }}" class="btn btn-success text-white"><i class="fa fa-plus"></i>Add new Client</a>
         @endcan
     </div>
     <div class="d-md-flex justify-content-between mt-5 mb-2">
