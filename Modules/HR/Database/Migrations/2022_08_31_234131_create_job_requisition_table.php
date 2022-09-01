cdccc148389d15a3381472e056ec8846a6c17915
rohit123-rawat <rohitrawat0881@gmail.com>
2022-09-01 18:36:44 +0530

    fix the error

---
+++ Modules/HR/Database/Migrations/2022_08_31_234131_create_job_requisition_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobRequisitionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_requisition', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('domain_id');
+            $table->unsignedInteger('opportunity_id');
+            $table->timestamps();
+
+            $table->foreign('domain_id')->references('id')->on('hr_job_domains');
+            $table->foreign('opportunity_id')->references('id')->on('hr_jobs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_requisition', function (Blueprint $table) {
+            $table->dropForeign([
+                'domain_id',
+                'opportunity_id',
+            ]);
+        });
+    }
+}
---
+++ Modules/HR/Entities/JobRequisition.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\HR\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobRequisition extends Model
+{
+    protected $fillable = ['domain_id', 'opportunity_id'];
+
+    protected $table = 'job_Requisition';
+}
