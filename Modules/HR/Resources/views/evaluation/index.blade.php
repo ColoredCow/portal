c595e8429e4466eee41cfcdb0a4c12d50a3ecf81
vikramaditaya kumar verma <ditya0504@gmail.com>
2022-09-03 12:30:33 +0530

    similar_btn_change

--- Modules/HR/Resources/views/evaluation/index.blade.php
+++ Modules/HR/Resources/views/evaluation/index.blade.php
@@ -20,7 +20,7 @@
         <div class="d-flex justify-content-between">
             <h1 class="mb-0">Segments</h1>
             <div>
-                <button class="btn btn-primary" data-toggle="modal" data-target="#createNewSegment">Add New</button>
+                <button class="btn btn-success" data-toggle="modal" data-target="#createNewSegment"> <i class="fa fa-plus mr-1"></i> Add New Segment</button>
             </div>
         </div>      
         <br>
