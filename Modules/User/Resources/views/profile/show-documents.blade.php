5555f1089387f5873d726bfe10208c10ca4c89fe
brajesh_lab <brajeshkum57@gmail.com>
2022-09-06 06:21:49 +0530

    Add modal for uploading employee docs

--- Modules/User/Resources/views/profile/show-documents.blade.php
+++ Modules/User/Resources/views/profile/show-documents.blade.php
@@ -9,16 +9,68 @@
   <div class="row">
     <div class="col-4">Pan Card</div>
     <div class="col-4"><a href="#"> View </a></div>
-    <div class="col-4"><a href="#" > Upload New </a></div>
-   
+    <div class="col-4"><a href="#" data-toggle="modal" data-target="#exampleModalCenter"> Upload New </a></div>
+    <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Upload Your PAN Card Here</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+         
+          <div>
+            <label for="file">upload file</label>
+            <input type="file" id="" class="form-control" />
+          </div>
+          <button type="Submit" class="btn btn-primary">submit</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 
   <br>
   <div class="row">
     <div class="col-4">Adhar Card</div>
     <div class="col-4"><a href="#"> View </a></div>
-    <div class="col-4"><a href="#" > Upload New </a></div>
+    <div class="col-4"><a href="#" data-toggle="modal" data-target="#exampleModalCenter"> Upload New </a></div>
+    <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Upload Your Aadhar card Here</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+         
+          <div>
+            <label for="file">upload file</label>
+            <input type="file" id="" class="form-control" />
+          </div>
+          <button type="Submit" class="btn btn-primary">submit</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 
 
@@ -27,6 +79,33 @@
   <div class="row">
     <div class="col-4">Signature</div>
     <div class="col-4"><a href="#"> View </a></div>
-    <div class="col-4"><a href="#" > Upload New </a></div>
+    <div class="col-4"><a href="#" data-toggle="modal" data-target="#exampleModalCenter"> Upload New </a></div>
+  
+  <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Upload Your Signature</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+         
+          <div>
+            <label for="file">upload file</label>
+            <input type="file" id="" class="form-control" />
+          </div>
+          <button type="Submit" class="btn btn-primary">submit</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
   </div>
+</div>
 </div>
