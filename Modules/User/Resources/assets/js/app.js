c5e73cb85a3a61f2061e0f28353fef5f2801fe6a
brajesh_lab <brajeshkum57@gmail.com>
2022-09-07 11:10:33 +0530

    one modal for uploading employee docs & adding form elements using javascript

--- Modules/User/Resources/assets/js/app.js
+++ Modules/User/Resources/assets/js/app.js
@@ -7,5 +7,39 @@
 
 
 $(document).ready(function() {
+   
 
+
+  $("#addPancard").click(function(){
+    document.getElementById("demo").innerHTML="Upload Your Pan Card";
+    var data = "<input type='file' name=pancard id='pan'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    document.getElementById('formelment').innerHTML=data;
+
+  })
+  
+  $("#addAadhar").click(function(){
+    document.getElementById("demo").innerHTML="Upload Your Aadhar Card";
+    var data = "<input type='file' name='aadhar' id='aadhar'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    document.getElementById('formelment').innerHTML=data;
+  
+  })
+
+  $("#addSignature").click(function(){
+    document.getElementById("demo").innerHTML="Upload Your Signature";
+    var data = "<input type='file' name='signature' id='signature'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    document.getElementById('formelment').innerHTML=data;
+  })
+   
 });
+
+
+
+
+   $("#removeform").click(function (){
+      document.getElementById("myform").remove();
+    })
+  
+
+
+
+
