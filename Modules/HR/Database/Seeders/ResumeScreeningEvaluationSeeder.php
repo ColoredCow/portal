e9e023f7f838c1549794fd6d2fec1a385a37ade7
Shubham Belwal <belwalshubham08@gmail.com>
2022-09-07 19:43:24 +0530

    fix identation

--- Modules/HR/Database/Seeders/ResumeScreeningEvaluationSeeder.php
+++ Modules/HR/Database/Seeders/ResumeScreeningEvaluationSeeder.php
@@ -16,66 +16,66 @@ class ResumeScreeningEvaluationSeeder extends Seeder
      */
     public function run()
     {
-        if (! app()->environment('production')) {
+        if (!app()->environment('production')) {
             $evaluationParametersList =
-		   [  
-                    [
-                        'id' => 1,
-                        'name' => 'Resume looks good?',
-                        'segment_id' => 1,
-                        'marks' => 1
-                    ],
+                [
+                [
+                    'id' => 1,
+                    'name' => 'Resume looks good?',
+                    'segment_id' => 1,
+                    'marks' => 1,
+                ],
 
-                    [
-                        'id' => 2,
-                        'name' => 'Seems expensive?',
-                        'segment_id' => 1,
-                        'marks' => 1,
-                        'parent_id' => 1
-                    ],
+                [
+                    'id' => 2,
+                    'name' => 'Seems expensive?',
+                    'segment_id' => 1,
+                    'marks' => 1,
+                    'parent_id' => 1,
+                ],
 
-                    [
-                        'id' => 3,
-                        'name' => 'Looks ambitious?',
-                        'segment_id' => 1,
-                        'marks' => 1,
-                        'parent_id' => 1
-                    ],
+                [
+                    'id' => 3,
+                    'name' => 'Looks ambitious?',
+                    'segment_id' => 1,
+                    'marks' => 1,
+                    'parent_id' => 1,
+                ],
 
-                    [
-                        'id' => 4,
-                        'name' => 'Proceed to next round?',
-                        'segment_id' => 1,
-                        'marks' => 1,
-                        'parent_id' => 1
-                    ],
+                [
+                    'id' => 4,
+                    'name' => 'Proceed to next round?',
+                    'segment_id' => 1,
+                    'marks' => 1,
+                    'parent_id' => 1,
+                ],
 
-                    [
-                        'id' => 5,
-                        'name' => 'Possible fitment for hills?',
-                        'segment_id' => 1,
-                        'marks' => 1,
-                        'parent_id' => 1
-                    ],
+                [
+                    'id' => 5,
+                    'name' => 'Possible fitment for hills?',
+                    'segment_id' => 1,
+                    'marks' => 1,
+                    'parent_id' => 1,
+                ],
 
-                    [
-                        'name' => 'Has leadership qualities? (if relevant)',
-                        'segment_id' => 2,
-                        'marks' => 1
-                    ],
+                [
+                    'name' => 'Has leadership qualities? (if relevant)',
+                    'segment_id' => 2,
+                    'marks' => 1,
+                ],
 
-                    [
-                        'name' => 'Relevant to ColoredCow',
-                        'segment_id' => 2,
-                        'marks'=> 1
-                    ],
+                [
+                    'name' => 'Relevant to ColoredCow',
+                    'segment_id' => 2,
+                    'marks' => 1,
+                ],
 
-				   [
-						'name' => 'Won any competition? (if relevant)',
-						'segment_id'=> 2,
-						'marks'=> 1
-				   ],
-			   ];
+                [
+                    'name' => 'Won any competition? (if relevant)',
+                    'segment_id' => 2,
+                    'marks' => 1,
+                ],
+            ];
             $evaluationParametersOptions = [
                 [
                     'value' => 'Yes',
@@ -84,9 +84,9 @@ class ResumeScreeningEvaluationSeeder extends Seeder
 
                 [
                     'value' => 'No',
-                    'marks' => '-1'
+                    'marks' => '-1',
                 ],
-		   ];
+            ];
             $round = Round::where('name', 'Resume Screening')->first();
             $round->evaluationParameters()->createMany($evaluationParametersList);
             $evaluationParameters = Parameter::all();
@@ -95,17 +95,17 @@ class ResumeScreeningEvaluationSeeder extends Seeder
             }
 
             DB::table('hr_evaluation_parameters')
-            ->where('id', 2)
-            ->update(['parent_option_id' => 1]);
+                ->where('id', 2)
+                ->update(['parent_option_id' => 1]);
             DB::table('hr_evaluation_parameters')
-            ->where('id', 3)
-            ->update(['parent_option_id' => 1]);
+                ->where('id', 3)
+                ->update(['parent_option_id' => 1]);
             DB::table('hr_evaluation_parameters')
-            ->where('id', 4)
-            ->update(['parent_option_id' => 2]);
+                ->where('id', 4)
+                ->update(['parent_option_id' => 2]);
             DB::table('hr_evaluation_parameters')
-            ->where('id', 5)
-            ->update(['parent_option_id' => 2]);
+                ->where('id', 5)
+                ->update(['parent_option_id' => 2]);
         }
     }
 }
