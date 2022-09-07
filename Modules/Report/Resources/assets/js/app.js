59cabaca62510ff695e600587bc393cba0fb8fb0
Pankaj Agrawal <pankaj@coloredcow.com>
2022-09-07 12:58:09 +0530

    fixes

--- Modules/Report/Resources/assets/js/app.js
+++ Modules/Report/Resources/assets/js/app.js
@@ -1,5 +1,68 @@
-$(document).ready(function () {
-	if ($(".show-modal")) {
-		$(".show-modal").modal("show");
-	}
+$(document).ready(function() {
+  if ($(".show-modal")) {
+    $(".show-modal").modal("show");
+  }
+
+  if ($("#financeReportRevenueTrends").length) {
+    financeReportRevenueTrendsReport();
+  }
 });
+
+function financeReportRevenueTrendsReport() {
+  const canvasElementId = "financeReportRevenueTrends";
+  const labels = [
+    "April (2022)",
+    "May (2022)",
+    "June (2022)",
+    "July (2022)",
+    "August (2022)",
+    "September (2022)",
+    "October (2022)",
+    "November (2022)",
+    "December (2022)",
+    "January (2023)",
+    "February (2023)",
+    "March (2023)",
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Revenue Trends",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [11, 12, 15, 20, 23, 35, 40, 45, 48, 52, 55, 58],
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [
+          {
+            barPercentage: 0.4,
+            scaleLabel: {
+              display: true,
+              labelString: "Months",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Rs (In Lacks)",
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  new Chart(canvasElementId, config);
+}
--- Modules/Report/Resources/views/finance/dashboard.blade.php
+++ Modules/Report/Resources/views/finance/dashboard.blade.php
@@ -40,7 +40,15 @@
                     </div>
                 </div>
             </div>
+        </div>
 
+        <div>
+            <div class="mt-4 w-60p">
+                <input type="hidden" name="team_members_effort" value="">
+                <input type="hidden" name="workingDays" value="">
+                <input type="hidden" name="users" value="">
+                <canvas class="w-full" id="financeReportRevenueTrends" style="width:38em !important"></canvas>
+            </div>
         </div>
     </div>
 @endsection
