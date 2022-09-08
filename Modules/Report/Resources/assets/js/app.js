acc51289fc1092b91712649b73b85451d0e67ac7
Pankaj Agrawal <pankaj@coloredcow.com>
2022-09-08 12:21:36 +0530

    fixes

--- Modules/Report/Resources/assets/js/app.js
+++ Modules/Report/Resources/assets/js/app.js
@@ -1,63 +1,63 @@
 $(function() {
-  if ($(".show-modal")) {
-    $(".show-modal").modal("show");
-  }
+	if ($(".show-modal")) {
+		$(".show-modal").modal("show");
+	}
 
-  if ($("#financeReportRevenueTrends").length) {
-    getData(
-      { type: "revenue-trend", filters: {} },
-      financeReportRevenueTrendsReport
-    );
-  }
+	if ($("#financeReportRevenueTrends").length) {
+		getData(
+			{ type: "revenue-trend", filters: {} },
+			financeReportRevenueTrendsReport
+		);
+	}
 });
 
 function financeReportRevenueTrendsReport(reportsData) {
-  const canvasElementId = "financeReportRevenueTrends";
-  const labels = reportsData.labels;
+	const canvasElementId = "financeReportRevenueTrends";
+	const labels = reportsData.labels;
 
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Revenue Trends",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: reportsData.data,
-      },
-    ],
-  };
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "Revenue Trends",
+				backgroundColor: "rgb(255, 99, 132)",
+				borderColor: "rgb(255, 99, 132)",
+				data: reportsData.data,
+			},
+		],
+	};
 
-  const config = {
-    type: reportsData.graph_type || "bar",
-    data: data,
-    options: {
-      responsive: true,
-      scales: {
-        xAxes: [
-          {
-            barPercentage: 0.4,
-            scaleLabel: {
-              display: true,
-              labelString: "months",
-            },
-          },
-        ],
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: "Rs (In Lacks)",
-            },
-          },
-        ],
-      },
-    },
-  };
+	const config = {
+		type: reportsData.graph_type || "bar",
+		data: data,
+		options: {
+			responsive: true,
+			scales: {
+				xAxes: [
+					{
+						barPercentage: 0.4,
+						scaleLabel: {
+							display: true,
+							labelString: "months",
+						},
+					},
+				],
+				yAxes: [
+					{
+						scaleLabel: {
+							display: true,
+							labelString: "Rs (In Lacks)",
+						},
+					},
+				],
+			},
+		},
+	};
 
-  new Chart(canvasElementId, config);
+	new Chart(canvasElementId, config);
 }
 
 function getData(params, callback) {
-  url = $("#get_report_data_url").val();
-  axios.get(url, { params }).then((res) => callback(res.data));
+	url = $("#get_report_data_url").val();
+	axios.get(url, { params }).then((res) => callback(res.data));
 }
