871c15e9bad4115ee006052756844b3a64024c25
brajesh_lab <brajeshkum57@gmail.com>
2022-09-08 06:07:03 +0530

    Removing some extra lines & spaces

--- Modules/User/Resources/assets/js/app.js
+++ Modules/User/Resources/assets/js/app.js
@@ -7,19 +7,19 @@
 $(document).ready(function() {
   $("#addPancard").click(function(){
     document.getElementById("fileName").innerHTML="Upload Your Pan Card";
-    var data = "<input type='file' name=pancard id='pan'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    var data="<input type='file' name=pancard id='pan'><br><br><input type='submit' class='btn btn-primary' value='submit'>";
     document.getElementById("formElement").innerHTML=data;
   });
   
   $("#addAadhar").click(function(){
     document.getElementById("fileName").innerHTML="Upload Your Aadhar Card";
-    var data = "<input type='file' name='aadhar' id='aadhar'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    var data="<input type='file' name='aadhar' id='aadhar'><br><br><input type='submit' class='btn btn-primary' value='submit'>";
     document.getElementById("formElement").innerHTML=data;
   });
 
   $("#addSignature").click(function(){
     document.getElementById("fileName").innerHTML="Upload Your Signature";
-    var data = "<input type='file' name='signature' id='signature'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
+    var data="<input type='file' name='signature'  id='signature'><br><br> <input type='submit' class='btn btn-primary' value='submit'>";
     document.getElementById("formElement").innerHTML=data;
   });
 });
