5cc27ebe4fd0d76865e082612c67f9d991390add
brajesh_lab <brajeshkum57@gmail.com>
2022-09-10 11:54:32 +0530

    Condition for Users Table seeder should not run on production

--- database/Seeders/DatabaseSeeder.php
+++ database/Seeders/DatabaseSeeder.php
@@ -13,12 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if (!app()->environment('production')) {
-            $this->call(UsersTableSeeder::class);
-        }
-        $this->call(RolesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(BooksPermissionsSeeder::class);
-        $this->call(BookCategoriesTableSeeder::class);  
+         // roles
+         $this->call(RolesTableSeeder::class);
+         $this->call(CountriesTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+ 
+         // books
+         $this->call(BooksPermissionsSeeder::class);
+         $this->call(BookCategoriesTableSeeder::class);
+  
     }
 }
