f9e3666c4158564acedc12ef10241c1f11e27604
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-12 16:04:55 +0530

    fixes

--- Modules/Revenue/Http/Controllers/RevenueController.php
+++ Modules/Revenue/Http/Controllers/RevenueController.php
@@ -4,15 +4,10 @@ namespace Modules\Revenue\Http\Controllers;
 
 use Modules\Revenue\Entities\Revenue;
 use Illuminate\Http\Request;
-use lluminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
 
 class RevenueController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
     public function index()
     {
         $revenueData = Revenue::orderby('name')->get();
@@ -20,20 +15,6 @@ class RevenueController extends Controller
         return view('revenue::index')->with('revenues', $revenueData);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
     public function store(Request $request)
     {
         $revenue = new Revenue;
@@ -48,32 +29,11 @@ class RevenueController extends Controller
         return redirect()->route('revenue.index')->with('status', "$revenue->name created successfully!!");
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
     public function show($id)
     {
         return view('revenue::index');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
     public function update(Request $request, $id)
     {
         $revenue = Revenue::find($id);
@@ -89,11 +49,6 @@ class RevenueController extends Controller
         return redirect()->route('revenue.index')->with('status', "$revenue->name updated successfully!!");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
     public function delete($id)
     {
         $revenue = Revenue::find($id);
