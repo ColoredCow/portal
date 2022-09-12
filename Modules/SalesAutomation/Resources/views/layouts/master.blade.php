8c657d770755e06fc8753a9f98243381d32b33f6
[ Minakshi Kharai ] <[minakshikharai1507@gmail.com ]>
2022-09-12 17:33:04 +0530

    disabled the client database link

--- Modules/SalesAutomation/Resources/views/layouts/master.blade.php
+++ Modules/SalesAutomation/Resources/views/layouts/master.blade.php
@@ -8,8 +8,9 @@
 					@foreach (config('salesautomation.tabs') as $tab)
 						@php
 							$active = in_array(\Route::current()->getName(), $tab['active']) ? 'active' : '';
-						@endphp
-						<a class="nav-link {{ $active }}" href="{{ $tab['route'] ? route($tab['route']) : '#' }}" role="tab">{{ $tab['label'] }}</a>
+
+@endphp
+						<a class="nav-link {{$tab['label'] === "Client Database" ? 'disabled' : ""}} {{ $active }}" href="{{ $tab['route'] ? route($tab['route']) : '#' }}" role="tab">{{ $tab['label'] }}</a>
 					@endforeach
 				</div>
 			</div>
