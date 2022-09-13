cc77bab998c5b22347c1cc9fd32a7bdfd0d5efd1
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-13 22:04:06 +0530

    CS Fix

--- Modules/Revenue/Routes/web.php
+++ Modules/Revenue/Routes/web.php
@@ -12,17 +12,8 @@
 */
 
 Route::prefix('revenue')->group(function () {
-<<<<<<< Updated upstream
     Route::get('/', 'RevenueProceedController@index')->name('revenue.proceeds.index');
     Route::post('/store', 'RevenueProceedController@store')->name('revenue.proceeds.store');
     Route::post('/update/{id}', 'RevenueProceedController@update')->name('revenue.proceeds.update');
     Route::get('/delete/{id}', 'RevenueProceedController@delete')->name('revenue.proceeds.delete');
-=======
-    Route::get('/', 'RevenueController@index')->name('revenue.index');
-    Route::get('create', 'RevenueController@create')->name('revenue.create');
-    Route::get('store', 'RevenueController@store')->name('revenue.store');
-    Route::get('/edit/{id}', 'RevenueController@edit')->name('revenue.edit');
-    Route::get('/delete/{id}', 'RevenueController@delete')->name('revenue.delete');
-    Route::post('/update/{id}', 'RevenueController@update')->name('revenue.update');
->>>>>>> Stashed changes
 });
