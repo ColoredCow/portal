7071a5a784be210b7db065a197f733daa7ac9f2d
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-13 22:32:19 +0530

    fixed error

--- Modules/Revenue/Database/Migrations/2022_09_09_171756_create_revenue_table.php
+++ Modules/Revenue/Database/Migrations/2022_09_09_171756_create_revenue_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRevenueProceedsTable extends Migration
+class CreateRevenueTable extends Migration
 {
     /**
      * Run the migrations.
