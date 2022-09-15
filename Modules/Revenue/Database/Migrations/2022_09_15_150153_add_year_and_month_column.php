d16b7246d1d74625011158c388980fc33be50936
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-15 15:14:50 +0530

    added Year and Month column

---
+++ Modules/Revenue/Database/Migrations/2022_09_15_150153_add_year_and_month_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddYearAndMonthColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('revenue_proceeds', function (Blueprint $table) {
+            $table->integer('year')->after('notes');
+            $table->integer('month')->after('notes');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('revenue_proceeds', function (Blueprint $table) {
+            $table->dropColumn(['month', 'year']);
+        });
+    }
+}
