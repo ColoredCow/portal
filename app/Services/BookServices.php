dd5562cc0611e2616243ddb7ab34a2dfb7e9ad94
amisha bisht <amishabisht33@gmail.comgit status>
2022-09-17 17:49:25 +0530

    resolved reviews

--- app/Services/BookServices.php
+++ app/Services/BookServices.php
@@ -8,6 +8,9 @@ use Vision\Request\Image\LocalImage;
 use Vision\Vision;
 use App\Models\KnowledgeCafe\Library\Book;
 use Illuminate\Support\Facades\DB;
+use PhpOption\None;
+
+use function PHPUnit\Framework\isNull;
 
 class BookServices
 {
@@ -70,7 +73,7 @@ class BookServices
     {
         $userBookReader = DB::table('book_readers')->select('*')->where('user_id', auth()->user()->id)->first()->library_book_id;
         $readBooks = Book::where('id', $userBookReader)->get();
-        $userBookBorrower = DB::table('book_borrower')->where('user_id', auth()->user()->id)->first()->library_book_id;
+        $userBookBorrower = DB::table('book_borrower')->where('user_id', auth()->user()->id)->first()->library_book_id ?? '-' ;
         $borrowedBooks = Book::where('id', $userBookBorrower)->get();
 
         return ['readBooks'=>$readBooks, 'borrowedBooks'=>$borrowedBooks];
