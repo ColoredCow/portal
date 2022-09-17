2df610ba20c579e4157bafe45d2dc895461306c0
vikramaditaya kumar verma <ditya0504@gmail.com>
2022-09-17 18:17:51 +0530

    Added menu section

--- resources/views/hr/recruitment/campaigns.blade.php
+++ resources/views/hr/recruitment/campaigns.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-md-12 text-center">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            @include('hr.menu')
+            <br><br>
+        </div>
+        <div class="col-md-12">
+            @include('status', ['errors' => $errors->all()])
+        </div>
+        <div class="col-md-12 text-center">
         <h1>Coming Soon!!</h1>
+        </div>
     </div>
+</div>
 @endsection
