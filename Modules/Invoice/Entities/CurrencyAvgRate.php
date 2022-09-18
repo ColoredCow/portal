080bbd40a5a5ce590f3c99e0f911b24fb6a623fd
Himanshunegi0332 <himanshunegi0332@gmail.com>
2022-09-18 12:09:39 +0530

    avg_usd_rates

--- Modules/Invoice/Entities/CurrencyAvgRate.php
+++ Modules/Invoice/Entities/CurrencyAvgRate.php
@@ -4,8 +4,8 @@ namespace Modules\Invoice\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CurrencyAvgRates extends Model
+class CurrencyAvgRate extends Model
 {
     protected $table = 'currency_avg_rate';
-    protected $fillable = ['currency', 'captured_for', 'avg_rate', 'created_at', 'updated_at'];
+    protected $fillable = ['currency', 'captured_for', 'avg_rate'];
 }
--- Modules/Invoice/Services/InvoiceService.php
+++ Modules/Invoice/Services/InvoiceService.php
@@ -24,7 +24,6 @@ use Modules\Invoice\Emails\SendPaymentReceivedMail;
 use Modules\Project\Entities\Project;
 use Modules\Invoice\Exports\YearlyInvoiceReportExport;
 use Modules\Invoice\Entities\LedgerAccount;
-use Modules\Invoice\Entities\CurrencyAvgRates;
 
 class InvoiceService implements InvoiceServiceContract
 {
@@ -158,12 +157,6 @@ class InvoiceService implements InvoiceServiceContract
         $invoice = Invoice::create($data);
         $this->saveInvoiceFile($invoice, $data['invoice_file']);
         $this->setInvoiceNumber($invoice, $data['sent_on']);
-        $currentDate = Carbon::now();
-        CurrencyAvgRates::create([
-            'currency' =>  $data['currency'],
-            'captured_for' => $currentDate,
-            'avg_rate' => $this->currencyService()->getCurrentRatesInINR(),
-        ]);
 
         return $invoice;
     }
