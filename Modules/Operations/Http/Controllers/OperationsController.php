aaef32f1d708bd400d5c519ad314d4cb8e1fd819
gulshan81 <gulshansoni613@gmail.com>
2022-09-19 19:09:29 +0530

    fixed the ci checks

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -2,19 +2,8 @@
 
 namespace Modules\Operations\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class OperationsController extends Controller
 {
-    public function index()
-    {
-        return view('operations::index');
-    }
-
-    public function create()
-    {
-        return view('operations::create');
-    }
 }
