0ffd527902eb15908d26b1f26b2b998526f77f33
gulshan81 <gulshansoni613@gmail.com>
2022-09-19 13:52:04 +0530

    fixed the ci checks

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
 {
     use Notifiable, HasRoles, HasWebsiteUser;
 
-    /** 
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
