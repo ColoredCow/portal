f1e1002267244e659c6e0ef12a05e6731e682560
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-19 12:03:28 +0530

    ci fix

--- Modules/Revenue/Entities/RevenueProceed.php
+++ Modules/Revenue/Entities/RevenueProceed.php
@@ -16,12 +16,12 @@ class RevenueProceed extends Model
     public function scopeApplyFilters($query, $filters)
     {
         if ($year = Arr::get($filters, 'year', false)) {
-            $query = $query->where(function ($q) use($year) {
+            $query = $query->where(function ($q) use ($year) {
                 $q->where('year', $year)->where('month','>', 3);
-            })->orWhere(function ($q) use($year) {
+            })->orWhere(function ($q) use ($year) {
                 $q->where('year', $year+1)->where('month','<',4);
             });
-            }
+        }
 
         return $query;
     }
