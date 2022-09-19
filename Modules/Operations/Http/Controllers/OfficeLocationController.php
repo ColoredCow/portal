2ef5b76db4edc9896ba82140db90cc4849195180
gulshan81 <gulshansoni613@gmail.com>
2022-09-19 18:56:14 +0530

    fixed the ci checks

--- Modules/Operations/Http/Controllers/OfficeLocationController.php
+++ Modules/Operations/Http/Controllers/OfficeLocationController.php
@@ -10,6 +10,12 @@ use Modules\Operations\Entities\OfficeLocation;
 
 class OfficeLocationController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     *
+     */
     public function index()
     {
         $officelocation = OfficeLocation::all();
@@ -21,13 +27,19 @@ class OfficeLocationController extends Controller
         ]);
     }
 
-
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('operations::create');
     }
 
-
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -49,19 +61,31 @@ class OfficeLocationController extends Controller
         return response()->json(['success' => 'success'], 200);
     }
 
-
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($id)
     {
         return view('operations::show');
     }
 
-
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     */
     public function edit($id)
     {
         // return view('operations::edit');
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
@@ -84,7 +108,10 @@ class OfficeLocationController extends Controller
         return response()->json(['success' => 'success'], 200);
     }
 
-
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     */
     public function destroy($id)
     {
         $officelocation = Officelocation::find($id);
--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -8,72 +8,13 @@ use Illuminate\Routing\Controller;
 
 class OperationsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
     public function index()
     {
         return view('operations::index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
     public function create()
     {
         return view('operations::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('operations::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('operations::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
