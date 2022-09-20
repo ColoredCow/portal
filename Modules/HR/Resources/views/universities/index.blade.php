6863c74191b538d3394a86033882763f1a0ca3b8
brajesh_lab <brajeshkum57@gmail.com>
2022-09-20 10:28:47 +0530

    Removing extra loop from index

--- Modules/HR/Resources/views/universities/index.blade.php
+++ Modules/HR/Resources/views/universities/index.blade.php
@@ -38,11 +38,11 @@
                 <th>Contact Details</th>
                 <th>Actions</th>
             </tr>
-           @foreach($applications as $application)
+          
                
           
             @foreach($universities as $university)
-            @if(($application->name)==($university->name))
+        
             <tr>
                 <td>
                     <a href="{{ route('universities.edit',$university) }}" class="font-weight-bold text-primary">{{$university->name}}</a>
@@ -73,8 +73,7 @@
                     </form>
                 </td>
             </tr>
-            @endif
-            @endforeach
+        
             @endforeach
         </table>
     </div>
