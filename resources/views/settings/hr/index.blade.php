a421a74ded1c4d286288d70d820d2ba5827ed8e7
Pankaj Kandpal <pankaj.kandpal@coloredcow.in>
2022-09-22 15:22:13 +0530

    code cleanups

--- resources/views/settings/hr/index.blade.php
+++ resources/views/settings/hr/index.blade.php
@@ -23,7 +23,7 @@
 		@foreach ($roundMailTypes as $type)
 			@php
 				$mailTemplate = $type['label'] . '_mail_template';
-					$round->{$mailTemplate} = $round->{$mailTemplate};
+				$round->{$mailTemplate} = $round->{$mailTemplate};
 			@endphp
 			<div class="card mt-4">
 				<form action="{{ route('hr.round.update', $round->id) }}" method="POST">
