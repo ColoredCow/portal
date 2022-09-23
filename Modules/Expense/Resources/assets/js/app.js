a2325376034d8685d3829c52d7d09a35a84c0510
Abhishek25081999 <negiabhi03@gmail.com>
2022-09-23 18:26:11 +0530

    esLint

--- Modules/Expense/Resources/assets/js/app.js
+++ Modules/Expense/Resources/assets/js/app.js
@@ -4,7 +4,7 @@ $(function() {
 	  frm.find("input").val("");
 	  $(".parent:last").after(frm);
 	});
-  });
+});
 $("body").on("click", ".btn-danger", function() {
-    var closest = $(this).closest(".parent").remove();
+	var closest = $(this).closest(".parent").remove();
 });
