f82954821e0d8a8640f9f7fda6a8710c695160e0
Lakshya Varshney <lakshya.varshney_iot18@gla.ac.in>
2022-09-24 15:48:03 +0530

    cleanups

--- resources/views/hr/application/inline-resume.blade.php
+++ resources/views/hr/application/inline-resume.blade.php
@@ -18,9 +18,6 @@
                 </span>
             </li>
         </ul>
-        <div class="d-none alert alert-success container " id="success" role="alert">
-            <strong>Saved Successfully!</strong>
-        </div>
     </div>
     <div class="card-body p-0">
         <iframe src="{{ $resume }}#zoom=80"></iframe>
