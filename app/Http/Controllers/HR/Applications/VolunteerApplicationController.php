8d1e001b84e06b9cb027e02333605029749e35e8
karan muralidharan <59615903+11karangm@users.noreply.github.com>
2022-09-30 18:11:50 +0530

    feature-2555-review

--- app/Http/Controllers/HR/Applications/VolunteerApplicationController.php
+++ app/Http/Controllers/HR/Applications/VolunteerApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\HR\Applications;
 
 use Modules\HR\Entities\Application;
+use Modules\HR\Entities\Job;
 use Illuminate\Support\Facades\Request;
 use Modules\HR\Http\Controllers\Recruitment\ApplicationController;
 
@@ -33,8 +34,32 @@ class VolunteerApplicationController extends ApplicationController
         $attr = [
             'applications' => $applications,
             'status' => request()->get('status'),
+            'volunteer_on_hold_count' => $this->getCount(['on-hold']),
+            'sent_for_approval_count' => $this->getCount(['sent-for-approval']),
+            'no_show_count' => $this->getCount(['no-show-reminded']),
+            'closed_count' => $this->getCount(['rejected']),
+            'open_count' => $this->getCount(['new','in-progress']),
+            'job-type' => $this->getApplicationType(),
+            'jobs' => Job::where('type','volunteer')->get(),
         ];
+        
 
-        return view('hr.application.volunteer.index')->with($attr);
+        return view('hr.application.volunteer.index')->
+            with($attr
+            );
+    }
+
+    public function getCount($current_status){
+
+        $jobs = Job::where('type','volunteer')->get('id');
+        $data = [];
+        foreach ($jobs as $job) {
+            $data[] = $job->id;
+        }
+        $todayCount = Application::whereIn('hr_job_id', $data)
+            ->whereIn('status',$current_status)
+            ->count();
+        return $todayCount;
+
     }
 }
