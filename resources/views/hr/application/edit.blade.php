ccf0b225f046fd23e5ff7b50ba8feacf0896e571
brajesh_lab <brajeshkum57@gmail.com>
2022-10-01 11:14:50 +0530

    hide send mail button from To Approve Tab

--- resources/views/hr/application/edit.blade.php
+++ resources/views/hr/application/edit.blade.php
@@ -1269,7 +1269,7 @@
                                                             {{-- <button type="button" class="btn btn-outline-danger ml-2 round-submit" data-action="reject" data-toggle="modal" data-target="#application_reject_modal">Reject</button> --}}
                                                             {{-- @endif --}}
                                                         @endif
-                                                        @if (!is_null($applicationRound->round_status) && !$applicationRound->mail_sent)
+                                                        @if ($applicationRound->mail_sent)
                                                             <button type="button" class="btn btn-primary ml-auto"
                                                                 data-toggle="modal"
                                                                 data-target="#round_{{ $applicationRound->id }}">Send
