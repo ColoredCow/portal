1a795040254d89e5310950c5e9de1e481a4ca419
Abhishek25081999 <negiabhi03@gmail.com>
2022-10-01 18:42:27 +0530

    CI fixes

--- Modules/Expense/Services/ExpenseService.php
+++ Modules/Expense/Services/ExpenseService.php
@@ -55,7 +55,7 @@ class ExpenseService
                     'file_type' => $file['type']
                 ]);
             }
-        }else{
+        } else {
             ExpenseFile::where('expense_id', $id)->delete();
         }
     }
