5c05d0cafe8c8da2d06898962b61e55a0a1f5594
gulshan81 <gulshansoni613@gmail.com>
2022-10-04 17:42:35 +0530

    fixed the ci checks

--- Modules/User/Resources/views/profile/show-basic-details.blade.php
+++ Modules/User/Resources/views/profile/show-basic-details.blade.php
@@ -74,24 +74,24 @@
         </div>
         <div>
             <div class="form-group">
-                <label class="font-weight-bold" for="">Name:</label>
+                <label class="font-weight-bold" for=""> Name:</label>
                 <span>{{ $user->name }}</span>
             </div>
             <div class="form-group">
-                <label class="font-weight-bold" for="">Nickame:</label>
+                <label class="font-weight-bold" for=""> Nickame:</label>
                 <span>{{ $user->nickname }}</span>
             </div>
             <div class="form-group">
-                <label class="font-weight-bold" for="">Designation:</label>
+                <label class="font-weight-bold" for=""> Designation:</label>
                 <span>{{ $user->employee->designation }}</span>
             </div>
             <div class="form-group">
-                <label class="font-weight-bold" for="">Location:</label>
+                <label class="font-weight-bold" for=""> Location:</label>
                 <span>{{ $user->officelocation->location}}</span>
             </div>
 
             <div class="form-group">
-                <label class="font-weight-bold" for="">Domain:</label>
+                <label class="font-weight-bold" for=""> Domain:</label>
                 @foreach ($domains as $domain )
                     <span>{{ $user->employee->domain_id == $domain['id'] ? $domain['domain'] : "" }}</span>
                 @endforeach
