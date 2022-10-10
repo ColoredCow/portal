ab32e09a22c08e308f86fafb9d0df78e08e2e9a4
Vimal <vm.21cs48@thdcihet.ac.in>
2022-10-10 14:37:05 +0530

    modal added to edit functionality

--- Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
+++ Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
@@ -40,7 +40,7 @@
 									    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 									    <button type="submit" class="btn btn-primary">Submit</button>
                                       </div>
-								    </form>
+</form>
 								
 								</div>
 								</div>
