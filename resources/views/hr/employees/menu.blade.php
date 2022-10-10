e7c8854f50acce066806c1e4c78204ea78fed026
karan muralidharan <59615903+11karangm@users.noreply.github.com>
2022-10-10 12:14:01 +0530

    work

--- resources/views/hr/employees/menu.blade.php
+++ resources/views/hr/employees/menu.blade.php
@@ -3,28 +3,27 @@
         @php
           $params = array_merge(['name' => 'employee'], ['status' => 'current']);
         @endphp
-        <a class="nav-item nav-link {{ Request::is('hr/employees*') ? 'active' : '' }}" href="{{ route('employees',$params) }}"><i class="fa fa-users"></i>&nbsp;Employees</a>
+        <a class="nav-item nav-link {{ request()->input('name') === 'employee' ? 'active' : '' }}" href="{{ route('employees',$params) }}"><i class="fa fa-users"></i>&nbsp; Employees </a>
     </li>
-
     <li class="nav-item">
         @php
           $params = array_merge(['name' => 'intern'], ['status' => 'current']);
         @endphp
-        <a style="margin:0em 1em" class="nav-item nav-link {{ Request::is('hr/employees*') ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Intern</a>
+        <a class="nav-item nav-link {{ request()->input('name') === 'intern' ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Intern</a>
     </li>
 
     <li class="nav-item">
-        @php
+        @php 
           $params = array_merge(['name' => 'contractor'], ['status' => 'current']);
         @endphp
-        <a class="nav-item nav-link {{ Request::is('hr/employees*') ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Contractor</a>
+        <a class="nav-item nav-link {{ request()->input('name') === 'contractor' ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Contractor</a>
     </li>
 
     <li class="nav-item">
         @php
           $params = array_merge(['name' => 'support-staff'], ['status' => 'current']);
         @endphp
-        <a style="margin:0em 1em" class="nav-item nav-link {{ Request::is('hr/employees*') ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Support Staff</a>
+        <a class="nav-item nav-link {{ request()->input('name') === 'support-staff' ? 'active' : '' }}" href="{{ route('employees', $params) }}"><i class="fa fa-users"></i>&nbsp;Support Staff</a>
     </li>
 
     <li class="nav-item">
