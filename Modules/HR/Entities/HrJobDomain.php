c4d25cf52975764c88ccccc33c73162b6d3f70f7
karan muralidharan <59615903+11karangm@users.noreply.github.com>
2022-10-11 11:15:47 +0530

    mapping

--- Modules/HR/Entities/HrJobDomain.php
+++ Modules/HR/Entities/HrJobDomain.php
@@ -3,6 +3,8 @@
 namespace Modules\HR\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\HR\Entities\HrJobDesignation;
+
 
 class HrJobDomain extends Model
 {
@@ -13,5 +15,10 @@ class HrJobDomain extends Model
     public function scopeSlug($query, $slug)
     {
         return $query->where('slug', $slug);
+    }
+
+    public function designations()
+    {
+        return $this->hasMany(HrJobDesignation::class);
     }
 }
