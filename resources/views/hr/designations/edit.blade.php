46420fcfe29ada582c78e93ff7833d54e3662ea2
karan muralidharan <59615903+11karangm@users.noreply.github.com>
2022-10-12 13:12:07 +0530

    removed endforeach

--- resources/views/hr/designations/edit.blade.php
+++ resources/views/hr/designations/edit.blade.php
@@ -20,8 +20,7 @@
                         @php
                         foreach($domains as $domain)
                             if($designation->domain_id!=NULL && $domain->id===($designation->domain_id)) $domainName = $domain->domain; 
-                            if($designation->domain_id==NULL) $domainName = ' ';
-                        endforeach   
+                            if($designation->domain_id==NULL) $domainName = ' '; 
                         @endphp 
                         <select name="domain" class="form-control" >
                             <option value="{{$designation->domain_id}}">{{$domainName}}</option>
