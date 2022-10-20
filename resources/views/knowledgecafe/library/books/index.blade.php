2d47858c9f24f5f0ddb6b186e5c8f95636406e65
Anjali Mehra <mehraanjali382@gmail.com>
2022-10-20 11:02:54 +0530

    added search icon

--- resources/views/knowledgecafe/library/books/index.blade.php
+++ resources/views/knowledgecafe/library/books/index.blade.php
@@ -25,7 +25,8 @@
     <div class="row mt-3 mb-2 px-2">
         <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 mr-2 mb-2 p-2 d-flex justify-content-center align-items-center">
             <input type="text" data-value="{{ request()->input('search') }}" class="form-control" id="search_input" placeholder="Search By book name" v-model="searchKey">
-            <button class="btn btn-info ml-2 py-1.5" @click="searchBooks()">Search</button>
+            
+            <button class="btn btn-info ml-2 py-1.5" @click="searchBooks()"><i class="fa fa-search">Search</i></button>
         </div>
         <div>
             <div class="col-lg-12 col-md-5 col-sm-6 col-xs-12 mr-2 mb-2 p-2 d-flex justify-content-center ">
