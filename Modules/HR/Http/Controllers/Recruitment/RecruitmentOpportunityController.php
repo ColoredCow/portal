b369ee144a1e6a99c7504ab5bd25cc59b5dbd766
vikramaditaya kumar verma <ditya0504@gmail.com>
2022-10-21 14:00:12 +0530

    changed code

--- Modules/HR/Http/Controllers/Recruitment/RecruitmentOpportunityController.php
+++ Modules/HR/Http/Controllers/Recruitment/RecruitmentOpportunityController.php
@@ -21,16 +21,15 @@ class RecruitmentOpportunityController extends JobController
     {
         $this->authorize('list', Job::class);
         $search = request()->query('title') ?? '';
+        $jobs = Job::with('applications', 'applications.applicant', 'jobRequisition')
+            ->typeRecruitment()
+            ->latest();
+        
         if ($search != '') {
-            $jobs = Job::where('title', 'LIKE', "%$search%")
-                ->paginate(config('constants.pagination_size'));
-        } else {
-            $jobs = Job::with('applications', 'applications.applicant', 'jobRequisition')
-                ->typeRecruitment()
-                ->latest()
-                ->paginate(config('constants.pagination_size'))
-                ->appends(Request::except('page'));
-        }
+            $jobs = $jobs->where('title', 'LIKE', "%$search%")->orwhere('type', 'LIKE', "%$search%");
+        }       
+        $jobs = $jobs->paginate(config('constants.pagination_size'))
+            ->appends(Request::except('page'));
 
         return view('hr.job.index')->with([
             'jobs' => $jobs,
