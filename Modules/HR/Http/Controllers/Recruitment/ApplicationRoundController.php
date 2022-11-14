d9b12adbc9bacf3ed7db8a9d436b8081422d96fe
anuj kumar <kanuj101890@gmail.com>
2022-11-14 22:21:18 +0530

    changes in routing according to toggle

--- Modules/HR/Http/Controllers/Recruitment/ApplicationRoundController.php
+++ Modules/HR/Http/Controllers/Recruitment/ApplicationRoundController.php
@@ -26,9 +26,9 @@ class ApplicationRoundController extends Controller
             $round->updateOrCreateEvaluation($request->validated()['round_evaluation']);
         }
 
-    $routeName = $round->application->job->type == 'internship' ? 'applications.internship.index' : 'applications.job.index';
-    $var = $request->validated();
-        if ($var['action'] === 'update' || $var === 'schedule-update') {
+        $routeName = $round->application->job->type == 'internship' ? 'applications.internship.index' : 'applications.job.index';
+        $var = $request->validated();
+        if ($var['action'] == 'update' || $var == 'schedule-update') {
             return redirect()->back()->with('status', 'success');
         } else {
             return (redirect()->route($routeName)->with('status', 'Application updated successfully!'));
