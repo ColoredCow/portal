e26e9b748d90dc311afe01e22e8b6c3d6f3a8a3a
rajatsajwan <rajatsajwan1998@gmail.com>
2022-11-29 19:49:51 +0530

    ci fix

--- Modules/Project/Entities/Project.php
+++ Modules/Project/Entities/Project.php
@@ -137,11 +137,11 @@ class Project extends Model implements Auditable
     }
 
     public function getVelocityForMonthAttribute($monthToSubtract, $startDate = null, $endDate = null)
-    {   
+    {
         $startDate = $startDate ?? $this->client->month_start_date;
         $endDate = $endDate ?? $this->client->month_end_date;
 
-        return $this->getExpectedHoursInMonthAttribute($startDate, $endDate) ? round($this->getHoursBookedForMonth($monthToSubtract, $startDate, $endDate) / ($this->getExpectedHoursInMonthAttribute($startDate, $endDate)), 2) : 0;     
+        return $this->getExpectedHoursInMonthAttribute($startDate, $endDate) ? round($this->getHoursBookedForMonth($monthToSubtract, $startDate, $endDate) / ($this->getExpectedHoursInMonthAttribute($startDate, $endDate)), 2) : 0;
     }
 
     public function getCurrentHoursForMonthAttribute()
