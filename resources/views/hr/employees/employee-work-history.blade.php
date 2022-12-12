0424154faa8fcb902ade41f51ac5b99b67025691
anuj kumar <kanuj101890@gmail.com>
2022-12-12 16:23:12 +0530

    ui and visuals changes in  employeeworkhistory page

--- resources/views/hr/employees/employee-work-history.blade.php
+++ resources/views/hr/employees/employee-work-history.blade.php
@@ -3,10 +3,10 @@
 <div class="container">
     <div><h1>Work History</h1></div>
     <div class="container">
-  <table class="table table-bordered justify-content-center m-5">
-  <thead>
+  <table class="table table-bordered justify-content-center m-5 p-3 mb-2 bg-white text-dark">
+  <thead class="bg-secondary text-white">
     <tr>
-      <th class="text-center" scope="col">Projects</th>
+      <th class="text-center " scope="col">Projects</th>
       <th class="text-center" scope="col">Clients</th>
       <th class="text-center" scope="col">TechStacks</th>
     </tr>
@@ -16,9 +16,9 @@
     <tr>
       <td class="text-center"><a href="{{route('project.edit', $details->project  )}}">{{ $details->project['name'] }}</a></td>    
       <td class="text-center">{{ $details->project->client['name'] }}</td>
-      <td class="text-center  ">
+      <td class="text-center ">
           @foreach ($allTechStacks[$details["project_id"]] as $TechStacks )
-            <div class="row justify-content-md-center "> 
+            <div class="row col align-self-center "> 
               @forelse($TechStacks as $tech)
               <div class="ml-3 col col-lg-2">  
                 <p>{{ $tech }}</p>
