e28500084488336c4eb0a2df5d6f15cb6692272a
anuj kumar <kanuj101890@gmail.com>
2022-12-23 20:12:58 +0530

    add new drop migration column

--- database/migrations/2022_12_23_200850_drop_designation_from_project_team_members.php
+++ database/migrations/2022_12_23_200850_drop_designation_from_project_team_members.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDesignationIdToProjectTeamMembers extends Migration
+class DropDesignationFromProjectTeamMembers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,10 @@ class AddDesignationIdToProjectTeamMembers extends Migration
     public function up()
     {
         Schema::table('project_team_members', function (Blueprint $table) {
-            $table->unsignedBigInteger('designation_id')->nullable();
-            $table->foreign('designation_id')->references('id')->on('hr_job_designation');
+            $table->dropColumn('designation');
         });
     }
-   
+
     /**
      * Reverse the migrations.
      *
@@ -27,8 +26,7 @@ class AddDesignationIdToProjectTeamMembers extends Migration
     public function down()
     {
         Schema::table('project_team_members', function (Blueprint $table) {
-            $table->dropForeign(['designation_id']);
-            $table->dropColumn(['designation_id']);
+            $table->string('designation');
         });
     }
 }
