b050ebcb8fd5ce84a512ced949945db309b6e7d1
race45 <sushantsaurabh098@gmail.com>
2022-12-23 19:13:24 +0530

    Inactive client not getting shown when creating a new project

--- Modules/Project/Services/ProjectService.php
+++ Modules/Project/Services/ProjectService.php
@@ -108,19 +108,15 @@ class ProjectService implements ProjectServiceContract
     }
 
     public function getClients($status = 'active')
-        {
-            if($status == 'active'){
-                $client = Client::where('status', 'active')->orderBy('name')->get();
+    {
+        if ($status == 'active'){
+            $client = Client::where('status', 'active')->orderBy('name')->get();
+        }elseif ($status == 'inactive'){
+                $client = Client::where('status', 'inactive')->orderBy('name')->get();
+        }else {
+                $client = Client::where('status', 'active')->orWhere('status', 'inactive')->orderBy('name')->get();
         }
-            elseif($status == 'inactive'){
-                 $client = Client::where('status', 'inactive')->orderBy('name')->get();
-        }
-            else{
-                 $client = Client::where('status', 'active')
-                       ->orWhere('status', 'inactive')
-                       ->orderBy('name')->get();
-        }
-        return $client;
+    return $client;
     }
 
     public function getTeamMembers()
