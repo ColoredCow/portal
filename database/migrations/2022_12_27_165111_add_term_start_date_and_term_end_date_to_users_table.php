ffe42010182b4ab0268b5f6135edbc7bcc7d4a04
Rahul <rahul.panwar16@outlook.com>
2022-12-27 17:02:09 +0530

    add_columns_to_invoices_table

---
+++ database/migrations/2022_12_27_165111_add_term_start_date_and_term_end_date_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTermStartDateAndTermEndDateToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->date('term_start_date');
+            $table->date('term_end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn('term_start_date');
+            $table->dropColumn('term_end_date');
+
+        });
+    }
+}
