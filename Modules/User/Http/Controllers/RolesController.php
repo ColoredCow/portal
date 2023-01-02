52fa66c639bac82492ed041c00485ccc904e79fd
Shubham Belwal <belwalshubham08@gmail.com>
2023-01-02 15:31:58 +0530

    minor identation fixses

--- Modules/User/Http/Controllers/RolesController.php
+++ Modules/User/Http/Controllers/RolesController.php
@@ -11,7 +11,7 @@ use Modules\User\Services\addRoleModel;
 
 class RolesController extends ModuleBaseController
 {
-    /**
+    /*
      * Display a listing of the resource.
      */
     use AuthorizesRequests;
