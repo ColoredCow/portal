22df8eb87170c42a33dba3bf4f5f00ae19b622ab
rajatsajwan <rajatsajwan1998@gmail.com>
2023-01-19 17:31:31 +0530

    ci fix

--- Modules/HR/Routes/web.php
+++ Modules/HR/Routes/web.php
@@ -99,7 +99,7 @@ Route::middleware('auth')->group(function () {
             Route::post('/teaminteraction', 'JobApplicationController@generateTeamInteractionEmail');
             Route::get('/finishinterview', 'JobApplicationController@markInterviewFinished')->name('markInterviewFinished');
             Route::get('/onHoldEmail', 'JobApplicationController@generateOnHoldEmail');
-            Route::get('/onRejectEmail','JobApplicationController@generateRejectEmail');
+            Route::get('/onRejectEmail', 'JobApplicationController@generateRejectEmail');
             Route::resource('internship', 'InternshipApplicationController')
                 ->only(['index', 'edit'])
                 ->names(['index' => 'applications.internship.index', 'edit' => 'applications.internship.edit']);
