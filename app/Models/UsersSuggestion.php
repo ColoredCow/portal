f1335849bdc210154c658364b6b177c29b8e472c
anuj kumar <kanuj101890@gmail.com>
2023-01-19 19:19:11 +0530

    remove imported user model in user_suggestion

--- app/Models/UsersSuggestion.php
+++ app/Models/UsersSuggestion.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Modules\HR\Entities\Employee;
-use Modules\User\Entities\User;
 
 class UsersSuggestion extends Model
 {
