7e0f8427cee7bd4c930172e816e1c5dc77262ce3
nishant kumar <nishantumu@gmail.com>
2023-01-21 13:06:59 +0530

    phase 1.1 codetrek module

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -148,6 +148,12 @@
         </li>
     @endcan
 
+    @can('codetrek.view')
+    <li class="nav-item">
+        <a class="nav-item nav-link" href="{{('/codetrek') }}">CodeTrek</a>
+    </li>
+@endcan
+
     @can('media.view')
         <li class="nav-item">
             <a class="nav-item nav-link" href="{{ route('media.index') }}">Media</a>
