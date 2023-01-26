6459094f0f72f8cc37633b61fd46a8a349539ba3
nishant kumar <nishantumu@gmail.com>
2023-01-26 11:31:57 +0530

    phase 1.2 added applicant module

--- Modules/CodeTrek/Resources/views/index.blade.php
+++ Modules/CodeTrek/Resources/views/index.blade.php
@@ -1,9 +1,55 @@
 @extends('codetrek::layouts.master')
-
+@section('heading','CodeTrek')
 @section('content')
 <div class="container">
-   <div class="col-12  text-center">
-        <h5 >Coming Soon</h5>
-   </div>
-  </div>
+     <div class="col-lg-12 d-flex justify-content-between align-items-center mx-auto">
+          <div>
+              <h1>@yield('heading')</h1>
+          </div>
+      <div>
+        @include('codetrek::modals.add-applicant-modal')
+     </div>
+      </div><br><br>
+     <ul class="nav nav-pills">
+      @php
+          $request = request()->all();
+      @endphp
+      <li class="nav-item mr-3">
+          @php
+              $request['status'] = 'applicants';
+          @endphp
+          <a class="nav-link {{ (request()->input('status', 'active') == 'applicants')  ? 'active' : '' }}" href="{{ route('codetrek.index', $request)  }}"><i class="fa fa-list-ul"></i> Applicants</a>
+      </li>
+
+      <li class="nav-item">
+          @php
+              $request['status'] = 'reports';
+          @endphp
+          <a class="nav-link {{ (request()->input('status', 'active') == 'reports') ? 'active' : '' }}"  href="{{ route('codetrek.index', $request)  }}"><i class="fa fa-pie-chart"></i> Reports</a>
+      </li>
+
+  </ul>
+      @if (request()->status == 'applicants')
+      <div>
+        <br>
+          <table class="table table-bordered table-striped">
+               <thead class="thead-dark">
+                   <tr class="text-center sticky-top">
+                       <th class="col-md-4">Name</th>
+                       <th class="col-md-2"> Days in CodeTrek</th>
+                       <th> Status</th>
+                       <th>Feedbacks</th>
+                   </tr>
+                   <td>
+                    No applicant found
+                   </td>
+                   </table>
+      </div>
+      @elseif (request()->status=='reports')
+      <div>
+        <br><br>
+        <p align="center">Coming Soon</p>
+      </div>
+      @endif
+</div>
 @endsection
---
+++ Modules/CodeTrek/Resources/views/modals/add-applicant-modal.blade.php
@@ -0,0 +1,72 @@
+@section('popup',' Add new applicant')
+
+<button type="button" class="btn btn-success float-right" data-toggle="modal" data-target="#photoGallery">
+    <a><i class="fa fa-plus"></i>@yield('popup')</a>
+</button>
+<div id="add_applicant_details_form">
+<div class="modal fade" id="photoGallery" tabindex="-1" role="dialog" aria-labelledby="photoGalleryLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add new applicant</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="first_name" class="field-required">First Name</label>
+                        <input type="text" class="form-control" name="name" id="first_name" placeholder="Enter first name"
+                            required="required" value="">
+                    </div>
+                    <div class="form-group offset-md-1 col-md-5">
+                        <label for="last_name" class="field-required">Last Name</label>
+                        <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter last name"
+                            required="required" value="">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="email" class="field-required">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Enter applicant email"
+                            required="required" value="">
+                    </div>
+                    <div class="form-group offset-md-1 col-md-5">
+                        <label for="github_username" class="field-required">Github username</label>
+                        <input type="text" class="form-control" name="github_username" id="github_username" placeholder="Enter applicant's username"
+                            required="required" value="">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="start_date" class="field-required">Start Date</label>
+                        <input type="date" class="form-control" name="start_date" id="start_date" required="required" value="">
+                    </div>
+                    <div class="form-group offset-md-1 col-md-5">
+                        <label for="university" class="field-required">Enter University</label>
+                        <input type="text" class="form-control" name="university" id="university" placeholder="Enter University"
+                            required="required" value="">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="Course" class="field-required">Course</label>
+                        <input type="course" class="form-control" name="course" id="course" placeholder="Enter course name"
+                            required="required" value="">
+                    </div>
+                    <div class="form-group offset-md-1 col-md-5">
+                        <label for="graduation_year" class="field-required">Graduation Year</label>
+                        <select v-model="graduation_year" name="graduation_year" id="graduation_year" class="form-control" required>
+                            <option value="">Select Graduation Year</option>
+                         </select>
+                    </div>
+                </div>
+            <div class="card-footer">
+                     <button type="button" class="btn btn-primary" id="save-btn-action">Create</button>
+                 </div>
+             </div>
+         </div>
+     </div>
+    </div>
+</div>
