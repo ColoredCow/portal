bff46c76fa08c7a76bd6d1f5bd7409b6772c3686
race45 <sushantsaurabh098@gmail.com>
2023-01-27 17:25:35 +0530

    did some changes in country label

--- Modules/Client/Resources/views/subviews/create-client-details.blade.php
+++ Modules/Client/Resources/views/subviews/create-client-details.blade.php
@@ -58,7 +58,7 @@
             </select>
         </div>
         <div class="form-group offset-md-6 col-md-5">
-            <label for="channel_partner_id">Select Country</label>
+            <label for="select_country">Select Country</label>
             <select name="country_id" id="countryId" class="form-control">
                 <option value="">Select Country</option>
                 @foreach ($countries as $country)
