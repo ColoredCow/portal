75d82a5f7ca8625b063c1eee9f24c64bfd781d04
anuj kumar <kanuj101890@gmail.com>
2023-02-08 21:37:52 +0530

    fix the indentation

--- Modules/Invoice/Services/InvoiceService.php
+++ Modules/Invoice/Services/InvoiceService.php
@@ -165,7 +165,7 @@ class InvoiceService implements InvoiceServiceContract
     public function update($data, $invoice)
     {
         $invoiceValue = $this->getUpdatedAmountForRemainingInvoice($invoice);
-        if ($invoice->amount+$invoice->gst == $invoiceValue['totalProjectAmount']) {
+        if ($invoice->amount + $invoice->gst == $invoiceValue['totalProjectAmount']) {
             $data['status'] = 'paid';
         }
         $this->updateOrCreateInvoiceRemainingDetails($data, $invoice);
