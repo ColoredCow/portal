fefd3644d62db7aab7815770407dee4f45d2a6b5
rajatsajwan <rajatsajwan1998@gmail.com>
2023-02-09 15:18:27 +0530

    ci fix

--- Modules/Project/Entities/Project.php
+++ Modules/Project/Entities/Project.php
@@ -350,7 +350,7 @@ class Project extends Model implements Auditable
             return $priviousDateData->sent_on;
         } else {
             if ($startDateOfProject->start_date) {
-                return  date("Y-m-d", strtotime($startDateOfProject->start_date));
+                return  date('Y-m-d', strtotime($startDateOfProject->start_date));
             } else {
                 $currentYear = Carbon::now()->year;
                 $currentMonth = Carbon::now()->month;
