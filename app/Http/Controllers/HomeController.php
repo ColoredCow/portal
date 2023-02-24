bb802baaef35cfc5d4a5d7a7a40f2a29d4f094a6
Code Tommorrow <priyankakaintura1408@gmail.com>
2023-02-24 14:06:48 +0530

    Changes made to pass the pr checks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,6 @@ class HomeController extends Controller
     public function index()
     {
         $unreadBook = (session('disable_book_suggestion')) ? null : Book::getRandomUnreadBook();
-       
         return view('home')->with(['book' => $unreadBook]);
     }
 
