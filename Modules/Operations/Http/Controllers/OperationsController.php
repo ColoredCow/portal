2bfd43f1004bee4fe9aaec47c4ae2da0746b9ab5
Harsh <hv982837@gmail.com>
2023-02-27 21:40:32 +0530

    fixed checks

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -19,58 +19,48 @@ class OperationsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * @return Renderable
      */
     public function create()
     {
-        return view('operations::create');
+
     }
 
     /**
      * Store a newly created resource in storage.
-     * @param Request $request
      */
-    public function store(Request $request)
+    public function store()
     {
         //
     }
 
     /**
      * Show the specified resource.
-     * @param int $id
-     * @return Renderable
      */
-    public function show($id)
+    public function show()
     {
-        return view('operations::show');
+
     }
 
     /**
      * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
      */
-    public function edit($id)
+    public function edit()
     {
-        return view('operations::edit');
+
     }
 
     /**
      * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     *
      */
-    public function update(Request $request, $id)
+    public function update()
     {
         //
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param int $id
      */
-    public function destroy($id)
+    public function destroy()
     {
         //
     }
