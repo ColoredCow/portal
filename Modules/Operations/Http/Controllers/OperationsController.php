404cea3b25c80d074ed9695019f390014ee5becc
harsh vardhan <hv982837@gmail.com>
2023-03-11 17:41:52 +0530

    added opertions modules

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -2,14 +2,12 @@
 
 namespace Modules\Operations\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Routing\Controller;
 
 class OperationsController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Renderable
      */
     public function index()
     {
