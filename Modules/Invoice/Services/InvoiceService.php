fa6cacc13874d8dcd9f6cd44b295b309d2660274
Gaurav chamoli <gauravchamoli37@gmail.com>
2023-03-14 12:37:18 +0530

    ci check fix

--- Modules/Invoice/Services/InvoiceService.php
+++ Modules/Invoice/Services/InvoiceService.php
@@ -169,7 +169,7 @@ class InvoiceService implements InvoiceServiceContract
         if (isset($data['send_mail'])) {
             $emailData = $this->getSendEmailData($data, $invoice);
             Mail::queue(new SendPaymentReceivedMail($invoice, $emailData));
-            $webHookUrl=$invoice->project->google_chat_webhook_url
+            $webHookUrl = $invoice->project->google_chat_webhook_url
             ?? $invoice->client->google_chat_webhook_url;
             Notification::route('googleChat', $webHookUrl)
                 ->notify(new SendPaymentReceivedNotification($invoice));
