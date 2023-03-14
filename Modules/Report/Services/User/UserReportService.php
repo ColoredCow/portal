aec98dbcceacdba1dc1b4c7b9be47a613526cbf6
rahulpanwar16 <rahul.panwar16@outlook.com>
2023-03-14 15:28:34 +0530

    fixed the functionality

--- Modules/Report/Services/User/UserReportService.php
+++ Modules/Report/Services/User/UserReportService.php
@@ -19,53 +19,31 @@ class UserReportService
 
     public function fteTrend($user)
     {
-        $startMonth = today()->subMonths(17)->format('Y-m');
-        $endMonth = today()->format('Y-m');
-        $projectTeamMemberId = $user->projectTeamMembers->pluck('id');
-        $reportFteData = $this->getMonthsFteAttribute($startMonth, $endMonth, $projectTeamMemberId);
+        $projectTeamMemberList = $user->projectTeamMembers->pluck('id');
+        $reportFteData = $this->getMonthsFteAttribute($projectTeamMemberList);
 
         return  $reportFteData;
     }
 
-    public function getMonthsFteAttribute($startMonth, $endMonth, $projectTeamMemberId)
+    public function getMonthsFteAttribute($projectTeamMemberList)
     {
-        $data = [];
-        $date = Carbon::createFromFormat('Y-m', $startMonth);
-        $month = $date->format('m');
-        $year = $date->format('Y');
-        $endDate = Carbon::createFromFormat('Y-m', $endMonth);
-        $months = [];
+        $startMonth = today()->subMonthsNoOverflow(17);
+        $endMonth = today();
+        while ($startMonth <= $endMonth) {
+            $months[] = $startMonth->format('Y-m');
+            $teamMemberActualEffort = ProjectTeamMemberEffort::whereIn('project_team_member_id', $projectTeamMemberList)->whereMonth('added_on', $startMonth)->whereYear('added_on', $startMonth)->sum('actual_effort');
 
-        $currentMonth = Carbon::createFromFormat('Y-m', $startMonth);
-        while ($currentMonth->format('Y-m') <= $endMonth) {
-            $months[] = $currentMonth->format('Y-m');
-            $currentMonth->addMonth();
-        }
-        while ($date->lte($endDate)) {
-            $teamMemberEffort = ProjectTeamMemberEffort::where('project_team_member_id', $projectTeamMemberId)->whereMonth('added_on', $month)->whereYear('added_on', $year)->get();
-            if ($teamMemberEffort->isNotEmpty()) {
-                $actualEffort = 0;
-                foreach ($teamMemberEffort as $effort) {
-                    $actualEffort += $effort->actual_effort;
-                }
-
-                $monthEndDate = Carbon::parse($teamMemberEffort->last()->added_on);
-                $monthStartDate = Carbon::createFromDate((int) $year, (int) $month, 1);
-                $project = new Project;
-                $daysTillToday = count($project->getWorkingDaysList($monthStartDate, $monthEndDate));
-                if ($daysTillToday > 0) {
-                    $monthlyFte = round($actualEffort / ($daysTillToday * config('efforttracking.minimum_expected_hours')), 2);
-                } else {
-                    $monthlyFte = 0;
-                }
+            $monthStartDate = $startMonth->copy()->firstOfMonth();
+            $monthEndDate = $startMonth->copy()->lastOfMonth();
+            $project = new Project;
+            $workingDaysInAMonth = count($project->getWorkingDaysList($monthStartDate, $monthEndDate));
+            if ($workingDaysInAMonth > 0) {
+                $monthlyFte = round($teamMemberActualEffort / ($workingDaysInAMonth * config('efforttracking.minimum_expected_hours')), 2);
             } else {
                 $monthlyFte = 0;
             }
-            $startMonth = Carbon::createFromFormat('Y-m', $startMonth)->addMonth()->format('Y-m');
-            $date = Carbon::createFromFormat('Y-m', $startMonth);
-            $month = $date->format('m');
-            $year = $date->format('Y');
             $data[] = $monthlyFte;
+            $startMonth->addMonth();
         }
 
         return [
