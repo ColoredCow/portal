ca87ddcc928564b14eedaa357f90a0646d97baf9
harsh vardhan <hv982837@gmail.com>
2023-03-17 16:56:51 +0530

    added operations modules

---
+++ Modules/Operations/Database/Seeders/OperationsPermissionTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\operations\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission;
+
+class operationsPermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $applicantPermissions = [
+            ['name' => 'operations.view'],
+        ];
+
+        foreach ($applicantPermissions as $permission) {
+            Permission::updateOrCreate($permission);
+        }
+
+        // set permissions for admin role
+        $adminRole = Role::where(['name' => 'admin'])->first();
+        foreach ($applicantPermissions as $permission) {
+            $adminRole->givePermissionTo($permission);
+        }
+
+        // Give permissions to super-admin role
+        $superAdminRole = Role::where(['name' => 'super-admin'])->first();
+        foreach ($applicantPermissions as $permission) {
+            $superAdminRole->givePermissionTo($permission['name']);
+        }
+    }
+}
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -113,6 +113,17 @@
             </div>
         </li>
     @endcan
+     @can('operations.view')
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown_operations" class="nav-link dropdown-toggle" href="#" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Operations <span class="caret"></span>
+            </a>
+            <div class="dropdown-menu z-index-1100" aria-labelledby="navbarDropdown_operations">
+                <a class="dropdown-item"  href="{{route('office_location.index')}}" >Office location</a>
+               
+            </div>
+        </li>
+    @endcan
 
     @if (auth()->user()->canAny(['weeklydoses.view', 'library_books.view']))
         <li class="nav-item dropdown">
