2f5be1d753f8b6f9c083dfe5b39d0fb3b0db2f3f
Himanshunegi0332 <himanshunegi0332@gmail.com>
2023-03-24 22:19:48 +0530

    updated

--- Modules/Project/Notifications/GoogleChat/NotificationToUpdateEffortForProject.php
+++ Modules/Project/Notifications/GoogleChat/NotificationToUpdateEffortForProject.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Project\Notifications\GoogleChat;
 
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Modules\Project\Entities\Project;
@@ -33,7 +34,8 @@ class NotificationToUpdateEffortForProject extends Notification
     {
         $projects = Project::all();
         foreach ($projects as $project) {
-            $interval = date_diff($project->client->billingDetails->billing_date, today());
+            $date = Carbon::today()->setDay($project->client->billingDetails->billing_date);
+            $interval = date_diff($date, today());
 
             if ($interval->days == 1) {
                 return GoogleChatMessage::create()
