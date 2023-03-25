114a5c2a6edf4bd47889fc7ee257042f70265176
Gaurav chamoli <gauravchamoli37@gmail.com>
2023-03-25 18:43:54 +0530

    Ticket module init

---
+++ Modules/Ticket/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('ticket::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('ticket.name') !!}
+    </p>
+@endsection
