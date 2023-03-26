6c7f4a645ec8dc118506081f0ece369290efdec1
Gaurav chamoli <gauravchamoli37@gmail.com>
2023-03-26 12:50:26 +0530

    Ticket module init

--- Modules/Ticket/Providers/TicketServiceProvider.php
+++ Modules/Ticket/Providers/TicketServiceProvider.php
@@ -108,6 +108,7 @@ class TicketServiceProvider extends ServiceProvider
                 $paths[] = $path . '/modules/' . $this->moduleNameLower;
             }
         }
+
         return $paths;
     }
 }
