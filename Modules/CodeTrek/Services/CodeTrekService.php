3214304662132f356df66da29a8a7720a09017cf
Hemraj <bhartihemu111@gmail.com>
2023-03-27 17:08:58 +0530

    feature/2848

--- Modules/CodeTrek/Services/CodeTrekService.php
+++ Modules/CodeTrek/Services/CodeTrekService.php
@@ -7,68 +7,68 @@ use Modules\CodeTrek\Entities\CodeTrekApplicantRoundDetail;
 
 class CodeTrekService
 {
-	public function getCodeTrekApplicants()
-	{
-		$applicants = CodeTrekApplicant::all();
+    public function getCodeTrekApplicants()
+    {
+        $applicants = CodeTrekApplicant::all();
 
-		return ['applicants'=> $applicants];
-	}
-	public function store($data)
-	{
-		$applicant = new CodeTrekApplicant();
+        return ['applicants'=> $applicants];
+    }
+    public function store($data)
+    {
+        $applicant = new CodeTrekApplicant();
 
-		$applicant->first_name = $data['first_name'];
-		$applicant->last_name = $data['last_name'];
-		$applicant->email = $data['email_id'];
-		$applicant->phone = $data['phone'] ?? null;
-		$applicant->github_user_name = $data['github_username'];
-		$applicant->course = $data['course'] ?? null;
-		$applicant->start_date = $data['start_date'];
-		$applicant->graduation_year = $data['graduation_year'] ?? null;
-		$applicant->university = $data['university_name'] ?? null;
-		$applicant->save();
+        $applicant->first_name = $data['first_name'];
+        $applicant->last_name = $data['last_name'];
+        $applicant->email = $data['email_id'];
+        $applicant->phone = $data['phone'] ?? null;
+        $applicant->github_user_name = $data['github_username'];
+        $applicant->course = $data['course'] ?? null;
+        $applicant->start_date = $data['start_date'];
+        $applicant->graduation_year = $data['graduation_year'] ?? null;
+        $applicant->university = $data['university_name'] ?? null;
+        $applicant->save();
 
-		$this->moveApplicantToRound($applicant, $data);
+        $this->moveApplicantToRound($applicant, $data);
 
-		return $applicant;
-	}
-	public function edit($codeTrekApplicant)
-	{
-		$codeTrekApplicant = CodeTrekApplicant::find($codeTrekApplicant->id);
+        return $applicant;
+    }
+    public function edit($codeTrekApplicant)
+    {
+        $codeTrekApplicant = CodeTrekApplicant::find($codeTrekApplicant->id);
 
-		return $codeTrekApplicant;
-	}
-	public function update($data, $codeTrekApplicant)
-	{
-		$applicant = CodeTrekApplicant::findOrFail($codeTrekApplicant->id);
-		$applicant->first_name = $data['first_name'];
-		$applicant->last_name = $data['last_name'];
-		$applicant->email = $data['email_id'];
-		$applicant->phone = $data['phone'] ?? null;
-		$applicant->github_user_name = $data['github_username'];
-		$applicant->course = $data['course'] ?? null;
-		$applicant->start_date = $data['start_date'];
-		$applicant->graduation_year = $data['graduation_year'] ?? null;
-		$applicant->university = $data['university_name'] ?? null;
-		$applicant->save();
+        return $codeTrekApplicant;
+    }
+    public function update($data, $codeTrekApplicant)
+    {
+        $applicant = CodeTrekApplicant::findOrFail($codeTrekApplicant->id);
+        $applicant->first_name = $data['first_name'];
+        $applicant->last_name = $data['last_name'];
+        $applicant->email = $data['email_id'];
+        $applicant->phone = $data['phone'] ?? null;
+        $applicant->github_user_name = $data['github_username'];
+        $applicant->course = $data['course'] ?? null;
+        $applicant->start_date = $data['start_date'];
+        $applicant->graduation_year = $data['graduation_year'] ?? null;
+        $applicant->university = $data['university_name'] ?? null;
+        $applicant->save();
 
-		return $applicant;
-	}
+        return $applicant;
+    }
 
-	public function evaluate($codeTrekApplicant)
-	{
-		$applicant = CodeTrekApplicant::find($codeTrekApplicant->id);
-		$roundDetails = CodeTrekApplicantRoundDetail::where('applicant_id', $applicant->id)->get();
-
-		return $roundDetails;
-	}
-	public function moveApplicantToRound($applicant, $data)
-	{
-		$applicationRound = new CodeTrekApplicantRoundDetail();
-		$applicationRound->applicant_id = $applicant->id;
-		$applicationRound->round_name = 'level-1';
-		$applicationRound->feedback = null;
-		$applicationRound->start_date = $data['start_date'];
-		$applicationRound->save();
-	}
+    public function evaluate($codeTrekApplicant)
+    {
+        $applicant = CodeTrekApplicant::find($codeTrekApplicant->id);
+        $roundDetails = CodeTrekApplicantRoundDetail::where('applicant_id', $applicant->id)->get();
+         
+        return $roundDetails;
+    }
+    public function moveApplicantToRound($applicant, $data)
+    {
+        $applicationRound = new CodeTrekApplicantRoundDetail();
+        $applicationRound->applicant_id = $applicant->id;
+        $applicationRound->round_name = 'level-1';
+        $applicationRound->feedback = null;
+        $applicationRound->start_date = $data['start_date'];
+        $applicationRound->save();
+    }
 }
