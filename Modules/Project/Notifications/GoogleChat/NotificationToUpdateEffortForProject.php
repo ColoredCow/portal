8b5a5febe1aa3d587c586e0ceee9c7936bf40db4
Himanshunegi0332 <himanshunegi0332@gmail.com>
2023-03-27 21:56:08 +0530

    updated

--- Modules/Project/Notifications/GoogleChat/NotificationToUpdateEffortForProject.php
+++ Modules/Project/Notifications/GoogleChat/NotificationToUpdateEffortForProject.php
@@ -36,7 +36,7 @@ class NotificationToUpdateEffortForProject extends Notification
         foreach ($projects as $project) {
             $date = Carbon::today()->setDay($project->client->billingDetails->billing_date);
             if ($date > today()) {
-                $interval = today()->diffInDays($date);
+                $interval = Carbon::now()->diffInDays($date);
                 if ($interval == 1) {
                     return GoogleChatMessage::create()
                         ->mentionAll('', ' Please check and update the efforts sheet to avoid last minutes updates at the end of the billing cycle.');
