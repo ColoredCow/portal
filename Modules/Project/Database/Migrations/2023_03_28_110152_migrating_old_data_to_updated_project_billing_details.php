7cd0299639daa5025d94b63bc18188da1a3d3d15
yuvraj752 <yuvrajrana752@gmail.com>
2023-03-28 14:13:21 +0530

    migrate old project_billing_details data to updated project_billing_details

---
+++ Modules/Project/Database/Migrations/2023_03_28_110152_migrating_old_data_to_updated_project_billing_details.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class MigratingOldDataToUpdatedProjectBillingDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $projects = DB::table('projects')->select('id','billing_frequency')->get();
+        $projects_meta = DB::table('project_meta')->select('project_id','value')->get();
+        
+        foreach ($projects as $project) {
+            DB::table('project_billing_details')->where('project_id', $project->id)->update([
+                'billing_frequency' => $project->billing_frequency
+            ]);
+        }
+
+        foreach ($projects_meta as $meta) {
+            DB::table('project_billing_details')->where('project_id', $meta->project_id)->update([
+                'billing_level' => $meta->value
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
