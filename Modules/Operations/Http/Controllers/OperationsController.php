af12ddd57463236cb9733c7929986fb20baf0acd
harsh vardhan <hv982837@gmail.com>
2023-03-29 13:02:57 +0530

    checks pass

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -19,7 +19,6 @@ class OperationsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * @return Renderable
      */
     public function create()
     {
@@ -28,18 +27,14 @@ class OperationsController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Show the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function show($id)
     {
@@ -49,7 +44,6 @@ class OperationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function edit($id)
     {
@@ -60,20 +54,16 @@ class OperationsController extends Controller
      * Update the specified resource in storage.
      * @param Request $request
      * @param int $id
-     * @return Renderable
      */
     public function update(Request $request, $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      * @param int $id
-     * @return Renderable
      */
     public function destroy($id)
     {
-        //
     }
 }
