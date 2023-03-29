51dba9c470139e4c8da74451424445c784517215
harsh vardhan <hv982837@gmail.com>
2023-03-29 13:40:30 +0530

    checks pass

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -2,13 +2,14 @@
 
 namespace Modules\Operations\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Routing\Controller;
 
 class OperationsController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @return Renderable
      */
     public function index()
     {
