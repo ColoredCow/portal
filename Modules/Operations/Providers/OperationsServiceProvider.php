57346ca2430a4c48c9f3a6cd0ba99566d80fc74a
harsh vardhan <hv982837@gmail.com>
2023-03-29 18:25:48 +0530

    checks pass

--- Modules/Operations/Providers/OperationsServiceProvider.php
+++ Modules/Operations/Providers/OperationsServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Modules\Operations\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\Factory;
 
 class OperationsServiceProvider extends ServiceProvider
 {
