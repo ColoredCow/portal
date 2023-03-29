8b6447f0ee1e052948a90e1dfe9c19225a03723d
harsh vardhan <hv982837@gmail.com>
2023-03-29 16:35:57 +0530

    checks pass

--- Modules/Operations/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('operations::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('operations.name') !!}
-    </p>
-@endsection
