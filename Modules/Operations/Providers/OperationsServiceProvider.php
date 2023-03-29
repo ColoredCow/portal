7f4669599d49b12aae4231a15cb62c1e841d2c7c
harsh vardhan <hv982837@gmail.com>
2023-03-29 18:22:57 +0530

    checks pass

--- Modules/Operations/Providers/OperationsServiceProvider.php
+++ Modules/Operations/Providers/OperationsServiceProvider.php
@@ -8,12 +8,12 @@ use Illuminate\Database\Eloquent\Factory;
 class OperationsServiceProvider extends ServiceProvider
 {
     /**
-     * @var string $moduleName
+     * @var string
      */
     protected $moduleName = 'Operations';
 
     /**
-     * @var string $moduleNameLower
+     * @var string
      */
     protected $moduleNameLower = 'operations';
 
@@ -51,7 +51,8 @@ class OperationsServiceProvider extends ServiceProvider
             module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
         ], 'config');
         $this->mergeConfigFrom(
-            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+            module_path($this->moduleName, 'Config/config.php'),
+            $this->moduleNameLower
         );
     }
 
@@ -107,6 +108,7 @@ class OperationsServiceProvider extends ServiceProvider
                 $paths[] = $path . '/modules/' . $this->moduleNameLower;
             }
         }
+
         return $paths;
     }
 }
