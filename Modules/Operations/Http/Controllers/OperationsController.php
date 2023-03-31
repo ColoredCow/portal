8577a4b80723e8927dcb776a6eacbf50e4af6cd2
harsh vardhan <hv982837@gmail.com>
2023-03-31 16:56:49 +0530

    module created

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -19,61 +19,49 @@ class OperationsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * @return Renderable
      */
     public function create()
     {
-        return view('operations::create');
     }
 
     /**
      * Store a newly created resource in storage.
      * @param Request $request
-     * @return Renderable
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Show the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function show($id)
     {
-        return view('operations::show');
     }
 
     /**
      * Show the form for editing the specified resource.
      * @param int $id
-     * @return Renderable
      */
     public function edit($id)
     {
-        return view('operations::edit');
     }
 
     /**
      * Update the specified resource in storage.
      * @param Request $request
      * @param int $id
-     * @return Renderable
      */
     public function update(Request $request, $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      * @param int $id
-     * @return Renderable
      */
     public function destroy($id)
     {
-        //
     }
 }
