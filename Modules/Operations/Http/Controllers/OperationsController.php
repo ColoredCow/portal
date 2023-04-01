52035c505a86ba79e383ed73886aef0b1791b546
harsh vardhan <hv982837@gmail.com>
2023-04-01 16:57:18 +0530

    passing tests

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -12,7 +12,7 @@ class OperationsController extends Controller
      */
     public function index()
     {
-        return view('operations::office-location.index');
+        return view('operations::index');
     }
     
 
