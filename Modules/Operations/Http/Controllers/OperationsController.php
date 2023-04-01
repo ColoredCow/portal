649af29c1e7298e12e05c5f4e075396e4e4d645a
harsh vardhan <hv982837@gmail.com>
2023-04-01 11:45:44 +0530

    passes some checks

--- Modules/Operations/Http/Controllers/OperationsController.php
+++ Modules/Operations/Http/Controllers/OperationsController.php
@@ -8,34 +8,14 @@ use Illuminate\Routing\Controller;
 
 class OperationsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
     public function index()
     {
         $data = ['name' => 'John'];
 
         return view('operations::office-location.coming_soon', $data);
-    }
-
-    public function create()
-    {
-    }
-
-    public function store(Request $request)
-    {
-    }
-
-    public function show($id)
-    {
-    }
-
-    public function edit($id)
-    {
-    }
-
-    public function update(Request $request, $id)
-    {
-    }
-    
-    public function destroy($id)
-    {
     }
 }
