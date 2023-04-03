286c124532ffaf0ccdf077ae5c576f8b2e9a4e19
harsh vardhan <hv982837@gmail.com>
2023-04-03 16:14:26 +0530

    added permissions

--- Modules/Operations/policies/OperationsPolicy.php
+++ Modules/Operations/policies/OperationsPolicy.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\operations\Policies;
+namespace MOdules\Operations\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Modules\User\Entities\User;
@@ -18,64 +18,5 @@ class OperationsPolicy
     public function viewAny(User $user)
     {
         return $user->can('operations.view');
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Operations $operations)
-    {
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     */
-    public function create(User $user)
-    {
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Operations $operations)
-    {
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     * @param  \App\Models\Operations  $operations
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Operations $operations)
-    {
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     */
-    public function restore(User $user, Operations $operations)
-    {
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \Modules\User\Entities\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Operations $operations)
-    {
     }
 }
