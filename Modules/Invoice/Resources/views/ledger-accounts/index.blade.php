5df51806f5dd9cef91ca2363c3dd482e42adeb49
MayankMukul <mayankmukul123@gmail.com>
2023-04-09 08:17:01 +0530

    responsive-ledger-accounts-page

--- Modules/Invoice/Resources/views/ledger-accounts/index.blade.php
+++ Modules/Invoice/Resources/views/ledger-accounts/index.blade.php
@@ -8,7 +8,7 @@
             <div class="card-body">
                 <form action="{{ route('ledger-accounts.index') }}" method="GET">
                     <div class='d-flex'>
-                        <div class='w-30p'>
+                        <div class='col-sm-3 col'>
                             <div>
                                 <label for="clientId" class="field-required">{{ __('Client') }}</label>
                             </div>
@@ -20,7 +20,7 @@
                                 </select>
                             </div>
                         </div>
-                        <div class='w-30p ml-2'>
+                        <div class='col-sm-3 col ml-1'>
                             <div>
                                 <label for="projects" class="field-required">
                                     {{ __('Project') }}
@@ -43,34 +43,36 @@
                         @csrf
                         <input type="hidden" name="client_id" value="{{ optional($client)->id }}">
                         <input type="hidden" name="project_id" value="{{ optional($project)->id }}">
-                        <table class="table-bordered w-100p">
-                            <thead class="table-dark">
-                                <th class="py-2 px-1">{{ __('Date') }}</th>
-                                <th class="py-2 px-1">{{ __('Particulars') }}</th>
-                                <th class="py-2 px-1">{{ __('Credit') }}</th>
-                                <th class="py-2 px-1">{{ __('Debit') }}</th>
-                                <th class="py-2 px-1">{{ __('Balance') }}</th>
-                                <th></th>
-                            </thead>
-                            <tbody v-for="(ledgerAccountRow, index) in ledgerAccountData">
-                                <tr>
-                                    <td>
-                                        <input type="hidden" :name="`ledger_account_data[${index}][client_id]`" value="{{ optional($client)->id }}">
-                                        <input type="hidden" :name="`ledger_account_data[${index}][project_id]`" value="{{ optional($project)->id }}">
-                                        <input class="py-2 w-full" type="hidden" :name="`ledger_account_data[${index}][id]`" v-model="ledgerAccountRow.id">
-                                        <input class="py-2 w-full" :name="`ledger_account_data[${index}][date]`" v-model="ledgerAccountRow.date" type="date" required>
-                                    </td>
-                                    <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][particulars]`" v-model="ledgerAccountRow.particulars" type="text"></td>
-                                    <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][credit]`" v-model="ledgerAccountRow.credit" type="number" step="0.01"></td>
-                                    <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][debit]`" v-model="ledgerAccountRow.debit" type="number" step="0.01"></td>
-                                    <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][balance]`" v-model="ledgerAccountRow.balance" type="number" step="0.01"></td>
-                                    <td><div v-on:click="removeRow(index)" class="text-danger font-weight-bold fz-24 c-pointer px-1">&times;</div></td>
+                        <div class = "table-responsive">
+                            <table class="table-bordered w-100p ">
+                                <thead class="table-dark">
+                                    <th class="py-2 px-1">{{ __('Date') }}</th>
+                                    <th class="py-2 px-1">{{ __('Particulars') }}</th>
+                                    <th class="py-2 px-1">{{ __('Credit') }}</th>
+                                    <th class="py-2 px-1">{{ __('Debit') }}</th>
+                                    <th class="py-2 px-1">{{ __('Balance') }}</th>
+                                    <th></th>
+                                </thead>
+                                <tbody v-for="(ledgerAccountRow, index) in ledgerAccountData">
+                                    <tr>
+                                        <td>
+                                            <input type="hidden" :name="`ledger_account_data[${index}][client_id]`" value="{{ optional($client)->id }}">
+                                            <input type="hidden" :name="`ledger_account_data[${index}][project_id]`" value="{{ optional($project)->id }}">
+                                            <input class="py-2 w-full" type="hidden" :name="`ledger_account_data[${index}][id]`" v-model="ledgerAccountRow.id">
+                                            <input class="py-2 w-full" :name="`ledger_account_data[${index}][date]`" v-model="ledgerAccountRow.date" type="date" required>
+                                        </td>
+                                        <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][particulars]`" v-model="ledgerAccountRow.particulars" type="text"></td>
+                                        <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][credit]`" v-model="ledgerAccountRow.credit" type="number" step="0.01"></td>
+                                        <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][debit]`" v-model="ledgerAccountRow.debit" type="number" step="0.01"></td>
+                                        <td><input class="py-2 w-full" :name="`ledger_account_data[${index}][balance]`" v-model="ledgerAccountRow.balance" type="number" step="0.01"></td>
+                                        <td><div v-on:click="removeRow(index)" class="text-danger font-weight-bold fz-24 c-pointer px-1">&times;</div></td>
+                                    </tr>
+                                </tbody>
+                                <tr id="noDataSection" class="{{ count($ledgerAccountData) ? 'd-none' : '' }}">
+                                    <td colspan="6" class="p-3 text-center"> {{ __('No Data Available') }}</td>
                                 </tr>
-                            </tbody>
-                            <tr id="noDataSection" class="{{ count($ledgerAccountData) ? 'd-none' : '' }}">
-                                <td colspan="6" class="p-3 text-center"> {{ __('No Data Available') }}</td>
-                            </tr>
-                        </table>
+                            </table>
+                        </div>
                     </form>
                     <div class="mt-2 ml-2 ">
                         <a v-if="clientId" v-on:click="addNewRow()" href="javascript: void(0);" class="c-pointer text-decoration-none text-dark border-bottom border-dark">{{ __('Add new row') }}</a>
