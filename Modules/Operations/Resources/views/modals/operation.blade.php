c36ace0b9eedcffe3d9f646e256d893f88fb2a2b
harsh vardhan <hv982837@gmail.com>
2023-04-12 14:39:44 +0530

    office location phase 2

--- Modules/Operations/Resources/views/modals/operation.blade.php
+++ Modules/Operations/Resources/views/modals/operation.blade.php
@@ -23,8 +23,12 @@
                                 <label for="centreHead" class="field-required">Centre Head</label>
                                 <select v-model="location" name="centre_head" id="centreHead" class="form-control" required>
                                     <option value="">Select centre head</option>
+                                    @foreach(config('operations.centre_heads') as $centre)
+                                        <option value="{{ $centre['slug'] }}">{{ $centre['label'] }}</option>
+                                    @endforeach
                                 </select>
                             </div>
+                            
                         </div>
                         <div class="form-row">
                             <div class="form-group offset-md-1 col-md-5">
