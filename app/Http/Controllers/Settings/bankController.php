f6df3ee479bc2119e669e66c919fbe65122fb12a
AJAY <ajaysinghbisht195@gmail.com>
2023-04-13 18:36:44 +0530

    fixed

--- app/Http/Controllers/Settings/bankController.php
+++ app/Http/Controllers/Settings/bankController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers\Settings;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Setting\SettingRequest;
-use App\Models\Setting;
 
 class bankController extends Controller
 {
