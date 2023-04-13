ca4d44ff46e3ddb83dd4ddd867cbe8d98f7fb9b3
AJAY <ajaysinghbisht195@gmail.com>
2023-04-13 17:55:49 +0530

    fixed

--- app/Http/Controllers/Settings/bankController.php
+++ app/Http/Controllers/Settings/bankController.php
@@ -7,12 +7,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Setting\SettingRequest;
 use App\Models\Setting;
 
-
 class bankController extends Controller
 {
     public function index()
     {
         return view('Settings.bankdetails');
     }
-}  
-
+}
