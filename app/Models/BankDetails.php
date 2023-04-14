5ae304c88df2e30781bfaca376b9b5faa0476658
AJAY <ajaysinghbisht195@gmail.com>
2023-04-14 19:54:56 +0530

    fixed

--- app/Models/BankDetails.php
+++ app/Models/BankDetails.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class BankDetails extends Model
 {
     use HasFactory;
-    protected $table ='bank_details';
-    protected $fillable =['id','key','label','value'];
+    protected $table = 'bank_details';
+    protected $fillable = ['id', 'key', 'label', 'value'];
 }
