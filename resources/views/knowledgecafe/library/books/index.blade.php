d86e2c296a303c0ec60b46026fe1e70e4f8af3cf
Akshat semwal <akshatsemwal56@gmail.com>
2023-04-16 11:29:50 +0530

    bugfix/Add_new_button_overlap

--- resources/views/knowledgecafe/library/books/index.blade.php
+++ resources/views/knowledgecafe/library/books/index.blade.php
@@ -13,7 +13,7 @@
     <br>
     <br>
     <div class="row">
-        <div class="col-4 col-xs-6 pl-0">
+        <div class="col-4 col-xs-6 pl-4">
             <h4>Books({{count($books)}})</h4>
         </div>
         @can('library_books.create')
