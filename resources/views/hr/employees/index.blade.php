129ebd78d6ae471161bb489448e3865c827c3ecb
nivesh1000 <nivesh.nk1001@gmail.com>
2023-04-16 12:02:35 +0530

    replaced inline classes with bootstrap classes

--- resources/views/hr/employees/index.blade.php
+++ resources/views/hr/employees/index.blade.php
@@ -5,7 +5,7 @@
     <br>
     @include('hr.employees.menu')
     <br><br>
-    <div class="d-flex justify-content-between ">
+    <div class="d-flex justify-content-between">
         <h1 class="d-flex mt-2" >{{request()->get('name')}} ({{count($employees)}})</h1>
         <form id="employeeFilterForm" class="d-md-flex justify-content-between ml-md-3">
             <input type="hidden" name="status" value="{{ request()->input('status', 'current') }}">
