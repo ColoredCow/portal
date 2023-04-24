fe91c1125230998536b1932453010320f0ef2b31
harsh vardhan <hv982837@gmail.com>
2023-04-24 14:35:15 +0530

    office location phase 2

--- database/migrations/2023_04_17_113950_create_office_locations_table.php
+++ database/migrations/2023_04_17_113950_create_office_locations_table.php
@@ -18,7 +18,7 @@ class CreateOfficeLocationsTable extends Migration
             $table->string('centre_name');
             $table->integer('centre_head_id')->unsigned();
             $table->integer('capacity');
-            $table->integer('current_people_count')->default(0);
+            $table->integer('current_people_count');
             $table->timestamps();
         });
         Schema::table('office_locations', function (Blueprint $table) {
@@ -33,6 +33,10 @@ class CreateOfficeLocationsTable extends Migration
      */
     public function down()
     {
+        Schema::table('office_locations', function (Blueprint $table) {
+            $table->dropForeign(['centre_head_id']);
+            $table->dropColumn(['centre_name', 'centre_head_id', 'capacity', 'current_people_count']);
+        });
         Schema::dropIfExists('office_locations');
     }
 }
