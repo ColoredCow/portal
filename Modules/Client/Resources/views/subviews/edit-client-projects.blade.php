5f90f3c3119ef5a0e1f8965debfcddbc80e5ef60
priyanka kaintura <priyankakaintura1408@gmail.com>
2023-04-25 17:25:18 +0530

    Corrected the alignment of client project page content

--- Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
+++ Modules/Client/Resources/views/subviews/edit-client-projects.blade.php
@@ -1,17 +1,17 @@
-<div class="card">
+<div class="overflow-x-scroll card" style="min-width:660px;">
     <div id="client_projects_container" class="collapse show">
         <div class="card-body">
-            <div class="d-flex px-5">
+        <div class="d-flex px-5">
                 <div class="w-33p ml-2">
                     <h5 class="font-muli-bold">Name</h5>
                 </div>
                 <div class="w-40p ml-2">
                     <h5 class="font-muli-bold">Team Members</h5>
                 </div>
-                <div class="w-20p mr-10">
+                <div class="w-30p ml-2">
                     <h5 class="font-muli-bold">Project Status</h5>
                 </div>
-                <div class="w-20p ml-15">
+                <div class="w-20p ml-2">
                     <h5 class="font-muli-bold">Actions</h5>
                 </div>
             </div>
@@ -23,17 +23,17 @@
                             {{ $project->name }}
                         </a>
                     </div>
-                    <div class="w-30p w-md-40p ml-2">
+                    <div class="w-40p w-md-40p ml-2">
                         @foreach($project->teamMembers ?:[] as $teamMember)
                             <span class="tooltip-wrapper" data-html="true" data-toggle="tooltip" title="{{ $teamMember->name }} - {{ config('project.designation')[$teamMember->pivot->designation] }}">
                                 <img src="{{ $teamMember->avatar }}" class="w-35 h-30 rounded-circle mr-1 mb-1">
                             </span>
                         @endforeach 
                     </div>
-                    <div class="w-30p mr-10">
+                    <div class="w-30p ml-2">
                         <h5>{{ config('project.status')[$project->status] }}</h5>
                     </div>
-                        <div class="w-10p mr-10">
+                        <div class="w-20p ml-2">
                             @include('client::subviews.create-client-projects-modal')
                             <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal{{$project->id}}">Remove</button>
                         </div>
