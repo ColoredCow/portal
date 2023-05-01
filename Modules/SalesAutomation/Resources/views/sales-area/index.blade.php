f63bfdaa6f9b9db6c2f7b4706c87260d3c6f3957
priyanka kaintura <priyankakaintura1408@gmail.com>
2023-05-01 18:45:55 +0530

    Changed the implementation of new Sales Area page to modal form in bootstrap

--- Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
+++ Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
@@ -3,18 +3,18 @@
 @section('salesautomation.content')
     <div class="row">
 		<div class="offset-md-9 col-md-3 text-right mb-3">
-			<a href="{{ route('sales-area.create') }}" class="btn btn-primary">New Sales Area</a>
+			<button class="btn btn-primary" data-toggle="modal" data-target="#newSalesArea">New Sales Area</button>
 		</div>
 	</div>
 	<table class="table table-striped table-bordered">
-		<thead class="thead-dark">
+		<thead class="thead-dark mw-100p">
 		    <tr>
 		      <th scope="col">Name</th>
 		      <th scope="col">Actions</th>
 		    </tr>
-		    <tbody>
-			@foreach ($salesAreas as $index => $salesArea)
-                
+		</thead>
+		<tbody>
+			@forelse ($salesAreas as $index => $salesArea)
 				<tr>
 					<td>{{ $salesArea->name }}</td>
 					<td class="w-25p">
@@ -52,8 +52,51 @@
 						</form>
 					</td>
 				</tr>
-			@endforeach		
-	    </thead>
+				@empty
+				<tr>
+                    <td colspan="2">
+						<div class="d-flex justify-content-center">
+							<img src="{{ URL('images/no-result.png') }}" class='img-center' alt="" width="50%">
+						</div>
+						<div class="container-fluid d-flex justify-content-center">
+							<span class="text-primary">NO DATA FOUND</span>
+						</div>
+                    <td>
+	           </tr>
+			@endforelse
+	    </tbody>
 	</table>
 	{{ $salesAreas->links() }}
 @endsection
+
+<div class="modal fade" id="newSalesArea">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-title d-flex justify-content-between mx-2 mb-0 mt-2">
+				<h2>New Sales Area</h2>
+				<button class="btn btn-secondary" data-dismiss="modal"><span>&times</span></button>
+			</div>
+			<form action="{{ route('sales-area.store') }}" method="POST">
+			@csrf
+				<div class="modal-body mt-1">
+					<div class="card my-0">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-12">
+									<div class="form-group">
+										<label for="name">Name<span class="text-danger">*</span></label>
+										<input type="text mw-100" class="form-control" name="name" id="name" value="{{ old('name') }}" autofocus required>
+									</div>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer d-flex justify-content-end px-3 my-0 pb-2">
+					<button type="submit" class="btn btn-secondary btn-lg mr-1" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary btn-lg">Submit</button>
+				</div>	
+			</form>
+		</div>
+	</div>
+</div>
