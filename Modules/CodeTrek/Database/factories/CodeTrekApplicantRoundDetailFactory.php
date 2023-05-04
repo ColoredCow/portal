6bfcb03cc4e559942b62b26b3d447460b89816b9
Mayank <mayank8630bisht@gmail.com>
2023-05-04 18:51:05 +0530

    Changes 1

--- Modules/CodeTrek/Database/factories/CodeTrekApplicantRoundDetailFactory.php
+++ Modules/CodeTrek/Database/factories/CodeTrekApplicantRoundDetailFactory.php
@@ -6,7 +6,6 @@ namespace Modules\CodeTrek\Database\Factories;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Faker\Factory as Faker;
 use Modules\CodeTrek\Entities\CodeTrekApplicantRoundDetail;
 
 class CodetrekApplicantRoundDetailFactory extends Factory
@@ -18,10 +17,9 @@ class CodetrekApplicantRoundDetailFactory extends Factory
      */
     protected $model = CodeTrekApplicantRoundDetail::class;
 
+
     public function definition()
     {
-        $faker = Faker::create();
-
         return [
             //'applicant_id' => CodeTrekApplicant::inRandomOrder()->first()->id,
             'feedback'=>'No',
