d67863a9d1b1342cb3dc0d2513fab839450084fb
devPriyaJ <priyajoshi73417@gmailcom>
2023-05-04 14:01:01 +0530

    added-toast

--- Modules/Project/Resources/views/index.blade.php
+++ Modules/Project/Resources/views/index.blade.php
@@ -1,14 +1,8 @@
 @extends('project::layouts.master')
 @section('content')
     <div class="container" id="vueContainer">
-        @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {{ session('success') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        @endif
+        @includeWhen(session('success'), 'toast', ['message' => session('success')])
+
         <div class='d-none d-md-flex justify-content-between'>
             @include('project::menu_header')
             @can('projects.create')
