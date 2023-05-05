5917ffa3c2d80dfd9400c433ea45478ada5ed525
Mayank <mayank8630bisht@gmail.com>
2023-05-05 10:16:57 +0530

    changes 3

--- Modules/CodeTrek/Entities/CodeTrekApplicant.php
+++ Modules/CodeTrek/Entities/CodeTrekApplicant.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\CodeTrek\Database\Factories\CodeTrekApplicantFactory;
+
 class CodeTrekApplicant extends Model
 {
     use SoftDeletes;
