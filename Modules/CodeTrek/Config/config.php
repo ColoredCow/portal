2b22659b059a9c910967102f7a2642edeb78c684
Akshiteshnaithani <akshitesh.naithani@gmail.com>
2023-05-10 12:20:53 +0530

    Change the status with applicant level

--- Modules/CodeTrek/Config/config.php
+++ Modules/CodeTrek/Config/config.php
@@ -19,23 +19,28 @@ return [
     'rounds' =>[
         'preparatory' => [
             'label' => 'Preparatory',
-            'slug' => 'Preparatory'
+            'slug' => 'Preparatory',
+            'class' => 'badge badge-primary'
         ],
         'level-1' =>[
             'label' =>'Level-1',
             'slug'  =>'level-1',
+            'class' => 'badge badge-secondary'
         ],
         'level-2' =>[
             'label' =>'Level-2',
-            'slug'  =>'level-2'
+            'slug'  =>'level-2',
+            'class' => 'badge badge-warning'
         ],
         'level-3'   =>[
             'label' =>'Level-3',
-            'slug'  =>'level-3'
+            'slug'  =>'level-3',
+            'class' => 'badge badge-success'
         ],
         'onboard'   =>[
             'label' =>'Onboarded',
-            'slug'  =>'onboarded'
+            'slug'  =>'onboarded',
+            'class' => 'badge badge-info'
         ],
     ]
 ];
