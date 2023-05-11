8ef0eee156f934218978c1b444cf75fc14563612
harsh vardhan <hv982837@gmail.com>
2023-05-11 23:09:51 +0530

    Add sidebar in homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -132,8 +132,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
     <script src="{{ asset('src/tinymce/js/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+
 
     @yield('js_scripts')
     @yield('vue_scripts')
