8b5963b754fd88a0a940211d791f71af4f95a6f0
Ajay Chauhan <ajayc0343@gmail.com>
2023-05-18 12:19:46 +0530

    Show clients status count on menu header

--- Modules/Client/Resources/views/menu_header.blade.php
+++ Modules/Client/Resources/views/menu_header.blade.php
@@ -6,13 +6,13 @@
         @php
             $request['status'] = 'active';
         @endphp
-        <a class="nav-link {{ (request()->input('status', 'active') == 'active')  ? 'active' : '' }}" href="{{ route('client.index', $request)  }}">Active Clients</a>
+        <a class="nav-link {{ (request()->input('status', 'active') == 'active')  ? 'active' : '' }}" href="{{ route('client.index', $request)  }}">Active Clients {{  (request()->input('status', 'active') == 'active') ?  '(' . $count . ')' : null }}</a>
     </li>
 
     <li class="nav-item">
         @php
             $request['status'] = 'inactive';
         @endphp
-        <a class="nav-link {{ (request()->input('status', 'active') == 'inactive') ? 'active' : '' }}"  href="{{ route('client.index', $request)  }}">Inactive Clients</a>
+        <a class="nav-link {{ (request()->input('status', 'active') == 'inactive') ? 'active' : '' }}"  href="{{ route('client.index', $request)  }}">Inactive Clients {{  (request()->input('status', 'active') == 'inactive') ?  '(' . $count . ')' : null }}</a>
     </li>
 </ul>
