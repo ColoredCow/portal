faab5323de9e66f4f297fd5a346528ac51b26196
Mayank <mayank8630bisht@gmail.com>
2023-05-19 16:54:38 +0530

    Changed name of codeTrekApplicatsTableseeder

--- Modules/CodeTrek/Database/Seeders/CodeTrekApplicantsTableSeeder.php
+++ Modules/CodeTrek/Database/Seeders/CodeTrekApplicantsTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Modules\CodeTrek\Entities\CodeTrekApplicant;
 use Modules\CodeTrek\Entities\CodeTrekApplicantRoundDetail;
 
-class CosetrekApplicantsTableSeeder extends Seeder
+class CodeTrekApplicantsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- Modules/CodeTrek/Database/Seeders/CodeTrekDatabaseSeeder.php
+++ Modules/CodeTrek/Database/Seeders/CodeTrekDatabaseSeeder.php
@@ -17,6 +17,6 @@ class CodeTrekDatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(CodeTrekPermissionTableSeeder::class);
-        $this->call(CosetrekApplicantsTableSeeder::class);
+        $this->call(CodeTrekApplicantsTableSeeder::class);
     }
 }
