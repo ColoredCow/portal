2f94a0a4469d708bac5ae3a7cc3b816c7216e327
Akshiteshnaithani <akshitesh.naithani@gmail.com>
2023-05-22 12:14:02 +0530

    Send mail to applicant

--- Modules/CodeTrek/Resources/views/mail/codetrek.blade.php
+++ Modules/CodeTrek/Resources/views/mail/codetrek.blade.php
@@ -9,6 +9,6 @@
 	<p>We hope this email finds you well. On behalf of the entire team at ColoredCow, We wanted to take a moment to congratulate you on the commencement of your CodeTrek internship. We are thrilled to have you on board and excited to see the valuable contributions you will bring to our organization. Once again, congratulations on starting your CodeTrek internship! You have the potential to achieve great things, and we look forward to witnessing your growth and accomplishments over the course of your internship.</p>
 	<p>Wishing you a successful and fulfilling CodeTrek internship journey!</p>
     <br>
-	<p class="line">Thanks,</p>
-	<p class="line">ColoredCow Portal</p>
+	<p class="line">Best regards,</p>
+	<p class="line">ColoredCow</p>
 </div>
