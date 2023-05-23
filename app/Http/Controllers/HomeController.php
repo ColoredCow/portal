82eba7d4323dacc18200654f879b87e530151d17
harsh vardhan <hv982837@gmail.com>
2023-05-23 11:39:33 +0530

    minor change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -72,7 +72,7 @@ class HomeController extends Controller
         return $userGroups;
     }
 
-    public function storeDropdownValue(Request $request)
+    public function storeEmployeeLocation(Request $request)
     {
         $validatedData = $request->validate([
             'Centre' => 'required',
