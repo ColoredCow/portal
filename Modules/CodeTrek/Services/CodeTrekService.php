dbf0baa2e5c7466be0ffb86a60ad0f895991a27a
Ajay Chauhan <ajayc0343@gmail.com>
2023-05-24 15:12:04 +0530

    done-suggested-changes

--- Modules/CodeTrek/Services/CodeTrekService.php
+++ Modules/CodeTrek/Services/CodeTrekService.php
@@ -17,8 +17,10 @@ class CodeTrekService
         if ($centre) {
             $applicants = $query->where('centre_id', $centre);
         }
-        $applicants = $search ? $query->whereRaw("CONCAT(first_name, ' ', last_name) like '%$search%'")->get() : $query->get();
-        $applicants = $query->paginate(config('constants.pagination_size'));
+        $applicants = $query->when($search, function ($query) use ($search) {
+            return $query->whereRaw("CONCAT(first_name, ' ', last_name) LIKE '%$search%'");
+        })
+        ->paginate(config('constants.pagination_size'));
 
         return ['applicants' => $applicants];
     }
