db5f654fc72bfe74f9d8231081e39443bc4bbe0f
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-05-26 16:11:32 +0530

    Bug/3092/Checkes fixed

--- Modules/Project/Http/Controllers/ProjectController.php
+++ Modules/Project/Http/Controllers/ProjectController.php
@@ -149,8 +149,9 @@ class ProjectController extends Controller
     public function projectResource(Request $request)
     {
         $resourceData = $this->service->getProjectsWithTeamMemberRequirementData($request['name']);
+       
         return view('project::resource-requirement', [
-            'resourceData' => $resourceData,
+        'resourceData' => $resourceData,
         ]);
     }
 }
