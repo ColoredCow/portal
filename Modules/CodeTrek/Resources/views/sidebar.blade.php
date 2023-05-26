1bdfa199886904b2b1219b6cb1c51519f9e2135c
harsh vardhan <hv982837@gmail.com>
2023-05-26 15:00:20 +0530

    minor change

--- Modules/CodeTrek/Resources/views/sidebar.blade.php
+++ Modules/CodeTrek/Resources/views/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div id="applicant-sidebar" class="position-fixed top-0 start-0 bg-white">
-    <h5 class="fw-bold" style="border-bottom: 1px solid black;">CodeTrek Applicants</h5>
-    <ul class="applicant-list">
+    <h5 class="fw-bold border-bottom pb-2">CodeTrek Applicants</h5>
+    <ul class="applicant-list list-unstyled">
         @foreach ($codeTrekApplicants as $codeTrekApplicant)
             <li data-id="{{ $codeTrekApplicant->id }}">
-                <h6><i class="fa fa-user"></i> {{ $codeTrekApplicant->first_name }} {{ $codeTrekApplicant->last_name }}</h6>
+                <h6><i class="fa fa-user me-1"></i> {{ $codeTrekApplicant->first_name }} {{ $codeTrekApplicant->last_name }}</h6>
             </li>
         @endforeach
     </ul>
