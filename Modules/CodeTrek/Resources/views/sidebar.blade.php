51e7d6a9ec02753bcf082876d06dc32b6ed67255
harsh vardhan <hv982837@gmail.com>
2023-05-30 15:32:38 +0530

    minor change

--- Modules/CodeTrek/Resources/views/sidebar.blade.php
+++ Modules/CodeTrek/Resources/views/sidebar.blade.php
@@ -2,12 +2,12 @@
     <h5 class="fw-bold border-bottom pb-2 ps-3">CodeTrek Applicants</h5>
     <ul class="applicant-list list-unstyled p-0 m-0 ps-3">
         @foreach ($codeTrekApplicants as $codeTrekApplicant)
-            <li data-id="{{ $codeTrekApplicant->id }}">
-                <h6 class="ms-1 mr-1">
-                    <i class="fa fa-user me-1 mr-1"></i>
-                    {{ $codeTrekApplicant->first_name }} {{ $codeTrekApplicant->last_name }}
-                </h6>
-            </li>
+        <li data-id="{{ $codeTrekApplicant->id }}" class="d-flex align-items-center">
+            <i class="fa fa-user me-1 mr-1"></i>
+            <h6 class="ml-1 mr-1">
+                {{ $codeTrekApplicant->first_name }} {{ $codeTrekApplicant->last_name }}
+            </h6>
+        </li>
         @endforeach
     </ul>
 </div>
