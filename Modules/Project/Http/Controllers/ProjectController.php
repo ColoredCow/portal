18b6fdce4bada1a8b1823b8ccc2cbb8a5783ca73
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-05-31 17:29:21 +0530

    projrct ID diasabled in URL & search option working in team members

--- Modules/Project/Http/Controllers/ProjectController.php
+++ Modules/Project/Http/Controllers/ProjectController.php
@@ -148,7 +148,7 @@ class ProjectController extends Controller
 
     public function projectResource(Request $request)
     {
-        $resourceData = $this->service->getProjectsWithTeamMemberRequirementData($request['name']);
+        $resourceData = $this->service->getProjectsWithTeamMemberRequirementData($request->all());
 
         return view('project::resource-requirement', ['resourceData' => $resourceData]);
     }
