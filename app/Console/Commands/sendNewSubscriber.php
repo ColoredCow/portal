9767b3fabff6cb03956eab1ae6149fc03bd7f8d6
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-05 19:28:02 +0530

    !empty() fixing

--- app/Console/Commands/sendNewSubscriber.php
+++ app/Console/Commands/sendNewSubscriber.php
@@ -56,7 +56,7 @@ class sendNewSubscriber extends Command
 
             $jobIds = Application::where('hr_applicant_id', $applicant->id)->pluck('hr_job_id')->toArray();
 
-            if (! $jobIds->isEmpty()) {
+            if (!empty($jobIds)) {
                 $subscriptionLists = [];
                 foreach ($jobIds as $jobId) {
                     $subscriptionList = Job::where('id', $jobId)->value('title');
