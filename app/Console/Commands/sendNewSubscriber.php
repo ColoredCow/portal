f784fd29f61c814c707505701685bfa1e81a68c5
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-06 12:23:00 +0530

    removing extra code

--- app/Console/Commands/sendNewSubscriber.php
+++ app/Console/Commands/sendNewSubscriber.php
@@ -57,12 +57,8 @@ class sendNewSubscriber extends Command
             $jobIds = Application::where('hr_applicant_id', $applicant->id)->pluck('hr_job_id')->toArray();
             if (! empty($jobIds)) {
                 $uniqueJobIds = array_unique($jobIds);
-                $subscriptionLists = [];
 
-                foreach ($uniqueJobIds as $jobId) {
-                    $subscriptionList = Job::where('id', $jobId)->value('title');
-                    $subscriptionLists[] = $subscriptionList;
-                }
+                $subscriptionLists = Job::whereIn('id', $uniqueJobIds)->pluck('title')->toArray();
 
                 if (! empty($subscriptionLists)) {
                     try {
