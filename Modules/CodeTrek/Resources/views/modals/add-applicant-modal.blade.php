8c0cf6baf6825609a989b972e8636227e737dbb9
Ajay Chauhan <ajayc0343@gmail.com>
2023-06-09 11:37:32 +0530

    functionality to provide feedbacks to codetrek candidates

--- Modules/CodeTrek/Resources/views/modals/add-applicant-modal.blade.php
+++ Modules/CodeTrek/Resources/views/modals/add-applicant-modal.blade.php
@@ -23,7 +23,7 @@
                             </div>
                             <div class="form-group offset-md-1 col-md-5">
                                  <label for="last_name" class="field-required">Last Name</label>
-                                 <input type="text" class="form-control" name="last_name" id="lastName" placeholder="Enter last name" required="required" value="">
+                                 <input type="text" class="form-control" name="last_name" id="lastName" placeholder="Enter last name" value="" required>
                             </div>
                         </div>
                         <div class="form-row">
