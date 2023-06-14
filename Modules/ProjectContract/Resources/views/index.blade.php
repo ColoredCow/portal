a1c98934431a7f59539c045b98ac0aaa0f4ad989
Anmol Gupta <guptaanmol248001@gmail.com>
2023-06-14 11:47:45 +0530

    Edited contact index page as per db

--- Modules/ProjectContract/Resources/views/index.blade.php
+++ Modules/ProjectContract/Resources/views/index.blade.php
@@ -18,23 +18,19 @@
     <thead class="thead-dark">
         <tr>
             <th class="w-30p sticky-top">Client Name</th>
-            <th class="sticky-top">URL</th>
             <th class="sticky-top">Authority Name</th>
-            <th class="sticky-top">Contract Date for signing</th>
             <th class="sticky-top">Contract Date for Effective</th>
             <th class="sticky-top">Contract Expiry Date</th>
-            <th class="sticky-top">Logo</th>
+            <th class="sticky-top">Status</th>
             <th class="sticky-top">Action</th>
         </tr>
         @foreach ($projects as $project)
         <tr>
             <td>{{$project->client->name}}</td>
-            <td><a href="{{$project->website_url}}"> <button class="btn btn-success fa fa mr-1  ">URL</button></td>
             <td>{{$project->authority_name}}</td>
-            <td>{{$project->contract_date_for_signing}}</td>
             <td>{{$project->contract_date_for_effective}}</td>
             <td>{{$project->contract_expiry_date}}</td>
-            <td><img src="{{ asset('storage/contractlogo/'.$project->logo_img) }}" class="w-35 h-30 rounded-circle mb-1"></td>
+            <td></td>
             <td>
                 <a href="{{route('projectcontract.edit', $project->id)}}" class="pl-1 btn btn-link" ><i class="text-success fa fa-edit fa-lg"></i></a>
                 <a href="{{route('projectcontract.delete', $project->id)}}" class="pl-1 btn btn-link" ><i class="text-danger fa fa-trash fa-lg"></i></a>
