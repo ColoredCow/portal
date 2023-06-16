3bb1878a7162bdce146995d3bb7bd3e3e8699dd8
Ajay Chauhan <ajayc0343@gmail.com>
2023-06-16 13:35:45 +0530

    done with the suggested changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2106,9 +2106,9 @@ $(document).ready(function() {
 	  $(".thumbs-up").css("color", "");
 	  $(".thumbs-down").css("color", "");
 	  if ($(this).val() === "positive") {
-        $(this).siblings(".thumbs-up").css("color", "green");
+            $(this).siblings(".thumbs-up").css("color", "green");
 	  } else if ($(this).val() === "negative") {
-        $(this).siblings(".thumbs-down").css("color", "red");
+            $(this).siblings(".thumbs-down").css("color", "red");
 	  }
 	});
 });
