0a056c9ba7adeaa49f3bc5d9e7f076e26de181e1
Anmol Gupta <guptaanmol248001@gmail.com>
2023-06-19 18:54:57 +0530

    Added migration to add contract-meta-history table

---
+++ Modules/ProjectContract/Database/Migrations/2023_06_19_182942_create-contract-meta-history-table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContractMetaHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contract_meta_history', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contract_id');
+            $table->unsignedBigInteger('review_id');
+            $table->string('key');
+            $table->string('value');
+            $table->timestamps();
+            $table->foreign('contract_id')->references('id')->on('contracts');
+            $table->foreign('review_id')->references('id')->on('contract_review');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contract_meta_history', function (Blueprint $table) {
+            $table->dropForeign([
+                'contract_id',
+                'review_id'
+            ]);
+        });
+    }
+}
