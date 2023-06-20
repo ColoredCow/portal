58bd118acc569b910f7647c8b41b41041344124c
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-06-20 15:49:12 +0530

    CI checks

--- app/Http/Controllers/KnowledgeCafe/Library/BookController.php
+++ app/Http/Controllers/KnowledgeCafe/Library/BookController.php
@@ -49,7 +49,8 @@ class BookController extends Controller
         $books->load('borrowers');
         $wishlistedBooksCount = $wishlistedBooks->count();
         $booksBorrowerCount = $booksBorrower->count();
-        
+
+    
         return view('knowledgecafe.library.books.index', compact('books', 'loggedInUser', 'categories', 'wishlistedBooksCount','booksBorrowerCount'));
     }
 
