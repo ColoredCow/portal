7d240ca7a912fa4d84033551fee678510b044e08
Priyanka Kaintura <112419110+Priyanka-Kaintura@users.noreply.github.com>
2023-06-22 19:42:59 +0530

    Update index.blade.php
    Changes made as requested.

--- Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
+++ Modules/SalesAutomation/Resources/views/sales-area/index.blade.php
@@ -70,34 +70,26 @@
 	{{ $salesAreas->links() }}
 @endsection
 
-<div class="modal fade" id="newSalesArea">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-title d-flex justify-content-between mx-2 mb-0 mt-2">
-				<h2>New Sales Area</h2>
-				<button class="btn btn-secondary" data-dismiss="modal"><span>&times</span></button>
-			</div>
-			<form action="{{ route('sales-area.store') }}" method="POST">
-			@csrf
-				<div class="modal-body mt-1">
-					<div class="card my-0">
-						<div class="card-body">
-							<div class="row">
-								<div class="col-12">
-									<div class="form-group">
-										<label for="name">Name<span class="text-danger">*</span></label>
-										<input type="text mw-100" class="form-control" name="name" id="name" value="{{ old('name') }}" autofocus required>
-									</div>
-								</div>
-							</div>	
-						</div>
+<form action="{{ route('sales-area.store') }}" method="POST">
+@csrf
+	<div class="modal fade" id="newSalesArea" tabindex="-1" role="dialog" aria-labelledby="NewSalesAreaModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="newSalesAreaModalLabel">New Sales Area</h5>
+					<button type="button" class="close" data-dismiss="modal"><span>&times</span></button>
+				</div>	
+				<div class="col-md-12">
+					<div class="form-group mt-2">
+						<label for="name">Name<span class="text-danger">*</span></label>
+						<input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" autofocus required>
 					</div>
 				</div>
-				<div class="modal-footer d-flex justify-content-end px-3 my-0 pb-2">
-					<button type="submit" class="btn btn-secondary btn-lg mr-1" data-dismiss="modal">Close</button>
-					<button type="submit" class="btn btn-primary btn-lg">Submit</button>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary btn-sm">Submit</button>
 				</div>	
-			</form>
+			</div>
 		</div>
 	</div>
-</div>
+</form>
