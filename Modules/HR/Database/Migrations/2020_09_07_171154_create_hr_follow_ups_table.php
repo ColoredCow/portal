3058b7f528a1a0b16037b6b81a04e19632387bcb
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-06-30 16:57:13 +0530

    add column in follow_ups table & foriegn key with hr_application table

--- Modules/HR/Database/Migrations/2020_09_07_171154_create_hr_follow_ups_table.php
+++ Modules/HR/Database/Migrations/2020_09_07_171154_create_hr_follow_ups_table.php
@@ -15,13 +15,16 @@ class CreateHrFollowUpsTable extends Migration
     {
         Schema::create('hr_follow_ups', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('hr_applicant_id');
             $table->unsignedInteger('hr_application_round_id');
             $table->text('checklist')->nullable();
             $table->text('comments')->nullable();
             $table->unsignedInteger('assigned_to')->nullable();
             $table->unsignedInteger('conducted_by')->nullable();
+            $table->unsignedTinyInteger('follow_up_attempts')->default(0);
             $table->timestamps();
 
+            $table->foreign('hr_applicant_id')->references('id')->on('hr_applications')->onDelete('cascade');
             $table->foreign('hr_application_round_id')->references('id')->on('hr_application_round')->onDelete('cascade');
             $table->foreign('assigned_to')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('conducted_by')->references('id')->on('users')->onDelete('cascade');
