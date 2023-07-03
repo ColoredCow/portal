d4ac0c76a7132a5610dac687915314f51adf020c
Mohit Gururani <mohitgururani12@gmail.com>
2023-07-03 21:29:01 +0530

    correcting checks

--- Modules/User/Services/UserService.php
+++ Modules/User/Services/UserService.php
@@ -2,7 +2,6 @@
 
 namespace Modules\User\Services;
 
-use Modules\HR\Entities\Employee;
 use Modules\User\Entities\User;
 use OfficeSuite\OfficeSuiteFacade;
 use Modules\User\Events\UserRemovedEvent;
