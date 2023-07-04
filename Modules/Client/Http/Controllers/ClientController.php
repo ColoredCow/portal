ee70ec40026bb3c20682725d5346eb02eae86ac5
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-07-04 16:57:34 +0530

    Revert changes

--- Modules/Client/Http/Controllers/ClientController.php
+++ Modules/Client/Http/Controllers/ClientController.php
@@ -7,8 +7,6 @@ use Modules\Client\Entities\Client;
 use Modules\Client\Http\Requests\ClientFormsRequest;
 use Modules\Client\Http\Requests\ClientRequest;
 use Modules\Client\Rules\ClientNameExist;
-use Modules\ClientAnalytics\Http\Controllers\ClientAnalyticsController;
-
 
 class ClientController extends ModuleBaseController
 {
--- Modules/Client/Routes/web.php
+++ Modules/Client/Routes/web.php
@@ -1,5 +1,4 @@
 <?php
-use Modules\ClientAnalytics\Http\Controllers\ClientAnalyticsController;
 
 /*
 |--------------------------------------------------------------------------
