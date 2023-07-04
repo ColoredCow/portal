40304b223edb3135e25f13b7892190e3e885119b
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-07-04 13:19:47 +0530

    suggested changes applied as per feedback

--- Modules/HR/Resources/views/application/follow_up.blade.php
+++ Modules/HR/Resources/views/application/follow_up.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <p>Hello Pankaj Kandpal,</p>
+    <p>Hello {{$user->name}},</p>
     <p>The following applications are present in the portal, which need to follow up on the phone call. Please proceed with the necessary actions:
     </p>
 </div>
