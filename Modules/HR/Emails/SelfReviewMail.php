cc9f0af01d7ad687e47a92dd5b26b9a955206a18
Akshiteshnaithani <akshitesh.naithani@gmail.com>
2023-07-04 11:55:34 +0530

    self review mail

--- Modules/HR/Emails/SelfReviewMail.php
+++ Modules/HR/Emails/SelfReviewMail.php
@@ -9,55 +9,56 @@ use Illuminate\Queue\SerializesModels;
 
 class SelfReviewMail extends Mailable
 {
-    use Queueable, SerializesModels;
+	use Queueable, SerializesModels;
 
-    public $employee, $targetedDate;
+	public $employee;
+	public $targetedDate;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($employee, $targetedDate = null)
-    {
-        $this->employee = $employee;
-        $this->targetedDate = $targetedDate ?? Carbon::now()->addDays(7)->format('d-m-y');
-    }
+	/**
+	 * Create a new message instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($employee, $targetedDate = null)
+	{
+		$this->employee = $employee;
+		$this->targetedDate = $targetedDate ?? Carbon::now()->addDays(7)->format('d-m-y');
+	}
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        $currentQuarter = ceil(date('n') / 3); // Calculate current quarter
-        $currentYear = date('Y'); // Get current year
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		$currentQuarter = ceil(date('n') / 3); // Calculate current quarter
+		$currentYear = date('Y'); // Get current year
 
-        $subject = 'Quarterly Self Review: ' . $this->getQuarterMonth($currentQuarter) . ' ' . $currentYear;
+		$subject = 'Quarterly Self Review: ' . $this->getQuarterMonth($currentQuarter) . ' ' . $currentYear;
 
-        return $this
-            ->from(config('hr.default.email'))
-            ->to($this->employee->first()->email)
-            ->subject($subject)
-            ->view('hr::mail.self-review');
-    }
+		return $this
+			->from(config('hr.default.email'))
+			->to($this->employee->first()->email)
+			->subject($subject)
+			->view('hr::mail.self-review');
+	}
 
-    /**
-     * Get the name of the quarter month based on the quarter number.
-     *
-     * @param int
-     * @return string
-     */
-    private function getQuarterMonth($quarterNumber)
-    {
-        $quarterMonths = [
-            1 => 'January - March',
-            2 => 'April - June',
-            3 => 'July - September',
-            4 => 'October - December',
-        ];
+	/**
+	 * Get the name of the quarter month based on the quarter number.
+	 *
+	 * @param int
+	 * @return string
+	 */
+	private function getQuarterMonth($quarterNumber)
+	{
+		$quarterMonths = [
+			1 => 'January - March',
+			2 => 'April - June',
+			3 => 'July - September',
+			4 => 'October - December',
+		];
 
-        return $quarterMonths[$quarterNumber] ?? '';
-    }
+		return $quarterMonths[$quarterNumber] ?? '';
+	}
 }
