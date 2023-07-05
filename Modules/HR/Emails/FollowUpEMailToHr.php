911b61c6045876e541c2a5c2803895dd9c9cb7fc
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-07-05 10:47:28 +0530

    Feedback applied pass the mail class in console command file

--- Modules/HR/Emails/FollowUpEMailToHr.php
+++ Modules/HR/Emails/FollowUpEMailToHr.php
@@ -31,7 +31,7 @@ class FollowUpEMailToHr extends Mailable
     public function build()
     {
         return $this->from(config('hr.default.email'), config('hr.default.name'))
-            ->subject('Follow up with applicants through a follow up mail')
+            ->subject('Follow up mail to HR with applicants.')
             ->view('hr::application.followupToHr');
     }
 }
