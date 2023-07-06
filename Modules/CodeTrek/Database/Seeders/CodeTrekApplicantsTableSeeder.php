c9d8266a710824719c50907fe838f73dc40439ed
AJAY <ajaysinghbisht195@gmail.com>
2023-07-06 13:35:05 +0530

    fixed

--- Modules/CodeTrek/Database/Seeders/CodeTrekApplicantsTableSeeder.php
+++ Modules/CodeTrek/Database/Seeders/CodeTrekApplicantsTableSeeder.php
@@ -3,9 +3,9 @@
 namespace Modules\CodeTrek\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Factories\Factory as EloquentFactory;
+// use Illuminate\Database\Eloquent\Factories\Factory as EloquentFactory;
 use Illuminate\Database\Eloquent\Model;
-use Modules\CodeTrek\Database\Factories\CodeTrekApplicantFactory;
+// use Modules\CodeTrek\Database\Factories\CodeTrekApplicantFactory;
 use Modules\CodeTrek\Entities\CodeTrekApplicant;
 
 class CodeTrekApplicantsTableSeeder extends Seeder
