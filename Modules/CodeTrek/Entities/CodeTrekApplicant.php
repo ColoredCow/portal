4cf7500f29ffeb873666b6629419aed9686b1c1f
Akshit Rawat <akshitrawat12072001@gmail.com>
2023-07-06 15:28:12 +0530

    CI checks solve

--- Modules/CodeTrek/Entities/CodeTrekApplicant.php
+++ Modules/CodeTrek/Entities/CodeTrekApplicant.php
@@ -26,10 +26,11 @@ class CodeTrekApplicant extends Model
     public function getDaysInCodetrekAttribute(self $applicant)
     {
         $internDate = Carbon::parse($applicant->internship_start_date);
-
+        
         if ($applicant->status == 'completed') {
             return $internDate->diffInDays($applicant->start_date);
         }
+        
         return now()->diffInDays($applicant->start_date);
     }
     
