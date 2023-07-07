85fd7aa93dda56df01506fa0b158516664b31872
Mohit Gururani <mohitgururani12@gmail.com>
2023-07-07 17:21:47 +0530

    created migration inside codetrek module

--- Modules/CodeTrek/Database/Migrations/2023_07_07_170254_AddDomainNameToCodetrekApplicantTable.php
+++ Modules/CodeTrek/Database/Migrations/2023_07_07_170254_AddDomainNameToCodetrekApplicantTable.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class AddDomainNameToCodeTrekApplicantTable extends Migration
+class AddDomainNameToCodetrekApplicantTable extends Migration
 {
     /**
      * Run the migrations.
