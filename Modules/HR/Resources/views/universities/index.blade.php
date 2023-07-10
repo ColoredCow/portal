f2167e6959893f59f8bda487cdff089077c4d788
suyashnautiyal <138096316+suyashnautiyal@users.noreply.github.com>
2023-07-10 12:00:15 +0530

    updated delete popup modal name
    I have made the suggested changes.

--- Modules/HR/Resources/views/universities/index.blade.php
+++ Modules/HR/Resources/views/universities/index.blade.php
@@ -62,9 +62,9 @@
                 </td>
                 <td>
                     <a href="{{ route('universities.edit', $university) }}" title="Edit" class="pr-1 btn btn-link"><i class="text-success fa ffa-lga-edit "></i></a>
-                    <button type="submit" class="pl-1 btn btn-link" title="Delete" data-toggle="modal" data-target="#DeleteUniversity{{ $university->id }}"><i class="text-danger fa fa-trash fa-lg"></i></button>
+                    <button type="submit" class="pl-1 btn btn-link" title="Delete" data-toggle="modal" data-target="#DeleteUniversityModal{{ $university->id }}"><i class="text-danger fa fa-trash fa-lg"></i></button>
                     @include('component.delete-modal', [
-                            modalId' => 'DeleteUniversity' . $university->id,
+                            modalId' => 'DeleteUniversityModal' . $university->id,
                             'title' => 'Delete University',
                             'body' => 'Are you sure you want to delete this university?',
                             'action' => route('universities.destroy', $university)
