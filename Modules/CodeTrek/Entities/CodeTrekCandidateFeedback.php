1b8f624adaf4ce4eefd1d69cb828c83ca22da996
Priyanka Kaintura <112419110+Priyanka-Kaintura@users.noreply.github.com>
2023-07-13 12:58:42 +0530

    Update CodeTrekCandidateFeedback.php

--- Modules/CodeTrek/Entities/CodeTrekCandidateFeedback.php
+++ Modules/CodeTrek/Entities/CodeTrekCandidateFeedback.php
@@ -3,10 +3,22 @@
 namespace Modules\CodeTrek\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Entities\User;
+use Modules\CodeTrek\Entities\CodeTrekFeedbackCategories;
 
 class CodeTrekCandidateFeedback extends Model
 {
     protected $guarded = [];
 
     protected $table = 'code_trek_candidate_feedback';
+
+    public function getPostedByUserName($userId)
+    {
+        return User::select('name')->where('id', $userId)->first()->name;
+    }
+
+    public function getFeedbackCategoryName($categoryId)
+    {
+        return CodeTrekFeedbackCategories::select('category_name')->where('id', $categoryId)->first()->category_name;
+    }
 }
