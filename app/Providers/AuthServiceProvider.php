6f1e877d7aa4c9c9ef0078f23c1aa8272ba7c380
Ranjeet-Eng <singhkalsiranjeet9@gmail.com>
2023-07-18 21:51:04 +0530

    used another logic to pass bank detail tab

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -36,6 +36,5 @@ class AuthServiceProvider extends ServiceProvider
         Gate::before(function ($user, $ability) {
             return $user->hasRole('super-admin') ? true : null;
         });
-
     }
 }
