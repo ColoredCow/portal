60b374441040d67100c5ab53a44b0c2300fe429f
Pankaj Agrawal <pankaj@coloredcow.com>
2023-07-31 17:18:55 +0530

    fix(*): setup seeders

--- Modules/Operations/Database/Seeders/OperationsDatabaseSeeder.php
+++ Modules/Operations/Database/Seeders/OperationsDatabaseSeeder.php
@@ -16,6 +16,7 @@ class OperationsDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(OfficeLocationTableSeeder::class);
+        $this->call(OperationsPermissionTableSeeder::class);
     }
 }
