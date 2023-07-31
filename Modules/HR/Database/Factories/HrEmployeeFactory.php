0613c2847019a116a807c95fba537bc4817a6278
Pankaj Agrawal <pankaj@coloredcow.com>
2023-07-31 18:19:14 +0530

    fix(*): setup seeders

--- Modules/HR/Database/Factories/HrEmployeeFactory.php
+++ Modules/HR/Database/Factories/HrEmployeeFactory.php
@@ -6,6 +6,8 @@ use Modules\User\Entities\User;
 use Modules\HR\Entities\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as Faker;
+use Modules\HR\Entities\HrJobDesignation;
+use Modules\HR\Entities\HrJobDomain;
 
 class HrEmployeeFactory extends Factory
 {
@@ -28,7 +30,9 @@ class HrEmployeeFactory extends Factory
         return [
             'name' => $faker->name,
             'user_id' => User::factory()->create()->id,
-            'staff_type' => 'Employee'
+            'staff_type' => 'Employee',
+            'designation_id' => optional(HrJobDesignation::inRandomOrder()->first())->id,
+            'domain_id' => optional(HrJobDomain::inRandomOrder()->first())->id,
         ];
     }
 }
