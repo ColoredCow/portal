99e426750659b1d45bdfdfe44ad2fde46b91099e
deepankar sajwan <deepankar@Deepankars-MacBook-Pro.local>
2023-09-26 18:15:19 +0530

    deleted unwanted code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,13 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-// Cypress.Commands.add('login', (user) => {
-//     cy.visit("/login");
-//     cy.get('input[name=email]').type(user.email)
-//     cy.get('input[name=password]').type(user.password)
-//     cy.get('[type="submit"]').first().click();
-//   })
-
 Cypress.Commands.add("login", (userRole) => {
 	cy.fixture("users.json").then((userData) => {
 		const user = userData.users[userRole];
