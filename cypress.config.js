a33b579c9e3630013812c6b05eb84613058f1eff
deepankar sajwan <deepankar@Deepankars-MacBook-Pro.local>
2023-09-26 18:24:34 +0530

    deleted unwanted code

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		"baseUrl": "http://127.0.0.1:8000",
-		"$schema": "https://on.cypress.io/cypress.schema.json",
 		// setupNodeEvents(on, config) {
 		// 	return config;
 		// },
