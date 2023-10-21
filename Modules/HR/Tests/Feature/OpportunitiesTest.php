daf83cec74f0f496fe87366410a893b113c76a25
AbhishekNegi25 <abhishek.negi@coloredcow.in>
2023-10-21 11:46:08 +0530

    Added test cases for hr-jobs

---
+++ Modules/HR/Tests/Feature/OpportunitiesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\HR\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OpportunitiesTest extends TestCase
+{
+  use RefreshDatabase;
+
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function test_create_a_job()
+  {
+    $this->signIn();
+    $response = $this->get('/home');
+    $response->assertStatus(200);
+  }
+}
+
