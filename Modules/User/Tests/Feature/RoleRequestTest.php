14fd65e96af1550b4269d25255f6a97bb6e746dc
Abhishek Negi <negiabhishek253@gmail.com>
2023-11-23 16:11:34 +0530

    updated test cases

--- Modules/User/Tests/Feature/RoleRequestTest.php
+++ Modules/User/Tests/Feature/RoleRequestTest.php
@@ -23,6 +23,11 @@ class RoleRequestTest extends TestCase
         parent::tearDown();
     }
 
+    public function test_roles_listing()
+    {
+        $response = $this->get(route('user.role-all'));
+        $response->assertStatus(200);
+    }
    
     public function create_user_role()
     {
@@ -31,13 +36,12 @@ class RoleRequestTest extends TestCase
             'guard_name' => 'web',
             'description' => 'Test Role Description',
         ];
-
-        $validator = Validator::make($roleTestData, (new RoleRequest())->rules());
-
-        $this->assertFalse($validator->fails());
+        $response = $this->from(route('user.role-all'))
+            ->post(route('roles.add-roles'), $roleTestData);
+        $response->assertSessionDoesntHaveErrors(['name', 'guard_name', 'description']);
+        $response->assertRedirect(route('user.role-all'));
     }
 
-   
     public function test_fails_validation_when_name_is_not_provided()
     {
         $roleTestData = [
@@ -50,7 +54,6 @@ class RoleRequestTest extends TestCase
         $this->assertTrue($validator->fails());
         $this->assertArrayHasKey('name', $validator->errors()->getMessages());
     }
-
    
     public function test_fails_validation_when_guard_name_is_not_provided()
     {
@@ -64,7 +67,6 @@ class RoleRequestTest extends TestCase
         $this->assertTrue($validator->fails());
         $this->assertArrayHasKey('guard_name', $validator->errors()->getMessages());
     }
-
    
     public function test_fails_validation_when_description_is_not_provided()
     {
@@ -72,9 +74,7 @@ class RoleRequestTest extends TestCase
             'name' => 'Test Role',
             'guard_name' => 'web',
         ];
-
         $validator = Validator::make($roleTestData, (new RoleRequest())->rules());
-
         $this->assertTrue($validator->fails());
         $this->assertArrayHasKey('description', $validator->errors()->getMessages());
     }
