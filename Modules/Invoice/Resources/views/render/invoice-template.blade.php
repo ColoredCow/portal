060668cc3e507fe146cfa982eb407bdf1abdee78
Abhishek Negi <negiabhishek253@gmail.com>
2024-02-22 11:10:09 +0530

    Added PAN label and make its non mandatory

--- Modules/Invoice/Resources/views/render/invoice-template.blade.php
+++ Modules/Invoice/Resources/views/render/invoice-template.blade.php
@@ -362,7 +362,7 @@
                             <p>{{ $client->country->initials == 'IN' && optional($client->addresses->first())->gst_number ? __('GSTIN: ') . optional($client->addresses->first())->gst_number : '' }}
                             </p>
                             <p>{{ optional($client->billing_contact)->phone }}</p>
-                            <p>{{ optional($client->addresses->first())->pan_number }}</p>
+                            <p>PAN : {{ optional($client->addresses->first())->pan_number }}</p>
                         </td>
                         <td>
                             <p class="fw-bold ml-1em">Details</p>
