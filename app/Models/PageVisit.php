9b14dbfc0c4ebe9a54a73bdaa9018b17bd5f8c0f
Shivam-Samant <shivamsamant000@gmail.com>
2024-03-13 15:20:52 +0530

    feat: created relationship for user

--- app/Models/PageVisit.php
+++ app/Models/PageVisit.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Entities\User;
 
 class PageVisit extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'page_path', 'visit_count'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
