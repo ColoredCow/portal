04a0089df2dc619799ab401bac45958484fe3397
Shivam-Samant <shivamsamant000@gmail.com>
2024-03-13 13:11:51 +0530

    code cleanups

--- app/Models/PageVisit.php
+++ app/Models/PageVisit.php
@@ -4,26 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\PageVisitable;
 
 class PageVisit extends Model
 {
-    use HasFactory;
+    use HasFactory, PageVisitable;
 
     protected $fillable = ['user_id', 'page_path', 'visit_count'];
-
-    public static function updateVisitCount($userId, $pagePath)
-    {
-        $today = now()->today();
-        
-        $visit = self::where('user_id', $userId)->where('page_path', $pagePath)->whereDate('created_at', $today)->first();
-
-        if (! $visit) {
-            $visit = new self;
-            $visit->user_id = $userId;
-            $visit->page_path = $pagePath;
-        }
-
-        $visit->visit_count++;
-        $visit->save();
-    }
 }
