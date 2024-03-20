5655f338a8d504f8ac188268afe2b4ea0024fe2d
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-03-20 18:39:03 +0530

    added columns

--- Modules/HR/Exports/EmployeePayrollExport.php
+++ Modules/HR/Exports/EmployeePayrollExport.php
@@ -9,6 +9,8 @@ use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
 
 class EmployeePayrollExport implements FromArray, WithHeadings, ShouldAutoSize, WithTitle, WithStyles
 {
@@ -29,7 +31,14 @@ class EmployeePayrollExport implements FromArray, WithHeadings, ShouldAutoSize, 
         return [
             ['Coloredcow Consulting Private Limited'],
             [Carbon::now()->format('F Y'), "Paid", Carbon::today()->toDateString()],
-            ["Employee Name", "Employee ID","Designation", "GROSS", "Basic Salary", "HRA", "Transport allowance", "Other Allowance","Food Allowance", "Total Salary", "Total No of Days", "Paid Days", "Employee ESI 0.75%", "Employee EPF 12 %", "TDS","Advance Recovery", "Food Deduction","Total Deduction", "Advance Salary", " Net Pay", " Employer ESI 3.25%", " EPF EMPLOYER SHARE 12%", " Administration charges FIXED 0.5%( BASIC SALARY)", " EDLI Charges FIXED 0.5%(MAXIMUM SALARY LIMIT 15000)", " CTC", " CTC Annual", "Health Insurance", "CTC Aggreed"]
+            ["Employee Name", "Employee ID","Designation", "GROSS", "Basic Salary", "HRA", "Transport allowance", "Other Allowance","Food Allowance", "Total Salary", "Total No of Days", "Paid Days", "Employee ESI 0.75%", "Employee EPF 12 %", "TDS","Advance Recovery", "Food Deduction","Total Deduction", "Advance Salary", " Net Pay", " Employer ESI 3.25%", " EPF EMPLOYER SHARE 12%", " Administration charges FIXED 0.5%( BASIC SALARY)", " EDLI Charges FIXED 0.5%(MAXIMUM SALARY LIMIT 15000)", " CTC", " CTC Annual", "Health Insurance", "CTC Agreed"]
+        ];
+    }
+
+    public function exportData()
+    {
+        $data = [
+            'hello'
         ];
     }
 
@@ -43,12 +52,9 @@ class EmployeePayrollExport implements FromArray, WithHeadings, ShouldAutoSize, 
      */
     public function styles(Worksheet $sheet)
     {
-        return [
-            1 => [
-                'font' => [
-                    'bold' => true,
-                ],
-            ],
-        ];
+        $sheet->mergeCells('M2:P2');
+        $sheet->setCellValue('M2', 'Deduction');
+        $sheet->mergeCells('U2:X2');
+        $sheet->setCellValue('U2', ' Employer Contribution');
     }
 }
