eca9098b48396c8a0351a1466da878917742cd31
shubtiwari <shubham.tiwari@coloredcow.in>
2024-04-17 13:24:19 +0530

    fix checks

--- resources/js/components/Dashboard/EmployeeProfitabilityList.vue
+++ resources/js/components/Dashboard/EmployeeProfitabilityList.vue
@@ -8,7 +8,7 @@
 				<div class="card-body pt-3 h-325 w-md-389 w-389 overflow-y-scroll">
 					<ul class="list list-group unstyled-list">
 						<li class="d-flex justify-content-between">
-						<div class="text-secondary"> Fetching latest data... </div>
+						<div class="text-secondary"> Work in progress </div>
 						<h4 v-text="sentInvoiceCount"></h4>
 						</li>
 						<hr class="mt-1" />
