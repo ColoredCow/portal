3bd5881388ce10ad905198af9aa74d8881aa8591
sameer <sameerbhardiya@gmail.com>
2024-04-18 11:33:09 +0530

    added getContractData function

--- Modules/Project/Http/Controllers/ProjectController.php
+++ Modules/Project/Http/Controllers/ProjectController.php
@@ -67,8 +67,10 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        $contract = ProjectContract::where('project_id', $project->id)->first();
-        $contractFilePath = $contract ? storage_path('app/' . $contract->contract_file_path) : null;
+        $contractData = $this->getContractData($project);
+        $contract = $contractData["contract"];
+        $contractFilePath = $contractData["contractFilePath"];
+    
         $currentDate = today(config('constants.timezone.indian'));
 
         if (now(config('constants.timezone.indian'))->format('H:i:s') < config('efforttracking.update_date_count_after_time')) {
@@ -137,9 +139,8 @@ class ProjectController extends Controller
         $designations = $this->service->getDesignations();
         $designationKeys = array_keys($designations);
 
-        $contract = ProjectContract::where('project_id', $project->id)->first();
-        $contractFilePath = $contract ? storage_path('app/' . $contract->contract_file_path) : null;
-        $contractName = basename($contractFilePath);
+        $contractData = $this->getContractData($project);
+        $contractName = $contractData["contractName"];
 
         return view('project::edit', [
             'project' => $project,
@@ -189,5 +190,18 @@ class ProjectController extends Controller
             'domainName' => $domainName,
             'jobName' => $jobName,
         ]);
+    }
+    // storing Contract related data here
+    private function getContractData(Project $project)
+    {
+        $contract = ProjectContract::where('project_id', $project->id)->first();
+        $contractFilePath = $contract ? storage_path('app/' . $contract->contract_file_path) : null;
+        $contractName = basename($contractFilePath);
+       
+        return [
+            "contract" => $contract,
+            "contractFilePath" => $contractFilePath,
+            "contractName" => $contractName
+        ];
     }
 }
