0e9ba23339f90b0e980f99de877b0b0b82d2eefe
sameer <sameerbhardiya@gmail.com>
2024-05-20 15:30:43 +0530

    project-contract-report-end-date-sort

--- Modules/Report/Services/Finance/ContractReportService.php
+++ Modules/Report/Services/Finance/ContractReportService.php
@@ -11,11 +11,28 @@ class ContractReportService implements ProjectServiceContract
     {
         $clientData = Client::query()
             ->with(['projects' => function ($query) {
-                $query->where('status', 'active');
+                $query->where('status', 'active')->orderBy('end_date', 'asc');
             }])
-            ->whereHas('projects')
-            ->orderBy('name')
-            ->get();
+            ->with('clientContracts')
+            ->with(['meta' => function ($query) {
+                $query->select('client_id', 'value');
+            }])
+            ->whereHas('projects', function ($query) {
+                $query->where('status', 'active');
+            })
+            ->get()
+            ->sortBy(function ($client) {
+                $metaValue = optional($client->meta->first())->value;
+
+                if ($metaValue == 'client') {
+                    return optional($client->clientContracts->sortBy('end_date')->first())->end_date;
+                } elseif ($metaValue == 'project' || is_null($metaValue)) {
+                    return optional($client->projects->sortBy('end_date')->first())->end_date;
+                } else {
+                    return null;
+                }
+            })
+            ->values();
 
         return $clientData;
     }
