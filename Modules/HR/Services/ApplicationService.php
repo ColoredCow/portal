1a2c08c7d604e9fb3cd78692bfa37f5f78d07347
Anirudh Panwar <anirudh.panwar@coloredcow.in>
2024-06-10 16:39:08 +0530

    CI fixes

--- Modules/HR/Services/ApplicationService.php
+++ Modules/HR/Services/ApplicationService.php
@@ -15,226 +15,188 @@ use Modules\HR\Entities\Job;
 use Modules\HR\Events\CustomMailTriggeredForApplication;
 use Modules\User\Entities\User;
 
-class ApplicationService implements ApplicationServiceContract {
+class ApplicationService implements ApplicationServiceContract
+{
+    public function index($applicationType)
+    {
+        $filters = [
+            'status' => request()->get('status') ?: 'non-rejected',
+            'job-type' => $applicationType,
+            'job' => request()->get('hr_job_id'),
+            'search' => request()->get('search'),
+            'tags' => request()->get('tags'),
+            'assignee' => request()->get('assignee'), // TODO
+            'round' => str_replace('-', ' ', request()->get('round')),
+        ];
 
-	public function index( $applicationType ) {
-		$filters = array(
-			'status'   => request()->get( 'status' ) ?: 'non-rejected',
-			'job-type' => $applicationType,
-			'job'      => request()->get( 'hr_job_id' ),
-			'search'   => request()->get( 'search' ),
-			'tags'     => request()->get( 'tags' ),
-			'assignee' => request()->get( 'assignee' ), // TODO
-			'round'    => str_replace( '-', ' ', request()->get( 'round' ) ),
-		);
+        $loggedInUserId = auth()->id();
+        $applications = Application::join('hr_application_round', function ($join) {
+            $join->on('hr_application_round.hr_application_id', '=', 'hr_applications.id')
+                ->where('hr_application_round.is_latest', true);
+        })
+            ->with(['applicant', 'job', 'tags', 'latestApplicationRound'])
+            ->whereHas('latestApplicationRound')
+            ->applyFilter($filters)
+            ->orderByRaw("FIELD(hr_application_round.scheduled_person_id, {$loggedInUserId} ) DESC")
+            ->orderByRaw('ISNULL(hr_application_round.scheduled_date) ASC')
+            ->orderByRaw('hr_application_round.scheduled_date ASC')
+            ->select('hr_applications.*')
+            ->latest()
+            ->paginate(config('constants.pagination_size'))
+            ->appends(request()->except('page'));
+        $countFilters = array_except($filters, ['status', 'round']);
+        $attr = [
+            'applications' => $applications,
+            'status' => request()->get('status'),
+        ];
+        $hrRounds = ['Resume Screening', 'Telephonic Interview', 'Introductory Call', 'Basic Technical Round', 'Detailed Technical Round', 'Team Interaction Round', 'HR Round', 'Trial Program', 'Volunteer Screening'];
+        $strings = array_pluck(config('hr.status'), 'label');
 
-		$loggedInUserId = auth()->id();
-		$applications   = Application::join(
-			'hr_application_round',
-			function ( $join ) {
-				$join->on( 'hr_application_round.hr_application_id', '=', 'hr_applications.id' )
-				->where( 'hr_application_round.is_latest', true );
-			}
-		)
-			->with( array( 'applicant', 'job', 'tags', 'latestApplicationRound' ) )
-			->whereHas( 'latestApplicationRound' )
-			->applyFilter( $filters )
-			->orderByRaw( "FIELD(hr_application_round.scheduled_person_id, {$loggedInUserId} ) DESC" )
-			->orderByRaw( 'ISNULL(hr_application_round.scheduled_date) ASC' )
-			->orderByRaw( 'hr_application_round.scheduled_date ASC' )
-			->select( 'hr_applications.*' )
-			->latest()
-			->paginate( config( 'constants.pagination_size' ) )
-			->appends( request()->except( 'page' ) );
-		$countFilters   = array_except( $filters, array( 'status', 'round' ) );
-		$attr           = array(
-			'applications' => $applications,
-			'status'       => request()->get( 'status' ),
-		);
-		$hrRounds       = array( 'Resume Screening', 'Telephonic Interview', 'Introductory Call', 'Basic Technical Round', 'Detailed Technical Round', 'Team Interaction Round', 'HR Round', 'Trial Program', 'Volunteer Screening' );
-		$strings        = array_pluck( config( 'hr.status' ), 'label' );
+        foreach ($strings as $string) {
+            $attr[camel_case($string) . 'ApplicationsCount'] = Application::applyFilter($countFilters)
+                ->where('status', $string)
+                ->whereHas('latestApplicationRound', function ($subQuery) {
+                    return $subQuery->where('is_latest', true);
+                })
+                ->count();
+        }
 
-		foreach ( $strings as $string ) {
-			$attr[ camel_case( $string ) . 'ApplicationsCount' ] = Application::applyFilter( $countFilters )
-				->where( 'status', $string )
-				->whereHas(
-					'latestApplicationRound',
-					function ( $subQuery ) {
-						return $subQuery->where( 'is_latest', true );
-					}
-				)
-				->count();
-		}
+        foreach ($hrRounds as $round) {
+            $attr[camel_case($round) . 'Count'] = Application::applyFilter($countFilters)
+                ->where('status', config('hr.status.in-progress.label'))
+                ->whereHas('latestApplicationRound', function ($subQuery) use ($round) {
+                    return $subQuery->where('is_latest', true)
+                        ->whereHas('round', function ($subQuery) use ($round) {
+                            return $subQuery->where('name', $round);
+                        });
+                })
+                ->count();
+        }
 
-		foreach ( $hrRounds as $round ) {
-			$attr[ camel_case( $round ) . 'Count' ] = Application::applyFilter( $countFilters )
-				->where( 'status', config( 'hr.status.in-progress.label' ) )
-				->whereHas(
-					'latestApplicationRound',
-					function ( $subQuery ) use ( $round ) {
-						return $subQuery->where( 'is_latest', true )
-						->whereHas(
-							'round',
-							function ( $subQuery ) use ( $round ) {
-								return $subQuery->where( 'name', $round );
-							}
-						);
-					}
-				)
-				->count();
-		}
+        $attr['jobs'] = Job::all();
+        $attr['tags'] = Tag::orderBy('name')->get();
 
-		$attr['jobs'] = Job::all();
-		$attr['tags'] = Tag::orderBy( 'name' )->get();
+        if (Module::has('User')) {
+            $attr['assignees'] = User::orderBy('name')->get();
+        }
+        
+        return $attr;
+    }
 
-		if ( Module::has( 'User' ) ) {
-			$attr['assignees'] = User::orderBy( 'name' )->get();
-		}
+    public function saveApplication($data, $subscriptionLists)
+    {
+        try {
+            $this->addSubscriberToCampaigns($data, $subscriptionLists);
+        } catch (\Exception $e) {
+            return redirect(route('applications.job.index'))->with('error', 'Error occurred while sending data to Campaign');
+        }
 
-		return $attr;
-	}
+        $data['name'] = $data['first_name'] . ' ' . $data['last_name'];
+        Applicant::_create($data);
 
-	public function saveApplication( $data, $subscriptionLists ) {
-		try {
-			$this->addSubscriberToCampaigns( $data, $subscriptionLists );
-		} catch ( \Exception $e ) {
-			return redirect( route( 'applications.job.index' ) )->with( 'error', 'Error occurred while sending data to Campaign' );
-		}
+        return true;
+    }
 
-		$data['name'] = $data['first_name'] . ' ' . $data['last_name'];
-		Applicant::_create( $data );
+    public function sendApplicationMail($application, $data)
+    {
+        // call event that triggers send custom application mail
+        $data['mail_sender_name'] = $data['mail_sender_name'] ?? auth()->user()->name;
+        event(new CustomMailTriggeredForApplication($application, $data));
+    }
 
-		return true;
-	}
+    public function markInterviewFinished($data)
+    {
+        $meetDate = config('timezone');
+        $meetDuration = Carbon::parse($meetDate);
+        $data->actual_end_time = $meetDuration;
+        $data->save();
+    }
 
-	public function sendApplicationMail( $application, $data ) {
-		// call event that triggers send custom application mail
-		$data['mail_sender_name'] = $data['mail_sender_name'] ?? auth()->user()->name;
-		event( new CustomMailTriggeredForApplication( $application, $data ) );
-	}
+    public function addSubscriberToCampaigns($parameters, $subscriptionLists)
+    {
+        $name = $parameters['first_name'] . ' ' . $parameters['last_name'];
+        $token = $this->getToken();
+        $CAMPAIGNS_TOOL_URL = config('constants.campaign_tool_credentials.url');
+        $url = $CAMPAIGNS_TOOL_URL . '/api/v1/addSubscriber';
 
-	public function markInterviewFinished( $data ) {
-		$meetDate              = config( 'timezone' );
-		$meetDuration          = Carbon::parse( $meetDate );
-		$data->actual_end_time = $meetDuration;
-		$data->save();
-	}
+        // check $subscriptionLists is array or not
+        $subscriptionLists = is_array($subscriptionLists) ? $subscriptionLists : [$subscriptionLists];
 
-	public function addSubscriberToCampaigns( $parameters, $subscriptionLists ) {
-		$name               = $parameters['first_name'] . ' ' . $parameters['last_name'];
-		$token              = $this->getToken();
-		$CAMPAIGNS_TOOL_URL = config( 'constants.campaign_tool_credentials.url' );
-		$url                = $CAMPAIGNS_TOOL_URL . '/api/v1/addSubscriber';
+        Http::withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type'=>'application/json',
+        ])
+        ->withToken($token)
+        ->post($url, [
+            'name' => $name,
+            'email' =>  $parameters['email'],
+            'phone' => $parameters['phone'],
+            'subscription_lists' => $subscriptionLists,
+        ]);
+    }
 
-		// check $subscriptionLists is array or not
-		$subscriptionLists = is_array( $subscriptionLists ) ? $subscriptionLists : array( $subscriptionLists );
+    public function getToken()
+    {
+        $savedToken = Cache::get('campaign_token');
+        if ($savedToken) {
+            return $savedToken;
+        }
 
-		Http::withHeaders(
-			array(
-				'Accept'       => 'application/json',
-				'Content-Type' => 'application/json',
-			)
-		)
-		->withToken( $token )
-		->post(
-			$url,
-			array(
-				'name'               => $name,
-				'email'              => $parameters['email'],
-				'phone'              => $parameters['phone'],
-				'subscription_lists' => $subscriptionLists,
-			)
-		);
-	}
+        $CAMPAIGNS_TOOL_URL = config('constants.campaign_tool_credentials.url');
+        $url = $CAMPAIGNS_TOOL_URL . '/oauth/token';
+        $response = Http::asForm()->post($url, [
+            'grant_type' => 'client_credentials',
+            'client_id' => config('constants.campaign_tool_credentials.client_id'),
+            'client_secret' => config('constants.campaign_tool_credentials.client_secret'),
+        ]);
 
-	public function getToken() {
-		$savedToken = Cache::get( 'campaign_token' );
-		if ( $savedToken ) {
-			return $savedToken;
-		}
+        $accessToken = $response->json()['access_token'];
+        // Store the token in the cache for 1 day.
+        Cache::put('campaign_token', $accessToken, 60 * 24);
 
-		$CAMPAIGNS_TOOL_URL = config( 'constants.campaign_tool_credentials.url' );
-		$url                = $CAMPAIGNS_TOOL_URL . '/oauth/token';
-		$response           = Http::asForm()->post(
-			$url,
-			array(
-				'grant_type'    => 'client_credentials',
-				'client_id'     => config( 'constants.campaign_tool_credentials.client_id' ),
-				'client_secret' => config( 'constants.campaign_tool_credentials.client_secret' ),
-			)
-		);
-
-		$accessToken = $response->json()['access_token'];
-		// Store the token in the cache for 1 day.
-		Cache::put( 'campaign_token', $accessToken, 60 * 24 );
-
-		return $accessToken;
-	}
-
-	public function getApplicationsForDate( $today, $selectedDate, $searchCategory, $selectedJob, $selectedOpportunity, $selectedRound ) {
-		$totalOpportunitiesCount = Job::where( 'status', 'published' )->count();
-		$todayApplications       = Application::whereDate( 'created_at', $today )->get();
-
-		$interviewApplicationsQuery = ApplicationRound::whereNotNull( 'calendar_meeting_id' )
+        return $accessToken;
+    }
+	
+	public function getApplicationsForDate($today, $selectedOperator, $searchCategory, $selectedJob, $selectedOpportunity, $selectedRound)
+    {
+		$totalOpportunitiesCount = Job::where('status', 'published')->count();
+		$todayApplications       = Application::whereDate('created_at', $today)->get();
+		$interviewApplicationsQuery = ApplicationRound::whereNotNull('calendar_meeting_id')
 			// ->whereNull('conducted_date')
 			// ->where('scheduled_person_id', auth()->id())
-			->with( 'application.applicant', 'application.job', 'round', 'meetingLink', 'scheduledPerson' )
-			->orderByRaw( 'hr_application_round.scheduled_date ASC' );
+			->with('application.applicant', 'application.job', 'round', 'meetingLink', 'scheduledPerson')
+			->orderByRaw('hr_application_round.scheduled_date ASC');
 
-		// Date filtering
-		if ( $selectedDate !== null ) {
-			$interviewApplicationsQuery->whereDate( 'scheduled_date', $selectedDate, $today );
-		} else {
-			$interviewApplicationsQuery->whereDate( 'scheduled_date', $today );
+        if ($selectedOperator !== null) {
+            $interviewApplicationsQuery->whereDate('scheduled_date', $selectedOperator, $today);
+        } else {
+            $interviewApplicationsQuery->whereDate('scheduled_date', $today);
+        }
+        if ($selectedJob !== null) {
+            $interviewApplicationsQuery->whereHas('application.job', function ($query) use ($selectedJob) {
+                $query->where('id', $selectedJob);
+            });
+        }
+        if ($selectedOpportunity !== null) {
+            $interviewApplicationsQuery->whereHas('application.job', function ($query) use ($selectedOpportunity) {
+                $query->where('opportunity_id', $selectedOpportunity);
+            });
+        }
+		if ($selectedRound !== null) {
+			$interviewApplicationsQuery->where('hr_round_id', $selectedRound);
 		}
-
-		// Additional filters
-		if ( $selectedJob !== null ) {
-			$interviewApplicationsQuery->whereHas(
-				'application.job',
-				function ( $query ) use ( $selectedJob ) {
-					$query->where( 'id', $selectedJob );
-				}
-			);
+		if ($searchCategory !== null) {
+			$interviewApplicationsQuery->where(function ($query) use ($searchCategory) {
+				$query->whereHas('application.applicant', function ($query) use ($searchCategory) {
+					$query->where('name', 'LIKE', "%{$searchCategory}%");
+				})->orWhereHas('application.job', function ($query) use ($searchCategory) {
+					$query->where('title', 'LIKE', "%{$searchCategory}%");
+				})->orWhereHas('round', function ($query) use ($searchCategory) {
+					$query->where('name', 'LIKE', "%{$searchCategory}%");
+				});
+			});
 		}
-
-		if ( $selectedOpportunity !== null ) {
-			$interviewApplicationsQuery->whereHas(
-				'application.job',
-				function ( $query ) use ( $selectedOpportunity ) {
-					$query->where( 'opportunity_id', $selectedOpportunity );
-				}
-			);
-		}
-
-		if ( $selectedRound !== null ) {
-			$interviewApplicationsQuery->where( 'hr_round_id', $selectedRound );
-		}
-
-		if ( $searchCategory !== null ) {
-			$interviewApplicationsQuery->where(
-				function ( $query ) use ( $searchCategory ) {
-					$query->whereHas(
-						'application.applicant',
-						function ( $query ) use ( $searchCategory ) {
-							$query->where( 'name', 'LIKE', "%{$searchCategory}%" );
-						}
-					)->orWhereHas(
-						'application.job',
-						function ( $query ) use ( $searchCategory ) {
-							$query->where( 'title', 'LIKE', "%{$searchCategory}%" );
-						}
-					)->orWhereHas(
-						'round',
-						function ( $query ) use ( $searchCategory ) {
-							$query->where( 'name', 'LIKE', "%{$searchCategory}%" );
-						}
-					);
-				}
-			);
-		}
-
-		$interviewApplications = $interviewApplicationsQuery->paginate( config( 'constants.pagination_size' ) );
+		$interviewApplications = $interviewApplicationsQuery->paginate(config('constants.pagination_size'));
 
 		// Process today's applications
 		$totalTodayApplication = $todayApplications->groupBy( 'job.id' )
@@ -245,10 +207,8 @@ class ApplicationService implements ApplicationServiceContract {
 				}
 			);
 
-		// Process interview applications
 		$todayInterviews = array();
 		$applicationType = array();
-
 		foreach ( $interviewApplications as $interviewApplication ) {
 			$application = $interviewApplication->application;
 			$job         = $application->job;
@@ -268,7 +228,7 @@ class ApplicationService implements ApplicationServiceContract {
 					'name' => $interviewApplication->scheduledPerson->name,
 				),
 				'meeting_time'     => $meetingTime,
-				'meeting_date'     => $selectedDate !== null ? $meetingDate : null,
+				'meeting_date'     => $selectedOperator !== null ? $meetingDate : null,
 			);
 
 			$jobType  = $job->type;
@@ -291,11 +251,11 @@ class ApplicationService implements ApplicationServiceContract {
 		}
 
 		return array(
-			'todayInterviews'    => $todayInterviews,
-			'todayApplications'  => $totalTodayApplication,
-			'totalOpportunities' => $totalOpportunitiesCount,
-			'applicationType'    => $applicationType,
-			'pagination'         => $interviewApplications->links(),
+			'todayInterviews'       => $todayInterviews,
+			'todayApplications'     => $totalTodayApplication,
+			'totalOpportunities'    => $totalOpportunitiesCount,
+			'applicationType'       => $applicationType,
+            'pagination'            => $interviewApplications->links(),
 		);
 	}
 }
