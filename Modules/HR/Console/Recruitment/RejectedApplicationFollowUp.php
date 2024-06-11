c5db0051171dcd864bbe303b033887c783c27649
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-06-11 16:54:09 +0530

    cleanups

--- Modules/HR/Console/Recruitment/RejectedApplicationFollowUp.php
+++ Modules/HR/Console/Recruitment/RejectedApplicationFollowUp.php
@@ -40,16 +40,16 @@ class RejectedApplicationFollowUp extends Command
             if ($applicationRound->getPreviousApplicationRound()) {
                 $awaitingForDays = $applicationRound->getPreviousApplicationRound()->conducted_date->diffInDays(today());
                 if ($awaitingForDays >10) {
-                $hrApplicationId = $application->id;
-                $jobId = $application->hr_job_id;
-                $interviewLink = $application->getScheduleInterviewLink();
-                $hrApplicantId = Application::where('id', $hrApplicationId)->first()->hr_applicant_id;
-                $jobTitle = Job::where('id', $jobId)->first()->title;
-                $updateStatus = Application::where('id', $hrApplicationId)->update([
-                    'status' => 'rejected',
-                    'is_unresponsive' => 1
-                ]);
-                Mail::send(new SendApplicationRejectionMail($hrApplicantId, $jobTitle, $interviewLink));
+                    $hrApplicationId = $application->id;
+                    $jobId = $application->hr_job_id;
+                    $interviewLink = $application->getScheduleInterviewLink();
+                    $hrApplicantId = Application::where('id', $hrApplicationId)->first()->hr_applicant_id;
+                    $jobTitle = Job::where('id', $jobId)->first()->title;
+                    $updateStatus = Application::where('id', $hrApplicationId)->update([
+                        'status' => 'rejected',
+                        'is_unresponsive' => 1
+                    ]);
+                    Mail::send(new SendApplicationRejectionMail($hrApplicantId, $jobTitle, $interviewLink));
                 }
             } else {
                 return;
