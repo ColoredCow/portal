7113f7dc611936c04168faff271244735f93a551
Anirudh Panwar <anirudh.panwar@coloredcow.in>
2024-06-14 14:39:41 +0530

    Fixed color theme and minor text changes

--- Modules/HR/Resources/views/application/today-interviews.blade.php
+++ Modules/HR/Resources/views/application/today-interviews.blade.php
@@ -1,36 +1,36 @@
 <?php if ( $todayInterviews ) : ?>
 	<?php foreach ( $todayInterviews as $key => $todayInterview ) : ?>
 		<div class="w-full">
-			<div class="d-flex justify-content-start align-items-center shadow-lg w-full py-2 px-3 mb-5 min-h-70" data-counter>
-				<div class="d-flex justify-content-start align-items-center fz-xl-14 text-white">
-					<div class="w-150 fz-18 text-start mr-5">
+			<div class="d-flex justify-content-start align-items-center shadow-lg w-full py-2 px-3 mb-5 min-h-50" data-counter>
+				<div class="d-flex justify-content-start align-items-center fz-xl-14 text-white font-weight-semibold">
+					<div class="w-150 fz-16 text-start mr-5" style="font-weight: 500">
 						<a class="text-decoration-none" href="{{ $todayInterview['scheduled_person']['id'] === auth()->id() ? route('applications.job.edit', $todayInterview['application']->id) : route('hr.applicant.details.show', ['applicationID' => $todayInterview['application']->id]) }}">
 							<span class="card-title">{{ $todayInterview['application']->applicant->name }}</span>
 						</a>
 					</div>
 					<div class="w-200 fz-18 text-left mr-5">
 						<a target="_blank"
-							class="font-muli-bold text-decoration-none"
+							class="font-muli-regular text-decoration-none"
 							href="{{ $todayInterview['meeting_link']->hangout_link }}">
 							<p class="{{! $todayInterview['meeting_date'] ? 'd-none' : ''}} text-dark mb-1 fz-14"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;{{ $todayInterview['meeting_date'] ? $todayInterview['meeting_date'] : ''}}</p>
 							<span><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;{{ $todayInterview['meeting_time'] }}</span>
 						</a>
 					</div>
-					<div class="w-200 text-center mr-5">
-						<span class="fz-12 badge badge-pill bg-theme-green interview-job-filter c-pointer" data-id="{{ $todayInterview['application']->job->id }}" value="{{ $todayInterview['application']->job->title }}">{{ $todayInterview['application']->job->title }}</span>
+					<div class="w-180 text-center mr-5 text-white">
+						<span class="fz-12 badge badge-pill bg-primary interview-job-filter c-pointer" data-id="{{ $todayInterview['application']->job->id }}" value="{{ $todayInterview['application']->job->title }}">{{ $todayInterview['application']->job->title }}</span>
 					</div>
-					<div class="w-200 text-center mr-5">
-					<span class="fz-12 badge badge-pill bg-success interview-round-filter c-pointer" data-id="{{ $todayInterview['round']->id }}" value="{{ $todayInterview['round']->name }}">
+					<div class="w-180 text-center mr-5 text-white">
+					<span class="fz-12 badge badge-pill bg-primary interview-round-filter c-pointer" data-id="{{ $todayInterview['round']->id }}" value="{{ $todayInterview['round']->name }}">
 					{{ $todayInterview['round']->isTrialRound() ? optional($todayInterview['round']->trialRound)->name : $todayInterview['round']->name }}</span>
 					</div>
 					<div class="w-100 text-center text-white mr-3">
-						<span class="fz-12 badge badge-pill bg-secondary interview-opportunity-filter c-pointer" data-id="{{ $todayInterview['application']->job->type }}" value="{{ $todayInterview['application']->job->type }}">{{ ucfirst($todayInterview['application']->job->type) }}</span>
+						<span class="fz-12 badge badge-pill bg-primary interview-opportunity-filter c-pointer" data-id="{{ $todayInterview['application']->job->type }}" value="{{ $todayInterview['application']->job->type }}">{{ ucfirst($todayInterview['application']->job->type) }}</span>
 					</div>
 					<?php if(! $todayInterview['meeting_date']) :?>
-						<div class="w-100 text-center text-white">
-							<button type="button" id="meet_time"
+						<div class="w-120 text-center text-white">
+							<button type="button"
 								value="{{ $todayInterview['id'] }}"
-								class="btn-sm btn-primary text-decoration-none finish_interview">Finish
+								class="btn-sm btn-primary text-decoration-none finish_interview font-weight-bold">Finish
 								Interview</button>
 						</div>
 					<?php endif; ?>
--- Modules/HR/Services/ApplicationService.php
+++ Modules/HR/Services/ApplicationService.php
@@ -174,7 +174,7 @@ class ApplicationService implements ApplicationServiceContract
 
     private function getTotalOpportunitiesCount()
     {
-        return Job::where('status', 'published')->count();
+        return Job::all()->count();
     }
     private function getTodayApplications($today)
     {
