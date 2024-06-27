eb92039eb538af631dd7022b3943037c70da1e0d
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-06-27 19:57:05 +0530

    added back the user observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use Carbon\Carbon;
+use Modules\HR\Entities\Employee;
+use Modules\User\Entities\User;
+
+class UserObserver
+{
+    /**
+     * Handle to the user "created" event.
+     *
+     * @param  \Modules\User\Entities\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        Employee::create([
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'joined_on' => Carbon::today(),
+        ]);
+    }
+}
