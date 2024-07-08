a4f72281dbcede2a453fb12379915bb18c2dc4dd
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-07-08 19:33:09 +0530

    created model

---
+++ Modules/Invoice/Entities/InvoiceActivity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Invoice\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Modules\Invoice\Entities\Invoice;
+
+class InvoiceActivity extends Model
+{
+    protected $table = 'invoice_activities';
+
+    protected $fillable = ['invoice_id', 'type', 'subject', 'content', 'to', 'from', 'receiver_name', 'cc', 'bcc', 'created_at', 'updated_at'];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+}
