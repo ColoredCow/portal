9b2ab4538d0d135595e32d2f19924de11f1ae579
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-07-18 19:08:45 +0530

    cleanups

--- Modules/Invoice/Resources/views/invoice-activity-modal.blade.php
+++ Modules/Invoice/Resources/views/invoice-activity-modal.blade.php
@@ -20,7 +20,7 @@
                         <div class="card">
                           <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
-                              <button class="btn" data-toggle="collapse" data-target="#activityaccordion{{$activity->id}}" aria-expanded="true" aria-controls="collapseOne">
+                              <button class="btn btn-link" data-toggle="collapse" data-target="#activityaccordion{{$activity->id}}" aria-expanded="true" aria-controls="collapseOne">
                                 Reminder Sent On: {{ date(config('constants.display_date_format'), strtotime($activity->created_at)) }}
                               </button>
                             </h5>
