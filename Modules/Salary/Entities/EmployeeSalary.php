f5dd60b43020faa8acf4b13aa748b185221e8a38
Anirudh Panwar <anirudh.panwar@coloredcow.in>
2024-07-30 16:24:52 +0530

    Code cleanups

--- Modules/Salary/Entities/EmployeeSalary.php
+++ Modules/Salary/Entities/EmployeeSalary.php
@@ -27,7 +27,7 @@ class EmployeeSalary extends Model
             return 0;
         }
 
-        $decryptedMonthlyGrossSalary = $monthlyGrossSalary !== null && $monthlyGrossSalary !== '' ? $this->decryptValue($monthlyGrossSalary) : 0;
+        $decryptedMonthlyGrossSalary = $monthlyGrossSalary ? $this->decryptValue($monthlyGrossSalary) : 0;
 
         return (float) $decryptedMonthlyGrossSalary;
     }
