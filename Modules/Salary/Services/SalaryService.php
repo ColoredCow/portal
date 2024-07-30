8d5d0769c66fea299a43f7fe0df3f87aaf43a421
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-07-30 13:38:22 +0530

    wip

--- Modules/Salary/Services/SalaryService.php
+++ Modules/Salary/Services/SalaryService.php
@@ -85,7 +85,7 @@ class SalaryService
 
                 $appraisalData = $salaryService->appraisalLetterData($request, $employee);
                 if ($employee->staff_type === 'Contractor') {
-                    $pdf = $salaryService->getContractorOnboardingLetterPdf($appraisalData);
+                    $pdf = $salaryService->getContractorOnboardingLetterPdf($appraisalData, $employee);
                     Mail::to($data['employeeEmail'])->send(new SendContractorOnboardingLetterMail($data, $pdf->inline($data['employeeName'] . '_Onboarding Letter_' . $formattedCommencementDate . '.pdf'), $formattedCommencementDate));
                 } else {
                     $pdf = $salaryService->getAppraisalLetterPdf($appraisalData);
