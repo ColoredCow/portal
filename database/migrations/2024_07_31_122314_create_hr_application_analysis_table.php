7132afa563e66d3211f327f3453253f47f633634
belwalshubham <belwalshubham08@gmail.com>
2024-07-31 12:47:05 +0530

    added migration for hr_application_analysis table

---
+++ database/migrations/2024_07_31_122314_create_hr_application_analysis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHrApplicationAnalysisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hr_application_analysis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('application_id');
+            $table->text('ai_instruction');
+            $table->text('ai_prompt');
+            $table->string('resume_link');
+            $table->text('analysis');
+            $table->timestamps();
+            
+            $table->foreign('application_id')->references('id')->on('hr_applications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hr_application_analysis');
+    }
+}
