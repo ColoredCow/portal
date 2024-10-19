c1ba6960ab1af88542a59f5e7c2102dca1ee355f
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-10-19 22:04:35 +0530

    fix(*): remove the unused prospects releted tables

--- database/migrations/2024_10_19_220338_remove-prospects-releted-table.php
+++ database/migrations/2024_10_19_220338_remove-prospects-releted-table.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RemoveProspectsTable extends Migration
+class RemoveProspectsReletedTable extends Migration
 {
     /**
      * Run the migrations.
