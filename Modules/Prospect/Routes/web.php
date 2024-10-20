fe65fc6ea9ff0f060923c7edb2d022e29b6596ff
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-10-20 20:50:54 +0530

    fix(*): fix the ci checks

--- Modules/Prospect/Routes/web.php
+++ Modules/Prospect/Routes/web.php
@@ -13,7 +13,7 @@
 
 Route::prefix('prospect')->middleware('auth')->group(function () {
     Route::get('/', 'ProspectController@index')->name('prospect.index');
-    Route::get('/create',  'ProspectController@create')->name('prospect.create');
+    Route::get('/create', 'ProspectController@create')->name('prospect.create');
     Route::post('/store', 'ProspectController@store')->name('prospect.store');
     Route::get('/{prospect}/edit', 'ProspectController@edit')->name('prospect.edit');
 });
