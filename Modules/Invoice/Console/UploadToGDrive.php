3177e2a76573938a20783a5c9bdc6535a246be9f
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-10-21 13:36:55 +0530

    cleanups

--- Modules/Invoice/Console/UploadToGDrive.php
+++ Modules/Invoice/Console/UploadToGDrive.php
@@ -51,24 +51,24 @@ class UploadToGDrive extends Command
     {
         $startDate = $this->argument('startDate')
             ? Carbon::parse($this->argument('startDate'))
-            : Carbon::now()->startOfMonth();
+            : Carbon::now()->subMonth()->startOfMonth();
 
         $endDate = $this->argument('endDate')
             ? Carbon::parse($this->argument('endDate'))
-            : Carbon::now()->endOfMonth();
+            : Carbon::now()->subMonth()->endOfMonth();
 
-        $this->info('Uploading invoices from {$startDate->toDateString()} to {$endDate->toDateString()}');
+        $this->info('Uploading invoices from ' . $startDate->toDateString() . ' to ' . $endDate->toDateString());
 
         $invoices = Invoice::whereBetween('sent_on', [$startDate, $endDate])->get();
         foreach ($invoices as $invoice) {
             if (! $invoice->file_path) {
                 continue;
             }
-            $monthFolderName = $invoice->sent_on->format('F Y');
-
+            $monthFolderName = $invoice->sent_on->format('M Y');
+            $financialYear = $this->getFinancialYear($invoice->sent_on);
             $fileLocalPath = storage_path('app/' . $invoice->file_path);
             $isIndianInvoice = substr($invoice->invoice_number, 0, 2) == 'IN';
-            $googleDriveLink = $this->uploadFileToGoogleDrive($fileLocalPath, $monthFolderName, $isIndianInvoice);
+            $googleDriveLink = $this->uploadFileToGoogleDrive($fileLocalPath, $monthFolderName, $financialYear, $isIndianInvoice);
 
             // Update the google_drive_link column of the invoice
             $invoice->google_drive_link = $googleDriveLink;
@@ -78,7 +78,7 @@ class UploadToGDrive extends Command
         }
     }
 
-    private function uploadFileToGoogleDrive($filePath, $monthFolderName, $isIndianInvoice)
+    private function uploadFileToGoogleDrive($filePath, $monthFolderName, $financialYearFolderName, $isIndianInvoice)
     {
         $googleDriveParentFolderId = config('invoice.invoice-google-drive-folder-id');
 
@@ -87,9 +87,11 @@ class UploadToGDrive extends Command
         $client->addScope(Drive::DRIVE);
         $client->setAccessType('offline');
         $service = new Drive($client);
-        $countryFolderName = $isIndianInvoice ? 'Indian' : 'International';
+        $countryFolderName = $isIndianInvoice ? 'Invoices - IN (Indian)' : 'Invoices - Ex (International)';
+        
         $invoiceCountryFolderId = $this->getOrCreateFolder($service, $countryFolderName, $googleDriveParentFolderId);
-        $monthFolderId = $this->getOrCreateFolder($service, $monthFolderName, $invoiceCountryFolderId);
+        $financialYearFolderId = $this->getOrCreateFolder($service, $financialYearFolderName, $invoiceCountryFolderId);
+        $monthFolderId = $this->getOrCreateFolder($service, $monthFolderName, $financialYearFolderId);
         $content = file_get_contents($filePath);
         $fileName = basename($filePath);
         $fileMetadata = new DriveFile([
@@ -135,5 +137,21 @@ class UploadToGDrive extends Command
         ]);
 
         return $folder->id;  // Return the newly created folder ID
+    }
+
+    private function getFinancialYear(Carbon $date) {
+        $year = $date->year;
+    
+        // If the date is between January and March, it belongs to the previous financial year.
+        if ($date->month < 4) {
+            $startYear = $year - 1;
+            $endYear = $year;
+        } else {
+            // If the date is between April and December, it belongs to the current financial year.
+            $startYear = $year;
+            $endYear = $year + 1;
+        }
+    
+        return "$startYear-" . substr($endYear, -2);
     }
 }
