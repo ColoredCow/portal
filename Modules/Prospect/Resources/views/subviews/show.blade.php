9e6accd0a2c59df2e75fba9d3506f5180411d7da
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-10-21 16:42:30 +0530

    feat(*): prospect comment Ui

--- Modules/Prospect/Resources/views/subviews/show.blade.php
+++ Modules/Prospect/Resources/views/subviews/show.blade.php
@@ -4,5 +4,6 @@
         <br>
         <h4 class="mb-5 font-weight-bold">Prospect Name:- {{ $prospect->organization_name }}</h4>
         @include('prospect::subviews.prospect-details')
+        @include('prospect::subviews.prospect-comments')
     </div>
 @endsection
