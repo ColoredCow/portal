1ca3e44fa0efaaf540700b6b543eb97f547ee70e
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-10-22 13:01:14 +0530

    fix(*): minor ui updates

--- Modules/Prospect/Resources/views/subviews/edit-prospect-comment.blade.php
+++ Modules/Prospect/Resources/views/subviews/edit-prospect-comment.blade.php
@@ -3,8 +3,8 @@
         <form action="{{ route('prospect.comment.update', [$prospect->id]) }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="form-group mt-3">
-                <label for="comment">Comment</label>
+            <div class="form-group">
+                <label for="comment" class="mb-4">Comment</label>
                 <textarea class="form-control" id="prospect_comment" name="prospect_comment" rows="3"
                     placeholder="Add your comment here"></textarea>
             </div>
