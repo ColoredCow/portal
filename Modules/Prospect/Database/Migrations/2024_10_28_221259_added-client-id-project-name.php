fdfbea3a9f366a4e093a9d923eae5a0707165921
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-10-28 22:26:16 +0530

    feat(*): added the client id and project name in the prospect table

---
+++ Modules/Prospect/Database/Migrations/2024_10_28_221259_added-client-id-project-name.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddedClientIdProjectName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prospects', function (Blueprint $table) {
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->string('project_name')->nullable();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prospects', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+            $table->dropColumn('client_id');
+            $table->dropColumn('project_name');
+        });
+    }
+}
