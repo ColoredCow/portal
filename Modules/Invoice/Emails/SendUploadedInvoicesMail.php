0f7c98b6e57edb4213a271bd9fd454aae0fd3c80
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-11-28 19:43:25 +0530

    send email to ca

---
+++ Modules/Invoice/Emails/SendUploadedInvoicesMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Invoice\Emails;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendUploadedInvoicesMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $invoicesFolderDetails;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Array $invoicesFolderDetails)
+    {
+        $this->invoicesFolderDetails = $invoicesFolderDetails;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $monthYear = now()->subMonth()->format('F Y');
+
+        return $this->to(config('invoice.ca-email.email'))
+            ->from(config('invoice.mail.finance-team.email'))
+            ->cc(config('invoice.ca-email.email'))
+            ->subject('Invoice details for ' . $monthYear)
+            ->view('invoice::mail.uploaded-invoice-folder-details-mail')
+            ->with([
+                'invoicesFolderDetails' => $this->invoicesFolderDetails,
+                'monthYear' => $monthYear
+            ]);
+    }
+}
---
+++ Modules/Invoice/Resources/views/mail/uploaded-invoice-folder-details-mail.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <p>Dear {{ config('invoice.ca-email.name') }}</p>
+    <p>Please find the invoices for the month of {{ $monthYear }} for GSTR1 filing.</p>
+    <div>
+        <p>1. Indian Invoices <a href="{{ $invoicesFolderDetails['indian']['folderLink'] }}">{{ $invoicesFolderDetails['indian']['folderLink'] }}</a></p>
+        <p>2. International invoices <a href="{{ $invoicesFolderDetails['international']['folderLink'] }}">{{ $invoicesFolderDetails['indian']['folderLink'] }}</a></p>
+    </div>
+    <br>
+    <p>Let me know if you have any questions.</p>
+    <p>Thanks</p>
+    <p>Mohit</p>
+</div>
