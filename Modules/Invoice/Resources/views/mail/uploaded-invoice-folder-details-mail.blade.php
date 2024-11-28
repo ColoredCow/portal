8852c4c98f557911e1d2ab63b1923e7ca8a1e866
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-11-28 23:36:50 +0530

    minor fix

--- Modules/Invoice/Resources/views/mail/uploaded-invoice-folder-details-mail.blade.php
+++ Modules/Invoice/Resources/views/mail/uploaded-invoice-folder-details-mail.blade.php
@@ -3,7 +3,7 @@
     <p>Please find the invoices for the month of {{ $monthYear }} for GSTR1 filing.</p>
     <div>
         <p>1. Indian Invoices <a href="{{ $invoicesFolderDetails['indian']['folderLink'] }}">{{ $invoicesFolderDetails['indian']['folderLink'] }}</a></p>
-        <p>2. International invoices <a href="{{ $invoicesFolderDetails['international']['folderLink'] }}">{{ $invoicesFolderDetails['indian']['folderLink'] }}</a></p>
+        <p>2. International invoices <a href="{{ $invoicesFolderDetails['international']['folderLink'] }}">{{ $invoicesFolderDetails['international']['folderLink'] }}</a></p>
     </div>
     <br>
     <p>Let me know if you have any questions.</p>
