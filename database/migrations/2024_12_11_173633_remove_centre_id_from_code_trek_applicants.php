b98ade0d9bf01423cd06aa8a1a37ff0cc171b316
sagar1862000 <2000sagarsharma@gmail.com>
2024-12-12 16:10:08 +0530

    m

--- database/migrations/2024_12_11_173633_remove_centre_id_from_code_trek_applicants.php
+++ database/migrations/2024_12_11_173633_remove_centre_id_from_code_trek_applicants.php
@@ -1,34 +1,34 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-class RemoveCentreIdFromCodeTrekApplicants extends Migration
-{
+// class RemoveCentreIdFromCodeTrekApplicants extends Migration
+// {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::table('code_trek_applicants', function (Blueprint $table) {
-            $table->dropForeign(['centre_id']); // Drops the foreign key constraint
-            $table->dropColumn('centre_id');   // Drops the column
-        });
-    }
+    // public function up()
+    // {
+    //     Schema::table('code_trek_applicants', function (Blueprint $table) {
+    //         $table->dropForeign(['centre_id']); // Drops the foreign key constraint
+    //         $table->dropColumn('centre_id');   // Drops the column
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::table('code_trek_applicants', function (Blueprint $table) {
-            $table->unsignedBigInteger('centre_id')->nullable();
-            $table->foreign('centre_id')->references('id')->on('office_locations');
-        });
-    }
-}
+    // public function down()
+    // {
+    //     Schema::table('code_trek_applicants', function (Blueprint $table) {
+    //         $table->unsignedBigInteger('centre_id')->nullable();
+    //         $table->foreign('centre_id')->references('id')->on('office_locations');
+    //     });
+    // }
+// }
