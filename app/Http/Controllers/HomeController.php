933e3d0b1ea8d7b390e10024da637410914e4e65
sagar1862000 <2000sagarsharma@gmail.com>
2024-12-12 17:50:11 +0530

    imports related error solved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\KnowledgeCafe\Library\Book;
-use Google_Client;
-use Google_Service_Directory;
+use App\Models\KnowledgeCafe\Library\Book; // Add this line to import the Book model
 use Illuminate\Http\Request;
 use Modules\User\Entities\UserMeta;
+use Illuminate\Http\RedirectResponse;
 
 class HomeController extends Controller
 {
@@ -25,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+
         $unreadBook = session('disable_book_suggestion') ? null : Book::getRandomUnreadBook();
 
         $selectedLocation = auth()->user()->office_location ?? 'Default Location';
@@ -36,43 +36,16 @@ class HomeController extends Controller
     }
 
     /**
-     * Fetch a user's groups from GSuite API.
+     * Store the user's office location.
      *
-     * @param  string $email Email of the user
-     *
-     * @return array         List of groups
+     * @param  Request  $request
+     * @return RedirectResponse
      */
-
-    // public function getUserGroups($email)
-    // {
-    //     $client = new Google_Client();
-    //     $client->useApplicationDefaultCredentials();
-    //     $client->setSubject(env('GOOGLE_SERVICE_ACCOUNT_IMPERSONATE'));
-    //     $client->addScope([
-    //         Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
-    //         Google_Service_Directory::ADMIN_DIRECTORY_GROUP_READONLY,
-    //     ]);
-
-    //     $dir = new Google_Service_Directory($client);
-    //     $googleGroups = $dir->groups->listGroups([
-    //         'userKey' => $email,
-    //     ]);
-    //     $groups = $googleGroups->getGroups();
-
-    //     $userGroups = [];
-    //     if (count($groups)) {
-    //         foreach ($groups as $group) {
-    //             $userGroups[$group->email] = $group->name;
-    //         }
-    //     }
-
-    //     return $userGroups;
-    // }
-
-    public function storeEmployeeLocation(Request $request)
+    public function storeEmployeeLocation(Request $request): RedirectResponse
     {
+
         $request->validate([
-            'centre_name' => 'required|string',
+            'centre_name' => 'required|string|max:255',
         ]);
 
         UserMeta::updateOrCreate(
@@ -80,6 +53,6 @@ class HomeController extends Controller
             ['meta_value' => $request->centre_name]
         );
 
-        return redirect('home');
+        return redirect()->route('home')->with('status', 'Location updated successfully!');
     }
 }
