d48a2de87773f6b2b56538d9da08c7bf1ca63657
sagar1862000 <2000sagarsharma@gmail.com>
2024-12-12 16:13:29 +0530

    M

--- database/migrations/2023_05_06_141606_add_centre_id_to_code_trek_applicant_table.php
+++ database/migrations/2023_05_06_141606_add_centre_id_to_code_trek_applicant_table.php
@@ -1,34 +1,34 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-class AddCentreIdToCodeTrekApplicantTable extends Migration
-{
+// class AddCentreIdToCodeTrekApplicantTable extends Migration
+// {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::table('code_trek_applicants', function (Blueprint $table) {
-            $table->unsignedBigInteger('centre_id')->nullable();
-            $table->foreign('centre_id')->references('id')->on('office_locations');
-        });
-    }
+    // public function up()
+    // {
+    //     Schema::table('code_trek_applicants', function (Blueprint $table) {
+    //         $table->unsignedBigInteger('centre_id')->nullable();
+    //         $table->foreign('centre_id')->references('id')->on('office_locations');
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::table('code_trek_applicants', function (Blueprint $table) {
-            $table->dropForeign('centre_id');
-            $table->dropColumn('centre_id');
-        });
-    }
-}
+    // public function down()
+    // {
+    //     Schema::table('code_trek_applicants', function (Blueprint $table) {
+    //         $table->dropForeign('centre_id');
+    //         $table->dropColumn('centre_id');
+    //     });
+    // }
+// }
