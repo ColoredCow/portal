d5a0f5a9d806a9bc179d26708fe523f88f164263
sagar1862000 <2000sagarsharma@gmail.com>
2024-12-12 16:18:40 +0530

    M

--- database/migrations/2023_05_06_141606_add_centre_id_to_code_trek_applicant_table.php
+++ database/migrations/2023_05_06_141606_add_centre_id_to_code_trek_applicant_table.php
@@ -1,11 +1,11 @@
 <?php
 
-// use Illuminate\Database\Migrations\Migration;
-// use Illuminate\Database\Schema\Blueprint;
-// use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-// class AddCentreIdToCodeTrekApplicantTable extends Migration
-// {
+class AddCentreIdToCodeTrekApplicantTable extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -31,4 +31,4 @@
     //         $table->dropColumn('centre_id');
     //     });
     // }
-// }
+}
