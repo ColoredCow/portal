f918bf9d543c1a383dbea3ef35f03ae769001d61
Vaibhav Rathore <vaibhav@coloredcow.com>
2024-12-24 18:54:59 +0530

    currency fix

--- Modules/Prospect/Entities/Prospect.php
+++ Modules/Prospect/Entities/Prospect.php
@@ -41,14 +41,13 @@ class Prospect extends Model
     public function getFormattedBudgetAttribute()
     {
         $budget = (string) $this->budget;
-        $currency = $this->currency;
 
         // if currency is less than one thousand
         if (strlen($budget) <= 3) {
             return $budget;
         }
 
-        $numberFormat = $currency == 'INR' ? 'en_IN' : 'en_US';
+        $numberFormat = $this->currency == 'INR' ? 'en_IN' : 'en_US';
         $formatter = new \NumberFormatter($numberFormat, \NumberFormatter::DECIMAL);
         $formattedBudget = $formatter->format($budget);
 
