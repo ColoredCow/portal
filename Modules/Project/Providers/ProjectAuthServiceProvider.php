0d8233565295d2697698671ff656449d41f9851b
Vaibhav Rathore <vaibhav@coloredcow.com>
2026-04-25 18:34:31 +0530

    feat(project): register ProjectInvoiceTermPolicy

--- Modules/Project/Providers/ProjectAuthServiceProvider.php
+++ Modules/Project/Providers/ProjectAuthServiceProvider.php
@@ -5,7 +5,9 @@ namespace Modules\Project\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider;
 use Modules\Project\Entities\Project;
 use Modules\Project\Entities\ProjectContract;
+use Modules\Project\Entities\ProjectInvoiceTerm;
 use Modules\Project\Policies\ProjectContractPolicy;
+use Modules\Project\Policies\ProjectInvoiceTermPolicy;
 use Modules\Project\Policies\ProjectPolicy;
 
 class ProjectAuthServiceProvider extends AuthServiceProvider
@@ -13,6 +15,7 @@ class ProjectAuthServiceProvider extends AuthServiceProvider
     protected $policies = [
         Project::class => ProjectPolicy::class,
         ProjectContract::class => ProjectContractPolicy::class,
+        ProjectInvoiceTerm::class => ProjectInvoiceTermPolicy::class,
     ];
 
     public function boot()
